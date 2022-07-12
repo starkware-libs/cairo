2a53dd5d69f08ea2c651a0b9a3a2246d011ce8ce
Ilya Lesokhin <ilya@starkware.co>
2022-07-12 13:17:36 +0000

    Add JumpInstruction and AssertEqInstruction.

---
+++ crates/casm/src/instructions.rs
@@ -0,0 +1,46 @@
+use std::fmt::Display;
+
+use crate::operand::{DerefOperand, ImmediateOperand, Register, ResOperand};
+
+#[cfg(test)]
+#[path = "instructions_test.rs"]
+mod instructions_test;
+
+enum Instruction {
+    JumpInstruction(JumpInstruction),
+    AssertEqInstruction(AssertEqInstruction),
+}
+impl Display for Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Instruction::JumpInstruction(insn) => write!(f, "{}", insn),
+            Instruction::AssertEqInstruction(insn) => write!(f, "{}", insn),
+        }
+    }
+}
+
+// Represents the instruction "jmp rel/abs".
+pub struct JumpInstruction {
+    target: ResOperand,
+    relative: bool,
+}
+impl Display for JumpInstruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "jmp {} {}",
+            if self.relative { "rel" } else { "abs" },
+            self.target,
+        )
+    }
+}
+
+pub struct AssertEqInstruction {
+    a: DerefOperand,
+    b: ResOperand,
+}
+impl Display for AssertEqInstruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return write!(f, "{} = {}", self.a, self.b);
+    }
+}
