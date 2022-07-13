2485dbcc40489cfcd4bf8e41c3d9d2640874987d
Shahar Papini <spapini@starkware.co>
2022-07-13 15:50:52 +0300

    Lexer

---
+++ crates/parser/src/lexer.rs
@@ -0,0 +1,241 @@
+#[cfg(test)]
+#[path = "lexer_test.rs"]
+mod tests;
+
+use filesystem::ids::FileId;
+use smol_str::SmolStr;
+use syntax::node::ast::{Terminal, Trivia};
+use syntax::node::db::GreenInterner;
+use syntax::node::ids::GreenId;
+use syntax::node::Token;
+use syntax::token::TokenKind;
+
+use crate::text::{TextOffset, TextSpan};
+
+pub struct Lexer<'a> {
+    db: &'a dyn GreenInterner,
+    // TODO(yuval): add diagnostics. FileId should be included in diagnostics.
+    source: FileId,
+    text: &'a str,
+    previous_position: TextOffset,
+    current_position: TextOffset,
+    done: bool,
+}
+
+impl<'a> Lexer<'a> {
+    // Ctors.
+    pub fn from_text(db: &'a dyn GreenInterner, source: FileId, text: &'a str) -> Lexer<'a> {
+        Lexer {
+            db,
+            source,
+            text,
+            previous_position: TextOffset::default(),
+            current_position: TextOffset::default(),
+            done: false,
+        }
+    }
+
+    // Helpers.
+    fn peek(&self) -> Option<char> {
+        self.text[self.current_position.as_usize()..].chars().next()
+    }
+
+    fn peek_nth(&self, n: usize) -> Option<char> {
+        self.text[self.current_position.as_usize()..].chars().nth(n)
+    }
+
+    fn take(&mut self) -> Option<char> {
+        let res = self.peek()?;
+        self.current_position.inc();
+        Some(res)
+    }
+
+    /// Takes a character while the given function returns true.
+    fn take_while<F>(&mut self, f: F)
+    where
+        F: Fn(char) -> bool,
+    {
+        while self.peek().map(&f).unwrap_or(false) {
+            self.take();
+        }
+    }
+
+    fn peek_span_text(&self) -> &'a str {
+        &self.text[self.previous_position.as_usize()..self.current_position.as_usize()]
+    }
+    fn peek_span(&self) -> TextSpan {
+        TextSpan { from: self.previous_position, to: self.current_position }
+    }
+
+    fn consume_span(&mut self) -> &str {
+        let val = self.peek_span_text();
+        self.previous_position = self.current_position;
+        val
+    }
+
+    // Trivia matchers.
+    fn match_trivia(&mut self, leading: bool) -> Vec<GreenId> {
+        let mut res: Vec<GreenId> = Vec::new();
+        while let Some(current) = self.peek() {
+            let trivium = match current {
+                ' ' | '\r' | '\t' => self.match_trivium_whitespace(),
+                '\n' => self.match_trivium_newline(),
+                '/' if self.peek_nth(1) == Some('/') => self.match_trivium_single_line_comment(),
+                _ => break,
+            };
+            res.push(trivium);
+            if current == '\n' && !leading {
+                break;
+            }
+        }
+        res
+    }
+
+    /// Assumes the next character is one of [' ', '\r', '\t'].
+    fn match_trivium_whitespace(&mut self) -> GreenId {
+        self.take_while(|s| matches!(s, ' ' | '\r' | '\t'));
+        Token::new_green(self.db, TokenKind::Whitespace, SmolStr::from(self.consume_span()))
+    }
+
+    /// Assumes the next character '/n'.
+    fn match_trivium_newline(&mut self) -> GreenId {
+        self.take();
+        Token::new_green(self.db, TokenKind::Newline, SmolStr::from(self.consume_span()))
+    }
+
+    /// Assumes the next 2 characters are "//".
+    fn match_trivium_single_line_comment(&mut self) -> GreenId {
+        self.take_while(|c| c != '\n');
+        Token::new_green(self.db, TokenKind::SingleLineComment, SmolStr::from(self.consume_span()))
+    }
+
+    // Token matchers.
+    fn take_token_literal_number(&mut self) -> TokenKind {
+        self.take_while(|c| c.is_ascii_digit());
+        TokenKind::LiteralNumber
+    }
+
+    /// Assumes the next character is [a-zA-Z_].
+    fn take_token_identifier(&mut self) -> TokenKind {
+        // TODO(spapini): Support or explicitly report general unicode characters.
+        self.take_while(|c| c.is_ascii_alphanumeric() || c == '_');
+
+        match self.peek_span_text() {
+            "false" => TokenKind::False,
+            "true" => TokenKind::True,
+            "fn" => TokenKind::Function,
+            "mod" => TokenKind::Module,
+            "struct" => TokenKind::Struct,
+            "let" => TokenKind::Let,
+            "return" => TokenKind::Return,
+            "_" => TokenKind::Underscore,
+            _ => TokenKind::Identifier,
+        }
+    }
+
+    /// Takes a single character and returns the given kind.
+    fn take_token_of_kind(&mut self, kind: TokenKind) -> TokenKind {
+        self.take();
+        kind
+    }
+
+    /// If the next character is `second_char`, returns `long_kind`, otherwise returns `short_kind`.
+    fn pick_kind(
+        &mut self,
+        second_char: char,
+        long_kind: TokenKind,
+        short_kind: TokenKind,
+    ) -> TokenKind {
+        self.take();
+        if self.peek() == Some(second_char) {
+            self.take();
+            long_kind
+        } else {
+            short_kind
+        }
+    }
+
+    fn match_terminal(&mut self) -> TerminalWithKind {
+        let leading_trivia = self.match_trivia(true);
+
+        let kind = if let Some(current) = self.peek() {
+            match current {
+                '0'..='9' => self.take_token_literal_number(),
+                ',' => self.take_token_of_kind(TokenKind::Comma),
+                ';' => self.take_token_of_kind(TokenKind::Semi),
+                '{' => self.take_token_of_kind(TokenKind::LBrace),
+                '}' => self.take_token_of_kind(TokenKind::RBrace),
+                '[' => self.take_token_of_kind(TokenKind::LBrack),
+                ']' => self.take_token_of_kind(TokenKind::RBrack),
+                '(' => self.take_token_of_kind(TokenKind::LParen),
+                ')' => self.take_token_of_kind(TokenKind::RParen),
+                '.' => self.pick_kind('.', TokenKind::DotDot, TokenKind::Dot),
+                '*' => self.take_token_of_kind(TokenKind::Mul),
+                '/' => self.take_token_of_kind(TokenKind::Div),
+                '+' => self.take_token_of_kind(TokenKind::Plus),
+                '-' => self.pick_kind('>', TokenKind::Arrow, TokenKind::Minus),
+                '<' => self.pick_kind('=', TokenKind::LE, TokenKind::LT),
+                '>' => self.pick_kind('=', TokenKind::GE, TokenKind::GT),
+                'a'..='z' | 'A'..='Z' | '_' => self.take_token_identifier(),
+                ':' => self.pick_kind(':', TokenKind::ColonColon, TokenKind::Colon),
+                '!' => self.pick_kind('=', TokenKind::Neq, TokenKind::Not),
+                '=' => self.pick_kind('=', TokenKind::EqEq, TokenKind::Eq),
+                '&' => self.pick_kind('&', TokenKind::AndAnd, TokenKind::And),
+                '|' if self.peek() == Some('|') => {
+                    self.take();
+                    self.take();
+                    TokenKind::OrOr
+                }
+                _ => {
+                    // TODO(yuval): Add to diagnostics instead of printing.
+                    println!(
+                        "Bad character at {:?}, offset {}",
+                        self.source,
+                        self.peek_span().from
+                    );
+                    self.take_token_of_kind(TokenKind::BadCharacters)
+                }
+            }
+        } else {
+            TokenKind::EndOfFile
+        };
+
+        let token_text = SmolStr::from(self.consume_span());
+        let trailing_trivia = self.match_trivia(false);
+
+        // TODO(yuval): log(verbose) "consumed text: ..."
+        TerminalWithKind {
+            terminal: Terminal::new_green(
+                self.db,
+                Trivia::new_green(self.db, leading_trivia),
+                Token::new_green(self.db, kind, token_text),
+                Trivia::new_green(self.db, trailing_trivia),
+            ),
+            kind,
+        }
+    }
+}
+
+pub struct TerminalWithKind {
+    /// The green ID of the terminal node
+    pub terminal: GreenId,
+    /// the kind of the inner token of the terminal
+    pub kind: TokenKind,
+}
+
+impl Iterator for Lexer<'_> {
+    type Item = TerminalWithKind;
+
+    /// Returns the next token. Once there are no more tokens left, returns token EOF.
+    /// One should not call this after EOF was returned. If one does, None is returned.
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.done {
+            return None;
+        }
+        let terminal_with_kind = self.match_terminal();
+        if terminal_with_kind.kind == TokenKind::EndOfFile {
+            self.done = true;
+        };
+        Some(terminal_with_kind)
+    }
+}
---
+++ crates/parser/src/lexer_test.rs
@@ -0,0 +1,400 @@
+use filesystem::ids::FileId;
+use salsa::{InternId, InternKey};
+use syntax::node::ast::{Terminal, Trivia};
+use syntax::node::db::GreenDatabase;
+use syntax::node::ids::GreenId;
+use syntax::node::{SyntaxNode, Token, TypedSyntaxNode};
+use syntax::token::TokenKind;
+
+use super::Lexer;
+
+// TODO(spapini): Use snapshot/regression tests.
+
+fn token_kind_to_text(kind: TokenKind) -> Vec<&'static str> {
+    match kind {
+        TokenKind::Identifier => vec!["abc", "_az12f", "A90g5__"],
+        TokenKind::LiteralNumber => {
+            vec!["0", "9", "00", "1234567890123456789012345678901234567890"]
+        }
+        TokenKind::False => vec!["false"],
+        TokenKind::Function => vec!["fn"],
+        TokenKind::Let => vec!["let"],
+        TokenKind::Module => vec!["mod"],
+        TokenKind::Struct => vec!["struct"],
+        TokenKind::True => vec!["true"],
+        TokenKind::Return => vec!["return"],
+        TokenKind::And => vec!["&"],
+        TokenKind::AndAnd => vec!["&&"],
+        TokenKind::Colon => vec![":"],
+        TokenKind::ColonColon => vec!["::"],
+        TokenKind::Comma => vec![","],
+        TokenKind::Div => vec!["/"],
+        TokenKind::Dot => vec!["."],
+        TokenKind::DotDot => vec![".."],
+        TokenKind::Eq => vec!["="],
+        TokenKind::EqEq => vec!["=="],
+        TokenKind::GE => vec![">="],
+        TokenKind::GT => vec![">"],
+        TokenKind::LE => vec!["<="],
+        TokenKind::LT => vec!["<"],
+        TokenKind::Minus => vec!["-"],
+        TokenKind::Mul => vec!["*"],
+        TokenKind::Neq => vec!["!="],
+        TokenKind::Not => vec!["!"],
+        TokenKind::OrOr => vec!["||"],
+        TokenKind::Plus => vec!["+"],
+        TokenKind::Semi => vec![";"],
+        TokenKind::Underscore => vec!["_"],
+        TokenKind::LBrace => vec!["{"],
+        TokenKind::RBrace => vec!["}"],
+        TokenKind::LBrack => vec!["["],
+        TokenKind::RBrack => vec!["]"],
+        TokenKind::LParen => vec!["("],
+        TokenKind::RParen => vec![")"],
+        TokenKind::Arrow => vec!["->"],
+        TokenKind::EndOfFile
+        | TokenKind::BadCharacters
+        | TokenKind::Missing
+        | TokenKind::SingleLineComment
+        | TokenKind::Whitespace
+        | TokenKind::Newline => vec![],
+    }
+}
+
+fn token_kinds() -> Vec<TokenKind> {
+    vec![
+        TokenKind::Identifier,
+        TokenKind::LiteralNumber,
+        TokenKind::False,
+        TokenKind::True,
+        TokenKind::Function,
+        TokenKind::Module,
+        TokenKind::Struct,
+        TokenKind::Let,
+        TokenKind::Return,
+        TokenKind::And,
+        TokenKind::AndAnd,
+        TokenKind::OrOr,
+        TokenKind::EqEq,
+        TokenKind::Neq,
+        TokenKind::GE,
+        TokenKind::GT,
+        TokenKind::LE,
+        TokenKind::LT,
+        TokenKind::Not,
+        TokenKind::Plus,
+        TokenKind::Minus,
+        TokenKind::Mul,
+        TokenKind::Div,
+        TokenKind::Colon,
+        TokenKind::ColonColon,
+        TokenKind::Comma,
+        TokenKind::Dot,
+        TokenKind::DotDot,
+        TokenKind::Eq,
+        TokenKind::Semi,
+        TokenKind::Underscore,
+        TokenKind::LBrace,
+        TokenKind::RBrace,
+        TokenKind::LBrack,
+        TokenKind::RBrack,
+        TokenKind::LParen,
+        TokenKind::RParen,
+        TokenKind::Arrow,
+        TokenKind::EndOfFile,
+        TokenKind::BadCharacters,
+    ]
+}
+
+fn token_separators() -> Vec<&'static str> {
+    vec![" ", "\t", "\n", " // Comment\n"]
+}
+
+fn need_separator(
+    kind0: TokenKind,
+    text0: &'static str,
+    kind1: TokenKind,
+    text1: &'static str,
+) -> bool {
+    if is_identifier_like(kind0)
+        && (is_identifier_like(kind1) || matches!(kind1, TokenKind::LiteralNumber))
+    {
+        return true;
+    }
+    if kind0 == TokenKind::LiteralNumber && kind0 == kind1 {
+        return true;
+    }
+    if (text0 == "&" && (text1 == "&" || text1 == "&&"))
+        || (text0 == "/" && text1 == "/")
+        || ((text0 == "=" || text0 == "!") && text1 == "=" || text1 == "==")
+        || ((text0 == "<" || text0 == ">") && (text1 == "=" || text1 == "=="))
+        || (text0 == ":" && (text1 == ":" || text1 == "::"))
+        || (text0 == "." && (text1 == "." || text1 == ".."))
+        || (text0 == "-" && (text1 == ">" || text1 == ">="))
+        || (kind0 == TokenKind::LiteralNumber && kind0 == kind1)
+    {
+        return true;
+    }
+    false
+}
+
+fn is_identifier_like(kind: TokenKind) -> bool {
+    matches!(
+        kind,
+        TokenKind::Identifier
+            | TokenKind::False
+            | TokenKind::True
+            | TokenKind::Function
+            | TokenKind::Module
+            | TokenKind::Struct
+            | TokenKind::Let
+            | TokenKind::Return
+            | TokenKind::Underscore
+    )
+}
+
+fn token_kind_and_text() -> Vec<(TokenKind, &'static str)> {
+    let mut res: Vec<(TokenKind, &'static str)> = Vec::new();
+    for kind in token_kinds() {
+        for text in token_kind_to_text(kind) {
+            res.push((kind, text));
+        }
+    }
+    res
+}
+
+fn trivia_kinds() -> Vec<TokenKind> {
+    vec![TokenKind::Whitespace, TokenKind::Newline, TokenKind::SingleLineComment]
+}
+fn trivia_kind_to_text(kind: TokenKind) -> Vec<&'static str> {
+    match kind {
+        TokenKind::SingleLineComment => vec!["// abc def\n", "///\n", "//=\n"],
+        TokenKind::Whitespace => vec![" ", "\t", "\r"],
+        TokenKind::Newline => vec!["\n"],
+        _ => vec![],
+    }
+}
+fn trivia_kind_and_text() -> Vec<(TokenKind, &'static str)> {
+    let mut res: Vec<(TokenKind, &'static str)> = Vec::new();
+    for kind in trivia_kinds() {
+        for text in trivia_kind_to_text(kind) {
+            res.push((kind, text));
+        }
+    }
+    res
+}
+
+fn test_source() -> FileId {
+    FileId::from_intern_id(InternId::from(100u32))
+}
+
+#[salsa::database(GreenDatabase)]
+#[derive(Default)]
+pub struct DatabaseImpl {
+    storage: salsa::Storage<DatabaseImpl>,
+}
+impl salsa::Database for DatabaseImpl {}
+
+#[test]
+fn test_lex_single_token() {
+    let db_val = DatabaseImpl::default();
+    let db = &db_val;
+    for (kind, text) in token_kind_and_text() {
+        let mut lexer = Lexer::from_text(db, test_source(), text);
+        let token = lexer.next().unwrap().terminal;
+        let token = Terminal::from_syntax_node(db, SyntaxNode::new_root(token)).token(db).raw(db);
+        assert_eq!(token.kind, kind, "Wrong token kind, with text: \"{}\".", text);
+        assert_eq!(token.text, text, "Wrong token text.");
+
+        assert_eq!(
+            lexer.next().unwrap().kind,
+            TokenKind::EndOfFile,
+            "Wrong eof token, with text: \"{}\".",
+            text
+        );
+        assert!(lexer.next().is_none(), "Expected end of lexer stream.");
+    }
+}
+
+#[test]
+fn test_lex_double_token() {
+    let db_val = DatabaseImpl::default();
+    let db = &db_val;
+    for (kind0, text0) in token_kind_and_text() {
+        for (kind1, text1) in token_kind_and_text() {
+            let mut separators = token_separators();
+            if !need_separator(kind0, text0, kind1, text1) {
+                separators.push("");
+            }
+            for separator in separators {
+                let text = format!("{}{}{}", text0, separator, text1);
+                let mut lexer = Lexer::from_text(db, test_source(), text.as_str());
+                let token = lexer.next().unwrap().terminal;
+
+                let token =
+                    Terminal::from_syntax_node(db, SyntaxNode::new_root(token)).token(db).raw(db);
+                assert_eq!(token.kind, kind0, "Wrong token kind0, with text: \"{}\".", text);
+                assert_eq!(token.text, text0, "Wrong token text0, with total text: \"{}\".", text);
+                let token = lexer.next().unwrap().terminal;
+
+                let token =
+                    Terminal::from_syntax_node(db, SyntaxNode::new_root(token)).token(db).raw(db);
+                assert_eq!(token.kind, kind1, "Wrong token kind1, with text: \"{}\".", text);
+                assert_eq!(token.text, text1, "Wrong token text1, with total text: \"{}\".", text);
+
+                assert_eq!(
+                    lexer.next().unwrap().kind,
+                    TokenKind::EndOfFile,
+                    "Wrong eof token, with text: \"{}\".",
+                    text
+                );
+                assert!(lexer.next().is_none(), "Expected end of lexer stream.");
+            }
+        }
+    }
+}
+
+#[test]
+fn test_lex_token_with_trivia() {
+    let db_val = DatabaseImpl::default();
+    let db = &db_val;
+    for (kind, token_text) in token_kind_and_text() {
+        for (_leading_kind, leading_trivia) in trivia_kind_and_text() {
+            for (_trailing_kind, trailing_trivia) in trivia_kind_and_text() {
+                let text = format!("{}{} {}", leading_trivia, token_text, trailing_trivia);
+                let mut lexer = Lexer::from_text(db, test_source(), text.as_str());
+                let green_terminal = lexer.next().unwrap().terminal;
+                let terminal = Terminal::from_syntax_node(db, SyntaxNode::new_root(green_terminal));
+                let token = terminal.token(db).raw(db);
+                assert_eq!(token.kind, kind, "Wrong token kind, with text: \"{}\".", text);
+                assert_eq!(token.text, token_text, "Wrong token text.");
+                // TODO: verify trivia kinds and texts
+
+                assert_eq!(
+                    lexer.next().unwrap().kind,
+                    TokenKind::EndOfFile,
+                    "Wrong eof token, with text: \"{}\".",
+                    text
+                );
+                assert!(lexer.next().is_none(), "Expected end of lexer stream.");
+            }
+        }
+    }
+}
+
+#[test]
+fn test_cases() {
+    let db_val = DatabaseImpl::default();
+    let db = &db_val;
+    let res: Vec<GreenId> = Lexer::from_text(db, test_source(), "let x: &T = @ 6; //  5+ 3;")
+        .map(|term_with_kind| term_with_kind.terminal)
+        .collect();
+    assert_eq!(
+        res,
+        vec![
+            Terminal::new_green(
+                db,
+                Trivia::new_green(db, vec![]),
+                Token::new_green(db, TokenKind::Let, "let".into()),
+                Trivia::new_green(
+                    db,
+                    vec![Token::new_green(db, TokenKind::Whitespace, " ".into())]
+                ),
+            ),
+            Terminal::new_green(
+                db,
+                Trivia::new_green(db, vec![]),
+                Token::new_green(db, TokenKind::Identifier, "x".into()),
+                Trivia::new_green(db, vec![]),
+            ),
+            Terminal::new_green(
+                db,
+                Trivia::new_green(db, vec![]),
+                Token::new_green(db, TokenKind::Colon, ":".into()),
+                Trivia::new_green(
+                    db,
+                    vec![Token::new_green(db, TokenKind::Whitespace, " ".into())]
+                ),
+            ),
+            Terminal::new_green(
+                db,
+                Trivia::new_green(db, vec![]),
+                Token::new_green(db, TokenKind::And, "&".into()),
+                Trivia::new_green(db, vec![]),
+            ),
+            Terminal::new_green(
+                db,
+                Trivia::new_green(db, vec![]),
+                Token::new_green(db, TokenKind::Identifier, "T".into()),
+                Trivia::new_green(
+                    db,
+                    vec![Token::new_green(db, TokenKind::Whitespace, " ".into())]
+                ),
+            ),
+            Terminal::new_green(
+                db,
+                Trivia::new_green(db, vec![]),
+                Token::new_green(db, TokenKind::Eq, "=".into()),
+                Trivia::new_green(
+                    db,
+                    vec![Token::new_green(db, TokenKind::Whitespace, " ".into())]
+                ),
+            ),
+            Terminal::new_green(
+                db,
+                Trivia::new_green(db, vec![]),
+                Token::new_green(db, TokenKind::BadCharacters, "@".into()),
+                Trivia::new_green(
+                    db,
+                    vec![Token::new_green(db, TokenKind::Whitespace, " ".into())]
+                ),
+            ),
+            Terminal::new_green(
+                db,
+                Trivia::new_green(db, vec![]),
+                Token::new_green(db, TokenKind::LiteralNumber, "6".into()),
+                Trivia::new_green(db, vec![]),
+            ),
+            Terminal::new_green(
+                db,
+                Trivia::new_green(db, vec![]),
+                Token::new_green(db, TokenKind::Semi, ";".into()),
+                Trivia::new_green(
+                    db,
+                    vec![
+                        Token::new_green(db, TokenKind::Whitespace, " ".into()),
+                        Token::new_green(db, TokenKind::SingleLineComment, "//  5+ 3;".into())
+                    ]
+                ),
+            ),
+            Terminal::new_green(
+                db,
+                Trivia::new_green(db, vec![]),
+                Token::new_green(db, TokenKind::EndOfFile, "".into()),
+                Trivia::new_green(db, vec![]),
+            )
+        ]
+    );
+}
+
+#[test]
+fn test_bad_character() {
+    let db_val = DatabaseImpl::default();
+    let db = &db_val;
+
+    let text = "@";
+    let mut lexer = Lexer::from_text(db, test_source(), text);
+    let green_terminal = lexer.next().unwrap().terminal;
+    let terminal = Terminal::from_syntax_node(db, SyntaxNode::new_root(green_terminal));
+    let token = terminal.token(db).raw(db);
+    assert_eq!(token.kind, TokenKind::BadCharacters, "Wrong token kind, with text: \"{}\".", text);
+    assert_eq!(token.text, text, "Wrong token text.");
+
+    assert_eq!(
+        lexer.next().unwrap().kind,
+        TokenKind::EndOfFile,
+        "Wrong eof token, with text: \"{}\".",
+        text
+    );
+    assert!(lexer.next().is_none(), "Expected end of lexer stream.");
+}
