939e8b0fc79489da9f2a03ec38d7e7b4c2f849e1
Shahar Papini <spapini@starkware.co>
2022-07-25 12:49:12 +0300

    Semantic structures

--- crates/semantic/src/db.rs
+++ crates/semantic/src/db.rs
@@ -1,5 +1,5 @@
 use defs::db::DefsGroup;
-use defs::ids::{FreeFunctionId, ModuleId};
+use defs::ids::{FreeFunctionId, ModuleId, StructId};
 
 use crate::ids::*;
 use crate::semantic;
@@ -10,20 +10,25 @@ pub trait SemanticGroup: DefsGroup {
     #[salsa::interned]
     fn function_instance(&self, id: FunctionInstanceLongId) -> FunctionInstanceId;
     #[salsa::interned]
-    fn type_instance(&self, id: TypeInstanceLongId) -> TypeInstanceId;
+    fn type_instance(&self, id: Type) -> TypeId;
     #[salsa::interned]
     fn expr(&self, expr: semantic::Expr) -> ExprId;
     #[salsa::interned]
     fn statement(&self, statement: semantic::Statement) -> StatementId;
 
     fn module_semantic(&self, item: ModuleId) -> semantic::Module;
-    fn function_semantic(&self, item: FreeFunctionId) -> semantic::Function;
+    fn struct_semantic(&self, item: StructId) -> semantic::Struct;
+    fn function_semantic(&self, item: FreeFunctionId) -> semantic::FreeFunction;
 }
 
 fn module_semantic(_db: &dyn SemanticGroup, _item: ModuleId) -> semantic::Module {
     todo!()
 }
 
-fn function_semantic(_db: &dyn SemanticGroup, _item: FreeFunctionId) -> semantic::Function {
+fn struct_semantic(_db: &dyn SemanticGroup, _item: StructId) -> semantic::Struct {
+    todo!()
+}
+
+fn function_semantic(_db: &dyn SemanticGroup, _item: FreeFunctionId) -> semantic::FreeFunction {
     todo!()
 }
---
+++ crates/semantic/src/lib.rs
@@ -0,0 +1,3 @@
+pub mod db;
+pub mod ids;
+pub mod semantic;
