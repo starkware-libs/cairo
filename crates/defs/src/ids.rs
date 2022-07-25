d1c65abe6cf1095a5e785bb7bf91bb7545bda4b4
Shahar Papini <spapini@starkware.co>
2022-07-25 12:48:54 +0300

    More definition ids

--- crates/defs/src/ids.rs
+++ crates/defs/src/ids.rs
@@ -8,8 +8,8 @@
 // * func foo<T>(a: T){ return (); }.
 // Has 3 definitions:
 //   * Function "foo".
-//   * Generic argument "T" (only the first occurrence of "T").
-//   * Function argument "a".
+//   * Generic parameter "T" (only the first occurrence of "T").
+//   * Function parameter "a".
 // * trait MyTrait{ func foo() -> (); }
 // Has 2 definitions:
 //   * Trait "MyTrait"
@@ -40,25 +40,81 @@ macro_rules! id_by_name_and_parent {
     };
 }
 
-// Module.
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
 pub enum ModuleId {
     CrateRoot(CrateId),
     Submodule(SubmoduleId),
 }
+id_by_name_and_parent!(SubmoduleId, SubmoduleLongId, ModuleId);
 
-// Module Items.
-// ModuleItemId - direct children of a module.
+/// Id for direct children of a module.
 #[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
 pub enum ModuleItemId {
-    Submodule(SubmoduleId),
+    FreeFunction(FreeFunctionId),
     Struct(StructId),
+    ExternType(ExternTypeId),
+    ExternFunction(ExternFunctionId),
 }
+id_by_name_and_parent!(FreeFunctionId, FreeFunctionLongId, ModuleId);
 id_by_name_and_parent!(StructId, StructLongId, ModuleId);
-id_by_name_and_parent!(SubmoduleId, SubmoduleLongId, ModuleId);
+id_by_name_and_parent!(ExternTypeId, ExternTypeLongId, ModuleId);
+id_by_name_and_parent!(ExternFunctionId, ExternFunctionLongId, ModuleId);
+
+// Struct items.
+id_by_name_and_parent!(MemberId, MemberLongId, StructId);
+
+// Id for any variable definition.
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+pub enum VarId {
+    Param(ParamId),
+    Local(LocalVarId),
+    // TODO(spapini): Add var from pattern matching.
+}
+
+/// Id for anything that can directly contain a parameter.
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub enum ParamContainerId {
+    FreeFunction(FreeFunctionId),
+    ExternFunction(ExternFunctionId),
+}
+id_by_name_and_parent!(ParamId, ParamLongId, ParamContainerId);
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub enum BlockLongId {
+    /// The block is the entire body of a function.
+    FunctionBody(FunctionWithBodyId),
+    /// The block is an inner block.
+    Inner {
+        parent: BlockId,
+        /// Index of subblock inside its containing block, among all direct subblocks.
+        index: usize,
+    },
+}
+define_short_id!(BlockId);
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub struct LocalVarLongId {
+    parent_block: BlockId,
+    name: SmolStr,
+    // TODO(spapini): Introduce another hierarchy when we support patterns instead of
+    //   single identifier let.
+    /// Index of local variable inside its containing block, among all direct local variables.
+    /// definitions with the same name.
+    index: usize,
+}
+define_short_id!(LocalVarId);
+
+/// Id for any function definition that also contains a body with code.
+/// For example, 'extern func' is nto included.
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub enum FunctionWithBodyId {
+    FreeFunction(FreeFunctionId),
+    // TODO(spapini): impl functions
+}
 
 // Symbol - anything that can be a "Go to definition" result.
 #[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
 pub enum Symbol {
     ModuleItem(ModuleItemId),
+    Var(VarId),
 }
