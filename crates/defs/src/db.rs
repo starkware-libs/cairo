5b707cea0c8594c46f2e20e74106eaca33492a6f
Shahar Papini <spapini@starkware.co>
2022-07-25 12:48:54 +0300

    More definition ids

--- crates/defs/src/db.rs
+++ crates/defs/src/db.rs
@@ -5,5 +5,43 @@ use crate::ids::*;
 #[salsa::query_group(DefsDatabase)]
 pub trait DefsGroup {
     #[salsa::interned]
+    fn intern_sub_module(&self, id: SubmoduleLongId) -> SubmoduleId;
+    #[salsa::interned]
+    fn intern_function(&self, id: FunctionLongId) -> FunctionId;
+    #[salsa::interned]
     fn intern_struct(&self, id: StructLongId) -> StructId;
+    #[salsa::interned]
+    fn intern_member(&self, id: MemberLongId) -> MemberId;
+    #[salsa::interned]
+    fn intern_enum(&self, id: EnumLongId) -> EnumId;
+    #[salsa::interned]
+    fn intern_variant(&self, id: VariantLongId) -> VariantId;
+    #[salsa::interned]
+    fn intern_trait(&self, id: TraitLongId) -> TraitId;
+    #[salsa::interned]
+    fn intern_impl(&self, id: ImplLongId) -> ImplId;
+    #[salsa::interned]
+    fn intern_use(&self, id: UseLongId) -> UseId;
+    #[salsa::interned]
+    fn intern_extern_type(&self, id: ExternTypeLongId) -> ExternTypeId;
+    #[salsa::interned]
+    fn intern_extern_function(&self, id: ExternFunctionLongId) -> ExternFunctionId;
+    #[salsa::interned]
+    fn intern_trait_associated_type(&self, id: TraitAssociatedTypeLongId) -> TraitAssociatedTypeId;
+    #[salsa::interned]
+    fn intern_trait_function(&self, id: TraitFunctionLongId) -> TraitFunctionId;
+    #[salsa::interned]
+    fn intern_impl_associated_type(&self, id: ImplAssociatedTypeLongId) -> ImplAssociatedTypeId;
+    #[salsa::interned]
+    fn intern_impl_function(&self, id: ImplFunctionLongId) -> ImplFunctionId;
+    #[salsa::interned]
+    fn intern_generic_type_arg(&self, id: GenericTypeParamLongId) -> GenericTypeParamId;
+    #[salsa::interned]
+    fn intern_generic_impl_arg(&self, id: GenericImplParamLongId) -> GenericImplParamId;
+    #[salsa::interned]
+    fn intern_function_arg(&self, id: FunctionParamLongId) -> FunctionParamId;
+    #[salsa::interned]
+    fn intern_block(&self, id: BlockLongId) -> BlockId;
+    #[salsa::interned]
+    fn intern_let_var(&self, id: LetVarLongId) -> LetVarId;
 }
--- crates/defs/src/ids.rs
+++ crates/defs/src/ids.rs
@@ -8,8 +8,8 @@
 // * func foo<T>(a: T){ return (); }.
 // Has 3 definitions:
 //   * Function "foo".
-//   * Generic argument "T" (only the first occurrence of "T").
-//   * Function argument "a".
+//   * Generic parameter "T" (only the first occurrence of "T").
+//   * Function parameter "a".
 // * trait MyTrait{ func foo() -> (); }
 // Has 2 definitions:
 //   * Trait "MyTrait"
@@ -43,16 +43,123 @@ macro_rules! id_by_name_and_parent {
 // TODO(spapini): Add ModuleLongId once crates are added.
 define_short_id!(ModuleId);
 
-// Module Items.
 // ModuleItemId - direct children of a module.
 #[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
 pub enum ModuleItemId {
+    Submodule(SubmoduleId),
+    Function(FunctionId),
     Struct(StructId),
+    Enum(EnumId),
+    Trait(TraitId),
+    Impl(ImplId),
+    Use(UseId),
+    ExternType(ExternTypeId),
+    ExternFunction(ExternFunctionId),
 }
+id_by_name_and_parent!(SubmoduleId, SubmoduleLongId, ModuleId);
+id_by_name_and_parent!(FunctionId, FunctionLongId, ModuleId);
 id_by_name_and_parent!(StructId, StructLongId, ModuleId);
+id_by_name_and_parent!(EnumId, EnumLongId, ModuleId);
+id_by_name_and_parent!(TraitId, TraitLongId, ModuleId);
+id_by_name_and_parent!(ImplId, ImplLongId, ModuleId);
+id_by_name_and_parent!(UseId, UseLongId, ModuleId);
+id_by_name_and_parent!(ExternTypeId, ExternTypeLongId, ModuleId);
+id_by_name_and_parent!(ExternFunctionId, ExternFunctionLongId, ModuleId);
+
+// Struct items.
+id_by_name_and_parent!(MemberId, MemberLongId, StructId);
+
+// Enum items.
+id_by_name_and_parent!(VariantId, VariantLongId, EnumId);
+
+// TraitItemId - direct children of a trait.
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+pub enum TraitItemId {
+    AssociatedType(TraitAssociatedTypeId),
+    Function(TraitFunctionId),
+}
+id_by_name_and_parent!(TraitAssociatedTypeId, TraitAssociatedTypeLongId, TraitId);
+id_by_name_and_parent!(TraitFunctionId, TraitFunctionLongId, TraitId);
+
+// ImplItemId - direct children of an impl.
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+pub enum ImplItemId {
+    AssociatedType(ImplAssociatedTypeId),
+    Function(ImplFunctionId),
+}
+id_by_name_and_parent!(ImplAssociatedTypeId, ImplAssociatedTypeLongId, ImplId);
+id_by_name_and_parent!(ImplFunctionId, ImplFunctionLongId, ImplId);
+
+// Generics.
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+pub enum GenericParamId {
+    Type(GenericTypeParamId),
+    Impl(GenericImplParamId),
+}
+id_by_name_and_parent!(GenericTypeParamId, GenericTypeParamLongId, ItemWithGenericsId);
+id_by_name_and_parent!(GenericImplParamId, GenericImplParamLongId, ItemWithGenericsId);
+
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+pub enum ItemWithGenericsId {
+    Function(FunctionId),
+    Struct(StructId),
+    Enum(EnumId),
+    Trait(TraitId),
+    TraitAssociatedType(TraitAssociatedTypeId),
+    TraitFunction(TraitFunctionId),
+    Impl(ImplId),
+    ImplAssociatedType(ImplAssociatedTypeId),
+    ImplFunction(ImplFunctionId),
+    ExternType(ExternTypeId),
+    ExternFunction(ExternFunctionId),
+}
+
+// Function parameters.
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub enum ItemWithFunctionSignatureId {
+    Function(FunctionId),
+    ImplFunction(ImplFunctionId),
+    TraitFunction(TraitFunctionId),
+    ExternFunction(ExternFunctionId),
+}
+id_by_name_and_parent!(FunctionParamId, FunctionParamLongId, ItemWithFunctionSignatureId);
+
+// Definitions from code blocks.
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+pub enum VarId {
+    Param(FunctionParamId),
+    Local(LetVarId),
+    // TODO(spapini): Add var from pattern matching.
+}
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub enum BlockLongId {
+    FunctionBody(ItemWithFunctionBodyId),
+    Inner { parent: BlockId, index: usize },
+}
+define_short_id!(BlockId);
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub struct LetVarLongId {
+    block: BlockId,
+    name: SmolStr,
+    index: usize,
+}
+define_short_id!(LetVarId);
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub enum ItemWithFunctionBodyId {
+    Function(FunctionId),
+    ImplFunction(ImplFunctionId),
+    ExternFunction(ExternFunctionId),
+}
 
 // Symbol - anything that can be a "Go to definition" result.
 #[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
 pub enum Symbol {
     ModuleItem(ModuleItemId),
+    TraitItem(TraitItemId),
+    ImplItem(ImplItemId),
+    GenericParam(GenericParamId),
+    Var(VarId),
 }
