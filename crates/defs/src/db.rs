f07447c6e01a71f8143d7ca181b1856ec88ed3a9
Shahar Papini <spapini@starkware.co>
2022-07-25 12:48:54 +0300

    More definition ids

--- crates/defs/src/db.rs
+++ crates/defs/src/db.rs
@@ -5,5 +5,19 @@ use crate::ids::*;
 #[salsa::query_group(DefsDatabase)]
 pub trait DefsGroup {
     #[salsa::interned]
+    fn intern_function(&self, id: FreeFunctionLongId) -> FreeFunctionId;
+    #[salsa::interned]
     fn intern_struct(&self, id: StructLongId) -> StructId;
+    #[salsa::interned]
+    fn intern_member(&self, id: MemberLongId) -> MemberId;
+    #[salsa::interned]
+    fn intern_extern_type(&self, id: ExternTypeLongId) -> ExternTypeId;
+    #[salsa::interned]
+    fn intern_extern_function(&self, id: ExternFunctionLongId) -> ExternFunctionId;
+    #[salsa::interned]
+    fn intern_function_arg(&self, id: ParamLongId) -> ParamId;
+    #[salsa::interned]
+    fn intern_block(&self, id: BlockLongId) -> BlockId;
+    #[salsa::interned]
+    fn intern_let_var(&self, id: LocalVarLongId) -> LocalVarId;
 }
