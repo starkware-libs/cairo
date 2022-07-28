41a189a71f68ac88d16f85b4f2c0199319522874
Ori Ziv <oriz@starkware.co>
2022-07-28 18:25:14 +0300

    extracting fmt impls into fmt.rs and changed some file formating.

--- crates/sierra/src/lib.rs
+++ crates/sierra/src/lib.rs
@@ -1,1 +1,2 @@
+pub mod fmt;
 pub mod program;
--- crates/sierra/src/program.rs
+++ crates/sierra/src/program.rs
@@ -1,5 +1,3 @@
-use std::fmt;
-
 // A full sierra program.
 #[derive(Clone, Debug)]
 pub struct Program {
@@ -17,7 +15,7 @@ pub struct Function {
     // The arguments for the function.
     pub args: Vec<TypedVar>,
     // The return types.
-    pub res_types: Vec<Type>,
+    pub ret_types: Vec<Type>,
     // The statement id where the function starts.
     pub entry: StatementId,
 }
@@ -40,21 +38,6 @@ pub struct Type {
     pub args: Vec<TemplateArg>,
 }
 
-impl fmt::Display for Type {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.name)?;
-        let mut iter = self.args.iter();
-        if let Some(ta) = iter.next() {
-            write!(f, "<{}", ta)?;
-            for ta in iter {
-                write!(f, ", {}", ta)?;
-            }
-            write!(f, ">")?;
-        }
-        Ok(())
-    }
-}
-
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub struct StatementId(pub usize);
 
@@ -65,33 +48,11 @@ pub enum TemplateArg {
     Value(i64),
 }
 
-impl fmt::Display for TemplateArg {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            TemplateArg::Type(t) => write!(f, "{}", t),
-            TemplateArg::Value(v) => write!(f, "{}", v),
-        }
-    }
-}
-
 // A possible statement.
 #[derive(Clone, Debug)]
 pub enum Statement {
     Invocation(Invocation),
     Return(Vec<Identifier>),
-}
-
-impl fmt::Display for Statement {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Statement::Invocation(invc) => writeln!(f, "{};", invc),
-            Statement::Return(ids) => {
-                write!(f, "return (")?;
-                write_comma_separated(f, ids)?;
-                writeln!(f, ");")
-            }
-        }
-    }
 }
 
 // An invocation statement.
@@ -105,42 +66,11 @@ pub struct Invocation {
     pub branches: Vec<BranchInfo>,
 }
 
-impl fmt::Display for Invocation {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}(", self.ext)?;
-        write_comma_separated(f, &self.args)?;
-        if self.branches.len() == 1 && self.branches[0].target == BranchTarget::Fallthrough {
-            write!(f, ") -> (")?;
-            write_comma_separated(f, &self.branches[0].results)?;
-            write!(f, ")")
-        } else {
-            write!(f, ") {{")?;
-            self.branches.iter().try_for_each(|b| write!(f, "{}", b))?;
-            write!(f, "}}")
-        }
-    }
-}
-
 // Describes an extension.
 #[derive(Clone, Debug)]
 pub struct Extension {
     pub name: String,
     pub tmpl_args: Vec<TemplateArg>,
-}
-
-impl fmt::Display for Extension {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.name)?;
-        let mut iter = self.tmpl_args.iter();
-        if let Some(ta) = iter.next() {
-            write!(f, "<{}", ta)?;
-            for ta in iter {
-                write!(f, ", {}", ta)?;
-            }
-            write!(f, ">")?;
-        }
-        Ok(())
-    }
 }
 
 // Describes the flow of a chosen extension's branch.
@@ -152,32 +82,10 @@ pub struct BranchInfo {
     pub results: Vec<Identifier>,
 }
 
-impl fmt::Display for BranchInfo {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}(", self.target)?;
-        write_comma_separated(f, &self.results)?;
-        write!(f, ")")
-    }
-}
-
 #[derive(Clone, Debug, PartialEq)]
 pub enum BranchTarget {
     // Continues a run to the next statement.
     Fallthrough,
     // Continues the run to provided statement.
     Statement(StatementId),
-}
-
-impl fmt::Display for BranchTarget {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            BranchTarget::Fallthrough => write!(f, "fallthrough"),
-            BranchTarget::Statement(s_id) => write!(f, "{}", s_id.0),
-        }
-    }
-}
-
-fn write_comma_separated(f: &mut fmt::Formatter, ids: &Vec<Identifier>) -> fmt::Result {
-    ids.iter().take(1).try_for_each(|n| write!(f, "{}", n.0))?;
-    ids.iter().skip(1).try_for_each(|n| write!(f, ", {}", n.0))
 }
