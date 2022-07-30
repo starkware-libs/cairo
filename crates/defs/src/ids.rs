560470ff5fd7d70f6d62e50e7d4cbb3e4245c816
Shahar Papini <43779613+spapinistarkware@users.noreply.github.com>
2022-07-30 13:58:47 +0200

    Definition ids (#12)
    * Definition ids

---
+++ crates/defs/src/ids.rs
@@ -0,0 +1,58 @@
+// The following ids represent all the definitions in the code.
+// Roughly, this refers to the first appearance of each identifier.
+// Everything that can be returned by "Go to definition" is a definition.
+//
+// Examples:
+// * let x = 5.
+// Has a definition for the variable "x".
+// * func foo<T>(a: T){ return (); }.
+// Has 3 definitions:
+//   * Function "foo".
+//   * Generic argument "T" (only the first occurrence of "T").
+//   * Function argument "a".
+// * trait MyTrait{ func foo() -> (); }
+// Has 2 definitions:
+//   * Trait "MyTrait"
+//   * TraitFunction "foo".
+// * impl A for MyTrait{ func foo() -> (){...} }
+// Has 2 definitions:
+//   * Impl "A"
+//   * ImplFunction "foo".
+//
+// Call sites, variable usages, assignments, etc. are NOT definitions.
+
+use db_utils::define_short_id;
+use smol_str::SmolStr;
+
+// Utility macro for defining ids.
+// Defines a long id representing some element by a parent element and a name.
+// Also defines a short id to be used for interning of the long id.
+// See the documentation of 'define_short_id' for more details.
+macro_rules! id_by_name_and_parent {
+    ($id:ident, $long_id:ident, $parent_ty:ty) => {
+        #[derive(Clone, Debug, Hash, PartialEq, Eq)]
+        pub struct $long_id {
+            pub parent: $parent_ty,
+            pub name: SmolStr,
+        }
+        define_short_id!($id);
+    };
+}
+
+// Module.
+// TODO(spapini): Add ModuleLongId once crates are added.
+define_short_id!(ModuleId);
+
+// Module Items.
+// ModuleItemId - direct children of a module.
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+pub enum ModuleItemId {
+    Struct(StructId),
+}
+id_by_name_and_parent!(StructId, StructLongId, ModuleId);
+
+// Symbol - anything that can be a "Go to definition" result.
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+pub enum Symbol {
+    ModuleItem(ModuleItemId),
+}
