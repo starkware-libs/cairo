e0338cc6f57e36593769343bcdae09ab754e3db1
Ilya Lesokhin <ilya@starkware.co>
2022-07-31 13:53:40 +0000

    Fix JumpInstruction and add JnzInstruction.

--- crates/casm/src/instructions.rs
+++ crates/casm/src/instructions.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-use crate::operand::{DerefOperand, ImmediateOperand, Register, ResOperand};
+use crate::operand::{DerefOperand, DerefOrImmediate, ImmediateOperand, Register, ResOperand};
 
 #[cfg(test)]
 #[path = "instructions_test.rs"]
@@ -9,12 +9,14 @@ mod instructions_test;
 // An enum of Cairo instructions.
 enum Instruction {
     AssertEq(AssertEqInstruction),
+    Jnz(JnzInstruction),
     Jump(JumpInstruction),
 }
 impl Display for Instruction {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Instruction::AssertEq(insn) => write!(f, "{}", insn),
+            Instruction::Jnz(insn) => write!(f, "{}", insn),
             Instruction::Jump(insn) => write!(f, "{}", insn),
         }
     }
@@ -22,12 +24,23 @@ impl Display for Instruction {
 
 // Represents the instruction "jmp rel/abs".
 pub struct JumpInstruction {
-    target: ResOperand,
+    target: DerefOrImmediate,
     relative: bool,
 }
 impl Display for JumpInstruction {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "jmp {} {}", if self.relative { "rel" } else { "abs" }, self.target,)
+    }
+}
+
+// Represents the instruction "jmp rel <jump_offset> if condition != 0".
+pub struct JnzInstruction {
+    jump_offset: DerefOrImmediate,
+    condition: DerefOperand,
+}
+impl Display for JnzInstruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "jmp rel {} if {} != 0", self.jump_offset, self.condition)
     }
 }
 
--- crates/casm/src/instructions_test.rs
+++ crates/casm/src/instructions_test.rs
@@ -1,23 +1,33 @@
 use std::fmt::Display;
 
-use crate::instructions::{AssertEqInstruction, Instruction, JumpInstruction};
-use crate::operand::{DerefOperand, ImmediateOperand, Register, ResOperand};
+use crate::instructions::{AssertEqInstruction, Instruction, JnzInstruction, JumpInstruction};
+use crate::operand::{DerefOperand, DerefOrImmediate, ImmediateOperand, Register, ResOperand};
 
 #[test]
 fn test_jump_format() {
     let abs_jmp_insn = JumpInstruction {
-        target: ResOperand::Immediate(ImmediateOperand { value: 3 }),
+        target: DerefOrImmediate::Immediate(ImmediateOperand { value: 3 }),
         relative: false,
     };
 
     assert_eq!(abs_jmp_insn.to_string(), "jmp abs 3");
 
     let rel_jmp_insn: Instruction = Instruction::Jump(JumpInstruction {
-        target: ResOperand::Immediate(ImmediateOperand { value: -5 }),
+        target: DerefOrImmediate::Immediate(ImmediateOperand { value: -5 }),
         relative: true,
     });
 
     assert_eq!(rel_jmp_insn.to_string(), "jmp rel -5");
+}
+
+#[test]
+fn test_jnz_format() {
+    let jnz_insn = JnzInstruction {
+        jump_offset: DerefOrImmediate::Immediate(ImmediateOperand { value: 205 }),
+        condition: DerefOperand { register: Register::AP, offset: 5 },
+    };
+
+    assert_eq!(jnz_insn.to_string(), "jmp rel 205 if [ap + 5] != 0");
 }
 
 #[test]
