c05ab82887d16776a71894a2f2cf79569f2ddc1b
Ori Ziv <oriz@starkware.co>
2022-08-01 10:52:24 +0300

    extracted test to another file.

--- crates/sierra/src/edit_state.rs
+++ crates/sierra/src/edit_state.rs
@@ -41,39 +41,3 @@ pub fn put_results<'a, V>(
     }
     Ok(state)
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    pub type State = HashMap<Identifier, i64>;
-
-    fn as_id(name: &str) -> Identifier {
-        Identifier(name.to_string())
-    }
-
-    #[test]
-    fn empty() {
-        assert_eq!(take_args(State::new(), vec![].into_iter()), Ok((State::new(), vec![])));
-        assert_eq!(put_results(State::new(), vec![].into_iter()), Ok(State::new()));
-    }
-
-    #[test]
-    fn basic_mapping() {
-        assert_eq!(
-            take_args(State::from([(as_id("arg"), 0)]), vec![&as_id("arg")].into_iter(),),
-            Ok((State::new(), vec![0]))
-        );
-        assert_eq!(
-            put_results(State::new(), vec![(&as_id("res"), 1)].into_iter(),),
-            Ok(State::from([(as_id("res"), 1)]))
-        );
-        assert_eq!(
-            take_args(State::new(), vec![&as_id("arg")].into_iter(),),
-            Err(Error::MissingReference(as_id("arg")))
-        );
-        assert_eq!(
-            put_results(State::from([(as_id("res"), 1)]), vec![(&as_id("res"), 1)].into_iter(),),
-            Err(Error::VariableOverride(as_id("res")))
-        );
-    }
-}
