1169b65e4445b23b10f81c30f30e145e74aaf043
Ori Ziv <oriz@starkware.co>
2022-08-01 16:45:36 +0300

    Added extensions specialization - add the general signature for extensions that are used in examples that exist in other prs.

---
+++ crates/sierra/src/extensions/unconditional_jump.rs
@@ -0,0 +1,23 @@
+use super::{
+    validate_no_args, Specialization, SpecializationBox, SpecializationError, Specializer,
+    SpecializerBox,
+};
+use crate::program::{Identifier, TemplateArg};
+
+struct UnconditionalJumpSpecializer {}
+impl Specializer for UnconditionalJumpSpecializer {
+    fn specialize(
+        &self,
+        tmpl_args: &[TemplateArg],
+    ) -> Result<SpecializationBox, SpecializationError> {
+        validate_no_args(tmpl_args)?;
+        Ok(Box::new(UnconditionalJump {}))
+    }
+}
+
+struct UnconditionalJump {}
+impl Specialization for UnconditionalJump {}
+
+pub(super) fn extensions() -> [(Identifier, SpecializerBox); 1] {
+    [(Identifier::Name("jump".into()), Box::new(UnconditionalJumpSpecializer {}))]
+}
