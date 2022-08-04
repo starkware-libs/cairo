7709a876fc91e5a6eb99a05fdf1629fbbf30f6e6
Ori Ziv <oriz@starkware.co>
2022-08-04 18:25:19 +0300

    added textual parsing for sierra - and moved display tests to be based on reformating of parsed code.

--- crates/sierra/src/lib.rs
+++ crates/sierra/src/lib.rs
@@ -1,3 +1,12 @@
+use lalrpop_util::lalrpop_mod;
+
 pub mod edit_state;
 pub mod fmt;
 pub mod program;
+
+lalrpop_mod!(
+    #[allow(clippy::all, unused_extern_crates)]
+    parser
+);
+
+pub type ProgramParser = parser::ProgramParser;
