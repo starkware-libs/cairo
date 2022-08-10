898b52978744d8738712946bc3a9754899c6bdb5
Ori Ziv <oriz@starkware.co>
2022-08-10 12:00:11 +0300

    Made specialization add to a concrete extension map.
    Test Plan: unit tests based.
    
    Reviewers: yuvalsw
    
    Pull Request: https://github.com/starkware-libs/cairo2/pull/47
    
    commit-id:94d84008

--- crates/sierra/src/extensions/core/mem.rs
+++ crates/sierra/src/extensions/core/mem.rs
@@ -9,7 +9,7 @@ use crate::program::GenericArg;
 fn as_single_type(args: &[GenericArg]) -> Result<ConcreteTypeId, SpecializationError> {
     match args {
         [GenericArg::Type(ty)] => Ok(ty.clone()),
-        _ => Err(SpecializationError::UnsupportedTemplateArg),
+        _ => Err(SpecializationError::UnsupportedGenericArg),
     }
 }
 
