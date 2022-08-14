b0210a5d19f3c78cafa620b52c93f0389415593d
Shahar Papini <43779613+spapinistarkware@users.noreply.github.com>
2022-08-14 16:55:14 +0300

    File summary (#25)
    * File summary

--- crates/filesystem/src/db.rs
+++ crates/filesystem/src/db.rs
@@ -7,6 +7,7 @@ use std::fs;
 use std::sync::Arc;
 
 use crate::ids::{CrateId, CrateLongId, FileId, FileLongId};
+use crate::span::{FileSummary, TextOffset};
 
 // Salsa database interface.
 #[salsa::query_group(FilesDatabase)]
@@ -21,6 +22,7 @@ pub trait FilesGroup {
     fn crates(&self) -> Vec<CrateId>;
     fn crate_root_file(&self, crate_id: CrateId) -> Option<FileId>;
     fn file_content(&self, file_id: FileId) -> Option<Arc<String>>;
+    fn file_summary(&self, file_id: FileId) -> Option<Arc<FileSummary>>;
 }
 
 // Configuration of the project. This is the only input, and it defines everything else.
@@ -36,13 +38,27 @@ fn crate_root_file(db: &dyn FilesGroup, crt: CrateId) -> Option<FileId> {
     db.project_config().crate_roots.get(&crt).copied()
 }
 
+// This query is treated as a pure computation. If the file content is changed, an explicit
+// invalidation should be called on the DB by another entity.
+// For example, in the language server, this is initiated by the language server itself.
 fn file_content(db: &dyn FilesGroup, file: FileId) -> Option<Arc<String>> {
     match db.lookup_intern_file(file) {
-        // TODO(spapini): If ide file watching is not enough, do it here.
         FileLongId::OnDisk(path) => match fs::read_to_string(path) {
             Ok(content) => Some(Arc::new(content)),
             Err(_) => None,
         },
         FileLongId::Virtual(virt) => Some(virt.content),
     }
+}
+fn file_summary(db: &dyn FilesGroup, file: FileId) -> Option<Arc<FileSummary>> {
+    let content = db.file_content(file)?;
+    let mut line_offsets = vec![TextOffset(0)];
+    let mut offset = 0;
+    for ch in content.chars() {
+        offset += 1;
+        if ch == '\n' {
+            line_offsets.push(TextOffset(offset));
+        }
+    }
+    Some(Arc::new(FileSummary { line_offsets, total_length: offset }))
 }
--- crates/filesystem/src/lib.rs
+++ crates/filesystem/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod db;
 pub mod ids;
+pub mod span;
