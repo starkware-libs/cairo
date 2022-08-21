ed3e6c6f6406c44f80e70382beb1c4075db9ae29
Ilya Lesokhin <ilya@starkware.co>
2022-08-21 07:27:18 +0000

    Add trait ApplyApChange.

---
+++ crates/casm/src/ap_change_test.rs
@@ -0,0 +1,19 @@
+use super::{BinOpOperand, DerefOrImmediate};
+use crate::ap_change::{ApChange, ApChangeError, ApplyApChange};
+use crate::operand::{DerefOperand, Operation, Register, ResOperand};
+
+#[test]
+fn test_res_operand_ap_change() {
+    let a = DerefOperand { register: Register::FP, offset: -3 };
+    let b = DerefOperand { register: Register::AP, offset: 3 };
+
+    let operand =
+        ResOperand::BinOp(BinOpOperand { op: Operation::Mul, a, b: DerefOrImmediate::Deref(b) });
+
+    assert_eq!(
+        operand.clone().apply_ap_change(ApChange::Known(5)).unwrap().to_string(),
+        "[fp + -3] * [ap + -2]"
+    );
+
+    assert_eq!(operand.apply_ap_change(ApChange::Unknown), Err(ApChangeError::UnknownApChange));
+}
