7055aadec90bc1afde9132aa60718e3baeab36ea
Ori Ziv <oriz@starkware.co>
2022-08-23 17:30:23 +0300

    Extracted non-zero as a wrapper type.
    commit-id:f27cd2cb

--- crates/sierra/src/extensions/core/mod.rs
+++ crates/sierra/src/extensions/core/mod.rs
@@ -2,20 +2,25 @@ use self::function_call::FunctionCallLibFunc;
 use self::gas::{GasBuiltinType, GasLibFunc};
 use self::integer::{IntegerLibFunc, IntegerType};
 use self::mem::{DeferredType, MemLibFunc};
+use self::non_zero::{NonZeroType, UnwrapNonZeroLibFunc};
 use self::unconditional_jump::UnconditionalJumpLibFunc;
-use super::GenericLibFunc;
+use super::{GenericLibFunc, SpecializationError};
+use crate::ids::ConcreteTypeId;
+use crate::program::GenericArg;
 use crate::{define_libfunc_hierarchy, define_type_hierarchy};
 
 pub mod function_call;
 pub mod gas;
 pub mod integer;
 pub mod mem;
+pub mod non_zero;
 pub mod unconditional_jump;
 
 define_type_hierarchy! {
     pub enum CoreType {
         GasBuiltin(GasBuiltinType),
         Integer(IntegerType),
+        NonZero(NonZeroType),
         Deferred(DeferredType),
     }, CoreTypeConcrete
 }
@@ -26,6 +31,15 @@ define_libfunc_hierarchy! {
         Gas(GasLibFunc),
         Integer(IntegerLibFunc),
         Mem(MemLibFunc),
+        UnwrapNonZero(UnwrapNonZeroLibFunc),
         UnconditionalJump(UnconditionalJumpLibFunc),
     }, CoreConcreteLibFunc
+}
+
+/// Helper for extracting the type from the template arguments.
+fn as_single_type(args: &[GenericArg]) -> Result<ConcreteTypeId, SpecializationError> {
+    match args {
+        [GenericArg::Type(ty)] => Ok(ty.clone()),
+        _ => Err(SpecializationError::UnsupportedGenericArg),
+    }
 }
