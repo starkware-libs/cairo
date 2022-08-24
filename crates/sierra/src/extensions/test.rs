9c13f91e51ce7c6ed8e0e459d19993b617ee4433
orizi <104711814+orizi@users.noreply.github.com>
2022-08-24 16:09:53 +0300

    Added specialization context to specialization functions.. (#108)

--- crates/sierra/src/extensions/test.rs
+++ crates/sierra/src/extensions/test.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
+
 use test_case::test_case;
 
 use super::core::{CoreLibFunc, CoreType};
+use super::lib_func::SpecializationContext;
 use super::SpecializationError::{
     self, UnsupportedGenericArg, UnsupportedId, WrongNumberOfGenericArgs,
 };
@@ -82,5 +85,14 @@ fn find_libfunc_specialization(
     id: &str,
     generic_args: Vec<GenericArg>,
 ) -> Result<(), SpecializationError> {
-    CoreLibFunc::by_id(&id.into()).ok_or(UnsupportedId)?.specialize(&generic_args).map(|_| ())
+    CoreLibFunc::by_id(&id.into())
+        .ok_or(UnsupportedId)?
+        .specialize(
+            SpecializationContext {
+                concrete_type_ids: &HashMap::new(),
+                functions: &HashMap::new(),
+            },
+            &generic_args,
+        )
+        .map(|_| ())
 }
