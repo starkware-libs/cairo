753033e23e8c19827c2c5b6e99eb4d6f32fa760f
ilyalesokhin-starkware <ilya@starkware.co>
2022-08-29 09:23:51 +0200

    Add felt operation. (#143)
    * Add felt operation.

--- crates/sierra/src/extensions/core/felt.rs
+++ crates/sierra/src/extensions/core/felt.rs
@@ -1,5 +1,18 @@
-use crate::extensions::{ConcreteType, NoGenericArgsGenericType};
-use crate::ids::GenericTypeId;
+use crate::extensions::core::integer::Operator;
+use crate::extensions::lib_func::SpecializationContext;
+use crate::extensions::{
+    ConcreteType, GenericLibFunc, NamedType, NoGenericArgsGenericType, NonBranchConcreteLibFunc,
+    SpecializationError,
+};
+use crate::ids::{ConcreteTypeId, GenericLibFuncId, GenericTypeId};
+use crate::program::GenericArg;
+
+fn get_felt_type(
+    context: &SpecializationContext<'_>,
+) -> Result<ConcreteTypeId, SpecializationError> {
+    let felt_type = context.get_concrete_type(FeltType::id(), &[])?;
+    Ok(felt_type)
+}
 
 /// Type for felt.
 /// The native type of the Cairo architecture.
@@ -12,3 +25,44 @@ impl NoGenericArgsGenericType for FeltType {
 #[derive(Default)]
 pub struct FeltConcreteType {}
 impl ConcreteType for FeltConcreteType {}
+
+/// Libfunc for operations on felts.
+pub struct FeltOperationLibFunc {
+    pub operator: Operator,
+}
+impl GenericLibFunc for FeltOperationLibFunc {
+    type Concrete = FeltBinaryOperationConcreteLibFunc;
+    fn by_id(id: &GenericLibFuncId) -> Option<Self> {
+        const FELT_ADD: GenericLibFuncId = GenericLibFuncId::new_inline("felt_add");
+        const FELT_MUL: GenericLibFuncId = GenericLibFuncId::new_inline("felt_mul");
+        match id {
+            id if id == &FELT_ADD => Some(FeltOperationLibFunc { operator: Operator::Add }),
+            id if id == &FELT_MUL => Some(FeltOperationLibFunc { operator: Operator::Mul }),
+            _ => None,
+        }
+    }
+    fn specialize(
+        &self,
+        context: SpecializationContext<'_>,
+        args: &[GenericArg],
+    ) -> Result<Self::Concrete, SpecializationError> {
+        let felt_type = get_felt_type(&context)?;
+        match args {
+            [] => Ok(FeltBinaryOperationConcreteLibFunc { operator: self.operator, felt_type }),
+            _ => Err(SpecializationError::WrongNumberOfGenericArgs),
+        }
+    }
+}
+
+pub struct FeltBinaryOperationConcreteLibFunc {
+    pub operator: Operator,
+    pub felt_type: ConcreteTypeId,
+}
+impl NonBranchConcreteLibFunc for FeltBinaryOperationConcreteLibFunc {
+    fn input_types(&self) -> Vec<ConcreteTypeId> {
+        vec![self.felt_type.clone(), self.felt_type.clone()]
+    }
+    fn output_types(&self) -> Vec<ConcreteTypeId> {
+        vec![self.felt_type.clone()]
+    }
+}
