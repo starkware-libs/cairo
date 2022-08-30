9f424187beb5e32b469ebf55191def0592a2b216
Shahar Papini <spapini@starkware.co>
2022-08-30 11:33:37 +0300

    SyntaxStablePtr

--- crates/syntax/src/node/db.rs
+++ crates/syntax/src/node/db.rs
@@ -1,11 +1,14 @@
 use super::green::GreenNode;
-use super::ids::GreenId;
+use super::ids::{GreenId, SyntaxStablePtrId};
+use super::stable_ptr::SyntaxStablePtr;
 
 // Salsa database interface.
 #[salsa::query_group(GreenDatabase)]
 pub trait GreenInterner {
     #[salsa::interned]
     fn intern_green(&self, field: GreenNode) -> GreenId;
+    #[salsa::interned]
+    fn intern_stable_ptr(&self, field: SyntaxStablePtr) -> SyntaxStablePtrId;
 }
 
 pub trait AsGreenInterner {
