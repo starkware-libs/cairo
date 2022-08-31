60e0f2092be2135d745e48c4f2c6264aa7f9206c
Shahar Papini <spapini@starkware.co>
2022-08-31 10:54:10 +0300

    Move module_file->filesystem, module_defs->defs

--- crates/defs/src/db.rs
+++ crates/defs/src/db.rs
@@ -1,9 +1,18 @@
+use diagnostics::{Diagnostics, WithDiagnostics};
+use diagnostics_proc_macros::with_diagnostics;
+use filesystem::db::FilesGroup;
+use filesystem::ids::ModuleId;
+use parser::db::ParserGroup;
+use parser::parser::ParserDiagnostic;
+use syntax::node::ast;
+use syntax::node::db::{AsSyntaxGroup, SyntaxGroup};
+
 use crate::ids::*;
 
-// Salsa database interface.
-// See ids.rs for further details.
+/// Salsa database interface.
+/// See [`super::ids`] for further details.
 #[salsa::query_group(DefsDatabase)]
-pub trait DefsGroup {
+pub trait DefsGroup: FilesGroup + SyntaxGroup + AsSyntaxGroup + ParserGroup {
     #[salsa::interned]
     fn intern_free_function(&self, id: FreeFunctionLongId) -> FreeFunctionId;
     #[salsa::interned]
@@ -20,10 +29,67 @@ pub trait DefsGroup {
     fn intern_block(&self, id: BlockLongId) -> BlockId;
     #[salsa::interned]
     fn intern_local_var(&self, id: LocalVarLongId) -> LocalVarId;
-    #[salsa::interned]
-    fn intern_submodule(&self, id: SubmoduleLongId) -> SubmoduleId;
+
+    fn module_items(
+        &self,
+        module_id: ModuleId,
+    ) -> WithDiagnostics<Option<ModuleItems>, ParserDiagnostic>;
 }
 
 pub trait AsDefsGroup {
     fn as_defs_group(&self) -> &(dyn DefsGroup + 'static);
+}
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub struct ModuleItems {
+    items: Vec<ModuleItemId>,
+}
+
+#[with_diagnostics]
+fn module_items(
+    diagnostics: &mut Diagnostics<ParserDiagnostic>,
+    db: &dyn DefsGroup,
+    module_id: ModuleId,
+) -> Option<ModuleItems> {
+    let syntax_group = db.as_syntax_group();
+
+    let syntax_file = db.file_syntax(db.module_file(module_id)?).unwrap(diagnostics)?;
+    let mut items = Vec::new();
+    for item in syntax_file.items(syntax_group).elements(syntax_group) {
+        match item {
+            ast::Item::Module(_module) => todo!(),
+            ast::Item::Function(function) => {
+                items.push(ModuleItemId::FreeFunction(db.intern_free_function(
+                    FreeFunctionLongId {
+                        parent: module_id,
+                        name:
+                            function.signature(syntax_group).name(syntax_group).text(syntax_group),
+                    },
+                )));
+            }
+            ast::Item::ExternFunction(extern_function) => {
+                items.push(ModuleItemId::ExternFunction(
+                    db.intern_extern_function(ExternFunctionLongId {
+                        parent: module_id,
+                        name: extern_function
+                            .signature(syntax_group)
+                            .name(syntax_group)
+                            .text(syntax_group),
+                    }),
+                ));
+            }
+            ast::Item::ExternType(_extern_type) => todo!(),
+            ast::Item::Trait(_tr) => todo!(),
+            ast::Item::Impl(_imp) => todo!(),
+            ast::Item::Struct(strct) => {
+                items.push(ModuleItemId::Struct(db.intern_struct(StructLongId {
+                    parent: module_id,
+                    name: strct.name(db.as_syntax_group()).text(db.as_syntax_group()),
+                })));
+            }
+            ast::Item::Enum(_en) => todo!(),
+            ast::Item::Use(_us) => todo!(),
+        }
+    }
+    Some(ModuleItems { items })
 }
--- crates/semantic/src/db.rs
+++ crates/semantic/src/db.rs
@@ -1,14 +1,7 @@
 use defs::db::{AsDefsGroup, DefsGroup};
-use defs::ids::{
-    ExternFunctionLongId, FreeFunctionId, FreeFunctionLongId, ModuleId, ModuleItemId, StructId,
-    StructLongId,
-};
-use diagnostics::{Diagnostics, WithDiagnostics};
-use diagnostics_proc_macros::with_diagnostics;
-use filesystem::ids::FileId;
+use defs::ids::{FreeFunctionId, StructId};
+use filesystem::ids::ModuleId;
 use parser::db::ParserGroup;
-use parser::parser::ParserDiagnostic;
-use syntax::node::ast::Item;
 
 use crate::ids::{
     ConcreteFunctionId, ConcreteFunctionLongId, ExprId, StatementId, TypeId, TypeLongId,
@@ -28,28 +21,16 @@ pub trait SemanticGroup: DefsGroup + AsDefsGroup + ParserGroup {
     fn intern_statement(&self, statement: semantic::Statement) -> StatementId;
 
     // Queries to compute the semantic model for definitions.
-    fn module_semantic(&self, item: ModuleId) -> semantic::Module;
     fn struct_semantic(&self, item: StructId) -> semantic::Struct;
     fn free_function_semantic(&self, item: FreeFunctionId) -> semantic::FreeFunction;
     fn expr_semantic(&self, item: ExprId) -> semantic::Expr;
     fn statement_semantic(&self, item: StatementId) -> semantic::Statement;
-
-    // TODO(yuval): consider moving to filesystem/defs crate.
-    fn module_file(&self, module_id: ModuleId) -> Option<FileId>;
-    fn module_items(
-        &self,
-        item: ModuleId,
-    ) -> WithDiagnostics<Option<Vec<ModuleItemId>>, ParserDiagnostic>;
 
     // Corelib.
     #[salsa::invoke(corelib::core_module)]
     fn core_module(&self) -> ModuleId;
     #[salsa::invoke(corelib::core_felt_ty)]
     fn core_felt_ty(&self) -> TypeId;
-}
-
-fn module_semantic(_db: &dyn SemanticGroup, _item: ModuleId) -> semantic::Module {
-    todo!()
 }
 
 fn struct_semantic(_db: &dyn SemanticGroup, _item: StructId) -> semantic::Struct {
@@ -69,64 +50,4 @@ fn expr_semantic(db: &dyn SemanticGroup, item: ExprId) -> semantic::Expr {
 
 fn statement_semantic(db: &dyn SemanticGroup, item: StatementId) -> semantic::Statement {
     db.lookup_intern_statement(item)
-}
-
-#[with_diagnostics]
-fn module_items(
-    diagnostics: &mut Diagnostics<ParserDiagnostic>,
-    db: &dyn SemanticGroup,
-    module_id: ModuleId,
-) -> Option<Vec<ModuleItemId>> {
-    let syntax_group = db.as_syntax_group();
-
-    let syntax_file = db.file_syntax(module_file(db, module_id)?).unwrap(diagnostics)?;
-    let mut module_items = Vec::new();
-    for item in syntax_file.items(syntax_group).elements(syntax_group) {
-        match item {
-            Item::Module(_module) => todo!(),
-            Item::Function(function) => {
-                module_items.push(ModuleItemId::FreeFunction(db.intern_free_function(
-                    FreeFunctionLongId {
-                        parent: module_id,
-                        name:
-                            function.signature(syntax_group).name(syntax_group).text(syntax_group),
-                    },
-                )));
-            }
-            Item::ExternFunction(extern_function) => {
-                module_items.push(ModuleItemId::ExternFunction(
-                    db.intern_extern_function(ExternFunctionLongId {
-                        parent: module_id,
-                        name: extern_function
-                            .signature(syntax_group)
-                            .name(syntax_group)
-                            .text(syntax_group),
-                    }),
-                ));
-            }
-            Item::ExternType(_extern_type) => todo!(),
-            Item::Trait(_tr) => todo!(),
-            Item::Impl(_imp) => todo!(),
-            Item::Struct(strct) => {
-                module_items.push(ModuleItemId::Struct(db.intern_struct(StructLongId {
-                    parent: module_id,
-                    name: strct.name(db.as_syntax_group()).text(db.as_syntax_group()),
-                })));
-            }
-            Item::Enum(_en) => todo!(),
-            Item::Use(_us) => todo!(),
-        }
-    }
-    Some(module_items)
-}
-
-fn module_file(db: &dyn SemanticGroup, module_id: ModuleId) -> Option<FileId> {
-    match module_id {
-        ModuleId::CrateRoot(crate_id) => db.crate_root_file(crate_id),
-        ModuleId::Submodule(submodule_id) => {
-            let _submodule_long_id = db.as_defs_group().lookup_intern_submodule(submodule_id);
-            // TODO(yuval): support submodules.
-            todo!()
-        }
-    }
 }
