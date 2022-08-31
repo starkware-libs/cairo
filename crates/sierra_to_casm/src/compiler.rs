b8ebd44170665a371bd63546db849b524061e6c7
Ilya Lesokhin <ilya@starkware.co>
2022-08-31 07:55:11 +0000

    Add relocate_instructions.

--- crates/sierra_to_casm/src/compiler.rs
+++ crates/sierra_to_casm/src/compiler.rs
@@ -8,6 +8,7 @@ use thiserror::Error;
 
 use crate::invocations::{check_references_on_stack, compile_invocation, InvocationError};
 use crate::references::{init_reference, ReferencesError};
+use crate::relocations::{relocate_instructions, RelocationEntry};
 use crate::type_sizes::get_type_size_map;
 
 #[cfg(test)]
@@ -32,8 +33,8 @@ pub struct CairoProgram {
 }
 impl Display for CairoProgram {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for instruction in &self.instructions {
-            writeln!(f, "{};", instruction)?
+        for instruction in self.instructions.iter() {
+            writeln!(f, "{};", instruction)?;
         }
         Ok(())
     }
@@ -41,6 +42,7 @@ impl Display for CairoProgram {
 
 pub fn compile(program: &Program) -> Result<CairoProgram, CompilationError> {
     let mut instructions = Vec::new();
+    let mut relocations: Vec<RelocationEntry> = Vec::new();
 
     // Maps statement_idx to program_offset.
     let mut statement_offsets = Vec::with_capacity(program.statements.len());
@@ -66,8 +68,10 @@ pub fn compile(program: &Program) -> Result<CairoProgram, CompilationError> {
                 // TODO(orizi): Also test types of the results using 'check_types_match' when the
                 // returning function is available.
 
+                let ret_instruction = RetInstruction {};
+                program_offset += ret_instruction.op_size();
                 instructions.push(Instruction {
-                    body: InstructionBody::Ret(RetInstruction {}),
+                    body: InstructionBody::Ret(ret_instruction),
                     inc_ap: false,
                 });
             }
@@ -80,8 +84,15 @@ pub fn compile(program: &Program) -> Result<CairoProgram, CompilationError> {
                     .map_err(CompilationError::ProgramRegistryError)?;
                 let compiled_invocation = compile_invocation(&type_sizes, libfunc, &invoke_refs)?;
 
-                for instruction in &instructions {
+                for instruction in &compiled_invocation.instructions {
                     program_offset += instruction.body.op_size();
+                }
+
+                for entry in compiled_invocation.relocations.into_iter() {
+                    relocations.push(RelocationEntry {
+                        instruction_idx: instructions.len() + entry.instruction_idx,
+                        relocation: entry.relocation,
+                    });
                 }
                 instructions.extend(compiled_invocation.instructions.into_iter());
 
@@ -94,5 +105,8 @@ pub fn compile(program: &Program) -> Result<CairoProgram, CompilationError> {
             }
         }
     }
+
+    relocate_instructions(relocations, statement_offsets, &mut instructions);
+
     Ok(CairoProgram { instructions })
 }
--- crates/sierra_to_casm/src/relocations.rs
+++ crates/sierra_to_casm/src/relocations.rs
@@ -1,4 +1,8 @@
+use casm::instructions::{CallInstruction, Instruction, InstructionBody};
+use casm::operand::{DerefOrImmediate, ImmediateOperand};
 use sierra::program::StatementIdx;
+
+type CodeOffset = usize;
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum Relocation {
@@ -7,10 +11,70 @@ pub enum Relocation {
     RelativeStatementID(StatementIdx),
 }
 
+impl Relocation {
+    pub fn apply(
+        &self,
+        instruction: &Instruction,
+        instruction_offset: CodeOffset,
+        statement_offsets: &[CodeOffset],
+    ) -> Instruction {
+        match self {
+            Relocation::RelativeStatementID(statement_id) => match instruction {
+                Instruction {
+                    body:
+                        InstructionBody::Call(CallInstruction {
+                            target:
+                                DerefOrImmediate::Immediate(ImmediateOperand { value: orig_target }),
+                            relative: true,
+                        }),
+                    inc_ap: false,
+                } => {
+                    return Instruction {
+                        body: InstructionBody::Call(CallInstruction {
+                            target: DerefOrImmediate::Immediate(ImmediateOperand {
+                                value: orig_target + statement_offsets[statement_id.0] as i128
+                                    - instruction_offset as i128,
+                            }),
+                            relative: true,
+                        }),
+                        inc_ap: false,
+                    };
+                }
+                _ => todo!("Bad relocation."),
+            },
+        }
+    }
+}
+
 #[derive(Debug, Eq, PartialEq)]
 pub struct RelocationEntry {
     // The index of the instruction that needs to be relocated.
-    pub instruction_idx: usize,
+    pub instruction_idx: CodeOffset,
     // The relocation the needs to be applied.
     pub relocation: Relocation,
+}
+
+/// Applies 'relocations' to 'instructions'.
+///
+/// This is currently O(instruction.len()) rather then O(relocations.len()),
+/// But it can be does as single pass that generates the bytecode and applies the relocations.
+pub fn relocate_instructions(
+    relocations: Vec<RelocationEntry>,
+    statement_offsets: Vec<usize>,
+    instructions: &mut Vec<Instruction>,
+) {
+    let mut program_offset = 0;
+    for (instruction_idx, instruction) in instructions.iter_mut().enumerate() {
+        match relocations.get(0) {
+            Some(RelocationEntry { instruction_idx: relocation_idx, relocation })
+                if *relocation_idx == instruction_idx =>
+            {
+                *instruction = relocation.apply(instruction, program_offset, &statement_offsets)
+            }
+
+            _ => (),
+        };
+
+        program_offset += instruction.body.op_size();
+    }
 }
