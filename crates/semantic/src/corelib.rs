d1b7f7de7074f9a2371c5b7b8941032354331ea9
Shahar Papini <spapini@starkware.co>
2022-08-31 10:04:15 +0300

    Semantic expr calculation skeleton

---
+++ crates/semantic/src/corelib.rs
@@ -0,0 +1,20 @@
+use defs::ids::{ExternTypeLongId, ModuleId};
+use filesystem::ids::CrateLongId;
+
+use crate::db::SemanticGroup;
+use crate::{ConcreteType, GenericType, TypeId, TypeLongId};
+
+pub fn core_module(db: &dyn SemanticGroup) -> ModuleId {
+    let core_crate = db.intern_crate(CrateLongId("core".into()));
+    ModuleId::CrateRoot(core_crate)
+}
+
+pub fn core_felt_ty(db: &dyn SemanticGroup) -> TypeId {
+    let core_module = db.core_module();
+    let extern_type_id =
+        db.intern_extern_type(ExternTypeLongId { parent: core_module, name: "felt".into() });
+    db.intern_type(TypeLongId::Concrete(ConcreteType {
+        generic_type: GenericType::External(extern_type_id),
+        generic_args: vec![],
+    }))
+}
