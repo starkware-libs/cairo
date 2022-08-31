1f404c3e91eed1cf8d9ca4ccd7f9d5bc602fd296
Lior Goldberg <lior@starkware.co>
2022-08-31 11:25:44 +0300

    Fix PR comments.

--- crates/sierra_generator/src/db.rs
+++ crates/sierra_generator/src/db.rs
@@ -1,14 +1,20 @@
 use semantic::db::SemanticGroup;
-use sierra::ids::{ConcreteLibFuncId, ConcreteTypeId, FunctionId};
-use sierra::program::{ConcreteLibFuncLongId, ConcreteTypeLongId};
 
 #[salsa::query_group(SierraGenDatabase)]
 pub trait SierraGenGroup: SemanticGroup {
     #[salsa::interned]
-    fn intern_concrete_lib_func(&self, id: ConcreteLibFuncLongId) -> ConcreteLibFuncId;
+    fn intern_concrete_lib_func(
+        &self,
+        id: sierra::program::ConcreteLibFuncLongId,
+    ) -> sierra::ids::ConcreteLibFuncId;
     #[salsa::interned]
-    fn intern_concrete_type(&self, id: ConcreteTypeLongId) -> ConcreteTypeId;
+    fn intern_concrete_type(
+        &self,
+        id: sierra::program::ConcreteTypeLongId,
+    ) -> sierra::ids::ConcreteTypeId;
     /// Creates a Sierra function id for a function id of the semantic model.
+    // TODO(lior): Can we have the short and long ids in the same place? Currently, the short
+    //   id is defined in sierra and the long id is defined in semantic.
     #[salsa::interned]
-    fn intern_function(&self, id: semantic::ConcreteFunctionId) -> FunctionId;
+    fn intern_function(&self, id: semantic::ConcreteFunctionId) -> sierra::ids::FunctionId;
 }
