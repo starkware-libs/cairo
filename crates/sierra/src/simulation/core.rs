e168235df95aa146aa426fd088340aab4934414c
orizi <104711814+orizi@users.noreply.github.com>
2022-09-01 15:06:09 +0300

    Adding a more simple fib test, updating simulation to support it. (#207)

--- crates/sierra/src/simulation/core.rs
+++ crates/sierra/src/simulation/core.rs
@@ -9,12 +9,11 @@ use crate::extensions::arithmetic::{
 use crate::extensions::core::CoreConcreteLibFunc::{
     self, Felt, FunctionCall, Gas, Integer, Mem, UnconditionalJump, UnwrapNonZero,
 };
+use crate::extensions::felt::FeltConcrete;
 use crate::extensions::function_call::FunctionCallConcreteLibFunc;
 use crate::extensions::gas::GasConcreteLibFunc::{GetGas, RefundGas};
 use crate::extensions::gas::{GetGasConcreteLibFunc, RefundGasConcreteLibFunc};
-use crate::extensions::integer::IntegerConcrete::{
-    Const, Duplicate, Ignore, JumpNotZero, Operation,
-};
+use crate::extensions::integer::IntegerConcrete;
 use crate::extensions::mem::MemConcreteLibFunc::{
     AlignTemps, AllocLocals, Rename, StoreLocal, StoreTemp,
 };
@@ -31,7 +30,6 @@ pub fn simulate<
     simulate_function: F,
 ) -> Result<(Vec<Vec<MemCell>>, usize), LibFuncSimulationError> {
     match libfunc {
-        Felt(_) => todo!("felt operations are yet to be simulated."),
         FunctionCall(FunctionCallConcreteLibFunc { function, .. }) => {
             Ok((simulate_function(&function.id, inputs)?, 0))
         }
@@ -49,66 +47,8 @@ pub fn simulate<
             let [MemCell { value: gas_counter }] = unpack_inputs::<1>(inputs)?;
             Ok((vec![vec![(gas_counter + count).into()]], 0))
         }
-        Integer(Const(ConstConcreteLibFunc { c, .. })) => {
-            unpack_inputs::<0>(inputs)?;
-            Ok((vec![vec![(*c).into()]], 0))
-        }
-        Integer(Operation(OperationConcreteLibFunc::Binary(BinaryOperationConcreteLibFunc {
-            operator,
-            ..
-        }))) => {
-            let [MemCell { value: lhs }, MemCell { value: rhs }] = unpack_inputs::<2>(inputs)?;
-            Ok((
-                vec![vec![
-                    match operator {
-                        Operator::Add => lhs + rhs,
-                        Operator::Sub => lhs - rhs,
-                        Operator::Mul => lhs * rhs,
-                        Operator::Div => lhs / rhs,
-                        Operator::Mod => lhs % rhs,
-                    }
-                    .into(),
-                ]],
-                0,
-            ))
-        }
-        Integer(Operation(OperationConcreteLibFunc::Const(
-            OperationWithConstConcreteLibFunc { operator, c, .. },
-        ))) => {
-            let [MemCell { value }] = unpack_inputs::<1>(inputs)?;
-            Ok((
-                vec![vec![
-                    match operator {
-                        Operator::Add => value + c,
-                        Operator::Sub => value - c,
-                        Operator::Mul => value * c,
-                        Operator::Div => value / c,
-                        Operator::Mod => value % c,
-                    }
-                    .into(),
-                ]],
-                0,
-            ))
-        }
-        Integer(Duplicate(_)) => {
-            let [MemCell { value }] = unpack_inputs::<1>(inputs)?;
-            Ok((vec![vec![value.into()], vec![value.into()]], 0))
-        }
-        Integer(Ignore(_)) => {
-            unpack_inputs::<1>(inputs)?;
-            Ok((vec![], 0))
-        }
-        Integer(JumpNotZero(_)) => {
-            let [MemCell { value }] = unpack_inputs::<1>(inputs)?;
-            if value != 0 {
-                // Non-zero - jumping to the success branch and providing a NonZero wrap to the
-                // given value.
-                Ok((vec![vec![value.into()]], 0))
-            } else {
-                // Zero - jumping to the failure branch.
-                Ok((vec![], 1))
-            }
-        }
+        Integer(libfunc) => simulate_integer_libfunc(libfunc, inputs),
+        Felt(libfunc) => simulate_felt_libfunc(libfunc, inputs),
         UnwrapNonZero(_) | Mem(Rename(_)) | Mem(StoreLocal(_)) | Mem(StoreTemp(_)) => {
             Ok((single_cell_identity::<1>(inputs)?, 0))
         }
@@ -140,4 +80,142 @@ fn single_cell_identity<const N: usize>(
 ) -> Result<Vec<Vec<MemCell>>, LibFuncSimulationError> {
     let cells = unpack_inputs::<N>(inputs)?;
     Ok(cells.into_iter().map(|cell| vec![cell]).collect())
+}
+
+/// Simulate integer library functions.
+fn simulate_integer_libfunc(
+    libfunc: &IntegerConcrete,
+    inputs: Vec<Vec<MemCell>>,
+) -> Result<(Vec<Vec<MemCell>>, usize), LibFuncSimulationError> {
+    match libfunc {
+        IntegerConcrete::Const(ConstConcreteLibFunc { c, .. }) => {
+            unpack_inputs::<0>(inputs)?;
+            Ok((vec![vec![(*c).into()]], 0))
+        }
+        IntegerConcrete::Operation(OperationConcreteLibFunc::Binary(
+            BinaryOperationConcreteLibFunc { operator, .. },
+        )) => {
+            let [MemCell { value: lhs }, MemCell { value: rhs }] = unpack_inputs::<2>(inputs)?;
+            Ok((
+                vec![vec![
+                    match operator {
+                        Operator::Add => lhs + rhs,
+                        Operator::Sub => lhs - rhs,
+                        Operator::Mul => lhs * rhs,
+                        Operator::Div => lhs / rhs,
+                        Operator::Mod => lhs % rhs,
+                    }
+                    .into(),
+                ]],
+                0,
+            ))
+        }
+        IntegerConcrete::Operation(OperationConcreteLibFunc::Const(
+            OperationWithConstConcreteLibFunc { operator, c, .. },
+        )) => {
+            let [MemCell { value }] = unpack_inputs::<1>(inputs)?;
+            Ok((
+                vec![vec![
+                    match operator {
+                        Operator::Add => value + c,
+                        Operator::Sub => value - c,
+                        Operator::Mul => value * c,
+                        Operator::Div => value / c,
+                        Operator::Mod => value % c,
+                    }
+                    .into(),
+                ]],
+                0,
+            ))
+        }
+        IntegerConcrete::Duplicate(_) => {
+            let [MemCell { value }] = unpack_inputs::<1>(inputs)?;
+            Ok((vec![vec![value.into()], vec![value.into()]], 0))
+        }
+        IntegerConcrete::Ignore(_) => {
+            unpack_inputs::<1>(inputs)?;
+            Ok((vec![], 0))
+        }
+        IntegerConcrete::JumpNotZero(_) => {
+            let [MemCell { value }] = unpack_inputs::<1>(inputs)?;
+            if value != 0 {
+                // Non-zero - jumping to the success branch and providing a NonZero wrap to the
+                // given value.
+                Ok((vec![vec![value.into()]], 0))
+            } else {
+                // Zero - jumping to the failure branch.
+                Ok((vec![], 1))
+            }
+        }
+    }
+}
+
+/// Simulate felt library functions.
+fn simulate_felt_libfunc(
+    libfunc: &FeltConcrete,
+    inputs: Vec<Vec<MemCell>>,
+) -> Result<(Vec<Vec<MemCell>>, usize), LibFuncSimulationError> {
+    match libfunc {
+        FeltConcrete::Const(ConstConcreteLibFunc { c, .. }) => {
+            unpack_inputs::<0>(inputs)?;
+            Ok((vec![vec![(*c).into()]], 0))
+        }
+        // TODO(orizi): Fix MemCell to based on felt and fix operation here.
+        FeltConcrete::Operation(OperationConcreteLibFunc::Binary(
+            BinaryOperationConcreteLibFunc { operator, .. },
+        )) => {
+            let [MemCell { value: lhs }, MemCell { value: rhs }] = unpack_inputs::<2>(inputs)?;
+            Ok((
+                vec![vec![
+                    match operator {
+                        Operator::Add => lhs + rhs,
+                        Operator::Sub => lhs - rhs,
+                        Operator::Mul => lhs * rhs,
+                        Operator::Div => todo!("Support full felt operations."),
+                        Operator::Mod => 0,
+                    }
+                    .into(),
+                ]],
+                0,
+            ))
+        }
+        // TODO(orizi): Fix MemCell to based on felt and fix operation here.
+        FeltConcrete::Operation(OperationConcreteLibFunc::Const(
+            OperationWithConstConcreteLibFunc { operator, c, .. },
+        )) => {
+            let [MemCell { value }] = unpack_inputs::<1>(inputs)?;
+            Ok((
+                vec![vec![
+                    match operator {
+                        Operator::Add => value + c,
+                        Operator::Sub => value - c,
+                        Operator::Mul => value * c,
+                        Operator::Div => todo!("Support full felt operations."),
+                        Operator::Mod => value % c,
+                    }
+                    .into(),
+                ]],
+                0,
+            ))
+        }
+        FeltConcrete::Duplicate(_) => {
+            let [MemCell { value }] = unpack_inputs::<1>(inputs)?;
+            Ok((vec![vec![value.into()], vec![value.into()]], 0))
+        }
+        FeltConcrete::Ignore(_) => {
+            unpack_inputs::<1>(inputs)?;
+            Ok((vec![], 0))
+        }
+        FeltConcrete::JumpNotZero(_) => {
+            let [MemCell { value }] = unpack_inputs::<1>(inputs)?;
+            if value != 0 {
+                // Non-zero - jumping to the success branch and providing a NonZero wrap to the
+                // given value.
+                Ok((vec![vec![value.into()]], 0))
+            } else {
+                // Zero - jumping to the failure branch.
+                Ok((vec![], 1))
+            }
+        }
+    }
 }
