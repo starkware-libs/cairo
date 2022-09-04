8b59cc14423b49697d491316b233bb81063e1936
Lior Goldberg <lior@starkware.co>
2022-09-04 13:11:11 +0300

    Add program_generator.rs to sierra_generator.

--- crates/sierra_generator/src/lib.rs
+++ crates/sierra_generator/src/lib.rs
@@ -4,6 +4,7 @@ pub mod expr_generator_context;
 pub mod function_generator;
 pub mod id_allocator;
 pub mod pre_sierra;
+pub mod program_generator;
 pub mod resolve_labels;
 #[cfg(test)]
 pub mod test_utils;
