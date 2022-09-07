8cf9ae1a674bbb011f17ad4ede798fa38ef8639f
Shahar Papini <spapini@starkware.co>
2022-09-07 15:28:33 +0300

    Initial LSP commit
    Copy paste from example for the tower-lsp library.

---
+++ crates/languageserver/src/bin/languageserver.rs
@@ -0,0 +1,17 @@
+use languageserver::Backend;
+use tower_lsp::{LspService, Server};
+
+#[tokio::main]
+async fn main() {
+    #[cfg(feature = "runtime-agnostic")]
+    use tokio_util::compat::{TokioAsyncReadCompatExt, TokioAsyncWriteCompatExt};
+
+    tracing_subscriber::fmt().init();
+
+    let (stdin, stdout) = (tokio::io::stdin(), tokio::io::stdout());
+    #[cfg(feature = "runtime-agnostic")]
+    let (stdin, stdout) = (stdin.compat(), stdout.compat_write());
+
+    let (service, socket) = LspService::new(|client| Backend { client });
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
