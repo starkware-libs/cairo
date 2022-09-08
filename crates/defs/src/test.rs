8ede5d2108d48ff34cf12941f497714688527040
Shahar Papini <spapini@starkware.co>
2022-09-08 16:07:28 +0300

    Support module items and add a test for .cairo

--- crates/defs/src/test.rs
+++ crates/defs/src/test.rs
@@ -1,10 +1,13 @@
 use debug::debug::DebugWithDb;
 use filesystem::db::{init_files_group, AsFilesGroup, FilesDatabase, FilesGroup};
+use filesystem::ids::FileLongId;
 use indoc::indoc;
 use parser::db::ParserDatabase;
 use syntax::node::db::{AsSyntaxGroup, SyntaxDatabase, SyntaxGroup};
+use utils::extract_matches;
 
 use crate::db::{DefsDatabase, DefsGroup};
+use crate::ids::{ModuleId, ModuleItemId};
 use crate::test_utils::setup_test_module;
 
 #[salsa::database(DefsDatabase, ParserDatabase, SyntaxDatabase, FilesDatabase)]
@@ -61,4 +64,29 @@ fn test_resolve() {
     };
 }
 
-// TODO(spapini): Test .cairo suffix on submodules.
+#[test]
+fn test_module_file() {
+    let mut db_val = DatabaseForTesting::default();
+    let module_id = setup_test_module(
+        &mut db_val,
+        indoc! {"
+            mod mysubmodule;
+        "},
+    );
+    let db = &db_val;
+    let item_id = extract_matches!(
+        db.module_item_by_name(module_id, "mysubmodule".into())
+            .expect("Unexpected diagnostics")
+            .unwrap(),
+        ModuleItemId::Submodule
+    );
+    let submodule_id = ModuleId::Submodule(item_id);
+    assert_eq!(
+        db.lookup_intern_file(db.module_file(module_id).unwrap()),
+        FileLongId::OnDisk("src/lib.cairo".into())
+    );
+    assert_eq!(
+        db.lookup_intern_file(db.module_file(submodule_id).unwrap()),
+        FileLongId::OnDisk("src/mysubmodule.cairo".into())
+    );
+}
