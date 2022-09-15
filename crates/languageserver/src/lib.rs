9e0bda7a96adc65383873cc4376e42a118f9cec9
Shahar Papini <spapini@starkware.co>
2022-09-15 17:04:45 +0300

    Language server semantic diagnostics

--- crates/languageserver/src/lib.rs
+++ crates/languageserver/src/lib.rs
@@ -2,6 +2,7 @@
 
 mod semantic_highlighting;
 
+use std::path::PathBuf;
 use std::sync::Arc;
 
 use db_utils::Upcast;
@@ -10,6 +11,8 @@ use filesystem::db::{AsFilesGroupMut, FilesGroup, FilesGroupEx, PrivRawFileConte
 use filesystem::ids::{FileId, FileLongId};
 use filesystem::span::TextPosition;
 use parser::db::ParserGroup;
+use project::ProjectConfig;
+use semantic::db::SemanticGroup;
 use semantic::test_utils::SemanticDatabaseForTesting;
 use semantic_highlighting::token_kind::SemanticTokenKind;
 use semantic_highlighting::SemanticTokensTraverser;
@@ -20,6 +23,8 @@ use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer};
 
 pub type RootDatabase = SemanticDatabaseForTesting;
+
+const MAX_CRATE_DETECTION_DEPTH: usize = 20;
 
 pub struct Backend {
     pub client: Client,
@@ -37,30 +42,36 @@ impl Backend {
         let path = uri.to_file_path().expect("Only file URIs are supported.");
         db.intern_file(FileLongId::OnDisk(path))
     }
-    async fn publish_diagnostics(
+    fn add_diagnostic(
         &self,
-        db: tokio::sync::MutexGuard<'_, RootDatabase>,
-        uri: Url,
-        file: FileId,
+        db: &(dyn SemanticGroup + 'static),
+        diags: &mut Vec<Diagnostic>,
+        location: diagnostics::DiagnosticLocation,
+        message: String,
     ) {
-        let syntax_diagnostics = db.file_syntax_diagnostics(file);
-        // TODO(spapini): Add semantic diagnostics.
+        let start =
+            from_pos(location.span.start.position_in_file(db.upcast(), location.file_id).unwrap());
+        let end =
+            from_pos(location.span.start.position_in_file(db.upcast(), location.file_id).unwrap());
+        diags.push(Diagnostic { range: Range { start, end }, message, ..Diagnostic::default() });
+    }
+    fn get_diagnostics(&self, db: &(dyn SemanticGroup + 'static), file: FileId) -> Vec<Diagnostic> {
         // TODO(spapini): Version.
         let mut diags = Vec::new();
-        for d in &syntax_diagnostics.0 {
-            let files_db: &dyn FilesGroup = (*db).upcast();
-            let location = d.location(files_db);
-            let start =
-                from_pos(location.span.start.position_in_file(files_db, location.file_id).unwrap());
-            let end =
-                from_pos(location.span.start.position_in_file(files_db, location.file_id).unwrap());
-            diags.push(Diagnostic {
-                range: Range { start, end },
-                message: d.format(files_db),
-                ..Diagnostic::default()
-            });
+        for d in &db.file_syntax_diagnostics(file).0 {
+            let location = d.location(db.upcast());
+            let message = d.format(db.upcast());
+            self.add_diagnostic(db, &mut diags, location, message)
         }
-        self.client.publish_diagnostics(uri, diags, None).await
+        // TODO(spapini): Do this outer loop in semantic.
+        for module_id in db.file_modules(file).iter().flatten() {
+            for d in db.module_semantic_diagnostics(*module_id).unwrap().0 {
+                let location = d.location(db.upcast());
+                let message = d.format(db.upcast());
+                self.add_diagnostic(db, &mut diags, location, message)
+            }
+        }
+        diags
     }
 }
 
@@ -135,9 +146,14 @@ impl LanguageServer for Backend {
     }
 
     async fn did_open(&self, params: DidOpenTextDocumentParams) {
-        let db = self.db().await;
-        let file = self.file(&db, params.text_document.uri.clone());
-        self.publish_diagnostics(db, params.text_document.uri, file).await;
+        let mut db = self.db().await;
+        let uri = params.text_document.uri;
+        let path = uri.path();
+        detect_crate_for(&mut db, path);
+
+        let file = self.file(&db, uri.clone());
+        let diags = self.get_diagnostics(&*db, file);
+        self.client.publish_diagnostics(uri, diags, None).await
     }
 
     async fn did_change(&self, params: DidChangeTextDocumentParams) {
@@ -149,9 +165,11 @@ impl LanguageServer for Backend {
                 return;
             };
         let mut db = self.db().await;
-        let file = self.file(&db, params.text_document.uri.clone());
+        let uri = params.text_document.uri;
+        let file = self.file(&db, uri.clone());
         db.override_file_content(file, Some(Arc::new(text.into())));
-        self.publish_diagnostics(db, params.text_document.uri, file).await;
+        let diags = self.get_diagnostics(&*db, file);
+        self.client.publish_diagnostics(uri, diags, None).await
     }
 
     async fn did_save(&self, params: DidSaveTextDocumentParams) {
@@ -192,5 +210,16 @@ impl LanguageServer for Backend {
         let mut data: Vec<SemanticToken> = Vec::new();
         SemanticTokensTraverser::default().find_semantic_tokens((*db).upcast(), &mut data, node);
         Ok(Some(SemanticTokensResult::Tokens(SemanticTokens { result_id: None, data })))
+    }
+}
+
+/// Tries to detect the crate root the config that contains a cairo file, and add it to the system.
+fn detect_crate_for(db: &mut tokio::sync::MutexGuard<'_, RootDatabase>, path: &str) {
+    let mut path = PathBuf::from(path);
+    for _ in 0..MAX_CRATE_DETECTION_DEPTH {
+        path.pop();
+        if let Ok(config) = ProjectConfig::from_directory(path.as_path()) {
+            db.with_project_config(config);
+        };
     }
 }
--- crates/project/src/lib.rs
+++ crates/project/src/lib.rs
@@ -2,8 +2,10 @@
 mod test;
 
 use std::collections::HashMap;
+use std::path::{Path, PathBuf};
 
 use serde::{Deserialize, Serialize};
+use smol_str::SmolStr;
 
 #[derive(thiserror::Error, Debug)]
 pub enum DeserializationError {
@@ -11,14 +13,36 @@ pub enum DeserializationError {
     TomlError(#[from] toml::de::Error),
     #[error(transparent)]
     IoError(#[from] std::io::Error),
+    #[error("PathError")]
+    PathError,
+}
+const PROJECT_FILE_NAME: &str = "cairo_project.toml";
+
+/// Cairo project config, including its file content and metadata about the file.
+/// This file is expected to be at a root of a crate and specify the crate name and locaiton and
+/// of its dependency crates.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct ProjectConfig {
+    pub base_path: PathBuf,
+    pub content: ProjectConfigContent,
+}
+/// Contents of a Cairo project config file.
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct ProjectConfigContent {
+    pub crate_roots: HashMap<SmolStr, PathBuf>,
 }
 
-#[derive(Clone, Debug, Default, PartialEq, Eq, Serialize, Deserialize)]
-pub struct ProjectConfig {
-    pub crate_roots: HashMap<String, String>,
-}
 impl ProjectConfig {
-    pub fn from_file(filename: &str) -> Result<Self, DeserializationError> {
-        Ok(toml::from_str(&std::fs::read_to_string(filename)?)?)
+    pub fn from_directory(directory: &Path) -> Result<Self, DeserializationError> {
+        Self::from_file(&directory.join(PROJECT_FILE_NAME))
+    }
+    pub fn from_file(filename: &Path) -> Result<Self, DeserializationError> {
+        let base_path = filename
+            .parent()
+            .and_then(|p| p.to_str())
+            .ok_or(DeserializationError::PathError)?
+            .into();
+        let content = toml::from_str(&std::fs::read_to_string(filename)?)?;
+        Ok(ProjectConfig { base_path, content })
     }
 }
