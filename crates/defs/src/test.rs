d16cdfd67c29cf059faf8266c06d908947438281
Shahar Papini <43779613+spapinistarkware@users.noreply.github.com>
2022-09-15 10:31:31 +0300

    Submodules test (#384)

--- crates/defs/src/test.rs
+++ crates/defs/src/test.rs
@@ -96,3 +96,35 @@ fn test_module_file() {
         FileLongId::OnDisk("src/mysubmodule.cairo".into())
     );
 }
+
+fn set_file_content(db: &mut DatabaseForTesting, path: &str, content: &str) {
+    let file_id = db.intern_file(FileLongId::OnDisk(path.into()));
+    db.as_files_group_mut().override_file_content(file_id, Some(Arc::new(content.into())));
+}
+
+#[test]
+fn test_submodules() {
+    let mut db_val = DatabaseForTesting::default();
+    let db = &mut db_val;
+
+    let crate_id = db.intern_crate(CrateLongId("test_crate".into()));
+    let root = Directory("src".into());
+    db.set_crate_root(crate_id, Some(root));
+
+    // Main module file.
+    set_file_content(db, "src/lib.cairo", "mod submod;");
+    set_file_content(db, "src/submod.cairo", "mod subsubmod;");
+    set_file_content(db, "src/submod/subsubmod.cairo", "func foo() {}");
+
+    // Find submodules.
+    let module_id = ModuleId::CrateRoot(crate_id);
+    let submodule_id = ModuleId::Submodule(
+        *db.module_data(module_id).unwrap().submodules.iter().next().unwrap().0,
+    );
+    let subsubmodule_id = ModuleId::Submodule(
+        *db.module_data(submodule_id).unwrap().submodules.iter().next().unwrap().0,
+    );
+
+    db.module_item_by_name(subsubmodule_id, "foo".into())
+        .expect("Expected to find foo() in subsubmodule.");
+}
