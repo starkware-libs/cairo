74fb9dbbb65cb46c0d61cad0fa500e5569338ecc
Lior Goldberg <lior@starkware.co>
2022-09-18 20:02:55 +0300

    Use replace_libfunc_ids_in_program in test_program_generator().

--- crates/sierra_generator/src/program_generator_test.rs
+++ crates/sierra_generator/src/program_generator_test.rs
@@ -3,7 +3,7 @@ use pretty_assertions::assert_eq;
 use semantic::test_utils::setup_test_module;
 
 use crate::db::SierraGenGroup;
-use crate::test_utils::SierraGenDatabaseForTesting;
+use crate::test_utils::{replace_libfunc_ids_in_program, SierraGenDatabaseForTesting};
 
 #[test]
 fn test_program_generator() {
@@ -26,31 +26,31 @@ fn test_program_generator() {
 
     let program = &*db.get_program_code(module_id).expect("").unwrap();
     assert_eq!(
-        program.to_string(),
+        replace_libfunc_ids_in_program(&db, program).to_string(),
         indoc! {"
             type [0] = felt;
             type [1] = NonZero<[0]>;
 
-            libfunc [4] = felt_drop;
-            libfunc [0] = revoke_ap_tracking;
-            libfunc [1] = felt_const<5>;
-            libfunc [2] = store_temp<[0]>;
-            libfunc [3] = function_call<user@[0]>;
-            libfunc [6] = felt_dup;
-            libfunc [5] = felt_add;
+            libfunc felt_drop = felt_drop;
+            libfunc revoke_ap_tracking = revoke_ap_tracking;
+            libfunc felt_const<5> = felt_const<5>;
+            libfunc store_temp<[0]> = store_temp<[0]>;
+            libfunc function_call<user@[0]> = function_call<user@[0]>;
+            libfunc felt_dup = felt_dup;
+            libfunc felt_add = felt_add;
 
-            [4]([0]) -> ();
-            [0]() -> ();
-            [1]() -> ([1]);
-            [2]([1]) -> ([2]);
-            [3]([2]) -> ([3]);
-            [2]([3]) -> ([4]);
+            felt_drop([0]) -> ();
+            revoke_ap_tracking() -> ();
+            felt_const<5>() -> ([1]);
+            store_temp<[0]>([1]) -> ([2]);
+            function_call<user@[0]>([2]) -> ([3]);
+            store_temp<[0]>([3]) -> ([4]);
             return([4]);
-            [0]() -> ();
-            [6]([0]) -> ([0], [4]);
-            [5]([0], [4]) -> ([1]);
-            [2]([1]) -> ([2]);
-            [2]([2]) -> ([3]);
+            revoke_ap_tracking() -> ();
+            felt_dup([0]) -> ([0], [4]);
+            felt_add([0], [4]) -> ([1]);
+            store_temp<[0]>([1]) -> ([2]);
+            store_temp<[0]>([2]) -> ([3]);
             return([3]);
 
             [1]@0([0]: [0]) -> ([0]);
