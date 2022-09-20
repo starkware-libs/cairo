034087063ccfed82773262745711eb42537a5566
Ori Ziv <oriz@starkware.co>
2022-09-20 13:06:09 +0300

    Added equation generation library for solving gas requirements.
    commit-id:8b627059

--- crates/sierra_gas/src/lib.rs
+++ crates/sierra_gas/src/lib.rs
@@ -1,2 +1,14 @@
+use sierra::program::StatementIdx;
+use thiserror::Error;
+
 #[allow(dead_code)]
 mod cost_expr;
+#[allow(dead_code)]
+mod generate_equations;
+
+/// Error occurring while calculating the costing of a program's variables.
+#[derive(Error, Debug, Eq, PartialEq)]
+pub enum CostError {
+    #[error("found an illegal statement index during cost calculations")]
+    StatementOutOfBounds(StatementIdx),
+}
