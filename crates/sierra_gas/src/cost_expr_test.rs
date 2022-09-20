c0ecb51093849bcf6039797e524334a8ff1ad4e0
Ori Ziv <oriz@starkware.co>
2022-09-20 13:00:31 +0300

    Adding a new CostExpr util inside a sierra gas library.
    commit-id:05804bd2

---
+++ crates/sierra_gas/src/cost_expr_test.rs
@@ -0,0 +1,22 @@
+use sierra::program::StatementIdx;
+
+use super::{CostExpr, Var};
+
+#[test]
+fn operations() {
+    let as_var = |idx| Var::LibFuncImplicitGasVariable(StatementIdx(idx));
+    assert_eq!(CostExpr::from_const(1) + CostExpr::from_const(2), CostExpr::from_const(3));
+    assert_eq!(CostExpr::from_const(1) - CostExpr::from_const(2), CostExpr::from_const(-1));
+    assert_eq!(
+        CostExpr::from_var(as_var(5)) + CostExpr::from_const(2),
+        CostExpr { const_term: 2, var_to_coef: [(as_var(5), 1)].into_iter().collect() }
+    );
+    assert_eq!(
+        CostExpr::from_var(as_var(5)) - CostExpr::from_const(2),
+        CostExpr { const_term: -2, var_to_coef: [(as_var(5), 1)].into_iter().collect() }
+    );
+    assert_eq!(
+        CostExpr::from_var(as_var(2)) - CostExpr::from_var(as_var(2)),
+        CostExpr::from_const(0)
+    );
+}
