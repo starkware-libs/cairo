6cbde72f101894ad4db12df3871ee10b13bb7415
Yuval Goldberg <yuval@starkware.co>
2022-09-23 01:11:59 +0300

    Added capability for partial parser tests

--- crates/parser/src/db_test.rs
+++ crates/parser/src/db_test.rs
@@ -1,8 +1,4 @@
-use std::sync::Arc;
-
 use db_utils::Upcast;
-use filesystem::db::FilesGroup;
-use filesystem::ids::{FileLongId, VirtualFile};
 use pretty_assertions::assert_eq;
 use smol_str::SmolStr;
 use syntax::node::ast::{ItemList, SyntaxFile, TerminalEndOfFile, TokenEndOfFile, Trivia};
@@ -10,7 +6,7 @@ use syntax::node::db::SyntaxGroup;
 use syntax::node::{SyntaxNode, Terminal, Token, TypedSyntaxNode};
 
 use crate::db::ParserGroup;
-use crate::test_utils::ParserDatabaseForTesting;
+use crate::test_utils::{create_virtual_file, ParserDatabaseForTesting};
 
 fn build_empty_file_green_tree(db: &dyn SyntaxGroup) -> SyntaxFile {
     let eof_token = TokenEndOfFile::new_green(db, SmolStr::from(""));
@@ -34,11 +30,7 @@ fn test_parser() {
     let db = ParserDatabaseForTesting::default();
 
     // Parse empty cairo file.
-    let file_id = db.intern_file(FileLongId::Virtual(VirtualFile {
-        parent: None,
-        name: "file.cairo".into(),
-        content: Arc::new("".into()),
-    }));
+    let file_id = create_virtual_file(&db, "file.cairo", "");
     let syntax_file = db.file_syntax(file_id).unwrap();
     let diagnostics = db.file_syntax_diagnostics(file_id);
     assert_eq!(diagnostics.format(&db), "");
