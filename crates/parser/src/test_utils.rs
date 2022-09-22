d4f686a41bc2a9d03c3e595fb370afbd1d75d9a7
Yuval Goldberg <yuval@starkware.co>
2022-09-23 01:11:59 +0300

    Added capability for partial parser tests

--- crates/parser/src/test_utils.rs
+++ crates/parser/src/test_utils.rs
@@ -5,7 +5,8 @@ use std::sync::Arc;
 use db_utils::Upcast;
 use diagnostics::Diagnostics;
 use filesystem::db::{init_files_group, FilesDatabase, FilesGroup};
-use filesystem::ids::{FileLongId, VirtualFile};
+use filesystem::ids::{FileId, FileLongId, VirtualFile};
+use smol_str::SmolStr;
 use syntax::node::db::{SyntaxDatabase, SyntaxGroup};
 use syntax::node::{SyntaxNode, TypedSyntaxNode};
 
@@ -33,14 +34,22 @@ impl Upcast<dyn SyntaxGroup> for ParserDatabaseForTesting {
     }
 }
 
-pub fn get_syntax_root_and_diagnostics(
+pub fn get_syntax_root_and_diagnostics_from_file(
     db: &ParserDatabaseForTesting,
     cairo_filename: &str,
 ) -> (SyntaxNode, Diagnostics<ParserDiagnostic>) {
     let file_id = db.intern_file(FileLongId::OnDisk(PathBuf::from(cairo_filename)));
     let contents = db.file_content(file_id).unwrap();
+    get_syntax_root_and_diagnostics(db, file_id, contents.as_str())
+}
+
+pub fn get_syntax_root_and_diagnostics(
+    db: &ParserDatabaseForTesting,
+    file_id: FileId,
+    contents: &str,
+) -> (SyntaxNode, Diagnostics<ParserDiagnostic>) {
     let mut diagnostics = Diagnostics::new();
-    let syntax_root = Parser::parse_file(db, &mut diagnostics, file_id, contents.as_str());
+    let syntax_root = Parser::parse_file(db, &mut diagnostics, file_id, contents);
     (syntax_root.as_syntax_node(), diagnostics)
 }
 
@@ -52,14 +61,22 @@ pub fn read_file(filename: &str) -> String {
 pub fn get_diagnostics(db: &mut ParserDatabaseForTesting, inputs: Vec<String>) -> Vec<String> {
     let code = &inputs[0];
 
-    let mut diagnostics = Diagnostics::new();
-    let file_id = db.intern_file(FileLongId::Virtual(VirtualFile {
-        parent: None,
-        name: "dummy_file.cairo".into(),
-        content: Arc::new(code.into()),
-    }));
-    Parser::parse_file(db, &mut diagnostics, file_id, code);
+    let file_id = create_virtual_file(db, "dummy_file.cairo".into(), code);
+    let (_, diagnostics) = get_syntax_root_and_diagnostics(db, file_id, code);
     vec![diagnostics.format(db)]
+}
+
+/// Creates a virtual file with the given content and returns its ID.
+pub fn create_virtual_file(
+    db: &ParserDatabaseForTesting,
+    file_name: SmolStr,
+    content: &str,
+) -> FileId {
+    db.intern_file(FileLongId::Virtual(VirtualFile {
+        parent: None,
+        name: file_name,
+        content: Arc::new(content.into()),
+    }))
 }
 
 #[macro_export]
