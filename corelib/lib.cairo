9fc4ef34f3820f827288181c0157d9f8d8e5ea93
Ori Ziv <oriz@starkware.co>
2022-09-29 14:24:28 +0300

    Made dup and drop generic.
    commit-id:d8dd5bb3

--- corelib/lib.cairo
+++ corelib/lib.cairo
@@ -2,8 +2,6 @@ enum bool {
     True: (),
     False: (),
 }
-extern func bool_dup(a: bool) -> (bool, bool);
-extern func bool_drop(a: bool);
 extern func bool_and(a: bool, b: bool) -> bool;
 extern func bool_or(a: bool, b: bool) -> bool;
 extern func bool_not(a: bool) -> bool;
@@ -11,16 +9,12 @@ extern func bool_not(a: bool) -> bool;
 extern type RangeCheck;
 
 extern type felt;
-extern func felt_dup(a: felt) -> (felt, felt);
-extern func felt_drop(a: felt);
 extern func felt_add(a: felt, b: felt) -> felt;
 extern func felt_sub(a: felt, b: felt) -> felt;
 extern func felt_mul(a: felt, b: felt) -> felt;
 
 extern type NonZero < T >;
 extern func unwrap_nz< T > (a: NonZero::<T>) -> T;
-extern func non_zero_felt_dup(a: NonZero::<felt>) -> (NonZero::<felt>, NonZero::<felt>);
-extern func non_zero_felt_drop(a: NonZero::<felt>);
 extern func felt_div(a: felt, b: NonZero::<felt>) -> felt;
 
 extern func felt_eq(a: felt, b: felt) -> bool;
@@ -29,6 +23,9 @@ extern func felt_le(a: felt, b: felt) -> (bool);
 extern func felt_ge(a: felt, b: felt) -> (bool);
 extern func felt_lt(a: felt, b: felt) -> (bool);
 extern func felt_gt(a: felt, b: felt) -> (bool);
+
+extern func dup < T >(obj: T) -> (T, T);
+extern func drop < T >(obj: T);
 
 // Refs.
 mod ref;
--- crates/sierra/src/extensions/modules/integer.rs
+++ crates/sierra/src/extensions/modules/integer.rs
@@ -1,6 +1,5 @@
 use super::arithmetic::{ArithmeticTraits, ConstLibFunc, OperationLibFunc};
 use super::jump_not_zero::{JumpNotZeroLibFunc, JumpNotZeroTraits};
-use super::pod::{DropLibFunc, DuplicateLibFunc, PodTraits};
 use crate::define_libfunc_hierarchy;
 use crate::extensions::types::{InfoOnlyConcreteType, TypeInfo};
 use crate::extensions::{NamedType, NoGenericArgsGenericType};
@@ -24,19 +23,12 @@ define_libfunc_hierarchy! {
     pub enum IntegerLibFunc {
         Operation(IntOperationLibFunc),
         Const(IntConstLibFunc),
-        Drop(IntDropLibFunc),
-        Duplicate(IntDuplicateLibFunc),
         JumpNotZero(IntJumpNotZeroLibFunc),
     }, IntegerConcrete
 }
 
 #[derive(Default)]
 pub struct IntegerTraits {}
-impl PodTraits for IntegerTraits {
-    const DROP: GenericLibFuncId = GenericLibFuncId::new_inline("int_drop");
-    const DUPLICATE: GenericLibFuncId = GenericLibFuncId::new_inline("int_dup");
-    const GENERIC_TYPE_ID: GenericTypeId = <IntegerType as NamedType>::ID;
-}
 impl ArithmeticTraits for IntegerTraits {
     const ADD: GenericLibFuncId = GenericLibFuncId::new_inline("int_add");
     const SUB: GenericLibFuncId = GenericLibFuncId::new_inline("int_sub");
@@ -50,8 +42,6 @@ impl JumpNotZeroTraits for IntegerTraits {
     const JUMP_NOT_ZERO: GenericLibFuncId = GenericLibFuncId::new_inline("int_jump_nz");
     const GENERIC_TYPE_ID: GenericTypeId = <IntegerType as NamedType>::ID;
 }
-pub type IntDropLibFunc = DropLibFunc<IntegerTraits>;
-pub type IntDuplicateLibFunc = DuplicateLibFunc<IntegerTraits>;
 pub type IntOperationLibFunc = OperationLibFunc<IntegerTraits>;
 pub type IntConstLibFunc = ConstLibFunc<IntegerTraits>;
 pub type IntJumpNotZeroLibFunc = JumpNotZeroLibFunc<IntegerTraits>;
