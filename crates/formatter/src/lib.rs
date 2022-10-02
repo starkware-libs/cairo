de3c0af98f5da0647b30bb11a05b10f7acf0d0ba
gilbens-starkware <gil@starkware.co>
2022-10-02 15:10:09 +0300

    Move formatter into its own crate

---
+++ crates/formatter/src/lib.rs
@@ -0,0 +1,43 @@
+//! Cairo formatter.
+
+pub mod formatter;
+pub mod node_properties;
+
+use syntax::node::db::SyntaxGroup;
+use syntax::node::SyntaxNode;
+
+use crate::formatter::Formatter;
+
+#[cfg(test)]
+mod test;
+
+pub fn get_formatted_file(
+    db: &dyn SyntaxGroup,
+    syntax_root: &SyntaxNode,
+    config: FormatterConfig,
+) -> String {
+    let mut formatter = Formatter::new(db, config);
+    formatter.format_node(syntax_root, false);
+    formatter.get_result()
+}
+
+pub struct FormatterConfig {
+    tab_size: usize,
+    max_line_length: usize,
+}
+
+// Config params
+// TODO(Gil): export to file and load from file
+const TAB_SIZE: usize = 4;
+const MAX_LINE_LENGTH: usize = 100;
+
+impl FormatterConfig {
+    pub fn new(tab_size: usize, max_line_length: usize) -> Self {
+        Self { tab_size, max_line_length }
+    }
+}
+impl Default for FormatterConfig {
+    fn default() -> Self {
+        Self::new(TAB_SIZE, MAX_LINE_LENGTH)
+    }
+}
