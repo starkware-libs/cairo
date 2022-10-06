4647438977c9687c94be7408f99b9a5d18378f1f
Lior Goldberg <lior@starkware.co>
2022-10-06 18:12:43 +0300

    Add file name with line number to error message.
    commit-id:6da1afbc

--- crates/utils/src/parse_test_file.rs
+++ crates/utils/src/parse_test_file.rs
@@ -15,24 +15,30 @@ struct Tag {
     content: String,
 }
 
+/// Represents a single test from the test file.
+pub struct Test {
+    pub attributes: OrderedHashMap<String, String>,
+    pub line_num: usize,
+}
+
 #[derive(Default)]
 struct TestBuilder {
-    tests: OrderedHashMap<String, OrderedHashMap<String, String>>,
-    current_test: OrderedHashMap<String, String>,
+    tests: OrderedHashMap<String, Test>,
+    current_test: Option<Test>,
     current_test_name: Option<String>,
     current_tag: Option<Tag>,
 }
 
-pub fn parse_test_file(
-    filename: &Path,
-) -> io::Result<OrderedHashMap<String, OrderedHashMap<String, String>>> {
+pub fn parse_test_file(filename: &Path) -> io::Result<OrderedHashMap<String, Test>> {
     let file = File::open(filename)?;
     let mut lines = io::BufReader::new(file).lines();
     let mut builder = TestBuilder::default();
+    let mut line_num: usize = 0;
     while let Some(Ok(line)) = lines.next() {
+        line_num += 1;
         if let Some(line) = line.strip_prefix(TAG_PREFIX) {
             if builder.current_test_name == None {
-                builder.set_test_name(line.into());
+                builder.set_test_name(line.into(), line_num);
             } else if line.starts_with("===") {
                 // Separate tests.
                 assert_eq!(
@@ -55,13 +61,14 @@ impl TestBuilder {
     /// Closes a tag if one is open, otherwise does nothing.
     fn close_open_tag(&mut self) {
         if let Some(tag) = &mut self.current_tag {
+            let attributes = &mut self.current_test.as_mut().unwrap().attributes;
             assert!(
-                !self.current_test.contains_key(&tag.name),
+                !attributes.contains_key(&tag.name),
                 "Duplicate tag '{}' found in test (test name: {}).",
                 tag.name,
                 self.current_test_name.as_ref().unwrap_or(&"<unknown>".into())
             );
-            self.current_test.insert(std::mem::take(&mut tag.name), tag.content.trim().to_string());
+            attributes.insert(std::mem::take(&mut tag.name), tag.content.trim().to_string());
             self.current_tag = None;
         }
     }
@@ -71,8 +78,9 @@ impl TestBuilder {
         self.current_tag = Some(Tag { name: line, content: "".into() });
     }
 
-    fn set_test_name(&mut self, line: String) {
+    fn set_test_name(&mut self, line: String, line_num: usize) {
         self.current_test_name = Some(line);
+        self.current_test = Some(Test { attributes: OrderedHashMap::default(), line_num });
     }
 
     fn add_content_line(&mut self, line: String) {
@@ -89,16 +97,15 @@ impl TestBuilder {
 
     fn new_test(&mut self) {
         self.close_open_tag();
-        let current_test = std::mem::take(&mut self.current_test);
         self.tests.insert(
             self.current_test_name.as_ref().expect("No name found for test.").into(),
-            current_test,
+            std::mem::take(&mut self.current_test).unwrap(),
         );
         self.current_test_name = None;
         self.current_tag = None;
     }
 
-    fn finalize(&mut self) -> OrderedHashMap<String, OrderedHashMap<String, String>> {
+    fn finalize(&mut self) -> OrderedHashMap<String, Test> {
         self.new_test();
         std::mem::take(&mut self.tests)
     }
@@ -168,15 +175,18 @@ macro_rules! test_file_test {
                 let tests = utils::parse_test_file(std::path::Path::new(filename))?;
                 // TODO(alont): global tags for all tests in a file.
                 for (name, test) in tests {
-                    assert_eq!(test["test_function_name"], stringify!($func));
+                    assert_eq!(test.attributes["test_function_name"], stringify!($func));
 
-                    let outputs = $func(&mut <$db_type>::default(), &test);
+                    let outputs = $func(&mut <$db_type>::default(), &test.attributes);
+                    let line_num = test.line_num;
+                    let full_filename = std::fs::canonicalize(filename)?;
+                    let full_filename_str = full_filename.to_str().unwrap();
 
                     for (key, value) in outputs {
                         pretty_assertions::assert_eq!(
                             value.trim(),
-                            test[key],
-                            "\"{name}\" failed."
+                            test.attributes[key],
+                            "Test \"{name}\" failed.\nIn {full_filename_str}:{line_num}"
                         );
                     }
                 }
