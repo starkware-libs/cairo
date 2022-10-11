ecb83b24aacf19c382134197395b23ae3dbc1cd0
Yuval Goldberg <yuval@starkware.co>
2022-10-11 17:08:05 +0300

    Added enum to Sierra

---
+++ crates/sierra/src/extensions/modules/enm.rs
@@ -0,0 +1,221 @@
+//! Sierra example:
+//! ```ignore
+//! type felt_ty = felt;
+//! type unit_ty = Tuple;
+//! type Option = Enum<felt_ty, unit_ty>;
+//! libfunc init_option_some = enum_init<Option, 0>;
+//! libfunc init_option_none = enum_init<Option, 1>;
+//! libfunc match_option = enum_match<Option>;
+//! ...
+//! felt_const<0>() -> (felt0);
+//! tuple_const() -> (unit);
+//! init_option_some(felt0) -> (some_id);
+//! init_option_none(unit) -> (none_id);
+//! match_option(some_id) {1000(some), 2000(none)};
+//! match_option(none_id) {1000(some), 2000(none)};
+//! ```
+
+use utils::try_extract_matches;
+
+use super::as_single_type;
+use crate::define_libfunc_hierarchy;
+use crate::extensions::lib_func::{
+    BranchSignature, LibFuncSignature, OutputVarInfo, SierraApChange, SignatureOnlyConcreteLibFunc,
+    SignatureSpecializationContext, SpecializationContext,
+};
+use crate::extensions::type_specialization_context::TypeSpecializationContext;
+use crate::extensions::types::TypeInfo;
+use crate::extensions::{
+    ConcreteType, NamedLibFunc, NamedType, OutputVarReferenceInfo, SignatureBasedConcreteLibFunc,
+    SpecializationError,
+};
+use crate::ids::{ConcreteTypeId, GenericLibFuncId, GenericTypeId};
+use crate::program::{ConcreteTypeLongId, GenericArg};
+
+/// Type representing an enum.
+#[derive(Default)]
+pub struct EnumType {}
+impl NamedType for EnumType {
+    type Concrete = EnumConcreteType;
+    const ID: GenericTypeId = GenericTypeId::new_inline("Enum");
+
+    fn specialize(
+        &self,
+        context: &dyn TypeSpecializationContext,
+        args: &[GenericArg],
+    ) -> Result<Self::Concrete, SpecializationError> {
+        Self::Concrete::new(context, args)
+    }
+}
+
+pub struct EnumConcreteType {
+    pub info: TypeInfo,
+    pub variants: Vec<ConcreteTypeId>,
+}
+impl EnumConcreteType {
+    fn new(
+        context: &dyn TypeSpecializationContext,
+        args: &[GenericArg],
+    ) -> Result<Self, SpecializationError> {
+        let mut duplicatable = true;
+        let mut droppable = true;
+        let mut variants: Vec<ConcreteTypeId> = Vec::new();
+        for arg in args {
+            let ty = try_extract_matches!(arg, GenericArg::Type)
+                .ok_or(SpecializationError::UnsupportedGenericArg)?
+                .clone();
+            let info = context.get_type_info(ty.clone())?;
+            if !info.storable {
+                return Err(SpecializationError::UnsupportedGenericArg);
+            }
+            if !info.duplicatable {
+                duplicatable = false;
+            }
+            if !info.droppable {
+                droppable = false;
+            }
+            variants.push(ty);
+        }
+        Ok(EnumConcreteType {
+            info: TypeInfo {
+                long_id: ConcreteTypeLongId {
+                    generic_id: "Enum".into(),
+                    generic_args: args.to_vec(),
+                },
+                duplicatable,
+                droppable,
+                storable: true,
+            },
+            variants,
+        })
+    }
+}
+impl ConcreteType for EnumConcreteType {
+    fn info(&self) -> &TypeInfo {
+        &self.info
+    }
+}
+
+define_libfunc_hierarchy! {
+    pub enum EnumLibFunc {
+        Init(EnumInitLibFunc),
+        Match(EnumMatchLibFunc),
+    }, EnumConcreteLibFunc
+}
+
+pub struct EnumInitConcreteLibFunc {
+    pub signature: LibFuncSignature,
+    /// The index of the relevant variant from the enum.
+    pub index: usize,
+}
+impl SignatureBasedConcreteLibFunc for EnumInitConcreteLibFunc {
+    fn signature(&self) -> &LibFuncSignature {
+        &self.signature
+    }
+}
+
+/// LibFunc for setting a value to an enum.
+#[derive(Default)]
+pub struct EnumInitLibFunc {}
+impl EnumInitLibFunc {
+    /// Creates the specialization of the enum-init libfunc with the given template arguments.
+    fn specialize_concrete_lib_func(
+        &self,
+        context: &dyn SignatureSpecializationContext,
+        args: &[GenericArg],
+    ) -> Result<EnumInitConcreteLibFunc, SpecializationError> {
+        let (enum_type, index) = match args {
+            [GenericArg::Type(enum_type), GenericArg::Value(index)] => (enum_type.clone(), *index),
+            [_, _] => return Err(SpecializationError::UnsupportedGenericArg),
+            _ => return Err(SpecializationError::WrongNumberOfGenericArgs),
+        };
+        let generic_args = context.get_type_info(enum_type.clone())?.long_id.generic_args;
+        let variant_types =
+            EnumConcreteType::new(context.as_type_specialization_context(), &generic_args)?
+                .variants;
+        let num_variants = variant_types.len();
+        if index < 0 || index as usize >= num_variants {
+            return Err(SpecializationError::IndexOutOfRange { index, range_size: num_variants });
+        }
+        let variant_type = variant_types[index as usize].clone();
+        Ok(EnumInitConcreteLibFunc {
+            signature: LibFuncSignature::new_non_branch(
+                vec![variant_type],
+                vec![OutputVarInfo {
+                    ty: enum_type,
+                    ref_info: OutputVarReferenceInfo::NewTempVar { idx: 0 },
+                }],
+                SierraApChange::Known,
+            ),
+            index: index as usize,
+        })
+    }
+}
+impl NamedLibFunc for EnumInitLibFunc {
+    type Concrete = EnumInitConcreteLibFunc;
+    const ID: GenericLibFuncId = GenericLibFuncId::new_inline("enum_init");
+
+    fn specialize_signature(
+        &self,
+        context: &dyn SignatureSpecializationContext,
+        args: &[GenericArg],
+    ) -> Result<LibFuncSignature, SpecializationError> {
+        Ok(self.specialize_concrete_lib_func(context, args)?.signature)
+    }
+
+    fn specialize(
+        &self,
+        context: &dyn SpecializationContext,
+        args: &[GenericArg],
+    ) -> Result<Self::Concrete, SpecializationError> {
+        self.specialize_concrete_lib_func(context.upcast(), args)
+    }
+}
+
+/// LibFunc for matching an enum.
+#[derive(Default)]
+pub struct EnumMatchLibFunc {}
+impl NamedLibFunc for EnumMatchLibFunc {
+    type Concrete = SignatureOnlyConcreteLibFunc;
+    const ID: GenericLibFuncId = GenericLibFuncId::new_inline("enum_match");
+
+    fn specialize_signature(
+        &self,
+        context: &dyn SignatureSpecializationContext,
+        args: &[GenericArg],
+    ) -> Result<LibFuncSignature, SpecializationError> {
+        let enum_type = as_single_type(args)?;
+        let generic_args = context.get_type_info(enum_type.clone())?.long_id.generic_args;
+        let variant_types =
+            EnumConcreteType::new(context.as_type_specialization_context(), &generic_args)?
+                .variants;
+        let branch_signatures = variant_types
+            .into_iter()
+            .map(|ty| BranchSignature {
+                vars: vec![OutputVarInfo {
+                    ty,
+                    ref_info: OutputVarReferenceInfo::NewTempVar { idx: 0 },
+                }],
+                ap_change: SierraApChange::Known,
+            })
+            .collect();
+
+        Ok(LibFuncSignature {
+            param_signatures: vec![enum_type.into()],
+            branch_signatures,
+            fallthrough: None,
+        })
+    }
+
+    // TODO(yuval): this repeats many times. consider adding an impl for all signature-only
+    // libfuncs.
+    fn specialize(
+        &self,
+        context: &dyn SpecializationContext,
+        args: &[GenericArg],
+    ) -> Result<Self::Concrete, SpecializationError> {
+        Ok(SignatureOnlyConcreteLibFunc {
+            signature: self.specialize_signature(context.upcast(), args)?,
+        })
+    }
+}
