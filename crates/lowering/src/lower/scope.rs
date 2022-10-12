7656d0902c6ca27981f3b1cbc8cee5726aff9994
Shahar Papini <spapini@starkware.co>
2022-10-12 14:10:29 +0300

    Lower block expression

--- crates/lowering/src/lower/scope.rs
+++ crates/lowering/src/lower/scope.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::collections::HashSet;
 use std::rc::Rc;
 
 use itertools::chain;
@@ -101,7 +102,20 @@ impl BlockScope {
             }
             BlockScopeEnd::Unreachable => BlockSealedEnd::Unreachable,
         };
-        BlockSealed { block: self, end }
+        let sealed = BlockSealed {
+            inputs: self.inputs,
+            pulled_semantic_vars: self.pulled_semantic_vars,
+            living_variables: self.living_variables,
+            semantic_variables: self.semantic_variables,
+            statements: self.statements,
+            end,
+        };
+        // TODO(spapini): Make PullUnifier create a block scope accessible only within a closure,
+        //   to force him looking at sealed blocks.
+        if let Some(unifier) = self.pull_unifier {
+            unifier.try_borrow_mut().unwrap().add_block_sealed(&sealed)
+        }
+        sealed
     }
 
     /// Internal. Puts a semantic variable to the semantic store, binds it to a new lowered
@@ -152,7 +166,11 @@ impl BlockScope {
 /// A block that was sealed after adding all the statements, just before determining the final
 /// inputs.
 pub struct BlockSealed {
-    block: BlockScope,
+    inputs: Vec<VariableId>,
+    pulled_semantic_vars: OrderedHashMap<semantic::VarId, VariableId>,
+    living_variables: OrderedHashSet<VariableId>,
+    semantic_variables: SemanticVariablesMap,
+    statements: Vec<Statement>,
     end: BlockSealedEnd,
 }
 
@@ -178,34 +196,32 @@ impl BlockSealed {
     pub fn finalize(
         self,
         ctx: &mut LoweringContext<'_>,
-        pulls: &[semantic::VarId],
-        pushes: &[semantic::VarId],
+        pull_unifier_finalized: &PullUnifierFinalized,
     ) -> (Block, BlockEndInfo) {
-        let BlockSealed { mut block, end } = self;
+        let BlockSealed {
+            inputs,
+            pulled_semantic_vars,
+            mut living_variables,
+            mut semantic_variables,
+            statements,
+            end,
+        } = self;
         // Pull extra semantic variables if necessary.
-        for semantic_var_id in pulls {
-            if !block.pulled_semantic_vars.contains_key(semantic_var_id) {
-                block.pull_semantic_variable(ctx, *semantic_var_id);
+        for (semantic_var_id, var) in pull_unifier_finalized.pulls.iter() {
+            if !pulled_semantic_vars.contains_key(semantic_var_id) {
+                living_variables.insert(var.0);
             }
         }
-        // Make sure inputs contain all pulled semantic variables.
-        for pulled_semantic_var_id in block.pulled_semantic_vars.keys() {
-            assert!(
-                pulls.contains(pulled_semantic_var_id),
-                "finalize() called with missing input semantic variables."
-            );
-        }
-        assert_eq!(block.pulled_semantic_vars.len(), pulls.len());
-
         // Compute drops.
         let (end, end_info) = match end {
             BlockSealedEnd::Callsite(maybe_output) => {
-                let pushes: Vec<_> = pushes
+                let pushes: Vec<_> = pull_unifier_finalized
+                    .pushes
                     .iter()
                     .map(|semantic_var_id| {
                         // TODO(spapini): Convert to a diagnostic.
-                        block
-                            .semantic_variables
+
+                        semantic_variables
                             .take(*semantic_var_id)
                             .expect("finalize() called with dead output semantic variables.")
                             .var()
@@ -222,9 +238,9 @@ impl BlockSealed {
             BlockSealedEnd::Unreachable => (BlockEnd::Unreachable, BlockEndInfo::End),
         };
         // TODO(spapini): Fix this in case of return.
-        let drops = block.living_variables.into_iter().collect();
+        let drops = living_variables.into_iter().collect();
 
-        (Block { inputs: block.inputs, statements: block.statements, drops, end }, end_info)
+        (Block { inputs, statements, drops, end }, end_info)
     }
 }
 
@@ -246,18 +262,26 @@ pub enum BlockEndInfo {
 #[derive(Default)]
 pub struct PullUnifier {
     parent_scope: Box<BlockScope>,
-    pulled: OrderedHashMap<semantic::VarId, OwnedVariable>,
+    pulls: OrderedHashMap<semantic::VarId, OwnedVariable>,
+    can_push: Option<HashSet<semantic::VarId>>,
+    // TODO(spapini): Optimized pushes by using shouldnt_push.
 }
 impl PullUnifier {
     /// Creates a new instance of PullUnifier within a limited closure.
     /// This is necessary for lifetime reasons.
-    pub fn with<T, F: FnOnce(Rc<RefCell<Self>>) -> T>(parent_scope: &mut BlockScope, f: F) -> T {
+    pub fn with<T, F: FnOnce(Rc<RefCell<Self>>) -> T>(
+        parent_scope: &mut BlockScope,
+        f: F,
+    ) -> (T, PullUnifierFinalized) {
         let new_parent_scope = Box::new(std::mem::take(parent_scope));
         let pull_unifier =
             Rc::new(RefCell::new(Self { parent_scope: new_parent_scope, ..Self::default() }));
         let res = f(pull_unifier.clone());
-        *parent_scope = *pull_unifier.take().parent_scope;
-        res
+        let unifier = pull_unifier.take();
+        let (finalized, returned_scope) = unifier.finalize();
+        *parent_scope = *returned_scope;
+
+        (res, finalized)
     }
 
     /// Pulls a semantic variable from an outer scope.
@@ -267,16 +291,47 @@ impl PullUnifier {
         semantic_var_id: semantic::VarId,
     ) -> Option<OwnedVariable> {
         // Try to take ownership from parent scope if the semantic variable is not present.
-        if !self.pulled.contains_key(&semantic_var_id) {
+        if !self.pulls.contains_key(&semantic_var_id) {
             if let Some(var) =
                 self.parent_scope.get_or_pull_semantic_variable(ctx, semantic_var_id).var()
             {
-                self.pulled.insert(semantic_var_id, var);
+                self.pulls.insert(semantic_var_id, var);
             }
         }
 
         // If we own it, give a copy.
-        let var = self.pulled.get(&semantic_var_id)?;
+        let var = self.pulls.get(&semantic_var_id)?;
         Some(OwnedVariable(var.0))
     }
+
+    /// Adds a sealed block to the unifier. This will help the unifier decide on the correct
+    /// pulls and pushes.
+    pub fn add_block_sealed(&mut self, block_sealed: &BlockSealed) {
+        // TODO(spapini): Make this prettier.
+        if !matches!(block_sealed.end, BlockSealedEnd::Callsite(_)) {
+            return;
+        }
+        let can_push: HashSet<_> = block_sealed.semantic_variables.alive().copied().collect();
+        if let Some(some_can_push) = &mut self.can_push {
+            *some_can_push = some_can_push.intersection(&can_push).copied().collect();
+        } else {
+            self.can_push = Some(can_push);
+        }
+    }
+
+    /// Finalizes the unifier, deciding on the correct pulls and pushes for all the blocks
+    /// encountered.
+    pub fn finalize(self) -> (PullUnifierFinalized, Box<BlockScope>) {
+        let pushes: Vec<_> = self.can_push.unwrap().into_iter().collect();
+        // TODO(spapini): Make stable.
+        // TODO(spapini): Optimize pushes by maintaining shouldnt_push.
+        (PullUnifierFinalized { pulls: self.pulls, pushes }, self.parent_scope)
+    }
+}
+
+/// Determined pulls and pushes. Generated after calling [`PullUnifier::finalize()`].
+#[derive(Default)]
+pub struct PullUnifierFinalized {
+    pulls: OrderedHashMap<semantic::VarId, OwnedVariable>,
+    pub pushes: Vec<semantic::VarId>,
 }
