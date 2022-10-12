0ebe43688de6b980abb91069e740f96c66ff45a2
Ori Ziv <oriz@starkware.co>
2022-10-12 12:10:58 +0300

    Refactored Sierra to Casm tests.
    Allows easier addition of success tests - specifically the existing
    sierra examples.
    
    commit-id:4f0d5dbf

--- crates/sierra_to_casm/src/compiler_test.rs
+++ crates/sierra_to_casm/src/compiler_test.rs
@@ -4,7 +4,6 @@ use std::path::PathBuf;
 use casm::ap_change::ApChange;
 use indoc::indoc;
 use pretty_assertions;
-use pretty_assertions::assert_eq;
 use sierra::ids::FunctionId;
 use sierra::ProgramParser;
 use test_case::test_case;
@@ -23,155 +22,157 @@ fn build_metadata(ap_change_data: &[(&str, i16)]) -> Metadata {
     }
 }
 
-#[test]
-fn good_flow() {
-    let prog = ProgramParser::new()
-        .parse(indoc! {"
-            type felt = felt;
-            type NonZeroFelt = NonZero<felt>;
-            type BoxFelt = Box<felt>;
-
-            libfunc finalize_locals = finalize_locals;
-            libfunc felt_add = felt_add;
-            libfunc felt_sub = felt_sub;
-            libfunc felt_dup = dup<felt>;
-            libfunc felt_jump_nz = felt_jump_nz;
-            libfunc felt_into_box = into_box<felt>;
-            libfunc felt_unbox = unbox<felt>;
-            libfunc jump = jump;
-            libfunc felt_unwrap_nz = unwrap_nz<felt>;
-            libfunc store_temp_felt = store_temp<felt>;
-            libfunc store_temp_box_felt = store_temp<BoxFelt>;
-            libfunc rename_felt = rename<felt>;
-            libfunc call_foo = function_call<user@foo>;
-
-            rename_felt([1]) -> ([1]);                      // #0
-            felt_dup([2]) -> ([2], [5]);                    // #1
-            felt_add([1], [2]) -> ([3]);                    // #2
-            store_temp_felt([3]) -> ([4]);                  // #3
-            store_temp_felt([5]) -> ([5]);                  // #4
-            store_temp_felt([4]) -> ([4]);                  // #5
-            call_foo([5], [4]) -> ([7], [8]);               // #6
-            felt_dup([8]) -> ([4], [8]);                    // #7
-            store_temp_felt([4]) -> ([4]);                  // #8
-            return([7], [8], [4]);                          // #9
-
-            finalize_locals() -> ();                        // #10
-            felt_jump_nz([1]) { 16([1]) fallthrough() };    // #11
-            felt_dup([2]) -> ([1], [2]);                    // #12
-            store_temp_felt([1]) -> ([1]);                  // #13
-            store_temp_felt([2]) -> ([2]);                  // #14
-            return ([1], [2]);                              // #15
-
-            jump() { 17() };                                // #16
-            felt_unwrap_nz([1]) -> ([1]);                   // #17
-            felt_dup([2]) -> ([2], [3]);                    // #18
-            felt_sub([1], [3]) -> ([1]);                    // #19
-            store_temp_felt([1]) -> ([1]);                  // #20
-            store_temp_felt([2]) -> ([2]);                  // #21
-            call_foo([1], [2]) -> ([1], [2]);               // #22
-            return ([1], [2]);                              // #23
-
-            felt_into_box([1]) -> ([2]);                    // #24
-            store_temp_box_felt([2]) -> ([2]);              // #25
-            felt_unbox([2]) -> ([3]);                       // #26
-            store_temp_felt([3]) -> ([3]);                  // #27
-            return ([3]);                                   // #28
-
-            test_program@0([1]: felt, [2]: felt) -> (felt, felt, felt);
-            foo@10([1]: felt, [2]: felt) -> (felt, felt);
-            box_and_back@24([1]: felt) -> (felt);
-        "})
-        .unwrap();
-    assert_eq!(
-        match compile(&prog, &build_metadata(&[("box_and_back", 2)])) {
-            Ok(compiled_program) => compiled_program.to_string(),
-            Err(error) => error.to_string(),
-        },
-        indoc! {"
-            [ap + 0] = [fp + -4] + [fp + -3], ap++;
-            [ap + 0] = [fp + -3], ap++;
-            [ap + 0] = [ap + -2], ap++;
-            call rel 4;
-            [ap + 0] = [ap + -1], ap++;
-            ret;
-            ap += 0;
-            jmp rel 5 if [fp + -4] != 0;
-            [ap + 0] = [fp + -3], ap++;
-            [ap + 0] = [fp + -3], ap++;
-            ret;
-            jmp rel 2;
-            [fp + -4] = [ap + 0] + [fp + -3], ap++;
-            [ap + 0] = [fp + -3], ap++;
-            call rel -11;
-            ret;
-            %{ memory[ap + 0] = segments.add() %}
-            [fp + -3] = [[ap + 0]], ap++;
-            [ap + 0] = [[ap + -1]], ap++;
-            ret;
-        "}
-    );
-}
-
-#[test]
-fn locals_test() {
-    let prog = ProgramParser::new()
-        .parse(indoc! {"
-            type felt = felt;
-            type UninitializedFelt = uninitialized<felt>;
-
-            libfunc finalize_locals = finalize_locals;
-            libfunc alloc_local_felt = alloc_local<felt>;
-            libfunc store_local_felt = store_local<felt>;
-            libfunc store_temp_felt = store_temp<felt>;
-
-            store_temp_felt([1]) -> ([1]);
-            alloc_local_felt() -> ([3]);
-            alloc_local_felt() -> ([4]);
-            store_local_felt([3], [1]) -> ([3]);
-            finalize_locals() -> ();
-            store_local_felt([4], [2]) -> ([4]);
-            store_temp_felt([3]) -> ([3]);
-            store_temp_felt([4]) -> ([4]);
-            return ([3], [4]);
-
-            test_program@0([1]: felt, [2]: felt) -> (felt, felt);
-        "})
-        .unwrap();
-    assert_eq!(
-        compile(&prog, &build_metadata(&[("test_program", 5)])).unwrap().to_string(),
-        indoc! {"
-            [ap + 0] = [fp + -4], ap++;
-            [fp + 1] = [ap + -1];
-            ap += 2;
-            [fp + 2] = [fp + -3];
-            [ap + 0] = [fp + 1], ap++;
-            [ap + 0] = [fp + 2], ap++;
-            ret;
-        "}
-    );
-}
-
-#[test]
-fn fib_program() {
+fn read_sierra_example_file(filename: &str) -> String {
     let path: PathBuf =
-        [env!("CARGO_MANIFEST_DIR"), "../sierra/examples/fib_no_gas.sierra"].iter().collect();
-    let prog = sierra::ProgramParser::new().parse(&fs::read_to_string(path).unwrap()).unwrap();
+        [env!("CARGO_MANIFEST_DIR"), "../sierra/examples/", filename].iter().collect();
+    fs::read_to_string(path).unwrap()
+}
+
+#[test_case(indoc! {"
+                type felt = felt;
+                type NonZeroFelt = NonZero<felt>;
+                type BoxFelt = Box<felt>;
+
+                libfunc finalize_locals = finalize_locals;
+                libfunc felt_add = felt_add;
+                libfunc felt_sub = felt_sub;
+                libfunc felt_dup = dup<felt>;
+                libfunc felt_jump_nz = felt_jump_nz;
+                libfunc felt_into_box = into_box<felt>;
+                libfunc felt_unbox = unbox<felt>;
+                libfunc jump = jump;
+                libfunc felt_unwrap_nz = unwrap_nz<felt>;
+                libfunc store_temp_felt = store_temp<felt>;
+                libfunc store_temp_box_felt = store_temp<BoxFelt>;
+                libfunc rename_felt = rename<felt>;
+                libfunc call_foo = function_call<user@foo>;
+
+                rename_felt([1]) -> ([1]);                      // #0
+                felt_dup([2]) -> ([2], [5]);                    // #1
+                felt_add([1], [2]) -> ([3]);                    // #2
+                store_temp_felt([3]) -> ([4]);                  // #3
+                store_temp_felt([5]) -> ([5]);                  // #4
+                store_temp_felt([4]) -> ([4]);                  // #5
+                call_foo([5], [4]) -> ([7], [8]);               // #6
+                felt_dup([8]) -> ([4], [8]);                    // #7
+                store_temp_felt([4]) -> ([4]);                  // #8
+                return([7], [8], [4]);                          // #9
+
+                finalize_locals() -> ();                        // #10
+                felt_jump_nz([1]) { 16([1]) fallthrough() };    // #11
+                felt_dup([2]) -> ([1], [2]);                    // #12
+                store_temp_felt([1]) -> ([1]);                  // #13
+                store_temp_felt([2]) -> ([2]);                  // #14
+                return ([1], [2]);                              // #15
+
+                jump() { 17() };                                // #16
+                felt_unwrap_nz([1]) -> ([1]);                   // #17
+                felt_dup([2]) -> ([2], [3]);                    // #18
+                felt_sub([1], [3]) -> ([1]);                    // #19
+                store_temp_felt([1]) -> ([1]);                  // #20
+                store_temp_felt([2]) -> ([2]);                  // #21
+                call_foo([1], [2]) -> ([1], [2]);               // #22
+                return ([1], [2]);                              // #23
+
+                felt_into_box([1]) -> ([2]);                    // #24
+                store_temp_box_felt([2]) -> ([2]);              // #25
+                felt_unbox([2]) -> ([3]);                       // #26
+                store_temp_felt([3]) -> ([3]);                  // #27
+                return ([3]);                                   // #28
+
+                test_program@0([1]: felt, [2]: felt) -> (felt, felt, felt);
+                foo@10([1]: felt, [2]: felt) -> (felt, felt);
+                box_and_back@24([1]: felt) -> (felt);
+            "},
+            &build_metadata(&[("box_and_back", 2)]),
+            indoc! {"
+                [ap + 0] = [fp + -4] + [fp + -3], ap++;
+                [ap + 0] = [fp + -3], ap++;
+                [ap + 0] = [ap + -2], ap++;
+                call rel 4;
+                [ap + 0] = [ap + -1], ap++;
+                ret;
+                ap += 0;
+                jmp rel 5 if [fp + -4] != 0;
+                [ap + 0] = [fp + -3], ap++;
+                [ap + 0] = [fp + -3], ap++;
+                ret;
+                jmp rel 2;
+                [fp + -4] = [ap + 0] + [fp + -3], ap++;
+                [ap + 0] = [fp + -3], ap++;
+                call rel -11;
+                ret;
+                %{ memory[ap + 0] = segments.add() %}
+                [fp + -3] = [[ap + 0]], ap++;
+                [ap + 0] = [[ap + -1]], ap++;
+                ret;
+            "};
+            "good_flow")]
+#[test_case(indoc! {"
+                type felt = felt;
+                type UninitializedFelt = uninitialized<felt>;
+
+                libfunc finalize_locals = finalize_locals;
+                libfunc alloc_local_felt = alloc_local<felt>;
+                libfunc store_local_felt = store_local<felt>;
+                libfunc store_temp_felt = store_temp<felt>;
+
+                store_temp_felt([1]) -> ([1]);
+                alloc_local_felt() -> ([3]);
+                alloc_local_felt() -> ([4]);
+                store_local_felt([3], [1]) -> ([3]);
+                finalize_locals() -> ();
+                store_local_felt([4], [2]) -> ([4]);
+                store_temp_felt([3]) -> ([3]);
+                store_temp_felt([4]) -> ([4]);
+                return ([3], [4]);
+
+                test_program@0([1]: felt, [2]: felt) -> (felt, felt);
+            "},
+            &build_metadata(&[("test_program", 5)]),
+            indoc! {"
+                [ap + 0] = [fp + -4], ap++;
+                [fp + 1] = [ap + -1];
+                ap += 2;
+                [fp + 2] = [fp + -3];
+                [ap + 0] = [fp + 1], ap++;
+                [ap + 0] = [fp + 2], ap++;
+                ret;
+            "};
+            "alloc_local and store_local")]
+#[test_case(read_sierra_example_file("fib_no_gas.sierra").as_str(),
+            &build_metadata(&[]),
+            indoc! {"
+                jmp rel 4 if [fp + -3] != 0;
+                [ap + 0] = [fp + -5], ap++;
+                ret;
+                [ap + 0] = [fp + -4], ap++;
+                [ap + 0] = [fp + -5] + [fp + -4], ap++;
+                [ap + 0] = [fp + -3] + -1, ap++;
+                call rel -8;
+                ret;
+            "};
+            "fib_no_gas")]
+#[test_case(read_sierra_example_file("collatz.sierra").as_str(),
+            &build_metadata(&[]),
+            indoc! {"
+            "} => ignore["Gas usage lowering not supported yet."];
+            "collatz")]
+#[test_case(read_sierra_example_file("fib_jumps.sierra").as_str(),
+            &build_metadata(&[]),
+            indoc! {"
+            "} => ignore["Gas usage lowering not supported yet."];
+            "fib_jumps")]
+#[test_case(read_sierra_example_file("fib_recursive.sierra").as_str(),
+            &build_metadata(&[]),
+            indoc! {"
+            "} => ignore["Gas usage lowering not supported yet."];
+            "fib_recursive")]
+fn sierra_to_casm(sierra_code: &str, metadata: &Metadata, expected_casm: &str) {
+    let program = ProgramParser::new().parse(sierra_code).unwrap();
     pretty_assertions::assert_eq!(
-        match compile(&prog, &build_metadata(&[])) {
-            Ok(compiled_program) => compiled_program.to_string(),
-            Err(error) => error.to_string(),
-        },
-        indoc! {"
-            jmp rel 4 if [fp + -3] != 0;
-            [ap + 0] = [fp + -5], ap++;
-            ret;
-            [ap + 0] = [fp + -4], ap++;
-            [ap + 0] = [fp + -5] + [fp + -4], ap++;
-            [ap + 0] = [fp + -3] + -1, ap++;
-            call rel -8;
-            ret;
-        "}
+        compile(&program, metadata).expect("Compilation failed.").to_string(),
+        expected_casm
     );
 }
 
@@ -460,11 +461,10 @@ expected: ApChange::Known(5) got: ApChange::Known(0).";
             "bad Ap change")]
 fn compiler_errors(sierra_code: &str, ap_change_data: &[(&str, i16)], expected_result: &str) {
     let prog = ProgramParser::new().parse(sierra_code).unwrap();
-    assert_eq!(
-        match compile(&prog, &build_metadata(ap_change_data)) {
-            Ok(compiled_program) => compiled_program.to_string(),
-            Err(error) => error.to_string(),
-        },
+    pretty_assertions::assert_eq!(
+        compile(&prog, &build_metadata(ap_change_data))
+            .expect_err("Compilation is expected to fail.")
+            .to_string(),
         expected_result
     );
 }
