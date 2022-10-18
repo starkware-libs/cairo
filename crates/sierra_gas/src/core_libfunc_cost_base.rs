02cfd6c11220097ea1632686ada034e919b6da83
Ori Ziv <oriz@starkware.co>
2022-10-18 17:04:38 +0300

    Refactored sierra-gas to enable getting errors in usage.
    commit-id:842b674c

--- crates/sierra_gas/src/core_libfunc_cost_base.rs
+++ crates/sierra_gas/src/core_libfunc_cost_base.rs
@@ -17,92 +17,93 @@ use sierra::extensions::mem::MemConcreteLibFunc::{
 };
 use sierra::program::Function;
 
+/// The operation required for extracting a libfunc's cost.
+pub trait CostOperations {
+    type CostType: Clone;
+
+    /// Get a cost from a constant value.
+    fn const_cost(&self, value: i32) -> Self::CostType;
+    /// Get a cost for the content of a function.
+    fn function_cost(&mut self, function: &Function) -> Self::CostType;
+    /// Get a cost for a variable for the current statement.
+    fn statement_var_cost(&self) -> Self::CostType;
+    /// Adds costs.
+    fn add(&self, lhs: Self::CostType, rhs: Self::CostType) -> Self::CostType;
+    /// Subtracts costs.
+    fn sub(&self, lhs: Self::CostType, rhs: Self::CostType) -> Self::CostType;
+}
+
 /// Returns some cost value for a libfunc - a helper function to implement costing both for creating
 /// gas equations and getting actual gas usage after having a solution.
-pub fn core_libfunc_cost_base<
-    CostType: std::ops::Add<Output = CostType> + std::ops::Sub<Output = CostType> + Clone,
-    FromConst: Fn(i32) -> CostType,
-    FromFunction: FnOnce(&Function) -> CostType,
-    FromVar: FnOnce() -> CostType,
->(
-    from_const: FromConst,
-    from_function: FromFunction,
-    from_var: FromVar,
+pub fn core_libfunc_cost_base<Ops: CostOperations>(
+    ops: &mut Ops,
     libfunc: &CoreConcreteLibFunc,
-) -> Vec<CostType> {
+) -> Vec<Ops::CostType> {
     match libfunc {
         // For the case of function calls - assumes a variable for the cost of running from a
         // function entry point and on - while also adding the call cost.
         FunctionCall(FunctionCallConcreteLibFunc { function, .. }) => {
-            vec![from_const(2) + from_function(function)]
+            let func_content_cost = ops.function_cost(function);
+            vec![ops.add(ops.const_cost(2), func_content_cost)]
         }
-        Gas(GetGas(_)) => vec![from_const(1) - from_var(), from_const(1)],
-        Gas(RefundGas(_)) => {
-            vec![from_const(1) + from_var()]
+        Gas(GetGas(_)) => {
+            vec![ops.sub(ops.const_cost(1), ops.statement_var_cost()), ops.const_cost(1)]
         }
-        Array(ArrayConcreteLibFunc::New(_)) => vec![from_const(1)],
-        Array(ArrayConcreteLibFunc::Append(_)) => vec![from_const(2)],
-        Integer(libfunc) => integer_libfunc_cost(from_const, libfunc),
-        Felt(libfunc) => felt_libfunc_cost(from_const, libfunc),
+        Gas(RefundGas(_)) => vec![ops.add(ops.const_cost(1), ops.statement_var_cost())],
+        Array(ArrayConcreteLibFunc::New(_)) => vec![ops.const_cost(1)],
+        Array(ArrayConcreteLibFunc::Append(_)) => vec![ops.const_cost(2)],
+        Integer(libfunc) => integer_libfunc_cost(ops, libfunc),
+        Felt(libfunc) => felt_libfunc_cost(ops, libfunc),
         Drop(_) | Dup(_) | ApTracking(_) | UnwrapNonZero(_) | Mem(Rename(_)) | Box(_) => {
-            vec![from_const(0)]
+            vec![ops.const_cost(0)]
         }
         Mem(StoreLocal(_) | AllocLocal(_) | StoreTemp(_) | AlignTemps(_) | FinalizeLocals(_))
-        | UnconditionalJump(_) => vec![from_const(1)],
-        Enum(EnumConcreteLibFunc::Init(_)) => vec![from_const(1)],
+        | UnconditionalJump(_) => vec![ops.const_cost(1)],
+        Enum(EnumConcreteLibFunc::Init(_)) => vec![ops.const_cost(1)],
         Enum(EnumConcreteLibFunc::Match(sig)) => {
-            vec![from_const(1); sig.signature.branch_signatures.len()]
+            vec![ops.const_cost(1); sig.signature.branch_signatures.len()]
         }
     }
 }
 
 /// Returns costs for integer libfuncs.
-fn integer_libfunc_cost<
-    CostType: std::ops::Add<Output = CostType> + std::ops::Sub<Output = CostType>,
-    FromConst: Fn(i32) -> CostType,
->(
-    from_const: FromConst,
+fn integer_libfunc_cost<Ops: CostOperations>(
+    ops: &Ops,
     libfunc: &IntegerConcrete,
-) -> Vec<CostType> {
+) -> Vec<Ops::CostType> {
     // TODO(orizi): When sierra_to_casm actually supports integers - fix costs.
     match libfunc {
         IntegerConcrete::Operation(OperationConcreteLibFunc::Binary(
             BinaryOperationConcreteLibFunc { operator, .. },
         )) => match operator {
-            Operator::Add | Operator::Sub => vec![from_const(5)],
+            Operator::Add | Operator::Sub => vec![ops.const_cost(5)],
             Operator::Mul | Operator::Div | Operator::Mod => {
-                vec![from_const(7)]
+                vec![ops.const_cost(7)]
             }
         },
         IntegerConcrete::Operation(OperationConcreteLibFunc::Const(
             OperationWithConstConcreteLibFunc { operator, .. },
         )) => match operator {
-            Operator::Add | Operator::Sub => vec![from_const(3)],
+            Operator::Add | Operator::Sub => vec![ops.const_cost(3)],
             Operator::Mul | Operator::Div | Operator::Mod => {
-                vec![from_const(5)]
+                vec![ops.const_cost(5)]
             }
         },
         IntegerConcrete::Const(_) => {
-            vec![from_const(0)]
+            vec![ops.const_cost(0)]
         }
         IntegerConcrete::JumpNotZero(_) => {
-            vec![from_const(1), from_const(1)]
+            vec![ops.const_cost(1), ops.const_cost(1)]
         }
     }
 }
 
 /// Returns costs for felt libfuncs.
-fn felt_libfunc_cost<
-    CostType: std::ops::Add<Output = CostType> + std::ops::Sub<Output = CostType>,
-    FromConst: Fn(i32) -> CostType,
->(
-    from_const: FromConst,
-    libfunc: &FeltConcrete,
-) -> Vec<CostType> {
+fn felt_libfunc_cost<Ops: CostOperations>(ops: &Ops, libfunc: &FeltConcrete) -> Vec<Ops::CostType> {
     match libfunc {
-        FeltConcrete::Const(_) | FeltConcrete::Operation(_) => vec![from_const(0)],
+        FeltConcrete::Const(_) | FeltConcrete::Operation(_) => vec![ops.const_cost(0)],
         FeltConcrete::JumpNotZero(_) => {
-            vec![from_const(1), from_const(1)]
+            vec![ops.const_cost(1), ops.const_cost(1)]
         }
     }
 }
