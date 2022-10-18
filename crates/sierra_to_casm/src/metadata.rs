e120571b9f1652c57cb528a35dbd6231554f9885
orizi <104711814+orizi@users.noreply.github.com>
2022-10-18 10:04:15 +0200

    Added gas to compilation metadata. (#667)

--- crates/sierra_to_casm/src/metadata.rs
+++ crates/sierra_to_casm/src/metadata.rs
@@ -2,9 +2,12 @@ use std::collections::HashMap;
 
 use casm::ap_change::ApChange;
 use sierra::ids::FunctionId;
+use sierra_gas::gas_info::GasInfo;
 
 /// Metadata provided with a Sierra program to simplify the compilation to casm.
 pub struct Metadata {
     /// AP changes information for Sierra user functions.
     pub function_ap_change: HashMap<FunctionId, ApChange>,
+    /// Gas information for validating Sierra code and taking the apporiate amount of gas.
+    pub gas_info: GasInfo,
 }
