0c9202067c33e421b32bce74de2a2eb7d75224d5
Shahar Papini <spapini@starkware.co>
2022-10-20 17:13:17 +0300

    SierraToCasm: USe some macros

--- crates/casm/src/inline_test.rs
+++ crates/casm/src/inline_test.rs
@@ -7,15 +7,16 @@ use crate::{casm, deref};
 
 #[test]
 fn test_assert() {
-    let x = ResOperand::Immediate(ImmediateOperand { value: 1 });
+    let x = 1;
     let y = deref!([fp + 5]);
+    let z = 5;
 
     let ctx = casm! {
         [fp - 5] = x, ap++;
         [fp - 5] = [ap + 1] + [fp - 5], ap++;
         [fp + 5] = [ap + 1] + 2;
         [ap] = [ap + 1] * [fp - 5];
-        [fp - 5] = [ap + 1] * 2;
+        [fp - 5] = [ap + 1] * z;
         [fp - 5] = [ap + 1] * y;
         [fp - 5] = 1, ap++;
         [fp - 5] = [ap + 1];
