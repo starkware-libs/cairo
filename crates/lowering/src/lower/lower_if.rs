e5cd5ac932d9c134dd8c7158671d41abf5e797ac
Lior Goldberg <lior@starkware.co>
2022-10-21 12:03:17 +0300

    Support EqZero optimization in lower_if.rs.
    commit-id:f4d89ca2

--- crates/lowering/src/lower/lower_if.rs
+++ crates/lowering/src/lower/lower_if.rs
@@ -13,8 +13,24 @@ enum IfCondition {
 
 /// Analyzes the condition of an if statement into an [IfCondition] tree, to allow different
 /// optimizations.
-fn analyze_condition(_ctx: &LoweringContext<'_>, expr_id: semantic::ExprId) -> IfCondition {
+// TODO(lior): Make it an actual tree (handling && and ||).
+fn analyze_condition(ctx: &LoweringContext<'_>, expr_id: semantic::ExprId) -> IfCondition {
+    let expr = &ctx.function_def.exprs[expr_id];
+    if let semantic::Expr::FunctionCall(function_call) = expr {
+        if function_call.function == corelib::felt_eq(ctx.db)
+            && function_call.args.len() == 2
+            && is_zero(ctx, function_call.args[1])
+        {
+            return IfCondition::EqZero(function_call.args[0]);
+        };
+    };
+
     IfCondition::BoolExpr(expr_id)
+}
+
+fn is_zero(ctx: &LoweringContext<'_>, expr_id: semantic::ExprId) -> bool {
+    let expr = &ctx.function_def.exprs[expr_id];
+    matches!(expr, semantic::Expr::Literal(literal) if literal.value == 0)
 }
 
 /// Lowers an expression of type [semantic::ExprIf].
@@ -25,7 +41,7 @@ pub fn lower_expr_if(
 ) -> Result<LoweredExpr, LoweringFlowError> {
     match analyze_condition(ctx, expr.condition) {
         IfCondition::BoolExpr(_) => lower_expr_if_bool(ctx, scope, expr),
-        IfCondition::EqZero(_) => todo!(),
+        IfCondition::EqZero(tested_expr) => lower_expr_if_eq_zero(ctx, scope, expr, tested_expr),
     }
 }
 
@@ -71,6 +87,57 @@ pub fn lower_expr_if_bool(
             (corelib::true_variant(ctx.db), main_finalized.block),
             (corelib::false_variant(ctx.db), else_finalized.block),
         ],
+        end_info: finalized_merger.end_info,
+    };
+    let block_result = match_generator.add(ctx, scope);
+    lowered_expr_from_block_result(scope, block_result, finalized_merger.pushes)
+}
+
+/// Lowers an expression of type [semantic::ExprIf].
+pub fn lower_expr_if_eq_zero(
+    ctx: &mut LoweringContext<'_>,
+    scope: &mut BlockScope,
+    expr: &semantic::ExprIf,
+    tested_expr: semantic::ExprId,
+) -> Result<LoweredExpr, LoweringFlowError> {
+    // The condition cannot be unit.
+    let condition_var = lower_expr(ctx, scope, tested_expr)?.var(ctx, scope);
+
+    // Lower both blocks.
+    let (res, mut finalized_merger) = BlockFlowMerger::with(ctx, scope, &[], |ctx, merger| {
+        let main_block_scope = merger.run_in_subscope(ctx, vec![], |ctx, subscope, _| {
+            lower_block(
+                ctx,
+                subscope,
+                extract_matches!(&ctx.function_def.exprs[expr.if_block], semantic::Expr::Block),
+            )
+        });
+        let non_zero_type = corelib::core_nonzero_ty(ctx.db, corelib::core_felt_ty(ctx.db));
+        let else_block_scope =
+            merger.run_in_subscope(ctx, vec![non_zero_type], |ctx, subscope, _| {
+                lower_block(
+                    ctx,
+                    subscope,
+                    extract_matches!(
+                        &ctx.function_def.exprs[expr.else_block],
+                        semantic::Expr::Block
+                    ),
+                )
+            });
+        Some((main_block_scope, else_block_scope))
+    });
+
+    let (main_block_sealed, else_block_sealed) = res.ok_or(LoweringFlowError::Failed)?;
+    let main_finalized =
+        finalized_merger.finalize_block(ctx, main_block_sealed.ok_or(LoweringFlowError::Failed)?);
+    let else_finalized =
+        finalized_merger.finalize_block(ctx, else_block_sealed.ok_or(LoweringFlowError::Failed)?);
+
+    // Emit the statement.
+    let match_generator = generators::MatchExtern {
+        function: corelib::core_jump_nz_func(ctx.db),
+        inputs: vec![condition_var],
+        arms: vec![main_finalized.block, else_finalized.block],
         end_info: finalized_merger.end_info,
     };
     let block_result = match_generator.add(ctx, scope);
