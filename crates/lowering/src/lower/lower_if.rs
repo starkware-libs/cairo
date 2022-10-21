de2cc81c01e12e96463ecf039cc3c202c9afc5f9
Lior Goldberg <lior@starkware.co>
2022-10-21 11:31:45 +0300

    Add analyze_condition in lower_if.rs.
    commit-id:1f5de09f

--- crates/lowering/src/lower/lower_if.rs
+++ crates/lowering/src/lower/lower_if.rs
@@ -1,9 +1,22 @@
 use semantic::corelib;
 use utils::extract_matches;
 
+use crate::BlockId;
+
 use super::context::{LoweredExpr, LoweringContext, LoweringFlowError};
-use super::scope::{generators, BlockFlowMerger, BlockScope};
+use super::scope::{generators, BlockFlowMerger, BlockMergerFinalized, BlockScope};
 use super::{lower_block, lower_expr, lowered_expr_from_block_result};
+
+#[allow(dead_code)]
+enum IfCondition {
+    BoolExpr(semantic::ExprId),
+}
+
+/// Analyzes the condition of an if statement into an [IfCondition] tree, to allow different
+/// optimizations.
+fn analyze_condition(_ctx: &LoweringContext<'_>, expr_id: semantic::ExprId) -> IfCondition {
+    IfCondition::BoolExpr(expr_id)
+}
 
 /// Lowers an expression of type [semantic::ExprIf].
 pub fn lower_expr_if(
@@ -11,10 +24,6 @@ pub fn lower_expr_if(
     scope: &mut BlockScope,
     expr: &semantic::ExprIf,
 ) -> Result<LoweredExpr, LoweringFlowError> {
-    // The condition cannot be unit.
-    let condition_var =
-        extract_matches!(lower_expr(ctx, scope, expr.condition)?, LoweredExpr::AtVariable);
-
     // Lower both blocks.
     let unit_ty = corelib::unit_ty(ctx.db);
     let (res, mut finalized_merger) = BlockFlowMerger::with(ctx, scope, |ctx, merger| {
@@ -33,19 +42,45 @@ pub fn lower_expr_if(
             });
         Some((main_block_scope, else_block_scope))
     });
+
     let (main_block_sealed, else_block_sealed) = res.ok_or(LoweringFlowError::Failed)?;
     let main_finalized =
         finalized_merger.finalize_block(ctx, main_block_sealed.ok_or(LoweringFlowError::Failed)?);
     let else_finalized =
         finalized_merger.finalize_block(ctx, else_block_sealed.ok_or(LoweringFlowError::Failed)?);
 
+    match analyze_condition(ctx, expr.condition) {
+        IfCondition::BoolExpr(condition) => lower_expr_if_bool(
+            ctx,
+            scope,
+            condition,
+            main_finalized.block,
+            else_finalized.block,
+            finalized_merger,
+        ),
+    }
+}
+
+/// Lowers an expression of type [semantic::ExprIf].
+pub fn lower_expr_if_bool(
+    ctx: &mut LoweringContext<'_>,
+    scope: &mut BlockScope,
+    condition: semantic::ExprId,
+    main_finalized: BlockId,
+    else_finalized: BlockId,
+    finalized_merger: BlockMergerFinalized,
+) -> Result<LoweredExpr, LoweringFlowError> {
+    // The condition cannot be unit.
+    let condition_var =
+        extract_matches!(lower_expr(ctx, scope, condition)?, LoweredExpr::AtVariable);
+
     // Emit the statement.
     let match_generator = generators::MatchEnum {
         input: condition_var,
         concrete_enum_id: corelib::core_bool_enum(ctx.db),
         arms: vec![
-            (corelib::true_variant(ctx.db), main_finalized.block),
-            (corelib::false_variant(ctx.db), else_finalized.block),
+            (corelib::true_variant(ctx.db), main_finalized),
+            (corelib::false_variant(ctx.db), else_finalized),
         ],
         end_info: finalized_merger.end_info,
     };
