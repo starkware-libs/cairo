521dc495143ab9ddf4a3143981ef5aadc3420be2
Ori Ziv <oriz@starkware.co>
2022-10-23 17:53:10 +0300

    Separated felt from uint128 implementations.
    commit-id:0e88d75a

--- crates/sierra/src/extensions/modules/felt.rs
+++ crates/sierra/src/extensions/modules/felt.rs
@@ -1,9 +1,17 @@
 use super::jump_not_zero::{JumpNotZeroLibFunc, JumpNotZeroTraits};
-use super::wrapping_arithmetic::{ConstLibFunc, OperationLibFunc, WrappingArithmeticTraits};
-use crate::define_libfunc_hierarchy;
+use super::non_zero::NonZeroType;
+use crate::extensions::lib_func::{
+    DeferredOutputKind, LibFuncSignature, OutputVarInfo, SierraApChange,
+    SignatureSpecializationContext, SpecializationContext,
+};
 use crate::extensions::types::{InfoOnlyConcreteType, TypeInfo};
-use crate::extensions::{NamedType, NoGenericArgsGenericType};
+use crate::extensions::{
+    GenericLibFunc, NamedLibFunc, NamedType, NoGenericArgsGenericType, OutputVarReferenceInfo,
+    SignatureBasedConcreteLibFunc, SpecializationError,
+};
 use crate::ids::{GenericLibFuncId, GenericTypeId};
+use crate::program::GenericArg;
+use crate::{define_concrete_libfunc_hierarchy, define_libfunc_hierarchy};
 
 /// Type for felt.
 /// The native type of the Cairo architecture.
@@ -35,20 +43,189 @@ define_libfunc_hierarchy! {
 
 #[derive(Default)]
 pub struct FeltTraits {}
-impl WrappingArithmeticTraits for FeltTraits {
-    const ADD: GenericLibFuncId = GenericLibFuncId::new_inline("felt_add");
-    const SUB: GenericLibFuncId = GenericLibFuncId::new_inline("felt_sub");
-    const MUL: GenericLibFuncId = GenericLibFuncId::new_inline("felt_mul");
-    const DIV: GenericLibFuncId = GenericLibFuncId::new_inline("felt_div");
-    /// Modulo operation for felt would always return 0, as felt division is not interger division.
-    const MOD: GenericLibFuncId = GenericLibFuncId::new_inline("felt_mod");
-    const CONST: GenericLibFuncId = GenericLibFuncId::new_inline("felt_const");
-    const GENERIC_TYPE_ID: GenericTypeId = <FeltType as NamedType>::ID;
-}
 impl JumpNotZeroTraits for FeltTraits {
     const JUMP_NOT_ZERO: GenericLibFuncId = GenericLibFuncId::new_inline("felt_jump_nz");
     const GENERIC_TYPE_ID: GenericTypeId = <FeltType as NamedType>::ID;
 }
-pub type FeltOperationLibFunc = OperationLibFunc<FeltTraits>;
-pub type FeltConstLibFunc = ConstLibFunc<FeltTraits>;
 pub type FeltJumpNotZeroLibFunc = JumpNotZeroLibFunc<FeltTraits>;
+
+/// Felt arithmetic operators.
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum FeltOperator {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+/// Libfunc for felt operations.
+pub struct FeltOperationLibFunc {
+    pub operator: FeltOperator,
+}
+impl FeltOperationLibFunc {
+    fn new(operator: FeltOperator) -> Self {
+        Self { operator }
+    }
+}
+impl GenericLibFunc for FeltOperationLibFunc {
+    type Concrete = FeltOperationConcreteLibFunc;
+
+    fn by_id(id: &GenericLibFuncId) -> Option<Self> {
+        const ADD: GenericLibFuncId = GenericLibFuncId::new_inline("felt_add");
+        const SUB: GenericLibFuncId = GenericLibFuncId::new_inline("felt_sub");
+        const MUL: GenericLibFuncId = GenericLibFuncId::new_inline("felt_mul");
+        const DIV: GenericLibFuncId = GenericLibFuncId::new_inline("felt_div");
+        match id {
+            id if id == &ADD => Some(Self::new(FeltOperator::Add)),
+            id if id == &SUB => Some(Self::new(FeltOperator::Sub)),
+            id if id == &MUL => Some(Self::new(FeltOperator::Mul)),
+            id if id == &DIV => Some(Self::new(FeltOperator::Div)),
+            _ => None,
+        }
+    }
+
+    fn specialize_signature(
+        &self,
+        context: &dyn SignatureSpecializationContext,
+        args: &[GenericArg],
+    ) -> Result<LibFuncSignature, SpecializationError> {
+        let ty = context.get_concrete_type(FeltType::id(), &[])?;
+        match args {
+            [] => Ok(LibFuncSignature::new_non_branch(
+                vec![
+                    ty.clone(),
+                    if matches!(self.operator, FeltOperator::Div) {
+                        context.get_wrapped_concrete_type(NonZeroType::id(), ty.clone())?
+                    } else {
+                        ty.clone()
+                    },
+                ],
+                vec![OutputVarInfo {
+                    ty,
+                    ref_info: OutputVarReferenceInfo::Deferred(DeferredOutputKind::Generic),
+                }],
+                SierraApChange::Known,
+            )),
+            [GenericArg::Value(c)] => {
+                if matches!(self.operator, FeltOperator::Div) && *c == 0 {
+                    Err(SpecializationError::UnsupportedGenericArg)
+                } else {
+                    Ok(LibFuncSignature::new_non_branch(
+                        vec![ty.clone()],
+                        vec![OutputVarInfo {
+                            ty,
+                            ref_info: OutputVarReferenceInfo::Deferred(DeferredOutputKind::Generic),
+                        }],
+                        SierraApChange::Known,
+                    ))
+                }
+            }
+            _ => Err(SpecializationError::UnsupportedGenericArg),
+        }
+    }
+
+    fn specialize(
+        &self,
+        context: &dyn SpecializationContext,
+        args: &[GenericArg],
+    ) -> Result<Self::Concrete, SpecializationError> {
+        match args {
+            [] => Ok(FeltOperationConcreteLibFunc::Binary(FeltBinaryOperationConcreteLibFunc {
+                operator: self.operator,
+                signature: self.specialize_signature(context.upcast(), args)?,
+            })),
+            [GenericArg::Value(c)] => {
+                if matches!(self.operator, FeltOperator::Div) && *c == 0 {
+                    Err(SpecializationError::UnsupportedGenericArg)
+                } else {
+                    Ok(FeltOperationConcreteLibFunc::Const(FeltOperationWithConstConcreteLibFunc {
+                        operator: self.operator,
+                        c: *c as i128,
+                        signature: self.specialize_signature(context.upcast(), args)?,
+                    }))
+                }
+            }
+            _ => Err(SpecializationError::UnsupportedGenericArg),
+        }
+    }
+}
+
+pub struct FeltBinaryOperationConcreteLibFunc {
+    pub operator: FeltOperator,
+    pub signature: LibFuncSignature,
+}
+impl SignatureBasedConcreteLibFunc for FeltBinaryOperationConcreteLibFunc {
+    fn signature(&self) -> &LibFuncSignature {
+        &self.signature
+    }
+}
+
+/// Felt operations with a const.
+pub struct FeltOperationWithConstConcreteLibFunc {
+    pub operator: FeltOperator,
+    pub c: i128,
+    pub signature: LibFuncSignature,
+}
+define_concrete_libfunc_hierarchy! {
+    pub enum FeltOperationConcreteLibFunc {
+        Binary(FeltBinaryOperationConcreteLibFunc),
+        Const(FeltOperationWithConstConcreteLibFunc),
+    }
+}
+
+impl SignatureBasedConcreteLibFunc for FeltOperationWithConstConcreteLibFunc {
+    fn signature(&self) -> &LibFuncSignature {
+        &self.signature
+    }
+}
+
+/// LibFunc for creating a constant felt.
+#[derive(Default)]
+pub struct FeltConstLibFunc {}
+impl NamedLibFunc for FeltConstLibFunc {
+    type Concrete = FeltConstConcreteLibFunc;
+    const ID: GenericLibFuncId = GenericLibFuncId::new_inline("felt_const");
+
+    fn specialize_signature(
+        &self,
+        context: &dyn SignatureSpecializationContext,
+        _args: &[GenericArg],
+    ) -> Result<LibFuncSignature, SpecializationError> {
+        Ok(LibFuncSignature::new_non_branch(
+            vec![],
+            vec![OutputVarInfo {
+                ty: context.get_concrete_type(FeltType::id(), &[])?,
+                ref_info: OutputVarReferenceInfo::Const,
+            }],
+            SierraApChange::NotImplemented,
+        ))
+    }
+
+    fn specialize(
+        &self,
+        context: &dyn SpecializationContext,
+        args: &[GenericArg],
+    ) -> Result<Self::Concrete, SpecializationError> {
+        match args {
+            [GenericArg::Value(c)] => Ok(FeltConstConcreteLibFunc {
+                c: *c as i128,
+                signature: <Self as NamedLibFunc>::specialize_signature(
+                    self,
+                    context.upcast(),
+                    args,
+                )?,
+            }),
+            _ => Err(SpecializationError::UnsupportedGenericArg),
+        }
+    }
+}
+
+pub struct FeltConstConcreteLibFunc {
+    pub c: i128,
+    pub signature: LibFuncSignature,
+}
+impl SignatureBasedConcreteLibFunc for FeltConstConcreteLibFunc {
+    fn signature(&self) -> &LibFuncSignature {
+        &self.signature
+    }
+}
--- crates/sierra/src/extensions/modules/integer.rs
+++ crates/sierra/src/extensions/modules/integer.rs
@@ -1,14 +1,22 @@
 use super::jump_not_zero::{JumpNotZeroLibFunc, JumpNotZeroTraits};
-use super::wrapping_arithmetic::{ConstLibFunc, OperationLibFunc, WrappingArithmeticTraits};
-use crate::define_libfunc_hierarchy;
+use super::non_zero::NonZeroType;
+use crate::extensions::lib_func::{
+    DeferredOutputKind, LibFuncSignature, OutputVarInfo, SierraApChange,
+    SignatureSpecializationContext, SpecializationContext,
+};
 use crate::extensions::types::{InfoOnlyConcreteType, TypeInfo};
-use crate::extensions::{NamedType, NoGenericArgsGenericType};
+use crate::extensions::{
+    GenericLibFunc, NamedLibFunc, NamedType, NoGenericArgsGenericType, OutputVarReferenceInfo,
+    SignatureBasedConcreteLibFunc, SpecializationError,
+};
 use crate::ids::{GenericLibFuncId, GenericTypeId};
+use crate::program::GenericArg;
+use crate::{define_concrete_libfunc_hierarchy, define_libfunc_hierarchy};
 
 /// Type for uint128.
 #[derive(Default)]
-pub struct IntegerType {}
-impl NoGenericArgsGenericType for IntegerType {
+pub struct Uint128Type {}
+impl NoGenericArgsGenericType for Uint128Type {
     type Concrete = InfoOnlyConcreteType;
     const ID: GenericTypeId = GenericTypeId::new_inline("uint128");
 
@@ -25,28 +33,205 @@ impl NoGenericArgsGenericType for IntegerType {
 }
 
 define_libfunc_hierarchy! {
-    pub enum IntegerLibFunc {
-        WrappingOp(IntWrappingOpLibFunc),
-        Const(IntConstLibFunc),
-        JumpNotZero(IntJumpNotZeroLibFunc),
-    }, IntegerConcrete
+    pub enum Uint128LibFunc {
+        Operation(Uint128OperationLibFunc),
+        Const(Uint128ConstLibFunc),
+        JumpNotZero(Uint128JumpNotZeroLibFunc),
+    }, Uint128Concrete
 }
 
 #[derive(Default)]
-pub struct IntegerTraits {}
-impl WrappingArithmeticTraits for IntegerTraits {
-    const ADD: GenericLibFuncId = GenericLibFuncId::new_inline("uint128_wrapping_add");
-    const SUB: GenericLibFuncId = GenericLibFuncId::new_inline("uint128_wrapping_sub");
-    const MUL: GenericLibFuncId = GenericLibFuncId::new_inline("uint128_wrapping_mul");
-    const DIV: GenericLibFuncId = GenericLibFuncId::new_inline("uint128_div");
-    const MOD: GenericLibFuncId = GenericLibFuncId::new_inline("uint128_mod");
-    const CONST: GenericLibFuncId = GenericLibFuncId::new_inline("uint128_const");
-    const GENERIC_TYPE_ID: GenericTypeId = <IntegerType as NamedType>::ID;
-}
-impl JumpNotZeroTraits for IntegerTraits {
+pub struct Uint128Traits {}
+impl JumpNotZeroTraits for Uint128Traits {
     const JUMP_NOT_ZERO: GenericLibFuncId = GenericLibFuncId::new_inline("uint128_jump_nz");
-    const GENERIC_TYPE_ID: GenericTypeId = <IntegerType as NamedType>::ID;
+    const GENERIC_TYPE_ID: GenericTypeId = <Uint128Type as NamedType>::ID;
 }
-pub type IntWrappingOpLibFunc = OperationLibFunc<IntegerTraits>;
-pub type IntConstLibFunc = ConstLibFunc<IntegerTraits>;
-pub type IntJumpNotZeroLibFunc = JumpNotZeroLibFunc<IntegerTraits>;
+pub type Uint128JumpNotZeroLibFunc = JumpNotZeroLibFunc<Uint128Traits>;
+
+/// Operators for integers.
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum IntOperator {
+    WrappingAdd,
+    WrappingSub,
+    WrappingMul,
+    Div,
+    Mod,
+}
+
+/// Libfunc for uint128 operations.
+pub struct Uint128OperationLibFunc {
+    pub operator: IntOperator,
+}
+impl Uint128OperationLibFunc {
+    fn new(operator: IntOperator) -> Self {
+        Self { operator }
+    }
+}
+impl GenericLibFunc for Uint128OperationLibFunc {
+    type Concrete = Uint128OperationConcreteLibFunc;
+
+    fn by_id(id: &GenericLibFuncId) -> Option<Self> {
+        const WRAPPING_ADD: GenericLibFuncId = GenericLibFuncId::new_inline("uint128_wrapping_add");
+        const WRAPPING_SUB: GenericLibFuncId = GenericLibFuncId::new_inline("uint128_wrapping_sub");
+        const WRAPPING_MUL: GenericLibFuncId = GenericLibFuncId::new_inline("uint128_wrapping_mul");
+        const DIV: GenericLibFuncId = GenericLibFuncId::new_inline("uint128_div");
+        const MOD: GenericLibFuncId = GenericLibFuncId::new_inline("uint128_mod");
+        match id {
+            id if id == &WRAPPING_ADD => Some(Self::new(IntOperator::WrappingAdd)),
+            id if id == &WRAPPING_SUB => Some(Self::new(IntOperator::WrappingSub)),
+            id if id == &WRAPPING_MUL => Some(Self::new(IntOperator::WrappingMul)),
+            id if id == &DIV => Some(Self::new(IntOperator::Div)),
+            id if id == &MOD => Some(Self::new(IntOperator::Mod)),
+            _ => None,
+        }
+    }
+
+    fn specialize_signature(
+        &self,
+        context: &dyn SignatureSpecializationContext,
+        args: &[GenericArg],
+    ) -> Result<LibFuncSignature, SpecializationError> {
+        let ty = context.get_concrete_type(Uint128Type::id(), &[])?;
+        match args {
+            [] => Ok(LibFuncSignature::new_non_branch(
+                vec![
+                    ty.clone(),
+                    if matches!(self.operator, IntOperator::Div | IntOperator::Mod) {
+                        context.get_wrapped_concrete_type(NonZeroType::id(), ty.clone())?
+                    } else {
+                        ty.clone()
+                    },
+                ],
+                vec![OutputVarInfo {
+                    ty,
+                    ref_info: OutputVarReferenceInfo::Deferred(DeferredOutputKind::Generic),
+                }],
+                SierraApChange::Known,
+            )),
+            [GenericArg::Value(c)] => {
+                if matches!(self.operator, IntOperator::Div | IntOperator::Mod) && *c == 0 {
+                    Err(SpecializationError::UnsupportedGenericArg)
+                } else {
+                    Ok(LibFuncSignature::new_non_branch(
+                        vec![ty.clone()],
+                        vec![OutputVarInfo {
+                            ty,
+                            ref_info: OutputVarReferenceInfo::Deferred(DeferredOutputKind::Generic),
+                        }],
+                        SierraApChange::Known,
+                    ))
+                }
+            }
+            _ => Err(SpecializationError::UnsupportedGenericArg),
+        }
+    }
+
+    fn specialize(
+        &self,
+        context: &dyn SpecializationContext,
+        args: &[GenericArg],
+    ) -> Result<Self::Concrete, SpecializationError> {
+        match args {
+            [] => {
+                Ok(Uint128OperationConcreteLibFunc::Binary(Uint128BinaryOperationConcreteLibFunc {
+                    operator: self.operator,
+                    signature: self.specialize_signature(context.upcast(), args)?,
+                }))
+            }
+            [GenericArg::Value(c)] => {
+                if matches!(self.operator, IntOperator::Div | IntOperator::Mod) && *c == 0 {
+                    Err(SpecializationError::UnsupportedGenericArg)
+                } else {
+                    Ok(Uint128OperationConcreteLibFunc::Const(
+                        Uint128OperationWithConstConcreteLibFunc {
+                            operator: self.operator,
+                            c: *c as u128,
+                            signature: self.specialize_signature(context.upcast(), args)?,
+                        },
+                    ))
+                }
+            }
+            _ => Err(SpecializationError::UnsupportedGenericArg),
+        }
+    }
+}
+
+pub struct Uint128BinaryOperationConcreteLibFunc {
+    pub operator: IntOperator,
+    pub signature: LibFuncSignature,
+}
+impl SignatureBasedConcreteLibFunc for Uint128BinaryOperationConcreteLibFunc {
+    fn signature(&self) -> &LibFuncSignature {
+        &self.signature
+    }
+}
+
+/// uint128 operations with a const.
+pub struct Uint128OperationWithConstConcreteLibFunc {
+    pub operator: IntOperator,
+    pub c: u128,
+    pub signature: LibFuncSignature,
+}
+define_concrete_libfunc_hierarchy! {
+    pub enum Uint128OperationConcreteLibFunc {
+        Binary(Uint128BinaryOperationConcreteLibFunc),
+        Const(Uint128OperationWithConstConcreteLibFunc),
+    }
+}
+
+impl SignatureBasedConcreteLibFunc for Uint128OperationWithConstConcreteLibFunc {
+    fn signature(&self) -> &LibFuncSignature {
+        &self.signature
+    }
+}
+
+/// LibFunc for creating a constant uint128.
+#[derive(Default)]
+pub struct Uint128ConstLibFunc {}
+impl NamedLibFunc for Uint128ConstLibFunc {
+    type Concrete = Uint128ConstConcreteLibFunc;
+    const ID: GenericLibFuncId = GenericLibFuncId::new_inline("uint128_const");
+
+    fn specialize_signature(
+        &self,
+        context: &dyn SignatureSpecializationContext,
+        _args: &[GenericArg],
+    ) -> Result<LibFuncSignature, SpecializationError> {
+        Ok(LibFuncSignature::new_non_branch(
+            vec![],
+            vec![OutputVarInfo {
+                ty: context.get_concrete_type(Uint128Type::id(), &[])?,
+                ref_info: OutputVarReferenceInfo::Const,
+            }],
+            SierraApChange::NotImplemented,
+        ))
+    }
+
+    fn specialize(
+        &self,
+        context: &dyn SpecializationContext,
+        args: &[GenericArg],
+    ) -> Result<Self::Concrete, SpecializationError> {
+        match args {
+            [GenericArg::Value(c)] => Ok(Uint128ConstConcreteLibFunc {
+                c: *c as u128,
+                signature: <Self as NamedLibFunc>::specialize_signature(
+                    self,
+                    context.upcast(),
+                    args,
+                )?,
+            }),
+            _ => Err(SpecializationError::UnsupportedGenericArg),
+        }
+    }
+}
+
+pub struct Uint128ConstConcreteLibFunc {
+    pub c: u128,
+    pub signature: LibFuncSignature,
+}
+impl SignatureBasedConcreteLibFunc for Uint128ConstConcreteLibFunc {
+    fn signature(&self) -> &LibFuncSignature {
+        &self.signature
+    }
+}
