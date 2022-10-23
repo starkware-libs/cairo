66db87582e3144b81a0f765c4b872000b1dcc571
liorgold2 <38202661+liorgold2@users.noreply.github.com>
2022-10-23 11:48:31 +0300

    Check lowering diagnostics in example tests. (#738)

--- tests/examples_test.rs
+++ tests/examples_test.rs
@@ -4,6 +4,7 @@ use std::path::PathBuf;
 use std::sync::Arc;
 
 use defs::ids::ModuleId;
+use lowering::db::LoweringGroup;
 use pretty_assertions::assert_eq;
 use semantic::db::SemanticGroup;
 use semantic::test_utils::setup_test_module;
@@ -13,7 +14,7 @@ use sierra_generator::test_utils::{replace_sierra_ids_in_program, SierraGenDatab
 use sierra_to_casm::metadata::Metadata;
 use test_case::test_case;
 
-/// Setups the cairo lowerring to sierra db for the matching example.
+/// Setups the cairo lowering to sierra db for the matching example.
 fn setup(name: &str) -> (SierraGenDatabaseForTesting, ModuleId) {
     let dir = env!("CARGO_MANIFEST_DIR");
     // Pop the "/tests" suffix.
@@ -25,6 +26,16 @@ fn setup(name: &str) -> (SierraGenDatabaseForTesting, ModuleId) {
     let db = &mut db_val;
     let module_id =
         setup_test_module(db, &std::fs::read_to_string(path).unwrap()).unwrap().module_id;
+
+    db.module_semantic_diagnostics(module_id)
+        .unwrap()
+        .expect_with_db(db, "Unexpected semantic diagnostics");
+    db.module_lowering_diagnostics(module_id)
+        .unwrap()
+        .expect_with_db(db, "Unexpected lowering diagnostics.");
+    db.module_sierra_diagnostics(module_id)
+        .expect_with_db(db, "Unexpected Sierra generation diagnostics.");
+
     (db_val, module_id)
 }
 
@@ -39,7 +50,7 @@ fn get_expected_contents(name: &str, test_type: &str) -> String {
 /// Compiles the Cairo code for `name` to a Sierra program.
 fn compile_to_sierra(name: &str) -> (SierraGenDatabaseForTesting, Arc<sierra::program::Program>) {
     let (db, module_id) = setup(name);
-    db.module_sierra_diagnostics(module_id).expect("Creating sierra code should succeed");
+
     let sierra_program = db.module_sierra_program(module_id).unwrap();
     (db, sierra_program)
 }
@@ -87,10 +98,8 @@ fn cairo_to_casm(name: &str) {
 #[test_case("fib")]
 #[test_case("fib_box")]
 #[test_case("fib_array")]
-#[test_case("fib_uint128")]
+#[test_case("fib_uint128" => ignore["uint128 does not pass lowering."])]
 #[test_case("corelib_usage")]
-fn semantic_test(name: &str) {
-    // TODO(spapini): Get lowered representation instead when added to db.
-    let (db, module_id) = setup(name);
-    db.module_semantic_diagnostics(module_id).expect("Creating semantic model should succeed");
+fn lowering_test(name: &str) {
+    setup(name);
 }
