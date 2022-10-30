9ed27b2127df922f1b32b4381adaff815d4f72fc
Yuval Goldberg <yuval@starkware.co>
2022-10-30 12:18:14 +0200

    Changed sizes and ApChange to use usize instead of i16

--- crates/sierra_to_casm/src/environment/frame_state.rs
+++ crates/sierra_to_casm/src/environment/frame_state.rs
@@ -21,36 +21,34 @@ pub enum FrameState {
     /// finalize_locals was called and the frame has been finalized.
     Finalized,
     /// 'allocated' felts have been allocated for local variables.
-    Allocating { allocated: i16, last_ap_tracking: ApChange },
+    Allocating { allocated: usize, last_ap_tracking: ApChange },
 }
 
-// Returns the number of slots to allocates for locals and the new frame state.
+/// Returns the number of slots to allocate for locals and the new frame state.
 pub fn handle_finalize_locals(
     frame_state: FrameState,
     ap_tracking: ApChange,
-) -> Result<(i16, FrameState), FrameStateError> {
+) -> Result<(usize, FrameState), FrameStateError> {
     match frame_state {
         FrameState::Finalized => Err(FrameStateError::InvalidFinalizeLocals(frame_state)),
         FrameState::Allocating { allocated, last_ap_tracking } => {
             let allocated = match ap_tracking {
                 // TODO(ilya, 10/10/2022): Do we want to support allocating 0 locals?
-                ApChange::Known(_) if allocated == 0 => Ok(0),
-                ApChange::Known(offset) if ap_tracking == last_ap_tracking => {
-                    Ok(allocated - offset)
-                }
-                _ => Err(FrameStateError::InvalidFinalizeLocals(frame_state)),
-            }?;
+                ApChange::Known(_) if allocated == 0 => 0,
+                ApChange::Known(offset) if ap_tracking == last_ap_tracking => allocated - offset,
+                _ => return Err(FrameStateError::InvalidFinalizeLocals(frame_state)),
+            };
             Ok((allocated, FrameState::Finalized))
         }
     }
 }
 
-// Returns the offset of the newly allocated variable and the new frame state.
+/// Returns the offset of the newly allocated variable and the new frame state.
 pub fn handle_alloc_local(
     frame_state: FrameState,
     ap_tracking: ApChange,
-    allocation_size: i16,
-) -> Result<(i16, FrameState), FrameStateError> {
+    allocation_size: usize,
+) -> Result<(usize, FrameState), FrameStateError> {
     match frame_state {
         FrameState::Finalized => Err(FrameStateError::InvalidAllocLocal(frame_state)),
 
