d181466976d4d870850eda4bd9a9135398994e71
Ilya Lesokhin <ilya@starkware.co>
2022-11-06 08:06:01 +0000

    Skip empty children in format_internal.

--- crates/parser/src/colored_printer.rs
+++ crates/parser/src/colored_printer.rs
@@ -41,7 +41,7 @@ impl<'a> ColoredPrinter<'a> {
 fn is_missing_kind(kind: SyntaxKind) -> bool {
     matches!(kind, SyntaxKind::ExprMissing | SyntaxKind::StatementMissing)
 }
-fn is_empty_kind(kind: SyntaxKind) -> bool {
+pub fn is_empty_kind(kind: SyntaxKind) -> bool {
     matches!(
         kind,
         SyntaxKind::OptionStructArgExprEmpty
