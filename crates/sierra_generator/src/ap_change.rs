e7c88c6be905d35bb528e55871aea1e946767395
liorgold2 <38202661+liorgold2@users.noreply.github.com>
2022-11-06 11:42:22 +0200

    Add get_ap_change(). (#830)

--- crates/sierra_generator/src/ap_change.rs
+++ crates/sierra_generator/src/ap_change.rs
@@ -3,8 +3,11 @@
 mod test;
 
 use defs::ids::FreeFunctionId;
+use sierra::program::{GenBranchTarget, GenStatement};
+use utils::ordered_hash_map::OrderedHashMap;
 
 use crate::db::SierraGenGroup;
+use crate::pre_sierra;
 
 /// Query implementation of [SierraGenGroup::contains_cycle].
 pub fn contains_cycle(db: &dyn SierraGenGroup, function_id: FreeFunctionId) -> Option<bool> {
@@ -41,4 +44,121 @@ pub fn contains_cycle_handle_cycle(
     _function_id: &FreeFunctionId,
 ) -> Option<bool> {
     Some(true)
+}
+
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
+pub enum ApChange {
+    Known(usize),
+    Unknown,
+}
+
+/// Query implementation of [SierraGenGroup::get_ap_change].
+pub fn get_ap_change(db: &dyn SierraGenGroup, function_id: FreeFunctionId) -> Option<ApChange> {
+    let function = &*db.free_function_sierra(function_id)?;
+
+    // The ap change from the beginning of the function to the current instruction.
+    // None means the current instruction is not reachable.
+    let mut current_ap_change_opt = CurrentKnownApChange(Some(0));
+
+    // The ap change of the function.
+    // None means it's not known yet.
+    let mut function_ap_change_opt = CurrentKnownApChange(None);
+
+    let mut ap_change_at_label = OrderedHashMap::<pre_sierra::LabelId, usize>::default();
+
+    for statement in &function.body {
+        match statement {
+            pre_sierra::Statement::Sierra(GenStatement::Invocation(invocation)) => {
+                // Get the ap-change value, and mark the next instruction as unreachable.
+                // This will be overridden if we have a Fallthrough branch.
+                let current_ap_change = current_ap_change_opt
+                    .pop("Internal compiler error: found an unreachable statement.");
+
+                // Go over the branches.
+                for branch in &invocation.branches {
+                    // TODO(lior): Replace ap_change with the actual ap-change of the branch.
+                    let branch_ap_change = 0;
+                    let new_ap_change = current_ap_change + branch_ap_change;
+                    match branch.target {
+                        GenBranchTarget::Fallthrough => {
+                            if !current_ap_change_opt.merge_ap_change(new_ap_change) {
+                                return Some(ApChange::Unknown);
+                            }
+                        }
+                        GenBranchTarget::Statement(label_id) => {
+                            let mut at_label =
+                                CurrentKnownApChange(ap_change_at_label.get(&label_id).cloned());
+                            // Merge with the next jump.
+                            if !at_label.merge_ap_change(new_ap_change) {
+                                return Some(ApChange::Unknown);
+                            }
+                            ap_change_at_label.insert(label_id, at_label.0.unwrap());
+                        }
+                    }
+                }
+            }
+            pre_sierra::Statement::Sierra(GenStatement::Return(_return_statement)) => {
+                let current_ap_change = current_ap_change_opt
+                    .pop("Internal compiler error: found an unreachable statement.");
+                if !function_ap_change_opt.merge_ap_change(current_ap_change) {
+                    return Some(ApChange::Unknown);
+                }
+            }
+            pre_sierra::Statement::Label(pre_sierra::Label { id: label_id }) => {
+                if let Some(at_current_label) = ap_change_at_label.swap_remove(label_id) {
+                    if !current_ap_change_opt.merge_ap_change(at_current_label) {
+                        return Some(ApChange::Unknown);
+                    }
+                }
+            }
+            pre_sierra::Statement::PushValues(_) => panic!(
+                "Unexpected pre_sierra::Statement::PushValues in \
+                 calculate_statement_dups_and_drops()."
+            ),
+        }
+    }
+
+    // Make sure all labels were visited.
+    assert!(
+        ap_change_at_label.is_empty(),
+        "Internal compiler error: Unhandled label in 'store_variables'."
+    );
+    assert!(
+        current_ap_change_opt.is_none(),
+        "Internal compiler error: Found a reachable statement at the end of the function."
+    );
+    Some(ApChange::Known(
+        function_ap_change_opt.pop("Internal compiler error: Function does not return."),
+    ))
+}
+
+/// Helper struct for [get_ap_change].
+struct CurrentKnownApChange(Option<usize>);
+
+impl CurrentKnownApChange {
+    /// Merges two ap-changes.
+    /// current_ap_change_opt=None means there is not existing ap-change.
+    /// In this case it will be replaced by new_ap_change is
+    /// returned).
+    /// Returns true if the result is a known ap change and false otherwise.
+    fn merge_ap_change(&mut self, new_ap_change: usize) -> bool {
+        if let Some(_current_ap_change) = self.0 {
+            // TODO(lior): Use the if below, once ap values are correct.
+            // if new_ap_change != current_ap_change {
+            return false;
+        } else {
+            self.0 = Some(new_ap_change);
+        }
+        true
+    }
+
+    fn pop(&mut self, error_message: &str) -> usize {
+        let res = self.0.expect(error_message);
+        self.0 = None;
+        res
+    }
+
+    fn is_none(&self) -> bool {
+        self.0.is_none()
+    }
 }
