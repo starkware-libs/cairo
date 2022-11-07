c3fc7c7aafb39b34874fedceb49c44b21d58e0b3
Gil Ben-Shachar <gil@starkware.co>
2022-11-07 13:59:42 +0200

    Add enum_init to sierra generator

--- crates/sierra_generator/src/types.rs
+++ crates/sierra_generator/src/types.rs
@@ -22,8 +22,31 @@ pub fn get_concrete_type_id(
                 defs::ids::GenericTypeId::Struct(_) => {
                     todo!("Add support for struct types when they are supported in Sierra.")
                 }
-                defs::ids::GenericTypeId::Enum(_) => {
-                    todo!("Add support for enum types when they are supported in Sierra.")
+                defs::ids::GenericTypeId::Enum(enm) => {
+                    // TODO(Gil): This user type is just a temporary place holder. Lookup the real type once exist.
+                    let mut generic_args =
+                        vec![sierra::program::GenericArg::UserType(sierra::ids::UserTypeId {
+                            id: 0,
+                            debug_name: Some(enm.name(db.upcast())),
+                        })];
+                    generic_args.extend(
+                        db.enum_variants(enm)?
+                            .into_iter()
+                            .map(|(_, varinat_id)| {
+                                db.variant_semantic(enm, varinat_id)
+                                    .map(|variant| variant.ty)
+                                    .unwrap()
+                            })
+                            .map(|ty| {
+                                sierra::program::GenericArg::Type(
+                                    db.get_concrete_type_id(ty).unwrap(),
+                                )
+                            }),
+                    );
+                    Some(db.intern_concrete_type(ConcreteTypeLongId {
+                        generic_id: sierra::ids::GenericTypeId::from_string("Enum"),
+                        generic_args,
+                    }))
                 }
                 defs::ids::GenericTypeId::Extern(extrn) => {
                     Some(db.intern_concrete_type(ConcreteTypeLongId {
