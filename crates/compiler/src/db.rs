ea927406a810a0fce4610dee3d8becfdb061ecf8
ilyalesokhin-starkware <ilya@starkware.co>
2022-11-10 18:13:06 +0200

    Add Cairo RootDatabase. (#874)

---
+++ crates/compiler/src/db.rs
@@ -0,0 +1,59 @@
+use db_utils::Upcast;
+use defs::db::{DefsDatabase, DefsGroup};
+use filesystem::db::{init_files_group, AsFilesGroupMut, FilesDatabase, FilesGroup};
+use lowering::db::{LoweringDatabase, LoweringGroup};
+use parser::db::ParserDatabase;
+use semantic::db::{SemanticDatabase, SemanticGroup};
+use sierra_generator::db::SierraGenDatabase;
+use syntax::node::db::{SyntaxDatabase, SyntaxGroup};
+
+#[salsa::database(
+    DefsDatabase,
+    FilesDatabase,
+    LoweringDatabase,
+    ParserDatabase,
+    SemanticDatabase,
+    SierraGenDatabase,
+    SyntaxDatabase
+)]
+pub struct RootDatabase {
+    storage: salsa::Storage<RootDatabase>,
+}
+impl salsa::Database for RootDatabase {}
+impl Default for RootDatabase {
+    fn default() -> Self {
+        let mut res = Self { storage: Default::default() };
+        init_files_group(&mut res);
+        res
+    }
+}
+impl AsFilesGroupMut for RootDatabase {
+    fn as_files_group_mut(&mut self) -> &mut (dyn FilesGroup + 'static) {
+        self
+    }
+}
+impl Upcast<dyn FilesGroup> for RootDatabase {
+    fn upcast(&self) -> &(dyn FilesGroup + 'static) {
+        self
+    }
+}
+impl Upcast<dyn SyntaxGroup> for RootDatabase {
+    fn upcast(&self) -> &(dyn SyntaxGroup + 'static) {
+        self
+    }
+}
+impl Upcast<dyn DefsGroup> for RootDatabase {
+    fn upcast(&self) -> &(dyn defs::db::DefsGroup + 'static) {
+        self
+    }
+}
+impl Upcast<dyn SemanticGroup> for RootDatabase {
+    fn upcast(&self) -> &(dyn semantic::db::SemanticGroup + 'static) {
+        self
+    }
+}
+impl Upcast<dyn LoweringGroup> for RootDatabase {
+    fn upcast(&self) -> &(dyn lowering::db::LoweringGroup + 'static) {
+        self
+    }
+}
