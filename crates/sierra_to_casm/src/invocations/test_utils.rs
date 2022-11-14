168bfef15c4b99b5a208ee62411e97e047693b75
Ori Ziv <oriz@starkware.co>
2022-11-14 13:21:10 +0200

    Added support for testing branch libfuncs.
    commit-id:a83ef028

--- crates/sierra_to_casm/src/invocations/test_utils.rs
+++ crates/sierra_to_casm/src/invocations/test_utils.rs
@@ -9,7 +9,7 @@ use sierra::extensions::type_specialization_context::TypeSpecializationContext;
 use sierra::extensions::types::TypeInfo;
 use sierra::extensions::{ConcreteLibFunc, ConcreteType, GenericLibFuncEx, GenericTypeEx};
 use sierra::ids::{ConcreteTypeId, VarId};
-use sierra::program::{Invocation, StatementIdx};
+use sierra::program::{BranchInfo, BranchTarget, Invocation, StatementIdx};
 use sierra_gas::gas_info::GasInfo;
 
 use super::{compile_invocation, CompiledInvocation, ProgramInfo};
@@ -236,7 +236,19 @@ pub fn compile_libfunc(libfunc: &str, refs: Vec<ReferenceExpression>) -> Reduced
             &Invocation {
                 libfunc_id: "".into(),
                 args: (0..args.len()).map(VarId::from_usize).collect(),
-                branches: vec![],
+                branches: libfunc
+                    .branch_signatures()
+                    .iter()
+                    .enumerate()
+                    .map(|(i, branch)| BranchInfo {
+                        target: if libfunc.fallthrough() == Some(i) {
+                            BranchTarget::Fallthrough
+                        } else {
+                            BranchTarget::Statement(StatementIdx(i))
+                        },
+                        results: (0..branch.vars.len()).map(VarId::from_usize).collect(),
+                    })
+                    .collect(),
             },
             &libfunc,
             StatementIdx(0),
