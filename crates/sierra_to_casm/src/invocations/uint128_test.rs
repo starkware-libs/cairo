afe9e678932e7c21d76a97de2c3cb41c1545de32
orizi <104711814+orizi@users.noreply.github.com>
2022-11-14 16:57:34 +0100

    Added uint128 add and sub tests. (#932)

---
+++ crates/sierra_to_casm/src/invocations/uint128_test.rs
@@ -0,0 +1,83 @@
+use casm::ap_change::ApChange;
+use casm::{casm, deref};
+use num_bigint::BigInt;
+use pretty_assertions::assert_eq;
+use sierra::program::StatementIdx;
+
+use crate::invocations::test_utils::{
+    compile_libfunc, ReducedBranchChanges, ReducedCompiledInvocation,
+};
+use crate::ref_expr;
+use crate::relocations::{Relocation, RelocationEntry};
+
+#[test]
+fn test_add() {
+    assert_eq!(
+        compile_libfunc(
+            "uint128_add",
+            vec![ref_expr!([fp + 2]), ref_expr!([fp + 1]), ref_expr!([ap - 7])]
+        ),
+        ReducedCompiledInvocation {
+            instructions: casm! {
+                [ap + 0] = [fp + 1] + [ap - 7], ap++;
+                %{ memory[ap + 0] = memory [ap - 1] < (BigInt::from(2).pow(128)) %}
+                jmp rel 7 if [ap + 0] != 0, ap++;
+                [ap + 0] = [ap - 2] + (-BigInt::from(2).pow(128)), ap++;
+                [ap - 1] = [[deref!([fp + 2])]];
+                jmp rel 0;
+                [ap - 2] = [[deref!([fp + 2])]];
+            }
+            .instructions,
+            relocations: vec![RelocationEntry {
+                instruction_idx: 4,
+                relocation: Relocation::RelativeStatementId(StatementIdx(1))
+            }],
+            results: vec![
+                ReducedBranchChanges {
+                    refs: vec![ref_expr!([fp + 2] + 1), ref_expr!([ap - 2])],
+                    ap_change: ApChange::Known(2)
+                },
+                ReducedBranchChanges {
+                    refs: vec![ref_expr!([fp + 2] + 1)],
+                    ap_change: ApChange::Known(3)
+                }
+            ]
+        }
+    );
+}
+
+#[test]
+fn test_sub() {
+    assert_eq!(
+        compile_libfunc(
+            "uint128_sub",
+            vec![ref_expr!([ap - 2]), ref_expr!([ap - 1]), ref_expr!([fp + 7])]
+        ),
+        ReducedCompiledInvocation {
+            instructions: casm! {
+                [ap - 1] = [ap + 0] + [fp + 7], ap++;
+                %{ memory[ap + 0] = memory [ap - 1] < (BigInt::from(2).pow(128)) %}
+                jmp rel 7 if [ap + 0] != 0, ap++;
+                [ap + 0] = [ap - 2] + (BigInt::from(2).pow(128)), ap++;
+                [ap - 1] = [[deref!([ap - 5])]];
+                jmp rel 0;
+                [ap - 2] = [[deref!([ap - 4])]];
+            }
+            .instructions,
+            relocations: vec![RelocationEntry {
+                instruction_idx: 4,
+                relocation: Relocation::RelativeStatementId(StatementIdx(1))
+            }],
+            results: vec![
+                ReducedBranchChanges {
+                    refs: vec![ref_expr!([ap - 4] + 1), ref_expr!([ap - 2])],
+                    ap_change: ApChange::Known(2)
+                },
+                ReducedBranchChanges {
+                    refs: vec![ref_expr!([ap - 5] + 1)],
+                    ap_change: ApChange::Known(3)
+                }
+            ]
+        }
+    );
+}
