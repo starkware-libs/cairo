5304bb3b886890c5cfbc978e206a4f6d96da34df
Ilya Lesokhin <ilya@starkware.co>
2022-11-15 09:01:37 +0000

    Use ProjectConfig.

--- crates/compiler/src/cli.rs
+++ crates/compiler/src/cli.rs
@@ -8,8 +8,9 @@ use clap::Parser;
 use defs::db::DefsGroup;
 use defs::ids::ModuleId;
 use filesystem::db::{AsFilesGroupMut, FilesGroup, FilesGroupEx};
-use filesystem::ids::{CrateId, CrateLongId, Directory, FileLongId};
+use filesystem::ids::{CrateLongId, Directory, FileLongId};
 use lowering::db::LoweringGroup;
+use project::ProjectConfig;
 use semantic::db::SemanticGroup;
 use sierra_generator::db::SierraGenGroup;
 
@@ -28,68 +29,63 @@ struct Args {
 }
 
 /// Prints the diagnostics to stderr.
-fn print_diagnostics(db: &mut RootDatabase, crate_id: CrateId) {
-    for module_id in &*db.crate_modules(crate_id) {
-        if let Some(file_id) = db.module_file(*module_id) {
-            if db.file_content(file_id).is_none() {
-                if let ModuleId::CrateRoot(_) = *module_id {
-                    match db.lookup_intern_file(file_id) {
-                        FileLongId::OnDisk(path) => eprintln!("{} not found", path.display()),
-                        FileLongId::Virtual(_) => panic!("Missing virtual file."),
+fn print_diagnostics(db: &mut RootDatabase) {
+    for crate_id in db.crates() {
+        for module_id in &*db.crate_modules(crate_id) {
+            if let Some(file_id) = db.module_file(*module_id) {
+                if db.file_content(file_id).is_none() {
+                    if let ModuleId::CrateRoot(_) = *module_id {
+                        match db.lookup_intern_file(file_id) {
+                            FileLongId::OnDisk(path) => eprintln!("{} not found", path.display()),
+                            FileLongId::Virtual(_) => panic!("Missing virtual file."),
+                        }
                     }
+                } else {
+                    eprint!("{}", db.file_syntax_diagnostics(file_id).format(db));
                 }
-            } else {
-                eprint!("{}", db.file_syntax_diagnostics(file_id).format(db));
-            }
 
-            if let Some(diag) = db.module_semantic_diagnostics(*module_id) {
-                eprint!("{}", diag.format(db));
-            }
+                if let Some(diag) = db.module_semantic_diagnostics(*module_id) {
+                    eprint!("{}", diag.format(db));
+                }
 
-            if let Some(diag) = db.module_lowering_diagnostics(*module_id) {
-                eprint!("{}", diag.format(db));
-            }
+                if let Some(diag) = db.module_lowering_diagnostics(*module_id) {
+                    eprint!("{}", diag.format(db));
+                }
 
-            eprint!("{}", db.module_sierra_diagnostics(*module_id).format(db));
+                eprint!("{}", db.module_sierra_diagnostics(*module_id).format(db));
+            }
         }
     }
 }
 
-fn setup_create(db: &mut RootDatabase, path: &Path) -> Option<CrateId> {
-    let crate_id = db.intern_crate(CrateLongId("contract_crate".into()));
-
-    // TODO(ilya): Try to use the project crate.
-    if path.is_dir() {
-        db.set_crate_root(crate_id, Some(Directory(path.to_path_buf())));
-    } else {
-        match path.extension().and_then(OsStr::to_str) {
-            Some("cairo") => (),
-            _ => {
-                eprintln!("Only files with .cairo extension can be compiled.");
-                return None;
-            }
-        }
-
-        db.set_crate_root(crate_id, Some(Directory(path.parent().unwrap().to_path_buf())));
-
-        if let Some(file_stemp) = path.file_stem().and_then(OsStr::to_str) {
-            if file_stemp != "lib" {
-                // If file_stemp is not lib, create a fake lib file.
-
-                if !path.exists() {
-                    eprintln!("Couldn't read {}: No such file", path.to_string_lossy());
-                    return None;
-                }
-
-                let module_id = ModuleId::CrateRoot(crate_id);
-                let file_id = db.module_file(module_id).unwrap();
-                db.as_files_group_mut()
-                    .override_file_content(file_id, Some(Arc::new(format!("mod {};", file_stemp))));
-            }
+fn setup_single_file_crate(db: &mut RootDatabase, path: &Path) -> Result<(), ()> {
+    match path.extension().and_then(OsStr::to_str) {
+        Some("cairo") => (),
+        _ => {
+            eprintln!("Only files with .cairo extension can be compiled.");
+            return Err(());
         }
     }
 
-    Some(crate_id)
+    if let Some(file_stemp) = path.file_stem().and_then(OsStr::to_str) {
+        if file_stemp != "lib" {
+            // If file_stemp is not lib, create a fake lib file.
+
+            if !path.exists() {
+                eprintln!("Couldn't read {}: No such file", path.to_string_lossy());
+                return Err(());
+            }
+
+            let crate_id = db.intern_crate(CrateLongId("contract_crate".into()));
+            db.set_crate_root(crate_id, Some(Directory(path.parent().unwrap().to_path_buf())));
+
+            let module_id = ModuleId::CrateRoot(crate_id);
+            let file_id = db.module_file(module_id).unwrap();
+            db.as_files_group_mut()
+                .override_file_content(file_id, Some(Arc::new(format!("mod {};", file_stemp))));
+        }
+    }
+    Ok(())
 }
 
 fn main() -> ExitCode {
@@ -98,12 +94,21 @@ fn main() -> ExitCode {
     let mut db_val = RootDatabase::default();
     let db = &mut db_val;
 
-    let Some(crate_id) = setup_create(db, Path::new(&args.path)) else {
+    let path = Path::new(&args.path);
+    if path.is_dir() {
+        match ProjectConfig::from_directory(path) {
+            Ok(config) => db.with_project_config(config),
+            _ => {
+                eprintln!("Failed to load project config.");
+                return ExitCode::FAILURE;
+            }
+        };
+    } else if setup_single_file_crate(db, path).is_err() {
         return ExitCode::FAILURE;
-    };
+    }
 
     let Some(sierra_program) = db.get_sierra_program() else {
-        print_diagnostics(db, crate_id);
+        print_diagnostics(db);
         return ExitCode::FAILURE;
     };
 
