4688a66c1426dfda3ab871378d046ea7480e99ab
Ori Ziv <oriz@starkware.co>
2022-11-16 21:14:36 +0200

    Added lowering test for error propagation.
    commit-id:d5bdf178

--- crates/lowering/src/test.rs
+++ crates/lowering/src/test.rs
@@ -12,6 +12,7 @@ utils::test_file_test!(
         "src/test_data/assignment",
         "src/test_data/call",
         "src/test_data/enums",
+        "src/test_data/error_propagate",
         "src/test_data/extern",
         "src/test_data/if",
         "src/test_data/match",
