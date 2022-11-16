d8608a67cbbe377a0ad25081737129011ffd94b9
ilyalesokhin-starkware <ilya@starkware.co>
2022-11-16 12:53:32 +0200

    Add resolve_ids and output to stdout. (#949)

--- crates/compiler/src/cli.rs
+++ crates/compiler/src/cli.rs
@@ -17,6 +17,7 @@ use sierra_generator::db::SierraGenGroup;
 mod db;
 use db::RootDatabase;
 use parser::db::ParserGroup;
+use sierra_generator::replace_ids::replace_sierra_ids_in_program;
 
 /// Command line args parser.
 /// Exits with 0/1 if the input is formatted correctly/incorrectly.
@@ -25,7 +26,11 @@ use parser::db::ParserGroup;
 struct Args {
     /// The file to compile
     path: String,
-    output: String,
+    /// The output file name (default: stdout).
+    output: Option<String>,
+    /// Replaces sierra ids with human readable ones.
+    #[arg(short, long, default_value_t = false)]
+    replace_ids: bool,
 }
 
 /// Prints the diagnostics to stderr.
@@ -76,7 +81,7 @@ fn setup_single_file_crate(db: &mut RootDatabase, path: &Path) -> Result<(), ()>
                 return Err(());
             }
 
-            let crate_id = db.intern_crate(CrateLongId("contract_crate".into()));
+            let crate_id = db.intern_crate(CrateLongId(file_stemp.into()));
             db.set_crate_root(crate_id, Some(Directory(path.parent().unwrap().to_path_buf())));
 
             let module_id = ModuleId::CrateRoot(crate_id);
@@ -107,11 +112,21 @@ fn main() -> ExitCode {
         return ExitCode::FAILURE;
     }
 
-    let Some(sierra_program) = db.get_sierra_program() else {
+    let Some(mut sierra_program) = db.get_sierra_program() else {
         print_diagnostics(db);
         return ExitCode::FAILURE;
     };
 
-    fs::write(args.output, format!("{}", sierra_program)).expect("Failed to write output.");
+    if args.replace_ids {
+        sierra_program = Arc::new(replace_sierra_ids_in_program(db, &sierra_program));
+    }
+
+    match args.output {
+        Some(path) => {
+            fs::write(path, format!("{}", sierra_program)).expect("Failed to write output.")
+        }
+        None => println!("{}", sierra_program),
+    }
+
     ExitCode::SUCCESS
 }
