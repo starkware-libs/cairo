a518f22b9fa152ec1d562e9330b3786bc1c99b8a
ilyalesokhin-starkware <ilya@starkware.co>
2022-11-21 18:32:00 +0200

    Use BigUint. (#1033)

--- crates/starknet/src/contract_class.rs
+++ crates/starknet/src/contract_class.rs
@@ -1,4 +1,4 @@
-use num_bigint::BigInt;
+use num_bigint::BigUint;
 use num_traits::Num;
 use serde::ser::Serializer;
 use serde::{Deserialize, Deserializer, Serialize};
@@ -27,27 +27,27 @@ pub struct ContractEntryPoints {
 #[derive(Default, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct ContractEntryPoint {
     /// A field element that encodes the signature of the called function.
-    #[serde(serialize_with = "serialize_big_int", deserialize_with = "deserialize_big_int")]
-    pub selector: BigInt,
+    #[serde(serialize_with = "serialize_big_uint", deserialize_with = "deserialize_big_uint")]
+    pub selector: BigUint,
     /// The offset of the instruction that should be called within the contract bytecode.
-    #[serde(serialize_with = "serialize_big_int", deserialize_with = "deserialize_big_int")]
-    pub offset: BigInt,
+    #[serde(serialize_with = "serialize_big_uint", deserialize_with = "deserialize_big_uint")]
+    pub offset: BigUint,
 }
 
-fn serialize_big_int<S>(num: &BigInt, serializer: S) -> Result<S::Ok, S::Error>
+fn serialize_big_uint<S>(num: &BigUint, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
 {
     serializer.serialize_str(&format!("{:#x}", num))
 }
 
-fn deserialize_big_int<'a, D>(deserializer: D) -> Result<BigInt, D::Error>
+fn deserialize_big_uint<'a, D>(deserializer: D) -> Result<BigUint, D::Error>
 where
     D: Deserializer<'a>,
 {
     let s = &String::deserialize(deserializer)?;
     match s.strip_prefix("0x") {
-        Some(num_no_prefix) => BigInt::from_str_radix(num_no_prefix, 16)
+        Some(num_no_prefix) => BigUint::from_str_radix(num_no_prefix, 16)
             .map_err(|error| serde::de::Error::custom(format!("{}", error))),
         None => Err(serde::de::Error::custom(format!("{s} does not start with `0x` is missing."))),
     }
