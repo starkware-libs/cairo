7f80f6812c49932bf277385f1d9095353a9342f7
ilyalesokhin-starkware <ilya@starkware.co>
2022-11-22 18:11:20 +0200

    Add starknet-sierra-compile. (#1052)

---
+++ crates/starknet/src/starknet_sierra_compile.rs
@@ -0,0 +1,61 @@
+use std::collections::HashMap;
+use std::fs;
+
+use anyhow::{anyhow, Context};
+use clap::Parser;
+use sierra::ProgramParser;
+use sierra_gas::calc_gas_info;
+use sierra_to_casm::metadata::Metadata;
+use starknet::contract_class::{CasmContractClass, ContractClass, ContractEntryPoints};
+
+/// Command line args parser.
+/// Exits with 0/1 if the input is formatted correctly/incorrectly.
+#[derive(Parser, Debug)]
+#[clap(version, verbatim_doc_comment)]
+struct Args {
+    /// The file to compile
+    file: String,
+    /// The output file name (default: stdout).
+    output: Option<String>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let contract_class: ContractClass = serde_json::from_str(
+        &fs::read_to_string(&args.file)
+            .with_context(|| format!("Failed to read {}.", &args.file))?,
+    )
+    .with_context(|| "deserialization Failed.")?;
+
+    let program = ProgramParser::new()
+        .parse(&contract_class.sierra_program)
+        .map_err(|err| anyhow!("Sierra parsing failed.\n{}", err.to_string()))?;
+    let gas_info = calc_gas_info(&program).with_context(|| "Failed calculating gas variables.")?;
+
+    let gas_usage_check = true;
+    let cairo_program = sierra_to_casm::compiler::compile(
+        &program,
+        &Metadata { function_ap_change: HashMap::new(), gas_info },
+        gas_usage_check,
+    )
+    .with_context(|| "Compilation failed.")?;
+
+    let mut bytecode = vec![];
+    for instruction in cairo_program.instructions {
+        bytecode.extend(instruction.assemble().encode());
+    }
+
+    // TODO(ilya): Fix entry points.
+    let contract =
+        CasmContractClass { bytecode, entry_points_by_type: ContractEntryPoints::default() };
+
+    let res = serde_json::to_string_pretty(&contract).with_context(|| "Serialization failed.")?;
+
+    match args.output {
+        Some(path) => fs::write(path, res).with_context(|| "Failed to write output.")?,
+        None => println!("{}", res),
+    }
+
+    Ok(())
+}
