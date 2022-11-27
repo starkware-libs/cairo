055e645707fc89d355cfae18073918cb9177d79d
Gil Ben-Shachar <gil@starkware.co>
2022-11-27 14:59:10 +0200

    Add dict_squash libfunc to Sierra

---
+++ crates/sierra/src/extensions/modules/squashed_dict_felt_to.rs
@@ -0,0 +1,50 @@
+use super::as_single_type;
+use crate::extensions::type_specialization_context::TypeSpecializationContext;
+use crate::extensions::types::TypeInfo;
+use crate::extensions::{ConcreteType, NamedType, SpecializationError};
+use crate::ids::{ConcreteTypeId, GenericTypeId};
+use crate::program::GenericArg;
+
+/// Type representing a static squashed dictionary from a felt to any type of size one.
+#[derive(Default)]
+pub struct SquashedDictFeltToType {}
+impl NamedType for SquashedDictFeltToType {
+    type Concrete = SquashedDictFeltToConcreteType;
+    const ID: GenericTypeId = GenericTypeId::new_inline("SquashedDictFeltTo");
+
+    fn specialize(
+        &self,
+        context: &dyn TypeSpecializationContext,
+        args: &[GenericArg],
+    ) -> Result<Self::Concrete, SpecializationError> {
+        let ty = as_single_type(args)?;
+        let info = context.get_type_info(ty.clone())?;
+        // TODO(Gil): the implementation support values of size 1. Remove when other sizes are
+        // supported.
+        if info.storable && info.size == 1 {
+            Ok(SquashedDictFeltToConcreteType {
+                info: TypeInfo {
+                    long_id: Self::concrete_type_long_id(args),
+                    duplicatable: false,
+                    droppable: info.droppable,
+                    storable: true,
+                    size: 2,
+                },
+                ty,
+            })
+        } else {
+            Err(SpecializationError::UnsupportedGenericArg)
+        }
+    }
+}
+
+pub struct SquashedDictFeltToConcreteType {
+    pub info: TypeInfo,
+    pub ty: ConcreteTypeId,
+}
+
+impl ConcreteType for SquashedDictFeltToConcreteType {
+    fn info(&self) -> &TypeInfo {
+        &self.info
+    }
+}
