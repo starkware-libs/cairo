b9e26b9a832089cf94746b87bbc669704fe20b19
Shahar Papini <spapini@starkware.co>
2022-11-28 15:55:53 +0200

    Load default plugins

--- crates/languageserver/src/db.rs
+++ crates/languageserver/src/db.rs
@@ -3,6 +3,7 @@ use defs::db::{init_defs_group, DefsDatabase, DefsGroup};
 use filesystem::db::{init_files_group, AsFilesGroupMut, FilesDatabase, FilesGroup};
 use lowering::db::{LoweringDatabase, LoweringGroup};
 use parser::db::ParserDatabase;
+use plugins::get_default_plugins;
 use semantic::db::{SemanticDatabase, SemanticGroup};
 use syntax::node::db::{SyntaxDatabase, SyntaxGroup};
 
@@ -23,6 +24,8 @@ impl Default for RootDatabase {
         let mut res = Self { storage: Default::default() };
         init_files_group(&mut res);
         init_defs_group(&mut res);
+        // TODO(spapini): Consider taking from config.
+        res.set_macro_plugins(get_default_plugins());
         res
     }
 }
--- crates/plugins/src/lib.rs
+++ crates/plugins/src/lib.rs
@@ -1,5 +1,16 @@
+use std::sync::Arc;
+
+use defs::db::MacroPlugin;
+
+use crate::derive::DerivePlugin;
+use crate::panicable::PanicablePlugin;
+
 pub mod derive;
 pub mod panicable;
 
 #[cfg(test)]
 mod test;
+
+pub fn get_default_plugins() -> Vec<Arc<dyn MacroPlugin>> {
+    vec![Arc::new(DerivePlugin {}), Arc::new(PanicablePlugin {})]
+}
