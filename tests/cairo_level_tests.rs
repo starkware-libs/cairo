92e946d4ee0d6978c9385ca3f11528a1f571151a
Ori Ziv <oriz@starkware.co>
2022-11-30 18:22:35 +0200

    Made cairo-level test errors more explicit.
    commit-id:bfb115d6

--- tests/cairo_level_tests.rs
+++ tests/cairo_level_tests.rs
@@ -2,6 +2,7 @@
 
 use std::path::PathBuf;
 
+use casm::run::run_function;
 use compiler::db::RootDatabase;
 use compiler::diagnostics::check_diagnostics;
 use compiler::project::setup_project;
@@ -9,9 +10,16 @@ use num_bigint::BigInt;
 use sierra_generator::db::SierraGenGroup;
 use sierra_generator::replace_ids::replace_sierra_ids_in_program;
 
-use crate::common::run_sierra_program;
+use crate::common::get_runnable_casm;
 
 mod common;
+
+/// Converts a bigint to u64.
+fn bigint_to_uint64(num: &BigInt) -> u64 {
+    let (num_bigint::Sign::Plus, digits) = num.to_u64_digits() else {panic!("Negative number.");};
+    let [as_u64] = &digits[..] else {panic!("Number not in index range.");};
+    *as_u64
+}
 
 #[test]
 fn cairo_level_tests() {
@@ -21,12 +29,15 @@ fn cairo_level_tests() {
     let mut db = RootDatabase::default();
     let main_crate_ids = setup_project(&mut db, path.as_path()).expect("Project setup failed.");
     assert!(!check_diagnostics(&mut db));
-    let sierra_program = db.get_sierra_program(main_crate_ids).unwrap();
-    let sierra_func = replace_sierra_ids_in_program(&db, &sierra_program);
-    let results = run_sierra_program(&sierra_func, &[], 3, false);
-    assert_eq!(
-        results,
-        [/* ok */ 0, /* test_count */ 40, /* padding */ 0].map(BigInt::from),
-        "Got panic from cairo."
-    );
+    let sierra_program =
+        replace_sierra_ids_in_program(&db, &db.get_sierra_program(main_crate_ids).unwrap());
+    let instructions = get_runnable_casm(&sierra_program, &[], false);
+    let (cells, ap) = run_function(instructions).unwrap();
+    if *cells[ap - 3].as_ref().unwrap() == BigInt::from(1) {
+        let err_data_start = bigint_to_uint64(cells[ap - 2].as_ref().unwrap()) as usize;
+        let err_data_end = bigint_to_uint64(cells[ap - 1].as_ref().unwrap()) as usize;
+        panic!("Cairo run paniced. Error data is: {:?}", &cells[err_data_start..err_data_end]);
+    } else {
+        assert_eq!(cells[ap - 2], Some(BigInt::from(/* test_count */ 40)), "Update test count.");
+    }
 }
