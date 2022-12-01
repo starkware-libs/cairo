4b285675623fd1f41fb12afaf9648c57cc1f6aa3
Ori Ziv <oriz@starkware.co>
2022-12-01 13:44:39 +0200

    Extracted runner code into a runner lib - using it in tests as well.
    commit-id:c9eb9c63

--- crates/runner/src/cli.rs
+++ crates/runner/src/cli.rs
@@ -1,24 +1,15 @@
 //! Compiles and runs a Cairo program.
 
-use std::collections::HashMap;
 use std::path::Path;
-use std::sync::Arc;
 
 use anyhow::{Context, Ok};
-use casm::instructions::Instruction;
-use casm::{casm, casm_extend};
 use clap::Parser;
 use compiler::db::RootDatabase;
 use compiler::diagnostics::check_and_eprint_diagnostics;
 use compiler::project::setup_project;
-use itertools::chain;
-use sierra::extensions::builtin_cost::CostTokenType;
-use sierra::program::StatementIdx;
-use sierra_gas::calc_gas_info;
-use sierra_gas::gas_info::GasInfo;
+use runner::SierraCasmRunner;
 use sierra_generator::db::SierraGenGroup;
 use sierra_generator::replace_ids::replace_sierra_ids_in_program;
-use sierra_to_casm::metadata::Metadata;
 
 /// Command line args parser.
 /// Exits with 0/1 if the input is formatted correctly/incorrectly.
@@ -51,23 +42,28 @@ fn main() -> anyhow::Result<()> {
     let sierra_program = db
         .get_sierra_program(main_crate_ids)
         .with_context(|| "Compilation failed without any diagnostics.")?;
-    let function_sizes = function_to_input_output_sizes(&sierra_program, db);
-
-    let sierra_program = Arc::new(replace_sierra_ids_in_program(db, &sierra_program));
-    let main_func =
-        find_main(&sierra_program).with_context(|| "Main function not provided in module.")?;
-    let metadata = create_metadata(&sierra_program, args.available_gas.is_some())?;
-    let program =
-        sierra_to_casm::compiler::compile(&sierra_program, &metadata, args.available_gas.is_some())
-            .with_context(|| "Failed lowering to casm.")?;
-    let entry_code = create_entry_code(main_func, args.available_gas, metadata, &program)?;
-
-    let (input_size, output_size) = function_sizes[&main_func.entry_point];
-    let (memory, ap) = casm::run::run_function(chain!(entry_code, program.instructions).collect())
-        .with_context(|| "Failed running casm code.")?;
+    let runner = SierraCasmRunner::new(
+        replace_sierra_ids_in_program(db, &sierra_program),
+        args.available_gas.is_some(),
+    )
+    .with_context(|| "Failed setting up runner.")?;
+    let result = runner
+        .run_function("::main", &[], &args.available_gas)
+        .with_context(|| "Failed to run the function.")?;
+    match result.value {
+        runner::RunResultValue::Success(values) => {
+            println!("Run completed successfully, returning {values:?}")
+        }
+        runner::RunResultValue::Panic(values) => {
+            println!("Run panicked with err values: {values:?}")
+        }
+    }
+    if let Some(gas) = result.gas_counter {
+        println!("Remaining gas: {gas}");
+    }
     if args.print_full_memory {
         print!("Full memory: [");
-        for cell in &memory {
+        for cell in &result.memory {
             match cell {
                 None => print!("_, "),
                 Some(value) => print!("{value}, "),
@@ -75,110 +71,5 @@ fn main() -> anyhow::Result<()> {
         }
         println!("]");
     }
-    print!("Returned values: [");
-    let printed_result_size = output_size - input_size;
-    for cell in &memory[(ap - printed_result_size)..ap] {
-        match cell {
-            None => print!("_, "),
-            Some(value) => print!("{value}, "),
-        }
-    }
-    println!("]");
     Ok(())
-}
-
-/// Returns the instructions to add to the begining of the code to successfully call the main
-/// function.
-fn create_entry_code(
-    main_func: &sierra::program::GenFunction<StatementIdx>,
-    available_gas: Option<usize>,
-    metadata: Metadata,
-    program: &sierra_to_casm::compiler::CairoProgram,
-) -> Result<Vec<Instruction>, anyhow::Error> {
-    let mut ctx = casm! {};
-    for (i, ty) in main_func.signature.param_types.iter().enumerate() {
-        if &main_func.signature.ret_types[i] != ty {
-            anyhow::bail!("We only support main functions with no parameters.");
-        }
-        if ty == &"RangeCheck".into() {
-            casm_extend! {ctx,
-                %{ memory[ap + 0] = segments.add() %}
-                ap += 1;
-            }
-        } else if ty == &"GasBuiltin".into() {
-            if let Some(available_gas) = available_gas {
-                // TODO(lior): Handle the other token types.
-                let initial_gas = available_gas.checked_sub(
-                    metadata.gas_info.function_costs[&main_func.id][CostTokenType::Step] as usize,
-                );
-                if let Some(initial_gas) = initial_gas {
-                    casm_extend! {ctx,
-                        [ap + 0] = initial_gas, ap++;
-                    }
-                } else {
-                    anyhow::bail!("Not enough gas to call function.");
-                }
-            } else {
-                anyhow::bail!("GasBuiltin is required while no `available_gas` value provided.");
-            }
-        } else {
-            anyhow::bail!("Inputs for main are not supported.");
-        }
-    }
-    let before_final_call = ctx.current_code_offset;
-    let final_call_size = 3;
-    let offset = final_call_size
-        + program.debug_info.sierra_statement_info[main_func.entry_point.0].code_offset;
-    casm_extend! {ctx,
-        call rel offset;
-        ret;
-    }
-    assert_eq!(before_final_call + final_call_size, ctx.current_code_offset);
-    Ok(ctx.instructions)
-}
-
-/// Creates the metadata required for a Sierra program lowering to casm.
-fn create_metadata(
-    sierra_program: &Arc<sierra::program::Program>,
-    calc_gas: bool,
-) -> Result<Metadata, anyhow::Error> {
-    let gas_info = if calc_gas {
-        calc_gas_info(sierra_program).with_context(|| {
-            "Failed calculating gas usage, it is likely a call for `get_gas` is missing."
-        })?
-    } else {
-        GasInfo { variable_values: HashMap::new(), function_costs: HashMap::new() }
-    };
-    let metadata = Metadata { function_ap_change: HashMap::new(), gas_info };
-    Ok(metadata)
-}
-
-/// Find the main function of the program.
-fn find_main(
-    sierra_program: &Arc<sierra::program::Program>,
-) -> Option<&sierra::program::GenFunction<StatementIdx>> {
-    sierra_program
-        .funcs
-        .iter()
-        .find(|f| if let Some(name) = &f.id.debug_name { name.ends_with("::main") } else { false })
-}
-
-/// Returns the total sizes of inputs and outputs per a Sierra function entry point.
-fn function_to_input_output_sizes(
-    sierra_program: &Arc<sierra::program::Program>,
-    db: &mut RootDatabase,
-) -> HashMap<StatementIdx, (usize, usize)> {
-    let function_sizes =
-        HashMap::<StatementIdx, (usize, usize)>::from_iter(sierra_program.funcs.iter().map(|f| {
-            let mut input_size = 0;
-            for ty in &f.signature.param_types {
-                input_size += db.get_type_info(ty.clone()).unwrap().size;
-            }
-            let mut output_size = 0;
-            for ty in &f.signature.ret_types {
-                output_size += db.get_type_info(ty.clone()).unwrap().size;
-            }
-            (f.entry_point, (input_size, output_size))
-        }));
-    function_sizes
 }
--- tests/examples_test.rs
+++ tests/examples_test.rs
@@ -1,21 +1,17 @@
 use std::path::PathBuf;
 
-use casm::run::run_function_return_values;
 use compiler::db::RootDatabase;
 use compiler::diagnostics::check_and_eprint_diagnostics;
 use compiler::project::setup_project;
 use filesystem::ids::CrateId;
 use num_bigint::BigInt;
 use pretty_assertions::assert_eq;
+use runner::{RunResultValue, SierraCasmRunner};
 use sierra_generator::db::SierraGenGroup;
 use sierra_generator::replace_ids::replace_sierra_ids_in_program;
 use sierra_to_casm::test_utils::build_metadata;
 use test_case::test_case;
 use test_utils::compare_contents_or_fix_with_path;
-
-use crate::common::get_runnable_casm;
-
-mod common;
 
 /// Setups the cairo lowering to sierra db for the matching example.
 fn setup(name: &str) -> (RootDatabase, Vec<CrateId>) {
@@ -114,73 +110,71 @@ fn lowering_test(name: &str) {
     setup(name);
 }
 
-#[test_case("fib", &[1, 1, 7].map(BigInt::from), Some(&[21].map(BigInt::from)); "fib")]
+#[test_case(
+    "fib",
+    &[1, 1, 7].map(BigInt::from),
+    RunResultValue::Success(vec![BigInt::from(21)]);
+    "fib"
+)]
 #[test_case(
     "fib_counter",
     &[1, 1, 8].map(BigInt::from),
-    Some(&[34, 8].map(BigInt::from));
+    RunResultValue::Success([34, 8].map(BigInt::from).into_iter().collect());
     "fib_counter"
 )]
 #[test_case(
     "fib_struct",
     &[1, 1, 9].map(BigInt::from),
-    Some(&[55, 9].map(BigInt::from));
+    RunResultValue::Success([55, 9].map(BigInt::from).into_iter().collect());
     "fib_struct"
 )]
 #[test_case(
     "fib_uint128_checked",
     &[1, 1, 10].map(BigInt::from),
-    Some(&[/*ok*/0, /*fib*/89].map(BigInt::from));
+    RunResultValue::Success([/*ok*/0, /*fib*/89].map(BigInt::from).into_iter().collect());
     "fib_uint128_checked"
 )]
 #[test_case(
     "fib_uint128_checked",
     &[1, 1, 200].map(BigInt::from),
-    Some(&[/*err*/1, /*padding*/0].map(BigInt::from));
+    RunResultValue::Success([/*err*/1, /*padding*/0].map(BigInt::from).into_iter().collect());
     "fib_uint128_checked_overflow"
 )]
 #[test_case(
     "fib_uint128",
     &[1, 1, 10].map(BigInt::from),
-    Some(&[/*ok*/0, /*fib*/89, /*padding*/0].map(BigInt::from));
+    RunResultValue::Success(vec![BigInt::from(89)]);
     "fib_uint128"
 )]
 #[test_case(
     "fib_uint128",
     &[1, 1, 200].map(BigInt::from),
-    None;
+    RunResultValue::Panic(vec![BigInt::from(1)]);
     "fib_uint128_overflow"
 )]
 #[test_case(
     "fib_local",
     &[6].map(BigInt::from),
-    Some(&[13].map(BigInt::from));
+    RunResultValue::Success(vec![BigInt::from(13)]);
     "fib_local"
 )]
 #[test_case(
     "hash_chain",
     &[3].map(BigInt::from),
-    Some(&[
-        BigInt::parse_bytes(
-            b"2dca1ad81a6107a9ef68c69f791bcdbda1df257aab76bd43ded73d96ed6227d", 16).unwrap()])
+    RunResultValue::Success(vec![BigInt::parse_bytes(
+        b"2dca1ad81a6107a9ef68c69f791bcdbda1df257aab76bd43ded73d96ed6227d", 16).unwrap()])
     => ignore["reason"];
     "hash_chain")]
 #[test_case(
     "testing",
     &[],
-    Some(&[/*ok*/0, /*padding*/0, 0].map(BigInt::from));
+    RunResultValue::Success(vec![]);
     "testing")]
-fn run_function_test(name: &str, params: &[BigInt], expected_or_panic: Option<&[BigInt]>) {
-    let sierra_func = checked_compile_to_sierra(name);
-    let result_count = match &expected_or_panic {
-        None => 3,
-        Some(expected) => expected.len(),
-    };
-    let results =
-        run_function_return_values(get_runnable_casm(&sierra_func, params, false), result_count)
-            .expect("Run failed.");
-    match expected_or_panic {
-        None => assert_eq!(results[0], BigInt::from(1), "Expected getting panic result."),
-        Some(expected) => assert_eq!(results, expected),
-    }
+fn run_function_test(name: &str, params: &[BigInt], expected: RunResultValue) {
+    let runner = SierraCasmRunner::new(checked_compile_to_sierra(name), false)
+        .expect("Failed setting up runner.");
+    let result = runner
+        .run_function(/* find first */ "", params, &None)
+        .expect("Failed running the function.");
+    assert_eq!(result.value, expected);
 }
