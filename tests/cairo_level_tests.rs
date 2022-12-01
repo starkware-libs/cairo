3b3e78d4b8e77d8ec3432568f6b603bc980e3a5d
Ori Ziv <oriz@starkware.co>
2022-12-01 13:44:39 +0200

    Extracted runner code into a runner lib - using it in tests as well.
    commit-id:c9eb9c63

--- tests/cairo_level_tests.rs
+++ tests/cairo_level_tests.rs
@@ -2,20 +2,9 @@
 
 use std::path::PathBuf;
 
-use casm::run::run_function;
 use compiler::{compile_cairo_project_at_path, CompilerConfig};
 use num_bigint::BigInt;
-
-use crate::common::get_runnable_casm;
-
-mod common;
-
-/// Converts a bigint to u64.
-fn bigint_to_uint64(num: &BigInt) -> u64 {
-    let (num_bigint::Sign::Plus, digits) = num.to_u64_digits() else {panic!("Negative number.");};
-    let [as_u64] = &digits[..] else {panic!("Number not in index range.");};
-    *as_u64
-}
+use runner::{RunResultValue, SierraCasmRunner};
 
 #[test]
 fn cairo_level_tests() {
@@ -27,13 +16,9 @@ fn cairo_level_tests() {
         CompilerConfig { replace_ids: true, ..CompilerConfig::default() },
     )
     .expect("Compilation failed.");
-    let instructions = get_runnable_casm(&sierra_program, &[], false);
-    let (cells, ap) = run_function(instructions).unwrap();
-    if *cells[ap - 3].as_ref().unwrap() == BigInt::from(1) {
-        let err_data_start = bigint_to_uint64(cells[ap - 2].as_ref().unwrap()) as usize;
-        let err_data_end = bigint_to_uint64(cells[ap - 1].as_ref().unwrap()) as usize;
-        panic!("Cairo run paniced. Error data is: {:?}", &cells[err_data_start..err_data_end]);
-    } else {
-        assert_eq!(cells[ap - 2], Some(BigInt::from(/* test_count */ 48)), "Update test count.");
-    }
+    let runner =
+        SierraCasmRunner::new((*sierra_program).clone(), false).expect("Failed setting up runner.");
+    let result =
+        runner.run_function(/* find first */ "", &[], &None).expect("Failed running the function.");
+    assert_eq!(result.value, RunResultValue::Success(vec![/* test_count */ BigInt::from(48)]));
 }
