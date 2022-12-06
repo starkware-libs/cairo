6ee4ab3768bcf4b7ebcfb2c0c7fd0c9fe4a7999b
orizi <104711814+orizi@users.noreply.github.com>
2022-12-06 10:21:08 +0100

    Extracted runner code into a runner lib - using it in tests as well. (#1194)

--- tests/cairo_level_tests.rs
+++ tests/cairo_level_tests.rs
@@ -2,13 +2,9 @@
 
 use std::path::PathBuf;
 
-use casm::run::run_function;
 use compiler::{compile_cairo_project_at_path, CompilerConfig};
 use num_bigint::BigInt;
-
-use crate::common::get_runnable_casm;
-
-mod common;
+use runner::{RunResultValue, SierraCasmRunner};
 
 #[test]
 fn cairo_level_tests() {
@@ -20,13 +16,9 @@ fn cairo_level_tests() {
         CompilerConfig { replace_ids: true, ..CompilerConfig::default() },
     )
     .expect("Compilation failed.");
-    let instructions = get_runnable_casm(&sierra_program, &[], false);
-    let (cells, ap) = run_function(instructions).unwrap();
-    if *cells[ap - 3].as_ref().unwrap() == BigInt::from(1) {
-        let err_data_start = usize::try_from(cells[ap - 2].as_ref().unwrap()).unwrap();
-        let err_data_end = usize::try_from(cells[ap - 1].as_ref().unwrap()).unwrap();
-        panic!("Cairo run paniced. Error data is: {:?}", &cells[err_data_start..err_data_end]);
-    } else {
-        assert_eq!(cells[ap - 2], Some(BigInt::from(/* test_count */ 48)), "Update test count.");
-    }
+    let runner =
+        SierraCasmRunner::new((*sierra_program).clone(), false).expect("Failed setting up runner.");
+    let result =
+        runner.run_function(/* find first */ "", &[], &None).expect("Failed running the function.");
+    assert_eq!(result.value, RunResultValue::Success(vec![/* test_count */ BigInt::from(48)]));
 }
