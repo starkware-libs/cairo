51cdda7cef65edface18171d6923f7f234b57135
ilyalesokhin-starkware <ilya@starkware.co>
2022-12-06 11:11:52 +0200

    Add try_unpack_deref_with_offset and use it in build_pedersen_hash. (#1219)

--- crates/sierra_to_casm/src/invocations/pedersen.rs
+++ crates/sierra_to_casm/src/invocations/pedersen.rs
@@ -1,11 +1,18 @@
+#[cfg(test)]
+#[path = "pedersen_test.rs"]
+mod test;
+
 use casm::casm;
 use casm::operand::DerefOrImmediate;
+use num_bigint::BigInt;
+use num_traits::FromPrimitive;
 use sierra::extensions::felt::FeltOperator;
 use sierra::extensions::pedersen::PedersenConcreteLibFunc;
 
 use super::{CompiledInvocation, CompiledInvocationBuilder, InvocationError};
 use crate::references::{
-    try_unpack_deref, BinOpExpression, CellExpression, ReferenceExpression, ReferenceValue,
+    try_unpack_deref, try_unpack_deref_with_offset, BinOpExpression, CellExpression,
+    ReferenceExpression, ReferenceValue,
 };
 
 /// Builds instructions for Sierra array operations.
@@ -22,14 +29,16 @@ pub fn build(
 fn build_pedersen_hash(
     builder: CompiledInvocationBuilder<'_>,
 ) -> Result<CompiledInvocation, InvocationError> {
-    let (pedersen, x, y) = match builder.refs {
+    let ((pedersen_base, pedersen_offset), x, y) = match builder.refs {
         [
             ReferenceValue { expression: expr_pedersen, .. },
             ReferenceValue { expression: expr_x, .. },
             ReferenceValue { expression: expr_y, .. },
-        ] => {
-            (try_unpack_deref(expr_pedersen)?, try_unpack_deref(expr_x)?, try_unpack_deref(expr_y)?)
-        }
+        ] => (
+            try_unpack_deref_with_offset(expr_pedersen)?,
+            try_unpack_deref(expr_x)?,
+            try_unpack_deref(expr_y)?,
+        ),
         refs => {
             return Err(InvocationError::WrongNumberOfArguments {
                 expected: 3,
@@ -38,20 +47,26 @@ fn build_pedersen_hash(
         }
     };
 
+    if pedersen_offset > i16::MAX - 2 {
+        return Err(InvocationError::InvalidReferenceExpressionForArgument);
+    }
+
     let instructions = casm! {
-        x = [[pedersen]];
-        y = [[pedersen] + 1];
+        x = [[pedersen_base] + pedersen_offset];
+        y = [[pedersen_base] + (pedersen_offset + 1)];
     }
     .instructions;
     let output_expressions = [vec![
         ReferenceExpression {
             cells: vec![CellExpression::BinOp(BinOpExpression {
                 op: FeltOperator::Add,
-                a: pedersen,
-                b: DerefOrImmediate::Immediate(3.into()),
+                a: pedersen_base,
+                b: DerefOrImmediate::Immediate(BigInt::from_i16(pedersen_offset).unwrap() + 3),
             })],
         },
-        ReferenceExpression { cells: vec![CellExpression::DoubleDeref(pedersen, 2)] },
+        ReferenceExpression {
+            cells: vec![CellExpression::DoubleDeref(pedersen_base, pedersen_offset + 2)],
+        },
     ]
     .into_iter()]
     .into_iter();
