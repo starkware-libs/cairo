cda60226ec0fc4e32bd6ceb2b8d8307d9b9b7819
Lior Goldberg <lior@starkware.co>
2022-12-10 16:00:51 +0200

    Iterate over all builtins in *_get_gas().
    commit-id:a459ad90

--- crates/sierra_to_casm/src/invocations/builtin_cost.rs
+++ crates/sierra_to_casm/src/invocations/builtin_cost.rs
@@ -1,8 +1,8 @@
 use casm::ap_change::ApplyApChange;
-use casm::casm;
 use casm::operand::{CellRef, DerefOrImmediate, Register};
+use casm::{casm, casm_extend};
 use itertools::chain;
-use sierra::extensions::builtin_cost::{BuiltinCostConcreteLibFunc, BuiltinGetGasConcreteLibFunc};
+use sierra::extensions::builtin_cost::{BuiltinCostConcreteLibFunc, CostTokenType};
 use sierra::extensions::felt::FeltBinaryOperator;
 use sierra::program::{BranchInfo, BranchTarget};
 use utils::try_extract_matches;
@@ -17,25 +17,15 @@ pub fn build(
     builder: CompiledInvocationBuilder<'_>,
 ) -> Result<CompiledInvocation, InvocationError> {
     match libfunc {
-        BuiltinCostConcreteLibFunc::BuiltinGetGas(libfunc) => {
-            build_builtin_get_gas(libfunc, builder)
-        }
+        BuiltinCostConcreteLibFunc::BuiltinGetGas(_) => build_builtin_get_gas(builder),
     }
 }
 
 /// Handles the get gas invocation.
 fn build_builtin_get_gas(
-    libfunc: &BuiltinGetGasConcreteLibFunc,
     builder: CompiledInvocationBuilder<'_>,
 ) -> Result<CompiledInvocation, InvocationError> {
     // TODO(lior): Share code with get_gas().
-    let requested_count = builder
-        .program_info
-        .metadata
-        .gas_info
-        .variable_values
-        .get(&(builder.idx, libfunc.token_type))
-        .ok_or(InvocationError::UnknownVariableData)?;
     let (range_check_expression, gas_counter_expression, builtin_cost_expression) =
         match builder.refs {
             [
@@ -50,27 +40,6 @@ fn build_builtin_get_gas(
                 });
             }
         };
-    let gas_counter_value = try_extract_matches!(
-        gas_counter_expression
-            .try_unpack_single()
-            .map_err(|_| InvocationError::InvalidReferenceExpressionForArgument)?,
-        CellExpression::Deref
-    )
-    .ok_or(InvocationError::InvalidReferenceExpressionForArgument)?;
-    let range_check = try_extract_matches!(
-        range_check_expression
-            .try_unpack_single()
-            .map_err(|_| InvocationError::InvalidReferenceExpressionForArgument)?,
-        CellExpression::Deref
-    )
-    .ok_or(InvocationError::InvalidReferenceExpressionForArgument)?;
-    let builtin_cost = try_extract_matches!(
-        builtin_cost_expression
-            .try_unpack_single()
-            .map_err(|_| InvocationError::InvalidReferenceExpressionForArgument)?,
-        CellExpression::Deref
-    )
-    .ok_or(InvocationError::InvalidReferenceExpressionForArgument)?;
 
     let failure_handle_statement_id = match builder.invocation.branches.as_slice() {
         [
@@ -80,34 +49,88 @@ fn build_builtin_get_gas(
         _ => panic!("malformed invocation"),
     };
 
+    let builtin_cost = try_extract_matches!(
+        builtin_cost_expression
+            .try_unpack_single()
+            .map_err(|_| InvocationError::InvalidReferenceExpressionForArgument)?,
+        CellExpression::Deref
+    )
+    .ok_or(InvocationError::InvalidReferenceExpressionForArgument)?;
+
+    let variable_values = &builder.program_info.metadata.gas_info.variable_values;
+
+    // Compute the requested amount of gas. Start with [CostTokenType::Step].
+    let requested_steps = *variable_values
+        .get(&(builder.idx, CostTokenType::Step))
+        .ok_or(InvocationError::UnknownVariableData)?;
+    let mut compute_requested_amount = casm! { [ap] = requested_steps, ap++; };
+    let mut compute_requested_amount_ap_change: usize = 1;
+    for token_type in CostTokenType::iter() {
+        if *token_type == CostTokenType::Step {
+            continue;
+        }
+        let requested_count = variable_values
+            .get(&(builder.idx, *token_type))
+            .ok_or(InvocationError::UnknownVariableData)?;
+        let translated_builtin_cost =
+            builtin_cost.unchecked_apply_known_ap_change(compute_requested_amount_ap_change);
+        casm_extend!(
+            compute_requested_amount,
+            // TODO(lior): Fetch the cost of each builtin separately.
+            [ap] = translated_builtin_cost * (*requested_count), ap++;
+            [ap] = [ap - 2] + [ap - 1], ap++;
+        );
+        compute_requested_amount_ap_change += 2;
+    }
+
+    let gas_counter_value = try_extract_matches!(
+        gas_counter_expression
+            .try_unpack_single()
+            .map_err(|_| InvocationError::InvalidReferenceExpressionForArgument)?,
+        CellExpression::Deref
+    )
+    .ok_or(InvocationError::InvalidReferenceExpressionForArgument)?
+    .unchecked_apply_known_ap_change(compute_requested_amount_ap_change);
+    let range_check = try_extract_matches!(
+        range_check_expression
+            .try_unpack_single()
+            .map_err(|_| InvocationError::InvalidReferenceExpressionForArgument)?,
+        CellExpression::Deref
+    )
+    .ok_or(InvocationError::InvalidReferenceExpressionForArgument)?
+    .unchecked_apply_known_ap_change(compute_requested_amount_ap_change);
+
     // The code up to the success branch.
     let mut before_success_branch = casm! {
-        // Compute the requested amount of gas.
-        [ap + 0] = builtin_cost * (*requested_count), ap++;
         // Non-deterministically check if there is enough gas.
         %{
-            memory[ap + 0] = memory[ap - 1] <=
-                memory (gas_counter_value.unchecked_apply_known_ap_change(1))
+            memory[ap + 0] = memory[ap - 1] <= memory gas_counter_value
         %}
         jmp rel 0 if [ap + 0] != 0, ap++;
 
         // In this case amount > gas_counter_value, so amount - gas_counter_value - 1 >= 0.
-        [ap - 2] = [ap + 0] + (gas_counter_value.unchecked_apply_known_ap_change(2)), ap++;
+        [ap - 2] = [ap + 0] + (gas_counter_value.unchecked_apply_known_ap_change(1)), ap++;
         [ap + 0] = [ap - 1] + (-1), ap++;
-        [ap - 1] = [[range_check.unchecked_apply_known_ap_change(4)]];
+        [ap - 1] = [[range_check.unchecked_apply_known_ap_change(3)]];
 
         jmp rel 0; // Fixed in relocations.
     };
-    patch_jnz_to_end(&mut before_success_branch, 1);
-    let relocation_index = before_success_branch.instructions.len() - 1;
+    patch_jnz_to_end(&mut before_success_branch, 0);
+    let relocation_index =
+        compute_requested_amount.instructions.len() + before_success_branch.instructions.len() - 1;
     let success_branch = casm! {
        // Compute the remaining gas and check that it is nonnegative.
-       (gas_counter_value.unchecked_apply_known_ap_change(2)) = [ap + 0] + [ap - 2], ap++;
-       [ap - 1] = [[range_check.unchecked_apply_known_ap_change(3)]];
+       (gas_counter_value.unchecked_apply_known_ap_change(1)) = [ap + 0] + [ap - 2], ap++;
+       [ap - 1] = [[range_check.unchecked_apply_known_ap_change(2)]];
     };
 
     Ok(builder.build(
-        chain!(before_success_branch.instructions, success_branch.instructions).collect(),
+        chain!(
+            compute_requested_amount.instructions,
+            before_success_branch.instructions,
+            success_branch.instructions
+        )
+        .collect(),
         vec![RelocationEntry {
             instruction_idx: relocation_index,
             relocation: Relocation::RelativeStatementId(*failure_handle_statement_id),
@@ -116,7 +139,7 @@ fn build_builtin_get_gas(
             vec![
                 ReferenceExpression::from_cell(CellExpression::BinOp(BinOpExpression {
                     op: FeltBinaryOperator::Add,
-                    a: range_check.unchecked_apply_known_ap_change(3),
+                    a: range_check.unchecked_apply_known_ap_change(2),
                     b: DerefOrImmediate::from(1),
                 })),
                 ReferenceExpression::from_cell(CellExpression::Deref(CellRef {
@@ -128,11 +151,11 @@ fn build_builtin_get_gas(
             vec![
                 ReferenceExpression::from_cell(CellExpression::BinOp(BinOpExpression {
                     op: FeltBinaryOperator::Add,
-                    a: range_check.unchecked_apply_known_ap_change(4),
+                    a: range_check.unchecked_apply_known_ap_change(3),
                     b: DerefOrImmediate::from(1),
                 })),
                 ReferenceExpression::from_cell(CellExpression::Deref(
-                    gas_counter_value.unchecked_apply_known_ap_change(4),
+                    gas_counter_value.unchecked_apply_known_ap_change(3),
                 )),
             ]
             .into_iter(),
