835939945d03a2189c2a7292dfb12ea8283f3aa9
ilyalesokhin-starkware <ilya@starkware.co>
2022-12-11 10:40:24 +0200

    Better error message on return type mismatch. (#1276)

--- crates/sierra_generator/src/local_variables.rs
+++ crates/sierra_generator/src/local_variables.rs
@@ -250,7 +250,16 @@ fn handle_function_call(
             *known_ap_change = false;
         }
     }
-    state.register_outputs(inputs, outputs, &libfunc_signature.branch_signatures[0].vars);
+
+    let vars = &libfunc_signature.branch_signatures[0].vars;
+    assert_eq!(
+        outputs.len(),
+        vars.len(),
+        "Wrong number of outputs for '{}'. The 'extern' declaration of the libfunc does not match \
+         the Sierra definition.",
+        DebugReplacer { db }.replace_libfunc_id(&concrete_function_id)
+    );
+    state.register_outputs(inputs, outputs, vars);
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
