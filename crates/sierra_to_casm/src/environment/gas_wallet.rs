d20fcddd29c8802adb75f7885ce536906a484df1
liorgold2 <38202661+liorgold2@users.noreply.github.com>
2022-12-12 16:47:33 +0200

    Track all token types in sierra_to_casm (GasWallet). (#1290)

--- crates/sierra_to_casm/src/environment/gas_wallet.rs
+++ crates/sierra_to_casm/src/environment/gas_wallet.rs
@@ -1,32 +1,51 @@
 use std::fmt::Display;
 
+use sierra::extensions::builtin_cost::CostTokenType;
 use thiserror::Error;
+use utils::collection_arithmetics::add_maps;
+use utils::ordered_hash_map::OrderedHashMap;
 
 #[derive(Error, Debug, Eq, PartialEq)]
 pub enum GasWalletError {
-    #[error("Ran out of gas in the wallet, requested {request} while state is {state}.")]
-    OutOfGas { state: GasWallet, request: i64 },
+    #[error(
+        "Ran out of gas ({token_type:?}) in the wallet, requested {request:?} while state is \
+         {state}."
+    )]
+    OutOfGas {
+        state: GasWallet,
+        request: Box<OrderedHashMap<CostTokenType, i64>>,
+        token_type: CostTokenType,
+    },
 }
 
 /// Enviroment tracking the amount of gas available in a statement's context.
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum GasWallet {
     /// A known value.
-    Value(i64),
+    Value(OrderedHashMap<CostTokenType, i64>),
     /// If gas tracking is disabled, this value should be used for all the statements.
     Disabled,
 }
 impl GasWallet {
     /// Updates the value in the wallet by `request`. Can be both negative (for most libfuncs) and
     /// positive (for gas acquisition libfuncs).
-    pub fn update(&self, request: i64) -> Result<Self, GasWalletError> {
+    pub fn update(
+        &self,
+        request: OrderedHashMap<CostTokenType, i64>,
+    ) -> Result<Self, GasWalletError> {
         match &self {
             Self::Value(existing) => {
-                if existing + request >= 0 {
-                    Ok(GasWallet::Value(existing + request))
-                } else {
-                    Err(GasWalletError::OutOfGas { state: *self, request })
+                let new_value = add_maps(existing.clone(), request.clone());
+                for (token_type, val) in new_value.iter() {
+                    if *val < 0 {
+                        return Err(GasWalletError::OutOfGas {
+                            state: self.clone(),
+                            request: Box::new(request),
+                            token_type: *token_type,
+                        });
+                    }
                 }
+                Ok(GasWallet::Value(new_value))
             }
             Self::Disabled => Ok(Self::Disabled),
         }
@@ -36,7 +55,7 @@ impl GasWallet {
 impl Display for GasWallet {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Value(change) => write!(f, "GasWallet::Value({})", change),
+            Self::Value(value) => write!(f, "GasWallet::Value({:?})", value),
             Self::Disabled => write!(f, "GasWallet::Disabled"),
         }
     }
