bd0c787ffd2d890e842f0547cb983ba64708edec
yuvalsw <105596564+yuvalsw@users.noreply.github.com>
2022-12-13 14:34:00 +0200

    Fixed hidden path-not-found diagnostic (#1266)

--- crates/semantic/src/resolve_path.rs
+++ crates/semantic/src/resolve_path.rs
@@ -361,7 +361,7 @@ impl<'db> Resolver<'db> {
                     .db
                     .module_item_by_name(*module_id, ident)
                     .on_none(|| diagnostics.report(identifier, PathNotFound))?;
-                let generic_item = self.module_item_to_generic_item(module_item)?;
+                let generic_item = self.module_item_to_generic_item(diagnostics, module_item)?;
                 Some(self.specialize_generic_module_item(
                     diagnostics,
                     identifier,
@@ -471,7 +471,7 @@ impl<'db> Resolver<'db> {
                     .db
                     .module_item_by_name(*module_id, ident)
                     .on_none(|| diagnostics.report(identifier, PathNotFound))?;
-                self.module_item_to_generic_item(module_item)
+                self.module_item_to_generic_item(diagnostics, module_item)
             }
             ResolvedGenericItem::GenericType(GenericTypeId::Enum(enum_id)) => {
                 let variants = self
@@ -499,15 +499,17 @@ impl<'db> Resolver<'db> {
     /// Wraps a ModuleItem with the corresponding ResolveGenericItem.
     fn module_item_to_generic_item(
         &mut self,
+        diagnostics: &mut SemanticDiagnostics,
         module_item: ModuleItemId,
     ) -> Option<ResolvedGenericItem> {
         Some(match module_item {
             ModuleItemId::Submodule(id) => ResolvedGenericItem::Module(ModuleId::Submodule(id)),
             ModuleItemId::Use(id) => {
-                // TODO(spapini): Right now we call priv_use_semantic_data() directly for cycle
-                // handling. Otherwise, we need to handle cycle both on it and on the selector
-                // use_resolved_item(). Fix this,
-                self.db.priv_use_semantic_data(id)?.resolved_item?
+                // TODO(spapini): Before the last change, we called priv_use_semantic_data()
+                // directly for cycle handling. Do we need to handle cycle both on
+                // it and on the selector use_resolved_item() now?
+                diagnostics.diagnostics.extend(self.db.use_semantic_diagnostics(id));
+                self.db.use_resolved_item(id)?
             }
             ModuleItemId::FreeFunction(id) => {
                 ResolvedGenericItem::GenericFunction(GenericFunctionId::Free(id))
