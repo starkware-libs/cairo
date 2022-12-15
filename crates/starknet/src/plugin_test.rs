e3c927de449cd3e5e942aee1a0a69b3647e7a648
ilyalesokhin-starkware <ilya@starkware.co>
2022-12-15 11:02:34 +0200

    Add diagnostics to plugin test. (#1329)

--- crates/starknet/src/plugin_test.rs
+++ crates/starknet/src/plugin_test.rs
@@ -1,7 +1,9 @@
 use defs::db::{MacroPlugin, PluginResult};
+use diagnostics::{format_diagnostics, DiagnosticLocation};
 use parser::parser_test;
 use parser::test_utils::create_virtual_file;
 use parser::utils::{get_syntax_file_and_diagnostics, SimpleParserDatabase};
+use syntax::node::TypedSyntaxNode;
 use utils::ordered_hash_map::OrderedHashMap;
 
 use crate::plugin::StarkNetPlugin;
@@ -12,14 +14,24 @@ pub fn test_expand_contract(
 ) -> OrderedHashMap<String, String> {
     let cairo_code = &inputs["cairo_code"];
     let file_id = create_virtual_file(db, "dummy_file.cairo", cairo_code);
+
     let (syntax_file, diagnostics) = get_syntax_file_and_diagnostics(db, file_id, cairo_code);
     assert_eq!(diagnostics.format(db), "");
-
+    let file_syntax_node = syntax_file.as_syntax_node();
     let plugin = StarkNetPlugin {};
     let mut generated_items: Vec<String> = Vec::new();
+    let mut diagnostic_items: Vec<String> = Vec::new();
     for item in syntax_file.items(db).elements(db).into_iter() {
         let PluginResult { code, diagnostics } = plugin.generate_code(db, item);
-        assert_eq!(diagnostics.len(), 0);
+
+        diagnostic_items.extend(diagnostics.iter().map(|diag| {
+            let syntax_node = file_syntax_node.lookup_ptr(db, diag.stable_ptr);
+
+            let location =
+                DiagnosticLocation { file_id, span: syntax_node.span_without_trivia(db) };
+            format_diagnostics(db, &diag.message, location)
+        }));
+
         let content = match code {
             Some((_path, content)) => content,
             None => continue,
@@ -27,7 +39,11 @@ pub fn test_expand_contract(
         generated_items.push(formatter::format_string(db, content));
     }
 
-    OrderedHashMap::from([("generated_cairo_code".into(), generated_items.join("\n"))])
+    OrderedHashMap::from([
+        ("generated_cairo_code".into(), generated_items.join("\n")),
+        ("expected_diagnostics".into(), diagnostic_items.join("\n")),
+    ])
 }
 
-parser_test!(test1, ["src/plugin_test_data/starknet",], test_expand_contract);
+parser_test!(test_valid_expansion, ["src/plugin_test_data/starknet",], test_expand_contract);
+parser_test!(test_diagnostics, ["src/plugin_test_data/diagnostics",], test_expand_contract);
