76c7e29bad9c82bdc00ea585654e50408c6c366d
Ori Ziv <oriz@starkware.co>
2022-12-19 09:35:59 +0200

    Refactored write test, and added read test.
    fixed inline to support, and made util print the display instead of
    debug version of the casm.
    
    commit-id:816de35f

--- crates/sierra_to_casm/src/invocations/starknet/storage_test.rs
+++ crates/sierra_to_casm/src/invocations/starknet/storage_test.rs
@@ -1,10 +1,38 @@
-use itertools::Itertools;
+use casm::ap_change::ApChange;
+use casm::casm;
+use pretty_assertions::assert_eq;
+use sierra::program::StatementIdx;
 
-use crate::invocations::test_utils::compile_libfunc;
+use crate::invocations::test_utils::{
+    compile_libfunc, ReducedBranchChanges, ReducedCompiledInvocation,
+};
 use crate::ref_expr;
+use crate::relocations::{Relocation, RelocationEntry};
 
-// TODO(yuval): add a test for storage_read.
 // TODO(yuval): move tests to test infrastructure.
+
+#[test]
+fn test_storage_read() {
+    assert_eq!(
+        compile_libfunc("storage_read_syscall", vec![ref_expr!([fp + 1]), ref_expr!([ap + 5])],),
+        ReducedCompiledInvocation {
+            instructions: casm! {
+                [ap + 0] = 31066245855454734213960397939u128, ap++;
+                [ap + -1] = [[fp + 1] + 0];
+                // TODO(orizi): Fix wrong AP change.
+                [ap + 5] = [[fp + 1] + 1];
+                %{ syscall_handler.syscall(segments=segments, syscall_ptr=[fp + 1] + 0) %}
+                [ap + 0] = [[fp + 1] + 2], ap++;
+            }
+            .instructions,
+            relocations: vec![],
+            results: vec![ReducedBranchChanges {
+                refs: vec![ref_expr!([fp + 1] + 3), ref_expr!([ap - 1])],
+                ap_change: ApChange::Known(2)
+            }]
+        }
+    );
+}
 
 #[test]
 fn test_storage_write() {
@@ -17,19 +45,37 @@ fn test_storage_write() {
                 ref_expr!([ap + 5]),
                 ref_expr!([ap + 6])
             ],
-        )
-        .instructions
-        .iter()
-        .map(|y| y.to_string())
-        .join("\n"),
-        indoc::indoc! {"
-            [ap + 0] = 8038072152842849266968829064307, ap++
-            [ap + -1] = [[fp + 2] + 0]
-            [fp + 1] = [[fp + 2] + 1]
-            [ap + 4] = [[fp + 2] + 2]
-            [ap + 5] = [[fp + 2] + 3]
-            %{ syscall_handler.syscall(segments=segments, syscall_ptr=[fp + 2] + 0) %}
-            [ap + 0] = [[fp + 2] + 5], ap++
-            jmp rel 0 if [ap + -1] != 0"}
+        ),
+        ReducedCompiledInvocation {
+            instructions: casm! {
+                [ap + 0] = 8038072152842849266968829064307u128, ap++;
+                [ap + -1] = [[fp + 2] + 0];
+                [fp + 1] = [[fp + 2] + 1];
+                [ap + 4] = [[fp + 2] + 2];
+                [ap + 5] = [[fp + 2] + 3];
+                %{ syscall_handler.syscall(segments=segments, syscall_ptr=[fp + 2] + 0) %}
+                [ap + 0] = [[fp + 2] + 5], ap++;
+                jmp rel 0 if [ap + -1] != 0;
+            }
+            .instructions,
+            relocations: vec![RelocationEntry {
+                instruction_idx: 6,
+                relocation: Relocation::RelativeStatementId(StatementIdx(1,),),
+            },],
+            results: vec![
+                ReducedBranchChanges {
+                    refs: vec![ref_expr!([[fp + 2] + 4]), ref_expr!([fp + 2] + 7)],
+                    ap_change: ApChange::Known(2)
+                },
+                ReducedBranchChanges {
+                    refs: vec![
+                        ref_expr!([[fp + 2] + 4]),
+                        ref_expr!([fp + 2] + 7),
+                        ref_expr!([ap - 1])
+                    ],
+                    ap_change: ApChange::Known(2)
+                }
+            ]
+        }
     );
 }
