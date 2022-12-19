23dee2bf0b305c5a937584bebade756f4ef2921b
ilyalesokhin-starkware <ilya@starkware.co>
2022-12-19 13:49:18 +0200

    Update contract syntax. (#1359)

--- crates/starknet/src/contract.rs
+++ crates/starknet/src/contract.rs
@@ -1,11 +1,12 @@
-use defs::ids::{LanguageElementId, ModuleItemId, StructId};
+use anyhow::Context;
+use defs::ids::{FreeFunctionId, LanguageElementId, ModuleId, ModuleItemId, SubmoduleId};
+use diagnostics::ToOption;
+use filesystem::ids::CrateId;
 use num_bigint::BigUint;
 use semantic::db::SemanticGroup;
-use semantic::diagnostic::{NotFoundItemType, SemanticDiagnostics};
-use semantic::resolve_path::{ResolvedConcreteItem, Resolver};
-use semantic::ConcreteImplId;
 use sha3::{Digest, Keccak256};
-use syntax::node::{ast, TypedSyntaxNode};
+
+use crate::plugin::{CONTRACT_ATTR, EXTERNAL_MODULE};
 
 #[cfg(test)]
 #[path = "contract_test.rs"]
@@ -13,11 +14,14 @@ mod test;
 
 /// Represents a declaration of a contract.
 pub struct ContractDeclaration {
-    /// The id of the struct that defines the contracts storage.
-    pub struct_id: StructId,
-    /// A list of Expressions that specify the implementations included in the contract.
-    /// See resolve_contract_impls(...) for more detail.
-    pub impls: Vec<syntax::node::ast::Expr>,
+    /// The id of the module that defines the contract.
+    pub submodule_id: SubmoduleId,
+}
+
+impl ContractDeclaration {
+    pub fn module_id(&self) -> ModuleId {
+        ModuleId::Submodule(self.submodule_id)
+    }
 }
 
 /// A variant of eth-keccak that computes a value that fits in a StarkNet field element.
@@ -31,27 +35,22 @@ pub fn starknet_keccak(data: &[u8]) -> BigUint {
     BigUint::from_bytes_be(&result)
 }
 
-/// Finds the structs annotated as contracts in the current compilation units and
+/// Finds the inline modules annotated as contracts in the given crate_ids and
 /// returns the corresponding ContractDeclarations.
-pub fn find_contract_structs(db: &dyn SemanticGroup) -> Vec<ContractDeclaration> {
+pub fn find_contracts(db: &dyn SemanticGroup, crate_ids: &[CrateId]) -> Vec<ContractDeclaration> {
     let mut contracts = vec![];
-    for crate_id in db.crates() {
-        let modules = db.crate_modules(crate_id);
+    for crate_id in crate_ids {
+        let modules = db.crate_modules(*crate_id);
         for module_id in modules.iter() {
-            let Ok(module_items) = db.module_items(*module_id) else {
+            let Ok(submodules) = db.module_submodules(*module_id) else {
                 continue;
             };
 
-            for item in module_items.items.values() {
-                if let ModuleItemId::Struct(struct_id) = item {
-                    if let Ok(attrs) = db.struct_attributes(*struct_id) {
-                        if let [attr] = attrs.as_slice() {
-                            if attr.id == "contract" {
-                                contracts.push(ContractDeclaration {
-                                    struct_id: *struct_id,
-                                    impls: attr.args.clone(),
-                                });
-                            }
+            for module_id in submodules {
+                if let ModuleId::Submodule(submodule_id) = module_id {
+                    if let Ok(attrs) = db.module_attributes(module_id) {
+                        if attrs.iter().any(|attr| attr.id == CONTRACT_ATTR) {
+                            contracts.push(ContractDeclaration { submodule_id });
                         };
                     }
                 }
@@ -61,55 +60,25 @@ pub fn find_contract_structs(db: &dyn SemanticGroup) -> Vec<ContractDeclaration>
     contracts
 }
 
-/// Returns the ConcreteImplId's that should be included in a contract.
-pub fn resolve_contract_impls(
+/// Returns the list of external functions for a given contract.
+pub fn get_external_functions(
     db: &(dyn SemanticGroup + 'static),
     contract: &ContractDeclaration,
-) -> anyhow::Result<Vec<ConcreteImplId>> {
-    let syntax_db = db.upcast();
+) -> anyhow::Result<Vec<FreeFunctionId>> {
+    // The wrappers are currently in the parent module.
+    let parent_module_id = contract.submodule_id.module(db.upcast());
 
-    let module_file_id = contract.struct_id.module_file(db.upcast());
-    let mut diagnostics = SemanticDiagnostics::new(module_file_id);
-
-    let mut resolver =
-        Resolver::new(db, module_file_id, &db.struct_generic_params(contract.struct_id).unwrap());
-
-    let mut impls = vec![];
-
-    // TODO(ilya): Add error locations.
-    for expr in &contract.impls {
-        match expr {
-            ast::Expr::Path(path) => {
-                match resolver.resolve_concrete_path(&mut diagnostics, path, NotFoundItemType::Impl)
-                {
-                    Ok(ResolvedConcreteItem::Impl(concrete_impl_id)) => {
-                        impls.push(concrete_impl_id)
-                    }
-                    Ok(_item) => anyhow::bail!(
-                        "`{}` is not an `impl`.",
-                        path.as_syntax_node().get_text(syntax_db)
-                    ),
-                    Err(_) => {
-                        anyhow::bail!(
-                            "Failed to resolve `{}`.",
-                            path.as_syntax_node().get_text(syntax_db)
-                        )
-                    }
-                }
-            }
-
-            _ => {
-                anyhow::bail!(
-                    "Expected a path, Got `{}`.",
-                    expr.as_syntax_node().get_text(syntax_db)
-                )
-            }
-        }
+    match db
+        .module_items(parent_module_id)
+        .to_option()
+        .with_context(|| "Failed to get module items.")?
+        .items
+        .get(EXTERNAL_MODULE)
+    {
+        Some(ModuleItemId::Submodule(external_module_id)) => Ok(db
+            .module_free_functions(ModuleId::Submodule(*external_module_id))
+            .to_option()
+            .with_context(|| "Failed to get module items.")?),
+        _ => anyhow::bail!("Failed to get the entry points module."),
     }
-    let diag = diagnostics.build();
-    if !diag.get_all().is_empty() {
-        // TODO(ilya): Print diagnostics.
-        anyhow::bail!("Got diagnostics while resolving impl path: {}", diag.format(db.upcast()));
-    }
-    Ok(impls)
 }
--- crates/starknet/src/contract_test.rs
+++ crates/starknet/src/contract_test.rs
@@ -1,8 +1,10 @@
+use filesystem::db::FilesGroup;
 use indoc::indoc;
+use itertools::Itertools;
 use pretty_assertions::assert_eq;
 use semantic::test_utils::{setup_test_crate, SemanticDatabaseForTesting};
 
-use crate::contract::{find_contract_structs, resolve_contract_impls, starknet_keccak};
+use crate::contract::{find_contracts, get_external_functions, starknet_keccak};
 
 #[test]
 fn test_contract_resolving() {
@@ -11,46 +13,32 @@ fn test_contract_resolving() {
     let _crate_id = setup_test_crate(
         db,
         indoc! {"
-            struct NotAContract {}
+            mod NotAContract {}
 
-            trait IERC20 {}
-            impl ERC20Impl of IERC20 {}
+            #[contract]
+            mod ERC20 {
+                func internal_func(ref system: System) -> felt {
+                    1
+                }
+            }
 
-            #[contract(ERC20Impl)]
-            struct ERC20 {}
-
-            #[contract(ERC20)]
-            struct StructInsteadOfImpl {}
-
-            #[contract(1+2)]
-            struct BadImplExpr {}
-
-            #[contract(NoSuchPath)]
-            struct BadImplPath {}
+            mod __external {
+                func ep1() {}
+                func ep2() {}
+            }
         "},
     );
 
-    let contracts = find_contract_structs(db);
-    assert_eq!(contracts.len(), 4);
+    let contracts = find_contracts(db, &db.crates());
+    assert_eq!(contracts.len(), 1);
 
     assert_eq!(
-        format!("{:?}", resolve_contract_impls(db, &contracts[0]).unwrap()),
-        "[ConcreteImplId(0)]"
-    );
-
-    assert_eq!(
-        resolve_contract_impls(db, &contracts[1]).expect_err("").to_string(),
-        "`ERC20` is not an `impl`."
-    );
-
-    assert_eq!(
-        resolve_contract_impls(db, &contracts[2]).expect_err("").to_string(),
-        "Expected a path, Got `1+2`."
-    );
-
-    assert_eq!(
-        resolve_contract_impls(db, &contracts[3]).expect_err("").to_string(),
-        "Failed to resolve `NoSuchPath`."
+        get_external_functions(db, &contracts[0])
+            .unwrap()
+            .into_iter()
+            .map(|func_id| func_id.name(db))
+            .collect_vec(),
+        vec!["ep1", "ep2"]
     );
 }
 
