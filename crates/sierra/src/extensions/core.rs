f1793f2431ae03922f2e4a3b570eb181173b73b0
dorimedini-starkware <76901494+dorimedini-starkware@users.noreply.github.com>
2022-12-20 18:08:13 +0200

    Initial definitions for bitwise builtin (#1394)

--- crates/sierra/src/extensions/core.rs
+++ crates/sierra/src/extensions/core.rs
@@ -1,5 +1,6 @@
 use super::ap_tracking::RevokeApTrackingLibFunc;
 use super::array::{ArrayLibFunc, ArrayType};
+use super::bitwise::{BitwiseLibFunc, BitwiseType};
 use super::boolean::BoolLibFunc;
 use super::branch_align::BranchAlignLibFunc;
 use super::builtin_cost::{BuiltinCostLibFunc, BuiltinCostsType};
@@ -26,6 +27,7 @@ use crate::{define_libfunc_hierarchy, define_type_hierarchy};
 define_type_hierarchy! {
     pub enum CoreType {
         Array(ArrayType),
+        Bitwise(BitwiseType),
         Box(BoxType),
         Felt(FeltType),
         GasBuiltin(GasBuiltinType),
@@ -47,6 +49,7 @@ define_libfunc_hierarchy! {
     pub enum CoreLibFunc {
         ApTracking(RevokeApTrackingLibFunc),
         Array(ArrayLibFunc),
+        Bitwise(BitwiseLibFunc),
         BranchAlign(BranchAlignLibFunc),
         Bool(BoolLibFunc),
         Box(BoxLibFunc),
