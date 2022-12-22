7fe3d7b1b6da8de00255221ddef3a62652f4a5d3
Yuval Goldberg <yuval@starkware.co>
2022-12-22 20:28:54 +0200

    Added test for diagnostics of inline-inline modules...

--- crates/semantic/src/diagnostic_test.rs
+++ crates/semantic/src/diagnostic_test.rs
@@ -9,10 +9,10 @@ use defs::plugin::{
 use indoc::indoc;
 use pretty_assertions::assert_eq;
 use syntax::node::db::SyntaxGroup;
-use syntax::node::{ast, TypedSyntaxNode};
+use syntax::node::{ast, Terminal, TypedSyntaxNode};
 use test_log::test;
 
-use crate::db::SemanticGroup;
+use crate::db::{get_crate_semantic_diagnostics, SemanticGroup};
 use crate::patcher::{PatchBuilder, Patches};
 use crate::test_utils::{setup_test_crate, test_expr_diagnostics, SemanticDatabaseForTesting};
 use crate::{semantic_test, SemanticDiagnostic};
@@ -68,19 +68,31 @@ impl MacroPlugin for AddInlineModuleDummyPlugin {
         item_ast: syntax::node::ast::Item,
     ) -> PluginResult {
         match item_ast {
-            ast::Item::FreeFunction(func) => {
+            ast::Item::FreeFunction(func)
+                if func
+                    .attributes(db)
+                    .elements(db)
+                    .iter()
+                    .any(|attr| attr.attr(db).text(db) == "test_wrap_with_inner_module") =>
+            {
                 let mut builder = PatchBuilder::default();
                 builder.interpolate_patched(
                     db,
                     indoc! {"
-                        mod inner_mod {{
-                            // Comment.
-                            // Comment.
-                            // Comment.
-                            $func$
-                        }}
-                    "},
-                    [("func".to_string(), func.as_syntax_node())].into(),
+                    mod inner_mod {{
+                        // Comment 1.
+                        // Comment 2.
+                        $func_kw$ $name$ $generic_params$ $signature$ $body$
+                    }}
+                "},
+                    [
+                        ("func_kw".to_string(), func.function_kw(db).as_syntax_node()),
+                        ("name".to_string(), func.name(db).as_syntax_node()),
+                        ("generic_params".to_string(), func.generic_params(db).as_syntax_node()),
+                        ("signature".to_string(), func.signature(db).as_syntax_node()),
+                        ("body".to_string(), func.body(db).as_syntax_node()),
+                    ]
+                    .into(),
                 );
 
                 PluginResult {
@@ -94,7 +106,7 @@ impl MacroPlugin for AddInlineModuleDummyPlugin {
                     diagnostics: vec![],
                 }
             }
-            _ => PluginResult { code: None, diagnostics: vec![] },
+            _ => PluginResult::default(),
         }
     }
 }
@@ -132,6 +144,7 @@ fn test_inline_module_diagnostics() {
         db,
         indoc! {"
             mod a {
+                #[test_wrap_with_inner_module]
                 func bad() -> u128 {
                     return 5;
                 }
@@ -146,7 +159,7 @@ fn test_inline_module_diagnostics() {
         db.module_semantic_diagnostics(*submodule_id).unwrap().format(db),
         indoc! {r#"
             error: Unexpected return type. Expected: "core::integer::u128", found: "core::felt".
-             --> lib.cairo:3:16
+             --> lib.cairo:4:16
                     return 5;
                            ^
 
@@ -154,17 +167,64 @@ fn test_inline_module_diagnostics() {
     );
 
     // Test diagnostics within a generated inline module.
-    let submodule_submodules = db.module_submodules(*submodule_id).unwrap();
-    let subsubmodule_id = submodule_submodules.first().unwrap();
-
     assert_eq!(
-        db.module_semantic_diagnostics(*subsubmodule_id).unwrap().format(db),
+        get_crate_semantic_diagnostics(db, crate_id).format(db),
         indoc! {r#"
+            error: Unexpected return type. Expected: "core::integer::u128", found: "core::felt".
+             --> lib.cairo:4:16
+                    return 5;
+                           ^
+
             error: Plugin diagnostic: Mapped error.
-             --> lib.cairo:3:16
+             --> lib.cairo:4:16
                     return 5;
                            ^
 
             "#},
+    );
+}
+
+#[test]
+fn test_inline_inline_module_diagnostics() {
+    let mut db_val = SemanticDatabaseForTesting::default();
+    let db = &mut db_val;
+    let crate_id = setup_test_crate(
+        db,
+        indoc! {"
+            mod a {
+                func bad_a() -> u128 {
+                    return 1;
+                }
+            }
+            mod b {
+                mod c {
+                    func bad_c() -> u128 {
+                        return 2;
+                    }
+                }
+                mod d {
+                    func foo_d() {
+                    }
+                }
+            }
+            func foo() {
+                b::c::bad_c();
+            }
+       "},
+    );
+
+    assert_eq!(
+        get_crate_semantic_diagnostics(db, crate_id).format(db),
+        indoc! {r#"error: Unexpected return type. Expected: "core::integer::u128", found: "core::felt".
+             --> lib.cairo:3:16
+                    return 1;
+                           ^
+
+            error: Unexpected return type. Expected: "core::integer::u128", found: "core::felt".
+             --> lib.cairo:9:20
+                        return 2;
+                               ^
+
+    "#},
     );
 }
