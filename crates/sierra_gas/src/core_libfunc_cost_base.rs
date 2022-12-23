b5c13f637bffb69e5f6eb1ec82978e00e6ba7a49
liorgold2 <38202661+liorgold2@users.noreply.github.com>
2022-12-23 11:35:24 +0200

    Sierra->casm: Implement null() libfunc. (#1433)

--- crates/sierra_gas/src/core_libfunc_cost_base.rs
+++ crates/sierra_gas/src/core_libfunc_cost_base.rs
@@ -16,6 +16,7 @@ use sierra::extensions::gas::GasConcreteLibFunc::{GetGas, RefundGas};
 use sierra::extensions::mem::MemConcreteLibFunc::{
     AlignTemps, AllocLocal, FinalizeLocals, Rename, StoreLocal, StoreTemp,
 };
+use sierra::extensions::nullable::NullableConcreteLibFunc;
 use sierra::extensions::strct::StructConcreteLibFunc;
 use sierra::extensions::uint128::{IntOperator, Uint128Concrete, Uint128OperationConcreteLibFunc};
 use sierra::program::Function;
@@ -116,7 +117,9 @@ pub fn core_libfunc_cost_base<Ops: CostOperations>(
             ]
         }
         CoreConcreteLibFunc::StarkNet(libfunc) => starknet_libfunc_cost_base(ops, libfunc),
-        CoreConcreteLibFunc::Nullable(_) => unimplemented!("Nullable is not implemented yet."),
+        CoreConcreteLibFunc::Nullable(libfunc) => match libfunc {
+            NullableConcreteLibFunc::Null(_) => vec![ops.const_cost(0)],
+        },
     }
 }
 
