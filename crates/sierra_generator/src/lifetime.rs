1384fdb440d90afbe6cd44a612657a4332d9d00c
liorgold2 <38202661+liorgold2@users.noreply.github.com>
2022-12-27 09:08:39 +0200

    Add lifetime.rs with initial implementation. (#1486)

---
+++ crates/sierra_generator/src/lifetime.rs
@@ -0,0 +1,145 @@
+#[cfg(test)]
+#[path = "lifetime_test.rs"]
+mod test;
+
+use diagnostics::Maybe;
+use lowering::lower::Lowered;
+use lowering::{BlockId, VariableId};
+use utils::ordered_hash_map::OrderedHashMap;
+use utils::unordered_hash_set::UnorderedHashSet;
+
+pub type StatementLocation = (BlockId, usize);
+
+/// Represents the location where a drop statement for a variable should be added.
+// TODO(lior): Remove the following `allow(dead_code)` once all variants are used.
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum DropLocation {
+    BeginningOfBlock(BlockId),
+    PostStatement(StatementLocation),
+}
+
+/// Information returned by [find_variable_lifetime] regarding the lifetime of variables.
+#[derive(Default)]
+pub struct VariableLifetimeResult {
+    /// A map from [VariableId] to the statements where it is used, but not required after.
+    ///
+    /// Note that a variable may be mentioned twice. For example, when it's last used within two
+    /// branches.
+    ///
+    /// StatementLocation may point to a nonexisting statement after the end of the block -
+    /// this means that the last use was in `block.end`.
+    last_use: OrderedHashMap<VariableId, Vec<StatementLocation>>,
+    /// A map from [VariableId] to the statements where it should be dropped.
+    drops: OrderedHashMap<VariableId, Vec<DropLocation>>,
+}
+
+/// Given the lowering of a function, returns lifetime information for all the variables.
+/// See [VariableLifetimeResult].
+// TODO(lior): Remove the following `allow(dead_code)` once this function is used.
+#[allow(dead_code)]
+pub fn find_variable_lifetime(lowered_function: &Lowered) -> Maybe<VariableLifetimeResult> {
+    let mut res = VariableLifetimeResult::default();
+    inner_find_variable_lifetime(
+        lowered_function,
+        lowered_function.root?,
+        &mut VariableLifetimeState::default(),
+        &mut res,
+    );
+    Ok(res)
+}
+
+/// Helper function for [find_variable_lifetime].
+fn inner_find_variable_lifetime(
+    lowered_function: &Lowered,
+    block_id: BlockId,
+    state: &mut VariableLifetimeState,
+    res: &mut VariableLifetimeResult,
+) {
+    let block = &lowered_function.blocks[block_id];
+
+    // Go over the block in reverse order, starting from handling the block end.
+    match &block.end {
+        lowering::BlockEnd::Callsite(vars) | lowering::BlockEnd::Return(vars) => {
+            state.use_variables(vars, (block_id, block.statements.len()), res);
+        }
+        lowering::BlockEnd::Unreachable => {}
+    }
+
+    for (idx, statement) in block.statements.iter().enumerate().rev() {
+        let statement_location = (block_id, idx);
+
+        // Add the new variables from the statement's output.
+        state.handle_new_variables(&statement.outputs(), statement_location, res);
+
+        match statement {
+            lowering::Statement::Literal(_statement_literal) => {
+                todo!();
+            }
+            lowering::Statement::Call(_) => {}
+            lowering::Statement::CallBlock(_statement_call_block) => todo!(),
+            lowering::Statement::MatchExtern(_statement_match_extern) => todo!(),
+            lowering::Statement::MatchEnum(_statement_match_enum) => todo!(),
+            lowering::Statement::StructConstruct(_statement_struct_construct) => todo!(),
+            lowering::Statement::StructDestructure(_statement_struct_destructure) => todo!(),
+            lowering::Statement::EnumConstruct(_statement_enum_construct) => todo!(),
+        }
+
+        // Mark the input variables as required.
+        state.use_variables(&statement.inputs(), statement_location, res);
+    }
+}
+
+/// Helper struct with the state maintained by [inner_find_variable_lifetime].
+#[derive(Clone, Debug)]
+struct VariableLifetimeState {
+    /// A set of all the variables used after the current processed statement.
+    used_variables: UnorderedHashSet<VariableId>,
+}
+impl VariableLifetimeState {
+    fn default() -> Self {
+        VariableLifetimeState { used_variables: UnorderedHashSet::default() }
+    }
+
+    /// Handles new variables in the following cases:
+    ///
+    /// 1. Returned by a simple call.
+    /// 2. Returned by a branching libfunc (called once per branch).
+    fn handle_new_variables(
+        &mut self,
+        var_ids: &[VariableId],
+        statement_location: StatementLocation,
+        res: &mut VariableLifetimeResult,
+    ) {
+        for var_id in var_ids {
+            if !self.used_variables.contains(var_id) {
+                // The variable will not be used, drop it immediately after the statement.
+                let new_drop_location = DropLocation::PostStatement(statement_location);
+                if let Some(drop_locations) = res.drops.get_mut(var_id) {
+                    drop_locations.push(new_drop_location);
+                } else {
+                    res.drops.insert(*var_id, vec![new_drop_location]);
+                }
+            }
+        }
+    }
+
+    /// Mark the given `args` as required.
+    fn use_variables(
+        &mut self,
+        var_ids: &[VariableId],
+        statement_location: StatementLocation,
+        res: &mut VariableLifetimeResult,
+    ) {
+        for var_id in var_ids {
+            if self.used_variables.insert(*var_id) {
+                // This is the last use of the variable.
+                if let Some(statement_locations) = res.last_use.get_mut(var_id) {
+                    statement_locations.push(statement_location);
+                } else {
+                    res.last_use.insert(*var_id, vec![statement_location]);
+                }
+            }
+        }
+    }
+}
---
+++ crates/sierra_generator/src/lifetime_test.rs
@@ -0,0 +1,58 @@
+use debug::DebugWithDb;
+use itertools::Itertools;
+use lowering::db::LoweringGroup;
+use pretty_assertions::assert_eq;
+use semantic::test_utils::setup_test_function;
+use utils::ordered_hash_map::OrderedHashMap;
+
+use super::find_variable_lifetime;
+use crate::test_utils::SierraGenDatabaseForTesting;
+
+test_utils::test_file_test!(
+    variable_lifetime,
+    ["src/lifetime_test_data/simple",],
+    SierraGenDatabaseForTesting,
+    check_variable_lifetime
+);
+
+fn check_variable_lifetime(
+    db: &mut SierraGenDatabaseForTesting,
+    inputs: &OrderedHashMap<String, String>,
+) -> OrderedHashMap<String, String> {
+    // Parse code and create semantic model.
+    let test_function = setup_test_function(
+        db,
+        inputs["function_code"].as_str(),
+        inputs["function_name"].as_str(),
+        inputs["module_code"].as_str(),
+    )
+    .unwrap();
+
+    db.module_lowering_diagnostics(test_function.module_id)
+        .unwrap()
+        .expect_with_db(db, "Unexpected diagnostics.");
+
+    let lowered_function = &*db.free_function_lowered(test_function.function_id).unwrap();
+
+    let lowered_formatter = lowering::fmt::LoweredFormatter { db, lowered: lowered_function };
+    let lowered_str = format!("{:?}", lowered_function.debug(&lowered_formatter));
+
+    let find_variable_lifetime_res = find_variable_lifetime(lowered_function)
+        .expect("find_variable_lifetime failed unexpectedly");
+    let last_use_str = find_variable_lifetime_res
+        .last_use
+        .iter()
+        .map(|(var_id, location)| format!("{:?}: {location:?}", var_id.debug(&lowered_formatter)))
+        .join("\n");
+    let drop_str = find_variable_lifetime_res
+        .drops
+        .iter()
+        .map(|(var_id, location)| format!("{:?}: {location:?}", var_id.debug(&lowered_formatter)))
+        .join("\n");
+
+    OrderedHashMap::from([
+        ("lowering_format".into(), lowered_str),
+        ("last_use".into(), last_use_str),
+        ("drops".into(), drop_str),
+    ])
+}
