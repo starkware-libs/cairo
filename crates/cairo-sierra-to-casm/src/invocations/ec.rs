679386be4de8024e6d5fea8579991499a3fd09dd
Dori Medini <dori@starkware.co>
2022-12-28 15:24:23 +0200

    Extract EC point verification logic
    Signed-off-by: Dori Medini <dori@starkware.co>
    
    commit-id:3f4409e0

--- crates/cairo-sierra-to-casm/src/invocations/ec.rs
+++ crates/cairo-sierra-to-casm/src/invocations/ec.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-use cairo_casm::builder::CasmBuilder;
+use cairo_casm::builder::{CasmBuilder, Var};
 use cairo_casm::casm_build_extend;
 use cairo_casm::operand::ResOperand;
 use cairo_sierra::extensions::ec::EcConcreteLibFunc;
@@ -25,6 +25,21 @@ pub fn build(
     }
 }
 
+/// Extends the CASM builder to include verification that (x,y) is a point on the curve (and *not*
+/// the point at infinity).
+/// Populates the two "output vars" with the expected square of y, and the actual square of y. The
+/// caller may either assert they are equal, or compute the diff and handle it differently.
+fn verify_ec_point(casm_builder: &mut CasmBuilder, x: Var, y: Var, y2: Var, expected_y2: Var) {
+    casm_build_extend! {casm_builder,
+        const beta = (get_beta());
+        assert y2 = y * y;
+        tempvar x2 = x * x;
+        tempvar x3 = x2 * x;
+        tempvar alpha_x_plus_beta = x + beta; // Here we use the fact that Alpha is 1.
+        assert expected_y2 = x3 + alpha_x_plus_beta;
+    };
+}
+
 /// Handles instruction for creating an EC point.
 fn build_ec_point_try_create(
     builder: CompiledInvocationBuilder<'_>,
@@ -39,16 +54,15 @@ fn build_ec_point_try_create(
 
     // Assert (x,y) is on the curve.
     casm_build_extend! {casm_builder,
-        const beta = (get_beta());
-        tempvar y2 = y * y;
-        tempvar x2 = x * x;
-        tempvar x3 = x2 * x;
-        tempvar alpha_x_plus_beta = x + beta; // Here we use the fact that Alpha is 1.
-        tempvar expected_y2 = x3 + alpha_x_plus_beta;
-        tempvar diff;
-        assert y2 = diff + expected_y2;
+        tempvar y2;
+        tempvar expected_y2;
+    };
+    verify_ec_point(&mut casm_builder, x, y, y2, expected_y2);
+    casm_build_extend! {casm_builder,
+        tempvar diff = y2 - expected_y2;
         jump NotOnCurve if diff != 0;
     };
+
     let failure_handle = get_non_fallthrough_statement_id(&builder);
     Ok(builder.build_from_casm_builder(
         casm_builder,
