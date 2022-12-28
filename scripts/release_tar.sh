7a135bdf3218b6047ab26198b757acb2a310c68d
Shahar Papini <spapini@starkware.co>
2022-12-28 15:13:04 +0200

    Create artifacts on CI

---
+++ scripts/release_tar.sh
@@ -0,0 +1,16 @@
+NAMES="cairo-compile cairo-format cairo-language-server cairo-run cairo-test sierra-compile starknet-compile starknet-sierra-compile"
+TARGET=$1
+cargo build --release --target $TARGET
+rm -rf target/$TARGET/cairo
+rm -rf target/$TARGET.tar.gz
+mkdir -p target/$TARGET/cairo
+(
+    cd target/$TARGET
+    mkdir cairo/bin
+    for NAME in $NAMES
+    do
+        cp release/$NAME cairo/bin/
+    done
+    cp -R ../../corelib cairo/
+    tar czvf ../$TARGET.tar.gz cairo
+)
