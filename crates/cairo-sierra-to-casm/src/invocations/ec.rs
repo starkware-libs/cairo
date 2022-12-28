cf185bf1881160363153325b226bf7b21bf40703
Dori Medini <dori@starkware.co>
2022-12-28 16:00:49 +0200

    Implement ec_init_state libfunc
    Signed-off-by: Dori Medini <dori@starkware.co>
    
    commit-id:bffd4d5b

--- crates/cairo-sierra-to-casm/src/invocations/ec.rs
+++ crates/cairo-sierra-to-casm/src/invocations/ec.rs
@@ -22,6 +22,7 @@ pub fn build(
 ) -> Result<CompiledInvocation, InvocationError> {
     match libfunc {
         EcConcreteLibFunc::CreatePoint(_) => build_ec_point_try_create(builder),
+        EcConcreteLibFunc::InitState(_) => build_ec_init_state(builder),
     }
 }
 
@@ -37,6 +38,88 @@ fn verify_ec_point(casm_builder: &mut CasmBuilder, x: Var, y: Var, y2: Var, expe
         tempvar x3 = x2 * x;
         tempvar alpha_x_plus_beta = x + beta; // Here we use the fact that Alpha is 1.
         assert expected_y2 = x3 + alpha_x_plus_beta;
+    };
+}
+
+/// Extends the CASM builder to compute the sum of two EC points and store the result in the given
+/// variables.
+fn add_ec_points(
+    casm_builder: &mut CasmBuilder,
+    x0: Var,
+    y0: Var,
+    x1: Var,
+    y1: Var,
+    result_x: Var,
+    result_y: Var,
+) {
+    // Logic taken from ec_safe_add:
+    // https://github.com/starkware-industries/starkware/blob/dev/src/starkware/python/math_utils.py#L165
+    casm_build_extend! {casm_builder,
+        const zero = BigInt::from(0);
+        const one = BigInt::from(1);
+        const two = BigInt::from(2);
+        const three = BigInt::from(3);
+        // If either point is the point at infinity, return the other point.
+        jump Point0IsNotInfinity if x0 != 0;
+        jump Point0IsNotInfinity if y0 != 0;
+        assert result_x = x1;
+        assert result_y = y1;
+        jump Done;
+        Point0IsNotInfinity:
+        jump Point1IsNotInfinity if x1 != 0;
+        jump Point1IsNotInfinity if y1 != 0;
+        assert result_x = x0;
+        assert result_y = y0;
+        jump Done;
+        Point1IsNotInfinity:
+        // If the X coordinate is the same, either the points are equal or their sum is the point at
+        // infinity.
+        tempvar diff_x = x0 - x1;
+        jump NotSameX if diff_x != 0;
+        // X coordinate is identical. If y0 + y1 is zero, the sum is the point at infinity.
+        tempvar sum_y = y0 + y1;
+        jump ReturnDouble if sum_y != 0;
+        // If we are here, the sum of the points is the point at infinity.
+        assert result_x = zero;
+        assert result_y = zero;
+        jump Done;
+        ReturnDouble:
+        // The `X` coordinates are the same, which means `y0` is either equal to `y1` or equal to
+        // `-y1`. As `y0 + y1` is not 0, we must have `y0 == y1`. In addition, we know the `Y`
+        // coordinate is non-zero (so we can divide by `2 * Y`).
+        // Compute the "slope": `(3 * X * X + ALPHA) / (2 * Y)`, and use the slope to compute the
+        // doubled point:
+        // `result_x = slope * slope - 2 * X`
+        // `result_y = slope * (X - result_x) - Y`
+        tempvar x2 = x0 * x0;
+        tempvar x2_times_3 = three * x2;
+        tempvar numerator = x2_times_3 + one; // Alpha is 1 on our curve.
+        tempvar denominator = two * y0;
+        tempvar slope;
+        assert numerator = slope * denominator;
+        tempvar slope2 = slope * slope;
+        tempvar x_times_2 = two * x0;
+        assert x_times_2 = result_x + slope2;
+        tempvar x_minus_result_x = x0 - result_x;
+        tempvar x_minus_result_x_times_slope = x_minus_result_x * slope;
+        assert x_minus_result_x_times_slope = result_y + y0;
+        jump Done;
+        NotSameX:
+        // If we are here, then `p0 != p1` and `p0 != -p1`. Compute the "slope"
+        // (`(y0 - y1) / (x0 - x1)`), and use the slope to compute the sum:
+        // `result_x = slope * slope - x0 - x1`
+        // `result_y = slope * (x0 - result_x) - y0`
+        tempvar numerator = y0 - y1;
+        tempvar denominator = x0 - x1;
+        tempvar slope;
+        assert numerator = slope * denominator;
+        tempvar slope2 = slope * slope;
+        tempvar sum_x = x0 + x1;
+        assert slope2 = result_x + sum_x;
+        tempvar x_change = x0 - result_x;
+        tempvar slope_times_x_change = slope * x_change;
+        assert slope_times_x_change = result_y + y0;
+        Done:
     };
 }
 
@@ -67,5 +150,46 @@ fn build_ec_point_try_create(
     Ok(builder.build_from_casm_builder(
         casm_builder,
         [("Fallthrough", &[&[x, y]], None), ("NotOnCurve", &[], Some(failure_handle))],
+    ))
+}
+
+/// Handles instruction for initializing an EC state from an EC point.
+fn build_ec_init_state(
+    builder: CompiledInvocationBuilder<'_>,
+) -> Result<CompiledInvocation, InvocationError> {
+    let [expr_point] = builder.try_get_refs()?;
+    let [input_x, input_y] = expr_point.try_unpack()?;
+
+    let mut casm_builder = CasmBuilder::default();
+    let input_x = casm_builder.add_var(ResOperand::Deref(input_x.to_deref()?));
+    let input_y = casm_builder.add_var(ResOperand::Deref(input_y.to_deref()?));
+
+    // Sample a random point on the curve.
+    casm_build_extend! {casm_builder,
+        tempvar random_x;
+        tempvar random_y;
+        hint RandomEcPoint {} into { x: random_x, y: random_y };
+        // Assert the random point is on the curve.
+        tempvar y2;
+        tempvar expected_y2;
+    }
+    verify_ec_point(&mut casm_builder, random_x, random_y, y2, expected_y2);
+    casm_build_extend! {casm_builder,
+        assert y2 = expected_y2;
+        // Create a pointer to the random EC point to return as part of the state.
+        tempvar random_ptr;
+        hint AllocSegment {} into {dst: random_ptr};
+        assert random_x = random_ptr[0];
+        assert random_y = random_ptr[1];
+        // Allocate cells for the point sum and compute it.
+        tempvar result_x;
+        tempvar result_y;
+    };
+    add_ec_points(&mut casm_builder, input_x, input_y, random_x, random_y, result_x, result_y);
+
+    // The third entry in the EC state is a pointer to the originally sampled random EC point.
+    Ok(builder.build_from_casm_builder(
+        casm_builder,
+        [("Fallthrough", &[&[result_x, result_y, random_ptr]], None)],
     ))
 }
