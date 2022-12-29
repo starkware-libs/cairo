e035dbfb71692724f9ce841f3b91a976a8573a86
ZuphitF <zuphit@starkware.co>
2022-12-29 19:11:43 +0200

    add explicit boolean xor

--- crates/cairo-sierra-ap-change/src/core_libfunc_ap_change.rs
+++ crates/cairo-sierra-ap-change/src/core_libfunc_ap_change.rs
@@ -33,6 +33,7 @@ pub fn core_libfunc_ap_change(libfunc: &CoreConcreteLibfunc) -> Vec<ApChange> {
         CoreConcreteLibfunc::Bool(libfunc) => match libfunc {
             BoolConcreteLibfunc::And(_) => vec![ApChange::Known(0)],
             BoolConcreteLibfunc::Not(_) => vec![ApChange::Known(1)],
+            BoolConcreteLibfunc::Xor(_) => vec![ApChange::Known(1)],
         },
         CoreConcreteLibfunc::Box(libfunc) => match libfunc {
             BoxConcreteLibfunc::Into(_) => vec![ApChange::Known(1)],
