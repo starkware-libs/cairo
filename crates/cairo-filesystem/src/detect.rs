1fc25d73c8ecf8c2cf4378349d9d8f4b1312508c
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-01-02 02:15:42 +0700

    Check current file path parents for corelib
    #1595 Find `corelib` in current crate path (from file path)

--- crates/cairo-filesystem/src/detect.rs
+++ crates/cairo-filesystem/src/detect.rs
@@ -1,35 +1,38 @@
 use std::path::PathBuf;
 
+macro_rules! check_parents_for_corelib {
+    ($dir:ident) => {
+        $dir.pop();
+        $dir.push("corelib");
+        if $dir.exists() {
+            return $dir;
+        }
+        $dir.pop();
+        $dir.pop();
+        $dir.push("corelib");
+        if $dir.exists() {
+            return $dir;
+        }
+    };
+}
+
 pub fn detect_corelib() -> PathBuf {
     if let Ok(cargo_dir) = std::env::var("CARGO_MANIFEST_DIR") {
         // This is the directory of Cargo.toml of the current crate.
         // This is used for developement of the compiler.
         let mut dir = PathBuf::from(cargo_dir);
-        dir.pop();
-        dir.push("corelib");
-        if dir.exists() {
-            return dir;
-        }
-        dir.pop();
-        dir.pop();
-        dir.push("corelib");
-        if dir.exists() {
-            return dir;
-        }
+        check_parents_for_corelib!(dir);
     }
     if let Ok(mut dir) = std::env::current_exe() {
         dir.pop();
-        dir.pop();
-        dir.push("corelib");
-        if dir.exists() {
-            return dir;
-        }
-        dir.pop();
-        dir.pop();
-        dir.push("corelib");
-        if dir.exists() {
-            return dir;
-        }
+        check_parents_for_corelib!(dir);
     }
+    let this_file = file!();
+    // This is the directory of this file.
+    let mut dir = PathBuf::from(this_file);
+    dir.pop(); // Path to src
+    dir.pop(); // Path to this crate
+    check_parents_for_corelib!(dir);
+
     panic!("Corelib not found.")
 }
