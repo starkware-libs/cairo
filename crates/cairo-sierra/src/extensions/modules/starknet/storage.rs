90625b5f4a42d81dcae50610ef4dd3072dcd78c8
Ilya Lesokhin <ilya@starkware.co>
2023-01-02 11:32:01 +0000

    Update storage format.
    Added address_domain which is reserved for now and must be 0.

--- crates/cairo-sierra/src/extensions/modules/starknet/storage.rs
+++ crates/cairo-sierra/src/extensions/modules/starknet/storage.rs
@@ -12,7 +12,7 @@ use crate::extensions::{
 };
 use crate::ids::{GenericLibfuncId, GenericTypeId};
 
-/// Type for StarkNet storage address, a value in the range [0, 2 ** 251 - 256).
+/// Type for StarkNet storage address, a value in the range [0, 2 ** 251).
 #[derive(Default)]
 pub struct StorageAddressType {}
 impl NoGenericArgsGenericType for StorageAddressType {
@@ -43,33 +43,77 @@ impl NoGenericArgsGenericLibfunc for StorageReadLibfunc {
         &self,
         context: &dyn SignatureSpecializationContext,
     ) -> Result<LibfuncSignature, SpecializationError> {
+        let gas_builtin_ty = context.get_concrete_type(GasBuiltinType::id(), &[])?;
         let system_ty = context.get_concrete_type(SystemType::id(), &[])?;
         let addr_ty = context.get_concrete_type(StorageAddressType::id(), &[])?;
         let felt_ty = context.get_concrete_type(FeltType::id(), &[])?;
-        Ok(LibfuncSignature::new_non_branch_ex(
-            vec![
+
+        Ok(LibfuncSignature {
+            param_signatures: vec![
+                // Gas builtin
+                ParamSignature::new(gas_builtin_ty.clone()),
+                // System
                 ParamSignature {
                     ty: system_ty.clone(),
                     allow_deferred: false,
                     allow_add_const: true,
                     allow_const: false,
                 },
+                // Address domain
+                ParamSignature::new(felt_ty.clone()),
+                // Address
                 ParamSignature::new(addr_ty),
             ],
-            vec![
-                OutputVarInfo {
-                    ty: system_ty,
-                    ref_info: OutputVarReferenceInfo::Deferred(DeferredOutputKind::AddConst {
-                        param_idx: 0,
-                    }),
+            branch_signatures: vec![
+                // Success branch.
+                BranchSignature {
+                    vars: vec![
+                        // Gas builtin
+                        OutputVarInfo {
+                            ty: gas_builtin_ty.clone(),
+                            ref_info: OutputVarReferenceInfo::Deferred(DeferredOutputKind::Generic),
+                        },
+                        // System
+                        OutputVarInfo {
+                            ty: system_ty.clone(),
+                            ref_info: OutputVarReferenceInfo::Deferred(
+                                DeferredOutputKind::AddConst { param_idx: 1 },
+                            ),
+                        },
+                        // read_value
+                        OutputVarInfo {
+                            ty: felt_ty.clone(),
+                            ref_info: OutputVarReferenceInfo::Deferred(DeferredOutputKind::Generic),
+                        },
+                    ],
+                    ap_change: SierraApChange::Known { new_vars_only: false },
                 },
-                OutputVarInfo {
-                    ty: felt_ty,
-                    ref_info: OutputVarReferenceInfo::NewTempVar { idx: Some(0) },
+                // Failure branch.
+                BranchSignature {
+                    vars: vec![
+                        // Gas builtin
+                        OutputVarInfo {
+                            ty: gas_builtin_ty,
+                            ref_info: OutputVarReferenceInfo::Deferred(DeferredOutputKind::Generic),
+                        },
+                        // System
+                        OutputVarInfo {
+                            ty: system_ty,
+                            ref_info: OutputVarReferenceInfo::Deferred(
+                                DeferredOutputKind::AddConst { param_idx: 1 },
+                            ),
+                        },
+                        // Revert reason
+                        OutputVarInfo {
+                            ty: felt_ty,
+                            ref_info: OutputVarReferenceInfo::NewTempVar { idx: Some(0) },
+                        },
+                    ],
+                    ap_change: SierraApChange::Known { new_vars_only: false },
                 },
             ],
-            SierraApChange::Known { new_vars_only: false },
-        ))
+            fallthrough: Some(0),
+        })
     }
 }
 
@@ -98,13 +142,15 @@ impl NoGenericArgsGenericLibfunc for StorageWriteLibfunc {
                     allow_add_const: true,
                     allow_const: false,
                 },
+                // Address domain
+                ParamSignature::new(felt_ty.clone()),
                 // Address
                 ParamSignature::new(addr_ty),
                 // Value
                 ParamSignature::new(felt_ty.clone()),
             ],
             branch_signatures: vec![
-                // Success branch
+                // Success branch.
                 BranchSignature {
                     vars: vec![
                         // Gas builtin
@@ -122,6 +168,7 @@ impl NoGenericArgsGenericLibfunc for StorageWriteLibfunc {
                     ],
                     ap_change: SierraApChange::Known { new_vars_only: false },
                 },
+                // Failure branch.
                 BranchSignature {
                     vars: vec![
                         // Gas builtin
