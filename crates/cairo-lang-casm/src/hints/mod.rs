cdb1dd0f69e6be215f716f07f228b905c0f6b40e
Ori Ziv <oriz@starkware.co>
2023-01-04 11:41:53 +0200

    Added storage address from felt libfunc.
    commit-id:86fc3cbc

--- crates/cairo-lang-casm/src/hints/mod.rs
+++ crates/cairo-lang-casm/src/hints/mod.rs
@@ -46,6 +46,14 @@ pub enum Hint {
         quotient: CellRef,
         remainder: CellRef,
     },
+    /// Finds some `x` and `y` such that `x * scalar + y = value` and `x <= x_bound`.
+    LinearSplit {
+        value: ResOperand,
+        scalar: ResOperand,
+        x_bound: ResOperand,
+        x: CellRef,
+        y: CellRef,
+    },
     EnterScope,
     ExitScope,
     /// Represent a hint which is part of the dict_squash function. The hint_index is the position
@@ -143,6 +151,27 @@ impl Display for Hint {
                 write!(f, ", ")?;
                 fmt_res_operand(f, rhs)?;
                 write!(f, ")")?;
+            }
+            Hint::LinearSplit { value, scalar, x_bound, x, y } => {
+                writeln!(f)?;
+                write!(f, "value = ")?;
+                fmt_res_operand(f, value)?;
+                writeln!(f)?;
+                write!(f, "scalar = ")?;
+                fmt_res_operand(f, scalar)?;
+                writeln!(f)?;
+                write!(f, "x_bound = ")?;
+                fmt_res_operand(f, x_bound)?;
+                writeln!(f)?;
+                writedoc!(
+                    f,
+                    "
+                        x = min(value // scalar, x_bound)
+                        y = value - x * scalar
+                        memory{x} = x
+                        memory{y} = y
+                    "
+                )?;
             }
             Hint::EnterScope => write!(f, "vm_enter_scope()")?,
             Hint::ExitScope => write!(f, "vm_exit_scope()")?,
