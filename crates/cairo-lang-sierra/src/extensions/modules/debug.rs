4ca9d072218880f20e79645c50f8b5dc5a94799e
orizi <104711814+orizi@users.noreply.github.com>
2023-01-05 16:29:12 +0200

    Added basic print libfunc impl. (#1660)

--- crates/cairo-lang-sierra/src/extensions/modules/debug.rs
+++ crates/cairo-lang-sierra/src/extensions/modules/debug.rs
@@ -10,7 +10,7 @@ use crate::ids::GenericLibfuncId;
 define_libfunc_hierarchy! {
     pub enum DebugLibfunc {
         Print(PrintLibfunc),
-    }, MemConcreteLibfunc
+    }, DebugConcreteLibfunc
 }
 
 /// Libfunc for debug printing.
