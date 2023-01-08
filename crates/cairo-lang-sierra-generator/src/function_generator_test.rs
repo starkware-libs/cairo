f01a543932da30631fb4c81dcee31f654222c584
liorgold2 <38202661+liorgold2@users.noreply.github.com>
2023-01-08 14:14:43 +0200

    Add dup() statements in matches. (#1681)

--- crates/cairo-lang-sierra-generator/src/function_generator_test.rs
+++ crates/cairo-lang-sierra-generator/src/function_generator_test.rs
@@ -3,6 +3,9 @@ use crate::function_generator_test_utils::test_function_generator;
 cairo_lang_test_utils::test_file_test!(
     function_generator,
     "src/function_generator_test_data",
-    {simple: "simple"},
+    {
+        match_: "match",
+        simple: "simple",
+    },
     test_function_generator
 );
