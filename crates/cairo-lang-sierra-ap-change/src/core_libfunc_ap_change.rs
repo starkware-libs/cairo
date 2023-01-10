38e300e5c6d1b721c6f9aa66153b0e64c57f9fd9
amos rothberg <amos@starkware.co>
2023-01-10 16:37:01 +0200

    Updated code after base changes.

--- crates/cairo-lang-sierra-ap-change/src/core_libfunc_ap_change.rs
+++ crates/cairo-lang-sierra-ap-change/src/core_libfunc_ap_change.rs
@@ -52,7 +52,7 @@ pub fn core_libfunc_ap_change<InfoProvider: ApChangeInfoProvider>(
             BoolConcreteLibfunc::And(_) => vec![ApChange::Known(0)],
             BoolConcreteLibfunc::Not(_) => vec![ApChange::Known(1)],
             BoolConcreteLibfunc::Xor(_) => vec![ApChange::Known(1)],
-            BoolConcreteLibfunc::Or(_) => vec![ApChange::Known(3)],
+            BoolConcreteLibfunc::Or(_) => vec![ApChange::Known(2)],
             BoolConcreteLibfunc::Equal(_) => vec![ApChange::Known(1), ApChange::Known(1)],
         },
         CoreConcreteLibfunc::Box(libfunc) => match libfunc {
