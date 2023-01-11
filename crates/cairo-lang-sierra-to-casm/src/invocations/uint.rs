c3879eccc99547cf23b328d8cc7934870189bd27
Ori Ziv <oriz@starkware.co>
2023-01-11 14:38:56 +0200

    Extracts uint const implementation.
    commit-id:c8ffc910

--- crates/cairo-lang-sierra-to-casm/src/invocations/uint.rs
+++ crates/cairo-lang-sierra-to-casm/src/invocations/uint.rs
@@ -1,9 +1,18 @@
 use cairo_lang_casm::cell_expression::CellExpression;
-use cairo_lang_sierra::extensions::uint::Uint8Concrete;
-use num_bigint::BigInt;
+use cairo_lang_sierra::extensions::uint::{Uint8Concrete, UintConstConcreteLibfunc, UintTraits};
 
 use super::{CompiledInvocation, CompiledInvocationBuilder, InvocationError};
 use crate::references::ReferenceExpression;
+
+/// Builds invocations for uint const values.
+pub fn build_const<TUintTraits: UintTraits>(
+    libfunc: &UintConstConcreteLibfunc<TUintTraits>,
+    builder: CompiledInvocationBuilder<'_>,
+) -> Result<CompiledInvocation, InvocationError> {
+    Ok(builder.build_only_reference_changes(
+        [ReferenceExpression::from_cell(CellExpression::Immediate(libfunc.c.into()))].into_iter(),
+    ))
+}
 
 /// Builds instructions for Sierra u8 operations.
 pub fn build_u8(
@@ -11,9 +20,6 @@ pub fn build_u8(
     builder: CompiledInvocationBuilder<'_>,
 ) -> Result<CompiledInvocation, InvocationError> {
     match libfunc {
-        Uint8Concrete::Const(libfunc) => Ok(builder.build_only_reference_changes(
-            [ReferenceExpression::from_cell(CellExpression::Immediate(BigInt::from(libfunc.c)))]
-                .into_iter(),
-        )),
+        Uint8Concrete::Const(libfunc) => build_const(libfunc, builder),
     }
 }
