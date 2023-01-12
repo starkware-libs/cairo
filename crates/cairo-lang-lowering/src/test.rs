0392da7ccd92714e56485cc417d3399cbbcdaa36
Ilya Lesokhin <ilya@starkware.co>
2023-01-12 12:33:09 +0000

    Add inline diagnostics.

--- crates/cairo-lang-lowering/src/test.rs
+++ crates/cairo-lang-lowering/src/test.rs
@@ -1,5 +1,4 @@
 use cairo_lang_debug::DebugWithDb;
-use cairo_lang_diagnostics::DiagnosticsBuilder;
 use cairo_lang_plugins::get_default_plugins;
 use cairo_lang_semantic::db::SemanticGroup;
 use cairo_lang_semantic::test_utils::setup_test_function;
@@ -27,6 +26,7 @@ cairo_lang_test_utils::test_file_test!(
         struct_ :"struct",
         tests :"tests",
         tuple :"tuple",
+        inline_diagnostics :"inline_diagnostics",
     },
     test_function_lowering
 );
@@ -46,10 +46,8 @@ fn test_function_lowering(
     let structured_lowered =
         db.function_with_body_lowered_structured(test_function.function_id).unwrap();
     let flat_lowered = db.function_with_body_lowered_flat(test_function.function_id).unwrap();
-    let mut diagnostics = DiagnosticsBuilder::default();
-    diagnostics.extend(structured_lowered.diagnostics.clone());
-    diagnostics.extend(flat_lowered.diagnostics.clone());
-    let diagnostics = diagnostics.build();
+    let diagnostics =
+        db.function_with_body_lowering_diagnostics(test_function.function_id).unwrap();
 
     let lowered_formatter = LoweredFormatter { db, variables: &flat_lowered.variables };
     OrderedHashMap::from([
