0c815860b7a77d777f4c6870e58ca0102b91d8ee
maciektr <maciej.tratnowiecki@swmansion.com>
2023-01-19 16:07:12 +0100

    Add RootDatabase constructors, allow passing corelib object via project config

--- crates/cairo-lang-compiler/src/db.rs
+++ crates/cairo-lang-compiler/src/db.rs
@@ -2,16 +2,23 @@ use std::sync::Arc;
 
 use cairo_lang_defs::db::{DefsDatabase, DefsGroup, HasMacroPlugins};
 use cairo_lang_defs::plugin::MacroPlugin;
-use cairo_lang_filesystem::db::{init_files_group, AsFilesGroupMut, FilesDatabase, FilesGroup};
+use cairo_lang_filesystem::db::{
+    init_files_group, AsFilesGroupMut, FilesDatabase, FilesGroup, FilesGroupEx,
+};
+use cairo_lang_filesystem::detect::detect_corelib;
+use cairo_lang_filesystem::ids::{CrateLongId, Directory};
 use cairo_lang_lowering::db::{init_lowering_group, LoweringDatabase, LoweringGroup};
 use cairo_lang_parser::db::ParserDatabase;
 use cairo_lang_plugins::get_default_plugins;
+use cairo_lang_project::ProjectConfig;
 use cairo_lang_semantic::db::{SemanticDatabase, SemanticGroup, SemanticGroupEx};
 use cairo_lang_semantic::plugin::SemanticPlugin;
 use cairo_lang_sierra_generator::db::SierraGenDatabase;
 use cairo_lang_syntax::node::db::{SyntaxDatabase, SyntaxGroup};
 use cairo_lang_utils::Upcast;
 use {cairo_lang_defs as defs, cairo_lang_lowering as lowering, cairo_lang_semantic as semantic};
+
+pub const CORELIB_CRATE_NAME: &str = "core";
 
 #[salsa::database(
     DefsDatabase,
@@ -34,7 +41,34 @@ impl RootDatabase {
         res.set_semantic_plugins(plugins);
         res
     }
+
+    pub fn with_dev_corelib(plugins: Vec<Arc<dyn SemanticPlugin>>) -> Option<Self> {
+        let mut db = Self::new(plugins);
+        detect_corelib().map(|path| {
+            let core_crate = db.intern_crate(CrateLongId(CORELIB_CRATE_NAME.into()));
+            let core_root_dir = Directory(path);
+            db.set_crate_root(core_crate, Some(core_root_dir));
+            db
+        })
+    }
+
+    pub fn with_project_config(
+        plugins: Vec<Arc<dyn SemanticPlugin>>,
+        config: ProjectConfig,
+    ) -> Self {
+        let mut db = Self::new(plugins);
+        if let Some(corelib) = config.corelib {
+            let core_crate = db.intern_crate(CrateLongId(CORELIB_CRATE_NAME.into()));
+            db.set_crate_root(core_crate, Some(corelib));
+        }
+        db
+    }
+
+    pub fn default_with_dev_corelib() -> Option<Self> {
+        Self::with_dev_corelib(get_default_plugins())
+    }
 }
+
 impl Default for RootDatabase {
     fn default() -> Self {
         // TODO(spapini): Consider taking from config.
