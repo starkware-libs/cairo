2df8194c430374301f4de4623e3b8345eee650de
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-01-20 13:38:41 +0100

    init

---
+++ crates/cairo-lang-python-bindings/src/lib.rs
@@ -0,0 +1,43 @@
+use std::path::Path;
+use std::fs;
+
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+use pyo3::exceptions::RuntimeError;
+
+use cairo_lang_compiler::{
+    compile_cairo_project_at_path as compile_cairo_to_sierra_at_path,
+    CompilerConfig,
+};
+use cairo_lang_sierra_to_casm::compiler::compile_at_path as compile_sierra_to_casm_at_path;
+
+#[pyfunction]
+fn call_cairo_to_sierra_compiler(input_path: &str, output_path: Option<&str>) -> PyResult<Option<String>> {
+    let sierra_program = compile_cairo_to_sierra_at_path(Path::new(input_path), CompilerConfig::default())
+        .map_err(|e| PyErr::new::<RuntimeError, _>(format!("{}", e)))?;
+    let sierra_program_contents = format!("{}", sierra_program);
+    if let Some(path) = output_path {
+        fs::write(path, sierra_program_contents).map_err(|_| PyErr::new::<RuntimeError, _>("Failed to write output."))?;
+        return Ok(None);
+    }
+    Ok(Some(sierra_program_contents))
+}
+
+#[pyfunction]
+fn call_sierra_to_casm_compiler(input_path: &str, output_path: Option<&str>) -> PyResult<Option<String>> {
+    let cairo_program = compile_sierra_to_casm_at_path(input_path)
+        .map_err(|e| PyErr::new::<RuntimeError, _>(format!("{}", e)))?;
+    let cairo_program_contents = format!("{}", cairo_program);
+    if let Some(path) = output_path {
+        fs::write(path, cairo_program_contents).map_err(|_| PyErr::new::<RuntimeError, _>("Failed to write output."))?;
+        return Ok(None);
+    }
+    Ok(Some(cairo_program_contents))
+}
+
+#[pymodule]
+fn cairo_python_bindings(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(call_cairo_to_sierra_compiler))?;
+    m.add_wrapped(wrap_pyfunction!(call_sierra_to_casm_compiler))?;
+    Ok(())
+}
--- crates/cairo-lang-sierra-to-casm/src/cli.rs
+++ crates/cairo-lang-sierra-to-casm/src/cli.rs
@@ -1,19 +1,9 @@
 use std::fs;
 
-use cairo_lang_sierra::ProgramParser;
-use cairo_lang_sierra_to_casm::metadata::calc_metadata;
 use cairo_lang_utils::logging::init_logging;
+use cairo_lang_sierra_to_casm::compiler::compile_at_path;
+use cairo_lang_sierra_to_casm::compiler::Args;
 use clap::Parser;
-
-/// Command line args parser.
-/// Exits with 0/1 if the input is formatted correctly/incorrectly.
-#[derive(Parser, Debug)]
-#[clap(version, verbatim_doc_comment)]
-struct Args {
-    /// The file to compile
-    file: String,
-    output: String,
-}
 
 fn main() {
     init_logging(log::LevelFilter::Off);
@@ -21,16 +11,7 @@ fn main() {
 
     let args = Args::parse();
 
-    let sierra_code = fs::read_to_string(args.file).expect("Could not read file!");
-    let program = ProgramParser::new().parse(&sierra_code).unwrap();
-
-    let gas_usage_check = true;
-    let cairo_program = cairo_lang_sierra_to_casm::compiler::compile(
-        &program,
-        &calc_metadata(&program).expect("Failed calculating Sierra variables."),
-        gas_usage_check,
-    )
-    .expect("Compilation failed.");
+    let cairo_program = compile_at_path(&args.file[..]).expect("Compilation failed");
 
     fs::write(args.output, format!("{}", cairo_program)).expect("Failed to write output.");
 }
