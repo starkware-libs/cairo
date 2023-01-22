940ef09684c217f6f901506ba89b8a0dfd24655f
Ilya Lesokhin <ilya@starkware.co>
2023-01-22 08:51:24 +0000

    Use testing framework for solve_gas test.

--- crates/cairo-lang-sierra-gas/src/test.rs
+++ crates/cairo-lang-sierra-gas/src/test.rs
@@ -1,12 +1,21 @@
 use std::fs;
 use std::path::PathBuf;
 
-use cairo_lang_sierra::extensions::builtin_cost::CostTokenType;
-use cairo_lang_sierra::program::{Program, StatementIdx};
-use test_case::test_case;
+use cairo_lang_sierra::program::Program;
+use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
 
-use crate::gas_info::GasInfo;
-use crate::{calc_gas_info, CostError};
+use crate::calc_gas_info;
+
+cairo_lang_test_utils::test_file_test!(
+    inlining,
+    "src/test_data",
+    {
+
+        fib_jumps :"fib_jumps",
+        fib_recursive :"fib_recursive",
+    },
+    test_solve_gas
+);
 
 /// Returns a parsed example program from the example directory.
 fn get_example_program(name: &str) -> Program {
@@ -16,36 +25,11 @@ fn get_example_program(name: &str) -> Program {
     cairo_lang_sierra::ProgramParser::new().parse(&fs::read_to_string(path).unwrap()).unwrap()
 }
 
-#[test_case("fib_jumps",
-            Ok(GasInfo {
-                variable_values: [
-                    ((StatementIdx(40), CostTokenType::Step), 4),
-                    ((StatementIdx(27), CostTokenType::Step), 8),
-                    ((StatementIdx(49), CostTokenType::Step), 0),
-                    ((StatementIdx(13), CostTokenType::Step), 11),
-                    ((StatementIdx(3), CostTokenType::Step), 13),
-                ].into_iter().collect(),
-                function_costs: [(
-                    "Fibonacci".into(),
-                    [(CostTokenType::Step, 17)].into_iter().collect()
-                )].into_iter().collect()
-            });
-            "fib_jumps")]
-#[test_case("fib_recursive",
-            Ok(GasInfo {
-                variable_values: [
-                    ((StatementIdx(27), CostTokenType::Step), 39),
-                    ((StatementIdx(40), CostTokenType::Step), 0),
-                    ((StatementIdx(49), CostTokenType::Step), 0),
-                    ((StatementIdx(20), CostTokenType::Step), 4),
-                    ((StatementIdx(9), CostTokenType::Step), 6),
-                ].into_iter().collect(),
-                function_costs: [(
-                    "Fibonacci".into(),
-                    [(CostTokenType::Step, 14)].into_iter().collect()
-                )].into_iter().collect()
-            });
-            "fib_recursive")]
-fn solve_gas(path: &str, expected: Result<GasInfo, CostError>) {
-    pretty_assertions::assert_eq!(calc_gas_info(&get_example_program(path)), expected);
+fn test_solve_gas(inputs: &OrderedHashMap<String, String>) -> OrderedHashMap<String, String> {
+    let path = &inputs["test_file_name"];
+
+    OrderedHashMap::from([(
+        "gas_solution".into(),
+        format!("{}", calc_gas_info(&get_example_program(path)).unwrap()),
+    )])
 }
