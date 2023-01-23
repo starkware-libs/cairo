8f6f9e0c00f40814301adbe0dc55e7c30e2daeb1
Ori Ziv <oriz@starkware.co>
2023-01-23 13:39:15 +0200

    Refactored out validation number is in range.
    commit-id:f053a148

--- crates/cairo-lang-sierra-to-casm/src/invocations/starknet/storage.rs
+++ crates/cairo-lang-sierra-to-casm/src/invocations/starknet/storage.rs
@@ -5,6 +5,7 @@ use cairo_lang_sierra::extensions::consts::SignatureAndConstConcreteLibfunc;
 use num_bigint::BigInt;
 
 use super::{CompiledInvocation, CompiledInvocationBuilder, InvocationError};
+use crate::invocations::misc::validate_in_range;
 use crate::invocations::{add_input_variables, get_non_fallthrough_statement_id};
 use crate::references::ReferenceExpression;
 
@@ -48,28 +49,8 @@ pub fn build_storage_base_address_from_felt(
         buffer(2) range_check;
         deref addr;
     };
-    // For both checks later:
-    // We show that a number is in the range [0, bound) by writing it as:
-    //   A * x + y,
-    // where:
-    //   * K = low positive number (the lower the better, here it is 1 or 2).
-    //   * max_x = 2**128 - K.
-    //   * A = bound // max_x.
-    //   * B = bound % max_x.
-    //   * x is in the range [0, max_x],
-    //   * y is in the range [0, B):
-    //     * y is in the range [0, 2**128).
-    //     * y + 2**128 - B is in the range [0, 2**128).
-    //
-    // Note that the minimal possible value of the expression A * x + y is min_val = 0 (where x = y
-    // = 0), and the maximal value is obtained where x = max_x and y = B - 1:
-    //   max_val = (A * max_x + B) - 1 = bound - 1.
-    //
-    // As long as A <= B, every number in the range can be represented.
-    // In the second case, we choose K to be 2 in order to find A <= B.
     casm_build_extend! {casm_builder,
         const addr_bound = addr_bound;
-        const u128_limit_minus_1 = u128::MAX;
         // Allocating all vars in the beginning for easier AP-Alignment between the two branches,
         // as well as making sure we use `res` as the last cell, making it the last on stack.
         tempvar is_small;
@@ -82,45 +63,29 @@ pub fn build_storage_base_address_from_felt(
         hint TestLessThan {lhs: addr, rhs: addr_bound} into {dst: is_small};
         jump IsSmall if is_small != 0;
         assert res = addr - addr_bound;
-        // Here we want to make sure that `addr` > ADDR_BOUND and `res` < ADDR_BOUND,
-        // for that it is enough to show that `res` < PRIME - ADDR_BOUND.
-        // We use the method described above with (A, B) = divmod(PRIME - ADDR_BOUND, 2**128 - 1)
-        const a_imm = 0x110000000000000000_u128;
-        // 2**128 - B.
-        const b_imm_fix = (u128::MAX - 0x110000000000000101u128 + 1);
-        hint LinearSplit {value: res, scalar: a_imm, max_x: u128_limit_minus_1} into {x: x, y: y};
-        assert x_part = x * a_imm;
-        assert res = x_part + y;
-        // x < 2**128
-        assert x = *(range_check++);
-        // y < 2**128
-        assert y = *(range_check++);
-        // y + 2**128 - B < 2**128 ==> y < B
-        assert y_fixed = y + b_imm_fix;
-        assert y_fixed = *(range_check++);
+    }
+    validate_in_range::<1>(
+        &mut casm_builder,
+        0x110000000000000000_u128,
+        0x110000000000000101u128,
+        res,
+        range_check,
+        &[x, y, x_part, y_fixed],
+    );
+    casm_build_extend! {casm_builder,
         jump Done;
         IsSmall:
         assert res = addr;
-        // We now want to make sure `res` is less than ADDR_BOUND.
-        // We use the method described above with (A, B) = divmod(ADDR_BOUND, 2**128 - 2)
-        const a_imm = 0x8000000000000000000000000000000_u128;
-        // 2**128 - B.
-        const b_imm_fix = (u128::MAX - 0xfffffffffffffffffffffffffffff00_u128 + 1);
-        const u128_limit_minus_2 = u128::MAX - 1;
-        hint LinearSplit {value: res, scalar: a_imm, max_x: u128_limit_minus_2} into {x: x, y: y};
-        assert x_part = x * a_imm;
-        assert res = x_part + y;
-        // y < 2**128
-        assert y = *(range_check++);
-        // y + 2**128 - B < 2**128 ==> y < B
-        assert y_fixed = y + b_imm_fix;
-        assert y_fixed = *(range_check++);
-        // x < 2**128 && x != 2**128 - 1 ==> x < 2**128 - 1
-        assert x = *(range_check++);
-        assert diff = x - u128_limit_minus_1;
-        jump Done if diff != 0;
-        InfiniteLoop:
-        jump InfiniteLoop;
+    }
+    validate_in_range::<2>(
+        &mut casm_builder,
+        0x8000000000000000000000000000000_u128,
+        0xfffffffffffffffffffffffffffff00_u128,
+        res,
+        range_check,
+        &[x, y, x_part, y_fixed, diff],
+    );
+    casm_build_extend! {casm_builder,
         Done:
     };
     Ok(builder
