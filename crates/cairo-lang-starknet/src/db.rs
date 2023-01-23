13ed6b5c4708577945594429903fc46d4df28ec4
maciektr <maciej.tratnowiecki@swmansion.com>
2023-01-23 16:40:27 +0100

    Use vec macro instead of vec::from

--- crates/cairo-lang-starknet/src/db.rs
+++ crates/cairo-lang-starknet/src/db.rs
@@ -8,17 +8,12 @@ use crate::plugin::StarkNetPlugin;
 /// Returns a compiler database tuned to Starknet (e.g. Starknet plugin).
 pub fn get_starknet_database() -> RootDatabase {
     // Override implicit precedence for compatibility with the StarkNet OS.
-    let precedence = ["Pedersen", "RangeCheck", "Bitwise", "EcOp", "GasBuiltin", "System"];
+    let precedence = vec!["Pedersen", "RangeCheck", "Bitwise", "EcOp", "GasBuiltin", "System"];
 
     let mut plugins = get_default_plugins();
     plugins.push(Arc::new(StarkNetPlugin {}));
 
     let mut builder = RootDatabase::builder();
-    builder
-        .with_dev_corelib()
-        .unwrap()
-        .with_implicit_precedence(Vec::from(precedence))
-        .with_plugins(plugins);
-
+    builder.with_dev_corelib().unwrap().with_implicit_precedence(precedence).with_plugins(plugins);
     builder.build()
 }
