53151f20b5c602793667f0e73e2875b6e1a214de
Ilya Lesokhin <ilya@starkware.co>
2023-01-24 07:09:12 +0000

    Avoid silent failure in get_concrete_type_id.

--- crates/cairo-lang-sierra-generator/src/types.rs
+++ crates/cairo-lang-sierra-generator/src/types.rs
@@ -1,4 +1,4 @@
-use cairo_lang_diagnostics::{skip_diagnostic, Maybe};
+use cairo_lang_diagnostics::Maybe;
 use cairo_lang_semantic as semantic;
 use cairo_lang_semantic::items::enm::SemanticEnumEx;
 use cairo_lang_semantic::items::strct::SemanticStructEx;
@@ -86,8 +86,13 @@ pub fn get_concrete_type_id(
                 .collect(),
             }))
         }
-        semantic::TypeLongId::GenericParameter(_) => Err(skip_diagnostic()),
-        semantic::TypeLongId::Var(_) => panic!("Types should be fully resolved at this point."),
-        semantic::TypeLongId::Missing(diag_added) => Err(diag_added),
+        semantic::TypeLongId::GenericParameter(_)
+        | semantic::TypeLongId::Var(_)
+        | semantic::TypeLongId::Missing(_) => {
+            panic!(
+                "Types should be fully resolved at this point. Got: `{}`.",
+                type_id.format(db.upcast())
+            )
+        }
     }
 }
