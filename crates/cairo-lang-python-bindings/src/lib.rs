bdbd2e32674b3414ac5f7b98c402a9e39fe52ebc
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-01-27 15:55:35 +0100

    move find_all_tests to a separate file, remove starknet plugin

--- crates/cairo-lang-python-bindings/src/lib.rs
+++ crates/cairo-lang-python-bindings/src/lib.rs
@@ -23,7 +23,6 @@ use cairo_lang_starknet::contract_class::{
     compile_path as compile_starknet
 };
 use cairo_lang_starknet::casm_contract_class::CasmContractClass;
-use cairo_lang_starknet::plugin::StarkNetPlugin;
 use cairo_lang_semantic::plugin::SemanticPlugin;
 use cairo_lang_plugins::derive::DerivePlugin;
 use cairo_lang_plugins::panicable::PanicablePlugin;
@@ -32,12 +31,12 @@ use cairo_lang_compiler::diagnostics::check_and_eprint_diagnostics;
 use cairo_lang_compiler::project::setup_project;
 use cairo_lang_sierra_generator::replace_ids::replace_sierra_ids_in_program;
 use cairo_lang_sierra_generator::db::SierraGenGroup;
-use cairo_lang_defs::ids::{FreeFunctionId, FunctionWithBodyId, ModuleItemId};
+use cairo_lang_defs::ids::FunctionWithBodyId;
 use cairo_lang_diagnostics::ToOption;
-use cairo_lang_syntax::node::ast::Expr;
-use cairo_lang_filesystem::ids::CrateId;
-use cairo_lang_semantic::db::SemanticGroup;
-use cairo_lang_syntax::node::Token;
+
+mod find_tests;
+
+use find_tests::find_all_tests;
 
 #[pyfunction]
 fn call_cairo_to_sierra_compiler(input_path: &str, output_path: Option<&str>) -> PyResult<Option<String>> {
@@ -111,7 +110,6 @@ fn call_test_collector(path: &str) -> PyResult<String> {
         Arc::new(DerivePlugin {}),
         Arc::new(PanicablePlugin {}),
         Arc::new(ConfigPlugin { configs: HashSet::from(["test".to_string()]) }),
-        Arc::new(StarkNetPlugin {}), // should we use it? it is used conditionally depending on args.starknet
     ];
     let mut db_val = RootDatabase::new(plugins);
     let db = &mut db_val;
@@ -131,93 +129,6 @@ fn call_test_collector(path: &str) -> PyResult<String> {
     let sierra_program = replace_sierra_ids_in_program(db, &sierra_program);
     Ok(sierra_program.to_string())
 }
-
-
-/// Expectation for a result of a test.
-enum TestExpectation {
-    /// Running the test should not panic.
-    Success,
-    /// Running the test should result in a panic.
-    Panics,
-}
-
-/// The configuration for running a single test.
-#[allow(dead_code)]
-struct TestConfig {
-    /// The function id of the test function.
-    func_id: FreeFunctionId,
-    /// The amount of gas the test requested.
-    available_gas: Option<usize>,
-    /// The expected result of the run.
-    expectation: TestExpectation,
-    /// Should the test be ignored.
-    ignored: bool,
-}
-
-/// Finds the tests in the requested crates.
-fn find_all_tests(db: &dyn SemanticGroup, main_crates: Vec<CrateId>) -> Vec<TestConfig> {
-    let mut tests = vec![];
-    for crate_id in main_crates {
-        let modules = db.crate_modules(crate_id);
-        for module_id in modules.iter() {
-            let Ok(module_items) = db.module_items(*module_id) else {
-                continue;
-            };
-
-            for item in module_items.iter() {
-                if let ModuleItemId::FreeFunction(func_id) = item {
-                    if let Ok(attrs) =
-                        db.function_with_body_attributes(FunctionWithBodyId::Free(*func_id))
-                    {
-                        let mut is_test = false;
-                        let mut available_gas = None;
-                        let mut ignored = false;
-                        let mut should_panic = false;
-                        for attr in attrs {
-                            match attr.id.as_str() {
-                                "test" => {
-                                    is_test = true;
-                                }
-                                "available_gas" => {
-                                    // TODO(orizi): Provide diagnostics when this does not match.
-                                    if let [Expr::Literal(literal)] = &attr.args[..] {
-                                        available_gas = literal
-                                            .token(db.upcast())
-                                            .text(db.upcast())
-                                            .parse::<usize>()
-                                            .ok();
-                                    }
-                                }
-                                "should_panic" => {
-                                    should_panic = true;
-                                }
-                                "ignore" => {
-                                    ignored = true;
-                                }
-                                _ => {}
-                            }
-                        }
-                        if is_test {
-                            tests.push(TestConfig {
-                                func_id: *func_id,
-                                available_gas,
-                                expectation: if should_panic {
-                                    TestExpectation::Panics
-                                } else {
-                                    TestExpectation::Success
-                                },
-                                ignored,
-                            })
-                        }
-                    }
-                }
-            }
-        }
-    }
-    tests
-}
-
-
 
 #[pymodule]
 fn cairo_python_bindings(_py: Python, m: &PyModule) -> PyResult<()> {
