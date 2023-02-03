87141996dcc3e7a9a60cf62fdb88e2a5f8071525
msaug <msaug@protonmail.com>
2023-02-03 19:15:27 +0200

    feat: syscall/serde gen for structs

--- crates/cairo-lang-dojo/src/component.rs
+++ crates/cairo-lang-dojo/src/component.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::fmt::Debug;
+use std::path::PathBuf;
 
 use cairo_lang_defs::plugin::{
     DynGeneratedFileAuxData, PluginDiagnostic, PluginGeneratedFile, PluginResult,
@@ -9,6 +11,12 @@ use cairo_lang_syntax::node::db::SyntaxGroup;
 use cairo_lang_syntax::node::{ast, TypedSyntaxNode};
 use indoc::formatdoc;
 use smol_str::SmolStr;
+use cairo_lang_diagnostics::DiagnosticsBuilder;
+use cairo_lang_filesystem::detect::detect_corelib;
+use cairo_lang_filesystem::ids::FileId;
+use cairo_lang_parser::db::{file_syntax, priv_file_syntax_data};
+use cairo_lang_parser::parser::Parser;
+use cairo_lang_parser::utils::{get_syntax_root_and_diagnostics, get_syntax_root_and_diagnostics_from_file};
 
 use crate::plugin::DiagnosticRemapper;
 
@@ -16,6 +24,11 @@ pub struct Component {
     pub name: SmolStr,
     pub rewrite_nodes: Vec<RewriteNode>,
     pub diagnostics: Vec<PluginDiagnostic>,
+}
+
+pub struct ComponentImplementation {
+    storage_impl: RewriteNode,
+    serde_impl: RewriteNode,
 }
 
 impl Component {
@@ -36,7 +49,11 @@ impl Component {
                         continue;
                     }
 
-                    component.handle_component_struct(db, item_struct.clone());
+                    let component_impl = component.handle_component_struct(db, item_struct.clone());
+                    let corelib_path = detect_corelib().unwrap();
+                    modify_starknet_lib(db,&corelib_path, component_impl.storage_impl);
+                    modify_serde_lib(&corelib_path, component_impl.serde_impl);
+
                     matched_struct = true;
                 }
                 ast::Item::FreeFunction(item_function) => {
@@ -80,8 +97,117 @@ impl Component {
         }
     }
 
-    fn handle_component_struct(&mut self, db: &dyn SyntaxGroup, struct_ast: ast::ItemStruct) {
+    fn handle_component_struct(&mut self, db: &dyn SyntaxGroup, struct_ast: ast::ItemStruct) -> ComponentImplementation {
         self.rewrite_nodes.push(RewriteNode::Copied(struct_ast.as_syntax_node()));
+
+        let mut serialize = vec![];
+        let mut deserialize = vec![];
+        let mut read = vec![];
+        let mut write = vec![];
+        struct_ast.members(db).elements(db).iter().enumerate().for_each(|(i, member)| {
+            serialize.push(RewriteNode::interpolate_patched(
+                "Serde::<felt>::serialize(ref serialized, input.$key$);",
+                HashMap::from([(
+                    "key".to_string(),
+                    RewriteNode::Trimmed(member.name(db).as_syntax_node()),
+                )]),
+            ));
+
+            deserialize.push(RewriteNode::interpolate_patched(
+                "$key$: Serde::<felt>::deserialize(ref serialized)?,",
+                HashMap::from([(
+                    "key".to_string(),
+                    RewriteNode::Trimmed(member.name(db).as_syntax_node()),
+                )]),
+            ));
+
+            read.push(RewriteNode::interpolate_patched(
+                "$key$: storage_read_syscall(
+                    address_domain, storage_address_from_base_and_offset(base, $offset$_u8)
+                )?,",
+                HashMap::from([
+                    ("key".to_string(), RewriteNode::Trimmed(member.name(db).as_syntax_node())),
+                    ("offset".to_string(), RewriteNode::Text(i.to_string())),
+                ]),
+            ));
+
+            let final_token = if i == struct_ast.members(db).elements(db).len() - 1 { ";" } else { "" };
+            write.push(RewriteNode::interpolate_patched(
+                format!("storage_write_syscall(
+                    address_domain, storage_address_from_base_and_offset(base, $offset$_u8), \
+                 value.$key$){final_token}").as_str(),
+                HashMap::from([
+                    ("key".to_string(), RewriteNode::Trimmed(member.name(db).as_syntax_node())),
+                    ("offset".to_string(), RewriteNode::Text(i.to_string())),
+                ]),
+            ));
+        });
+
+        let serde_impl = RewriteNode::interpolate_patched(
+            "
+                impl $type_name$Serde of Serde::<$type_name$> {
+                    fn serialize(ref serialized: Array::<felt>, input: $type_name$) {
+                        $serialize$
+                    }
+                    fn deserialize(ref serialized: Array::<felt>) -> Option::<$type_name$> {
+                        Option::Some(
+                            $type_name$ {
+                                $deserialize$
+                            }
+                        )
+                    }
+                }
+            ",
+            HashMap::from([
+                (
+                    "type_name".to_string(),
+                    RewriteNode::Trimmed(struct_ast.name(db).as_syntax_node()),
+                ),
+                (
+                    "serialize".to_string(),
+                    RewriteNode::Modified(ModifiedNode { children: serialize }),
+                ),
+                (
+                    "deserialize".to_string(),
+                    RewriteNode::Modified(ModifiedNode { children: deserialize }),
+                ),
+            ]),
+        );
+
+        let storage_impl = RewriteNode::interpolate_patched(
+            "
+                impl StorageAccess$type_name$ of StorageAccess::<$type_name$> {
+                    fn read(address_domain: felt, base: StorageBaseAddress) -> SyscallResult::<$type_name$> {
+                        Result::Ok(
+                            $type_name$ {
+                                $read$
+                            }
+                        )
+                    }
+                    fn write(
+                        address_domain: felt, base: StorageBaseAddress, value: $type_name$
+                    ) -> SyscallResult::<()> {
+                        $write$
+                    }
+                }
+            ",
+            HashMap::from([
+                (
+                    "type_name".to_string(),
+                    RewriteNode::Trimmed(struct_ast.name(db).as_syntax_node()),
+                ),
+                (
+                    "read".to_string(),
+                    RewriteNode::Modified(ModifiedNode { children: read }),
+                ),
+                (
+                    "write".to_string(),
+                    RewriteNode::Modified(ModifiedNode { children: write }),
+                ),
+            ]),
+        );
+
+
         self.rewrite_nodes.push(RewriteNode::interpolate_patched(
             "
                     struct Storage {
@@ -113,7 +239,12 @@ impl Component {
                 "type_name".to_string(),
                 RewriteNode::Trimmed(struct_ast.name(db).as_syntax_node()),
             )]),
-        ))
+        ));
+
+        ComponentImplementation {
+            storage_impl,
+            serde_impl,
+        }
     }
 
     fn handle_component_functions(&mut self, db: &dyn SyntaxGroup, func: ast::FunctionWithBody) {
@@ -144,4 +275,13 @@ impl Component {
             ]),
         ))
     }
+}
+
+
+fn modify_starknet_lib(db: &dyn SyntaxGroup,path: &PathBuf, node: RewriteNode) {
+    todo!()
+}
+
+fn modify_serde_lib(path: &PathBuf, node: RewriteNode) {
+    todo!()
 }
