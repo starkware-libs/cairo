1a70747f132586b20988b9d46651f17a518f7e9f
maciektr <maciej.tratnowiecki@swmansion.com>
2023-02-06 11:28:11 +0100

    Merge FormatOutcome with FormattedFileContent

--- crates/cairo-lang-formatter/src/cairo_formatter.rs
+++ crates/cairo-lang-formatter/src/cairo_formatter.rs
@@ -4,7 +4,7 @@ use std::io::{stdin, Read};
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
-use anyhow::{bail, Context, Result};
+use anyhow::{anyhow, bail, Context, Result};
 use cairo_lang_filesystem::db::FilesGroup;
 use cairo_lang_filesystem::ids::{FileId, FileLongId, VirtualFile};
 use cairo_lang_parser::utils::{get_syntax_root_and_diagnostics, SimpleParserDatabase};
@@ -16,6 +16,7 @@ use crate::{get_formatted_file, FormatterConfig, CAIRO_FMT_IGNORE};
 
 const CAIRO_FILE_EXTENSION: &str = "cairo";
 
+#[derive(Clone)]
 pub struct FileDiff {
     pub original: String,
     pub formatted: String,
@@ -40,15 +41,18 @@ impl Debug for FileDiff {
 }
 
 #[derive(Debug)]
-pub enum FormattedFileContent {
-    Original(String),
-    Changed(FileDiff),
+pub enum FormatOutcome {
+    Identical(String),
+    DiffFound(FileDiff),
 }
 
-#[derive(Debug)]
-pub enum FormatOutcome {
-    Identical,
-    DiffFound,
+impl FormatOutcome {
+    pub fn into_output_text(self) -> String {
+        match self {
+            FormatOutcome::Identical(original) => original,
+            FormatOutcome::DiffFound(diff) => diff.formatted,
+        }
+    }
 }
 
 pub struct StdinFmt;
@@ -98,18 +102,11 @@ impl FormattableInput for StdinFmt {
     }
 }
 
-fn format_input(
-    input: &dyn FormattableInput,
-    config: &FormatterConfig,
-) -> Result<(FormatOutcome, FormattedFileContent)> {
+fn format_input(input: &dyn FormattableInput, config: &FormatterConfig) -> Result<FormatOutcome> {
     let db = SimpleParserDatabase::default();
     let file_id = input.to_file_id(&db).context("Unable to create virtual file.")?;
-    let original_text = match db.file_content(file_id) {
-        Some(value) => value,
-        None => {
-            bail!("Unable to read from input.");
-        }
-    };
+    let original_text =
+        db.file_content(file_id).ok_or_else(|| anyhow!("Unable to read from input."))?;
     let (syntax_root, diagnostics) = get_syntax_root_and_diagnostics(&db, file_id, &original_text);
     if !diagnostics.0.leaves.is_empty() {
         bail!(diagnostics.format(&db));
@@ -117,10 +114,10 @@ fn format_input(
     let formatted_text = get_formatted_file(&db, &syntax_root, config.clone());
 
     if &formatted_text == original_text.as_ref() {
-        Ok((FormatOutcome::Identical, FormattedFileContent::Original(original_text.to_string())))
+        Ok(FormatOutcome::Identical(original_text.to_string()))
     } else {
         let diff = FileDiff { original: original_text.to_string(), formatted: formatted_text };
-        Ok((FormatOutcome::DiffFound, FormattedFileContent::Changed(diff)))
+        Ok(FormatOutcome::DiffFound(diff))
     }
 }
 
@@ -142,56 +139,27 @@ impl CairoFormatter {
         builder.skip_stdout(true);
 
         let mut types_builder = TypesBuilder::new();
-        types_builder
-            .add(CAIRO_FILE_EXTENSION, format!("*.{CAIRO_FILE_EXTENSION}").as_str())
-            .unwrap();
+        types_builder.add(CAIRO_FILE_EXTENSION, &format!("*.{CAIRO_FILE_EXTENSION}")).unwrap();
         types_builder.select(CAIRO_FILE_EXTENSION);
         builder.types(types_builder.build().unwrap());
 
         builder
     }
 
-    /// Verifies that the path is formatted correctly.
-    pub fn check(&self, input: &dyn FormattableInput) -> Result<(FormatOutcome, Option<FileDiff>)> {
-        match format_input(input, &self.formatter_config)? {
-            (FormatOutcome::DiffFound, FormattedFileContent::Changed(diff)) => {
-                Ok((FormatOutcome::DiffFound, Some(diff)))
-            }
-            (FormatOutcome::Identical, FormattedFileContent::Original(_)) => {
-                Ok((FormatOutcome::Identical, None))
-            }
-            _ => unreachable!(),
-        }
-    }
-
     /// Formats the path in place, writing changes to the files.
     pub fn format_in_place(&self, input: &dyn FormattableInput) -> Result<FormatOutcome> {
         match format_input(input, &self.formatter_config)? {
-            (FormatOutcome::DiffFound, FormattedFileContent::Changed(diff)) => {
+            FormatOutcome::DiffFound(diff) => {
                 // Persist changes.
-                input.overwrite_content(diff.formatted)?;
-                Ok(FormatOutcome::DiffFound)
+                input.overwrite_content(diff.formatted.clone())?;
+                Ok(FormatOutcome::DiffFound(diff))
             }
-            (FormatOutcome::Identical, FormattedFileContent::Original(_)) => {
-                Ok(FormatOutcome::Identical)
-            }
-            _ => unreachable!(),
+            FormatOutcome::Identical(original) => Ok(FormatOutcome::Identical(original)),
         }
     }
 
     /// Formats the path and returns the formatted string.
-    pub fn format_to_string(
-        &self,
-        input: &dyn FormattableInput,
-    ) -> Result<(FormatOutcome, String)> {
-        match format_input(input, &self.formatter_config)? {
-            (FormatOutcome::DiffFound, FormattedFileContent::Changed(diff)) => {
-                Ok((FormatOutcome::DiffFound, diff.formatted))
-            }
-            (FormatOutcome::Identical, FormattedFileContent::Original(original)) => {
-                Ok((FormatOutcome::Identical, original))
-            }
-            _ => unreachable!(),
-        }
+    pub fn format_to_string(&self, input: &dyn FormattableInput) -> Result<FormatOutcome> {
+        format_input(input, &self.formatter_config)
     }
 }
