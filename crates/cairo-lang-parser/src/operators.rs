5ece21453eadcc4dbd75d53f05c443c9c5a24ec8
Shahar Papini <43779613+spapinistarkware@users.noreply.github.com>
2023-02-14 13:31:25 +0200

    Snapshot syntax fixes (#2105)

--- crates/cairo-lang-parser/src/operators.rs
+++ crates/cairo-lang-parser/src/operators.rs
@@ -10,11 +10,8 @@ pub fn get_unary_operator_precedence(kind: SyntaxKind) -> Option<usize> {
 }
 pub fn get_post_operator_precedence(kind: SyntaxKind) -> Option<usize> {
     match kind {
-        // TODO(spapini): This should precede unary operators.
         SyntaxKind::TerminalDot => Some(0),
         SyntaxKind::TerminalQuestionMark => Some(1),
-
-        // TODO(yuval): support unary-only/non-binary operators. "not" can't be binary.
         SyntaxKind::TerminalAt | SyntaxKind::TerminalNot => Some(2),
         SyntaxKind::TerminalMul | SyntaxKind::TerminalDiv | SyntaxKind::TerminalMod => Some(3),
         SyntaxKind::TerminalPlus | SyntaxKind::TerminalMinus => Some(4),
