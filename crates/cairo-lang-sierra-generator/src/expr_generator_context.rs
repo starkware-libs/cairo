aa5bba7ff485d7601e63f077aa940bf32950a781
ilyalesokhin-starkware <ilya@starkware.co>
2023-02-14 18:04:07 +0200

    Avoid `generate_push_values_statement_for_remapping` for a fallthroug… (#2110)

--- crates/cairo-lang-sierra-generator/src/expr_generator_context.rs
+++ crates/cairo-lang-sierra-generator/src/expr_generator_context.rs
@@ -73,6 +73,23 @@ impl<'a> ExprGeneratorContext<'a> {
         sierra_var
     }
 
+    /// Maps a lowering variable to the given Sierra variable.
+    /// I.e., every instance of `lowering_var_id` is replaced by lowering_var_id.
+    ///
+    /// This can be used to avoid generating `rename` invocations.
+    pub fn add_variable_mapping(
+        &mut self,
+        lowering_var_id: lowering::VariableId,
+        sierra_var_id: cairo_lang_sierra::ids::VarId,
+    ) {
+        assert!(
+            self.variables
+                .insert(SierraGenVar::LoweringVar(lowering_var_id), sierra_var_id)
+                .is_none(),
+            "Conflicting variable mapping for {lowering_var_id:?}"
+        );
+    }
+
     /// Same as [Self::get_sierra_variable] except that it operates of a list of variables.
     pub fn get_sierra_variables(
         &mut self,
