87ed8d6f2e73bd80e205b3bf3186cbea13492b86
Ilya Lesokhin <ilya@starkware.co>
2023-02-15 08:35:58 +0000

    Add logic to add missing fallthroughs.

--- crates/cairo-lang-lowering/src/inline/test.rs
+++ crates/cairo-lang-lowering/src/inline/test.rs
@@ -5,9 +5,11 @@ use cairo_lang_semantic::test_utils::setup_test_function;
 use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
 
 use crate::db::LoweringGroup;
+use crate::flow::add_fallthroughs;
 use crate::fmt::LoweredFormatter;
 use crate::inline::apply_inlining;
 use crate::test_utils::LoweringDatabaseForTesting;
+use std::ops::Deref;
 
 cairo_lang_test_utils::test_file_test!(
     inlining,
@@ -32,12 +34,17 @@ fn test_function_inlining(
         inputs["module_code"].as_str(),
     )
     .split();
-    let before =
-        (*db.priv_function_with_body_lowered_flat(test_function.function_id).unwrap()).clone();
-    let mut after = before.clone();
+    let before = db
+        .priv_concrete_function_with_body_lowered_flat(test_function.concrete_function_id)
+        .unwrap();
 
     let lowering_diagnostics = db.module_lowering_diagnostics(test_function.module_id).unwrap();
+
+    let mut after = before.deref().clone();
     apply_inlining(db, test_function.function_id, &mut after).unwrap();
+
+    // TODO(ilya): Add a seperate test for add_fallthroughs.
+    add_fallthroughs(&mut after);
 
     OrderedHashMap::from([
         ("semantic_diagnostics".into(), semantic_diagnostics),
