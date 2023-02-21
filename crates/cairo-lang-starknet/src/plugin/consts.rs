f3f403bae2dc6c4de99946e31d7dfe460ec3eb00
ilyalesokhin-starkware <ilya@starkware.co>
2023-02-21 13:20:23 +0200

    Add account contract (#2220)

--- crates/cairo-lang-starknet/src/plugin/consts.rs
+++ crates/cairo-lang-starknet/src/plugin/consts.rs
@@ -6,6 +6,7 @@ pub const EXTERNAL_MODULE: &str = "__external";
 pub const CONSTRUCTOR_MODULE: &str = "__constructor";
 
 pub(super) const ABI_ATTR: &str = "abi";
+pub(super) const ACCOUNT_CONTRACT_ATTR: &str = "account_contract";
 pub(super) const CONTRACT_ATTR: &str = "contract";
 pub(super) const EXTERNAL_ATTR: &str = "external";
 pub(super) const CONSTRUCTOR_ATTR: &str = "constructor";
