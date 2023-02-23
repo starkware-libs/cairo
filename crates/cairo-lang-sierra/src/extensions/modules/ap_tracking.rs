4704693b343c339fbde7109e6d3e68b19bb1c55a
Ori Ziv <oriz@starkware.co>
2023-02-23 19:06:04 +0200

    Added enable_ap_tracking libfunc.
    Preparation for major sierra gen performance.
    
    commit-id:ed29b116

--- crates/cairo-lang-sierra/src/extensions/modules/ap_tracking.rs
+++ crates/cairo-lang-sierra/src/extensions/modules/ap_tracking.rs
@@ -1,7 +1,15 @@
+use crate::define_libfunc_hierarchy;
 use crate::extensions::lib_func::{
     LibfuncSignature, SierraApChange, SignatureSpecializationContext,
 };
 use crate::extensions::{NoGenericArgsGenericLibfunc, SpecializationError};
+
+define_libfunc_hierarchy! {
+    pub enum ApTrackingLibfunc {
+        Revoke(RevokeApTrackingLibfunc),
+        Enable(EnableApTrackingLibfunc),
+    }, ApTrackingConcreteLibfunc
+}
 
 /// Revoke the ap tracking.
 /// This Libfunc is changes to ap_tracking state to unknown,
@@ -16,5 +24,25 @@ impl NoGenericArgsGenericLibfunc for RevokeApTrackingLibfunc {
         _context: &dyn SignatureSpecializationContext,
     ) -> Result<LibfuncSignature, SpecializationError> {
         Ok(LibfuncSignature::new_non_branch(vec![], vec![], SierraApChange::Unknown))
+    }
+}
+
+/// Enable ap tracking.
+/// This Libfunc is used to enable ap tracking to allow branches that may diverge and merge after
+/// this point to have an aligned ap.
+#[derive(Default)]
+pub struct EnableApTrackingLibfunc {}
+impl NoGenericArgsGenericLibfunc for EnableApTrackingLibfunc {
+    const STR_ID: &'static str = "enable_ap_tracking";
+
+    fn specialize_signature(
+        &self,
+        _context: &dyn SignatureSpecializationContext,
+    ) -> Result<LibfuncSignature, SpecializationError> {
+        Ok(LibfuncSignature::new_non_branch(
+            vec![],
+            vec![],
+            SierraApChange::Known { new_vars_only: true },
+        ))
     }
 }
