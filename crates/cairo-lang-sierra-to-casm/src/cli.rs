6a95f9ae8464617f759d8ae7c71bddac5bb789a9
Ilya Lesokhin <ilya@starkware.co>
2023-02-26 11:40:03 +0200

    Improve sierra-compile error message.

--- crates/cairo-lang-sierra-to-casm/src/cli.rs
+++ crates/cairo-lang-sierra-to-casm/src/cli.rs
@@ -1,9 +1,11 @@
 use std::fs;
 
+use anyhow::Context;
 use cairo_lang_sierra::ProgramParser;
 use cairo_lang_sierra_to_casm::metadata::calc_metadata;
 use cairo_lang_utils::logging::init_logging;
 use clap::Parser;
+use indoc::indoc;
 
 /// Command line args parser.
 /// Exits with 0/1 if the input is formatted correctly/incorrectly.
@@ -15,22 +17,26 @@ struct Args {
     output: String,
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     init_logging(log::LevelFilter::Off);
     log::info!("Starting Sierra compilation.");
 
     let args = Args::parse();
 
-    let sierra_code = fs::read_to_string(args.file).expect("Could not read file!");
-    let program = ProgramParser::new().parse(&sierra_code).unwrap();
+    let sierra_code = fs::read_to_string(args.file).with_context(|| "Could not read file!")?;
+    let Ok(program) = ProgramParser::new().parse(&sierra_code) else { anyhow::bail!(indoc!{"
+        Failed to parse sierra program.
+        If you are trying to compile a starknet contract you should use `starknet-sierra-compile`."
+    })};
 
     let gas_usage_check = true;
     let cairo_program = cairo_lang_sierra_to_casm::compiler::compile(
         &program,
-        &calc_metadata(&program, Default::default()).expect("Failed calculating Sierra variables."),
+        &calc_metadata(&program, Default::default())
+            .with_context(|| "Failed calculating Sierra variables.")?,
         gas_usage_check,
     )
-    .expect("Compilation failed.");
+    .with_context(|| "Compilation failed.")?;
 
-    fs::write(args.output, format!("{cairo_program}")).expect("Failed to write output.");
+    fs::write(args.output, format!("{cairo_program}")).with_context(|| "Failed to write output.")
 }
