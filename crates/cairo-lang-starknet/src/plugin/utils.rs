37e3487902acb58c04c6e4e7e9dffc24afcf3680
yuvalsw <105596564+yuvalsw@users.noreply.github.com>
2023-02-27 18:44:09 +0200

    Fixed contract code to not copy mut modifier to ABI (#2297)

--- crates/cairo-lang-starknet/src/plugin/utils.rs
+++ crates/cairo-lang-starknet/src/plugin/utils.rs
@@ -6,5 +6,13 @@ pub fn is_ref_param(db: &dyn SyntaxGroup, param: &ast::Param) -> bool {
     let param_modifiers = param.modifiers(db).elements(db);
     // TODO(yuval): This works only if "ref" is the only modifier. If the expansion was at the
     // semantic level, we could just ask if it's a reference.
-    param_modifiers.len() == 1 && matches!(param_modifiers[0], Modifier::Ref(_))
+    matches!(param_modifiers[..], [Modifier::Ref(_)])
+}
+
+/// Checks if the parameter is defined as a mut parameter.
+pub fn is_mut_param(db: &dyn SyntaxGroup, param: &ast::Param) -> bool {
+    let param_modifiers = param.modifiers(db).elements(db);
+    // TODO(yuval): This works only if "mut" is the only modifier. If the expansion was at the
+    // semantic level, we could just ask if it's a reference.
+    matches!(param_modifiers[..], [Modifier::Mut(_)])
 }
