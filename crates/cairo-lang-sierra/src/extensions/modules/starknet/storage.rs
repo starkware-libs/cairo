a83643650962accdadb26bc556e01e5b4fac714f
Ori Ziv <oriz@starkware.co>
2023-03-02 10:20:09 +0200

    Extracted syscall sierra libfunc into a util.
    commit-id:a3af46ae

--- crates/cairo-lang-sierra/src/extensions/modules/starknet/storage.rs
+++ crates/cairo-lang-sierra/src/extensions/modules/starknet/storage.rs
@@ -1,11 +1,9 @@
-use super::syscalls::SystemType;
-use crate::extensions::array::ArrayType;
+use super::syscalls::SyscallGenericLibfunc;
 use crate::extensions::consts::{ConstGenLibfunc, WrapConstGenLibfunc};
 use crate::extensions::felt::FeltType;
-use crate::extensions::gas::GasBuiltinType;
 use crate::extensions::lib_func::{
-    BranchSignature, DeferredOutputKind, LibfuncSignature, OutputVarInfo, ParamSignature,
-    SierraApChange, SignatureSpecializationContext,
+    DeferredOutputKind, LibfuncSignature, OutputVarInfo, ParamSignature, SierraApChange,
+    SignatureSpecializationContext,
 };
 use crate::extensions::range_check::RangeCheckType;
 use crate::extensions::try_from_felt::TryFromFelt;
@@ -15,7 +13,6 @@ use crate::extensions::{
     SpecializationError,
 };
 use crate::ids::GenericTypeId;
-use crate::program::GenericArg;
 
 /// Type for Starknet storage base address, a value in the range [0, 2 ** 251 - 256).
 #[derive(Default)]
@@ -154,167 +151,50 @@ impl NoGenericArgsGenericLibfunc for StorageBaseAddressFromFeltLibfunc {
 /// Libfunc for a storage read system call.
 #[derive(Default)]
 pub struct StorageReadLibfunc {}
-impl NoGenericArgsGenericLibfunc for StorageReadLibfunc {
+impl SyscallGenericLibfunc for StorageReadLibfunc {
     const STR_ID: &'static str = "storage_read_syscall";
 
-    fn specialize_signature(
-        &self,
+    fn input_tys(
         context: &dyn SignatureSpecializationContext,
-    ) -> Result<LibfuncSignature, SpecializationError> {
-        let gas_builtin_ty = context.get_concrete_type(GasBuiltinType::id(), &[])?;
-        let system_ty = context.get_concrete_type(SystemType::id(), &[])?;
-        let addr_ty = context.get_concrete_type(StorageAddressType::id(), &[])?;
-        let felt_ty = context.get_concrete_type(FeltType::id(), &[])?;
-        let felt_array_ty =
-            context.get_concrete_type(ArrayType::id(), &[GenericArg::Type(felt_ty.clone())])?;
+    ) -> Result<Vec<crate::ids::ConcreteTypeId>, SpecializationError> {
+        Ok(vec![
+            // Address domain
+            context.get_concrete_type(FeltType::id(), &[])?,
+            // Address
+            context.get_concrete_type(StorageAddressType::id(), &[])?,
+        ])
+    }
 
-        Ok(LibfuncSignature {
-            param_signatures: vec![
-                // Gas builtin
-                ParamSignature::new(gas_builtin_ty.clone()),
-                // System
-                ParamSignature {
-                    ty: system_ty.clone(),
-                    allow_deferred: false,
-                    allow_add_const: true,
-                    allow_const: false,
-                },
-                // Address domain
-                ParamSignature::new(felt_ty.clone()),
-                // Address
-                ParamSignature::new(addr_ty),
-            ],
-            branch_signatures: vec![
-                // Success branch.
-                BranchSignature {
-                    vars: vec![
-                        // Gas builtin
-                        OutputVarInfo {
-                            ty: gas_builtin_ty.clone(),
-                            ref_info: OutputVarReferenceInfo::Deferred(DeferredOutputKind::Generic),
-                        },
-                        // System
-                        OutputVarInfo {
-                            ty: system_ty.clone(),
-                            ref_info: OutputVarReferenceInfo::Deferred(
-                                DeferredOutputKind::AddConst { param_idx: 1 },
-                            ),
-                        },
-                        // Read result
-                        OutputVarInfo {
-                            ty: felt_ty,
-                            ref_info: OutputVarReferenceInfo::Deferred(DeferredOutputKind::Generic),
-                        },
-                    ],
-                    ap_change: SierraApChange::Known { new_vars_only: false },
-                },
-                // Failure branch.
-                BranchSignature {
-                    vars: vec![
-                        // Gas builtin
-                        OutputVarInfo {
-                            ty: gas_builtin_ty,
-                            ref_info: OutputVarReferenceInfo::Deferred(DeferredOutputKind::Generic),
-                        },
-                        // System
-                        OutputVarInfo {
-                            ty: system_ty,
-                            ref_info: OutputVarReferenceInfo::Deferred(
-                                DeferredOutputKind::AddConst { param_idx: 1 },
-                            ),
-                        },
-                        // Revert reason
-                        OutputVarInfo {
-                            ty: felt_array_ty,
-                            ref_info: OutputVarReferenceInfo::Deferred(DeferredOutputKind::Generic),
-                        },
-                    ],
-                    ap_change: SierraApChange::Known { new_vars_only: false },
-                },
-            ],
-            fallthrough: Some(0),
-        })
+    fn success_output_tys(
+        context: &dyn SignatureSpecializationContext,
+    ) -> Result<Vec<crate::ids::ConcreteTypeId>, SpecializationError> {
+        Ok(vec![context.get_concrete_type(FeltType::id(), &[])?])
     }
 }
 
 /// Libfunc for a storage write system call.
 #[derive(Default)]
 pub struct StorageWriteLibfunc {}
-impl NoGenericArgsGenericLibfunc for StorageWriteLibfunc {
+impl SyscallGenericLibfunc for StorageWriteLibfunc {
     const STR_ID: &'static str = "storage_write_syscall";
 
-    fn specialize_signature(
-        &self,
+    fn input_tys(
         context: &dyn SignatureSpecializationContext,
-    ) -> Result<LibfuncSignature, SpecializationError> {
-        let gas_builtin_ty = context.get_concrete_type(GasBuiltinType::id(), &[])?;
-        let system_ty = context.get_concrete_type(SystemType::id(), &[])?;
-        let addr_ty = context.get_concrete_type(StorageAddressType::id(), &[])?;
+    ) -> Result<Vec<crate::ids::ConcreteTypeId>, SpecializationError> {
         let felt_ty = context.get_concrete_type(FeltType::id(), &[])?;
-        let felt_array_ty =
-            context.get_concrete_type(ArrayType::id(), &[GenericArg::Type(felt_ty.clone())])?;
-        Ok(LibfuncSignature {
-            param_signatures: vec![
-                // Gas builtin
-                ParamSignature::new(gas_builtin_ty.clone()),
-                // System
-                ParamSignature {
-                    ty: system_ty.clone(),
-                    allow_deferred: false,
-                    allow_add_const: true,
-                    allow_const: false,
-                },
-                // Address domain
-                ParamSignature::new(felt_ty.clone()),
-                // Address
-                ParamSignature::new(addr_ty),
-                // Value
-                ParamSignature::new(felt_ty),
-            ],
-            branch_signatures: vec![
-                // Success branch.
-                BranchSignature {
-                    vars: vec![
-                        // Gas builtin
-                        OutputVarInfo {
-                            ty: gas_builtin_ty.clone(),
-                            ref_info: OutputVarReferenceInfo::Deferred(DeferredOutputKind::Generic),
-                        },
-                        // System
-                        OutputVarInfo {
-                            ty: system_ty.clone(),
-                            ref_info: OutputVarReferenceInfo::Deferred(
-                                DeferredOutputKind::AddConst { param_idx: 1 },
-                            ),
-                        },
-                    ],
-                    ap_change: SierraApChange::Known { new_vars_only: false },
-                },
-                // Failure branch.
-                BranchSignature {
-                    vars: vec![
-                        // Gas builtin
-                        OutputVarInfo {
-                            ty: gas_builtin_ty,
-                            ref_info: OutputVarReferenceInfo::Deferred(DeferredOutputKind::Generic),
-                        },
-                        // System
-                        OutputVarInfo {
-                            ty: system_ty,
-                            ref_info: OutputVarReferenceInfo::Deferred(
-                                DeferredOutputKind::AddConst { param_idx: 1 },
-                            ),
-                        },
-                        // Revert reason
-                        OutputVarInfo {
-                            ty: felt_array_ty,
-                            ref_info: OutputVarReferenceInfo::Deferred(DeferredOutputKind::Generic),
-                        },
-                    ],
-                    ap_change: SierraApChange::Known { new_vars_only: false },
-                },
-            ],
-            fallthrough: Some(0),
-        })
+        Ok(vec![
+            // Address domain
+            felt_ty.clone(),
+            // Address
+            context.get_concrete_type(StorageAddressType::id(), &[])?,
+            // Value
+            felt_ty,
+        ])
+    }
+
+    fn success_output_tys(
+        _context: &dyn SignatureSpecializationContext,
+    ) -> Result<Vec<crate::ids::ConcreteTypeId>, SpecializationError> {
+        Ok(vec![])
     }
 }
