540304ecb3a961e0cabe2e69f67ee652938268f4
liorgold2 <38202661+liorgold2@users.noreply.github.com>
2023-03-05 08:33:46 +0100

    Refactor casts.rs in preparation for downcast. (#2351)

--- crates/cairo-lang-sierra/src/extensions/modules/casts.rs
+++ crates/cairo-lang-sierra/src/extensions/modules/casts.rs
@@ -20,6 +20,40 @@ define_libfunc_hierarchy! {
     }, CastConcreteLibfunc
 }
 
+/// Returns a map from (concrete) integer type to the number of bits in the type.
+fn get_type_to_nbits_map(
+    context: &dyn SignatureSpecializationContext,
+) -> UnorderedHashMap<ConcreteTypeId, usize> {
+    vec![
+        (Uint8Type::ID, 8),
+        (Uint16Type::ID, 16),
+        (Uint32Type::ID, 32),
+        (Uint64Type::ID, 64),
+        (Uint128Type::ID, 128),
+    ]
+    .into_iter()
+    .filter_map(|(generic_type, n_bits)| {
+        Some((context.get_concrete_type(generic_type, &[]).ok()?, n_bits))
+    })
+    .collect()
+}
+
+/// Returns the number of bits for the given types.
+// TODO(lior): Convert to a generic function that can take arbitrary number of arguments once
+//   `try_map` is a stable feature.
+fn get_n_bits(
+    context: &dyn SignatureSpecializationContext,
+    from_type: &ConcreteTypeId,
+    to_type: &ConcreteTypeId,
+) -> Result<(usize, usize), SpecializationError> {
+    let type_to_n_bits = get_type_to_nbits_map(context);
+    let from_nbits =
+        *type_to_n_bits.get(from_type).ok_or(SpecializationError::UnsupportedGenericArg)?;
+    let to_nbits =
+        *type_to_n_bits.get(to_type).ok_or(SpecializationError::UnsupportedGenericArg)?;
+    Ok((from_nbits, to_nbits))
+}
+
 /// Libfunc for casting from one type to another where any input value can fit into the destination
 /// type. For example, from u8 to u64.
 #[derive(Default)]
@@ -33,24 +67,7 @@ impl SignatureOnlyGenericLibfunc for UpcastLibfunc {
         args: &[GenericArg],
     ) -> Result<LibfuncSignature, SpecializationError> {
         let (from_ty, to_ty) = args_as_two_types(args)?;
-
-        let type_to_n_bits: UnorderedHashMap<ConcreteTypeId, usize> = vec![
-            (Uint8Type::ID, 8),
-            (Uint16Type::ID, 16),
-            (Uint32Type::ID, 32),
-            (Uint64Type::ID, 64),
-            (Uint128Type::ID, 128),
-        ]
-        .into_iter()
-        .filter_map(|(generic_type, n_bits)| {
-            Some((context.get_concrete_type(generic_type, &[]).ok()?, n_bits))
-        })
-        .collect();
-
-        let from_nbits =
-            type_to_n_bits.get(&from_ty).ok_or(SpecializationError::UnsupportedGenericArg)?;
-        let to_nbits =
-            type_to_n_bits.get(&to_ty).ok_or(SpecializationError::UnsupportedGenericArg)?;
+        let (from_nbits, to_nbits) = get_n_bits(context, &from_ty, &to_ty)?;
 
         let is_valid = from_nbits <= to_nbits;
         if !is_valid {
@@ -58,7 +75,7 @@ impl SignatureOnlyGenericLibfunc for UpcastLibfunc {
         }
 
         Ok(LibfuncSignature::new_non_branch(
-            vec![from_ty.clone()],
+            vec![from_ty],
             vec![OutputVarInfo {
                 ty: to_ty,
                 ref_info: OutputVarReferenceInfo::SameAsParam { param_idx: 0 },
