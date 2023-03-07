9b1eedb200a8131552743efa10918ed0d8a2f839
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2023-03-07 16:22:55 +0100

    Improved message error

--- crates/cairo-lang-python-bindings/src/lib.rs
+++ crates/cairo-lang-python-bindings/src/lib.rs
@@ -162,7 +162,7 @@ fn validate_tests(sierra_program: Program, test_names: &Vec<String>) -> Result<(
                 anyhow::bail!("Test function {} must be panicable but it's not", test);
             }
             if return_type_name != "core::PanicResult::<()>" {
-                anyhow::bail!("Test function {} returns a value, it is required that test functions do not return values", test);
+                anyhow::bail!("Test function {} returns a value {}, it is required that test functions do not return values", test, return_type_name);
             }
         } else {
             anyhow::bail!("Couldn't read result type for test function {}poassible cause: Test function {} must be panicable but it's not", test, test);
