a98a95a7b51cbe47d91747ab0b8f6fa7e481a2ea
Gil Ben-Shachar <gil@starkware.co>
2023-03-07 10:18:12 +0200

    Fix cairo test CI script.

--- scripts/cairo_test.sh
+++ scripts/cairo_test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+exit_code=0
 cargo +nightly-2022-11-03 run --bin cairo-test -- --path corelib/
+(( exit_code = exit_code || $? ))
 cargo +nightly-2022-11-03 run --bin cairo-test -- --path tests/bug_samples/ --starknet
+(( exit_code = exit_code || $? ))
+exit $exit_code
