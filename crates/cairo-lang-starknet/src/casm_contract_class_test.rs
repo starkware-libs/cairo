789d00a223854c56e9e47364c7e91677cbdfa8e2
ilyalesokhin-starkware <ilya@starkware.co>
2023-03-14 18:05:16 +0200

    Validate the range of value in contract_class.sierra_program. (#2472)

--- crates/cairo-lang-starknet/src/casm_contract_class_test.rs
+++ crates/cairo-lang-starknet/src/casm_contract_class_test.rs
@@ -1,7 +1,12 @@
+use std::io::BufReader;
+
 use cairo_lang_test_utils::compare_contents_or_fix_with_path;
+use num_bigint::BigUint;
+use num_traits::Num;
 use test_case::test_case;
 
-use crate::casm_contract_class::CasmContractClass;
+use crate::casm_contract_class::{BigIntAsHex, CasmContractClass, StarknetSierraCompilationError};
+use crate::contract_class::ContractClass;
 use crate::test_utils::{get_example_file_path, get_test_contract};
 
 #[test_case("account")]
@@ -16,5 +21,25 @@ fn test_casm_contract_from_contract_class(example_file_name: &str) {
     compare_contents_or_fix_with_path(
         &get_example_file_path(format!("{example_file_name}.casm.json").as_str()),
         serde_json::to_string_pretty(&casm_contract).unwrap() + "\n",
+    );
+}
+
+#[test_case("test_contract")]
+fn test_casm_contract_from_contract_class_failure(example_file_name: &str) {
+    let f =
+        std::fs::File::open(get_example_file_path(&format!("{example_file_name}.json"))).unwrap();
+    let mut contract_class: ContractClass = serde_json::from_reader(BufReader::new(f)).unwrap();
+
+    let prime = BigUint::from_str_radix(
+        "800000000000011000000000000000000000000000000000000000000000001",
+        16,
+    )
+    .unwrap();
+
+    contract_class.sierra_program[17] = BigIntAsHex { value: prime };
+
+    assert_eq!(
+        CasmContractClass::from_contract_class(contract_class),
+        Err(StarknetSierraCompilationError::ValueOutOfRange)
     );
 }
