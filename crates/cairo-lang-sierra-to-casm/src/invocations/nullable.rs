1b7b2d616eda0f98b5045d056f83d0a4584d5a64
Lior Goldberg <lior@starkware.co>
2023-03-15 20:12:59 +0200

    Remove unnecessary assert from match_nullable.
    commit-id:491a40e4

--- crates/cairo-lang-sierra-to-casm/src/invocations/nullable.rs
+++ crates/cairo-lang-sierra-to-casm/src/invocations/nullable.rs
@@ -14,7 +14,16 @@ pub fn build(
 ) -> Result<CompiledInvocation, InvocationError> {
     match libfunc {
         NullableConcreteLibfunc::Null(_) => build_nullable_null(builder),
-        NullableConcreteLibfunc::NullableFromBox(_) => build_identity(builder),
+        NullableConcreteLibfunc::NullableFromBox(_) => {
+            // Note that the pointer of the Box is never zero:
+            // 1. If the size of the inner type is nonnegative, then values are written to the
+            //    memory address pointed by the pointer.
+            //    It follows that this address cannot be zero, since the Cairo-AIR guarantees that
+            //    all memory accesses have address >= 1.
+            // 2. If the size of the inner type is zero, then the pointer is set to 1.
+            //    see `build_into_box`.
+            build_identity(builder)
+        }
         NullableConcreteLibfunc::MatchNullable(libfunc) => {
             build_nullable_match_nullable(builder, libfunc)
         }
@@ -36,21 +45,6 @@ fn build_nullable_match_nullable(
     builder: CompiledInvocationBuilder<'_>,
     libfunc: &SignatureAndTypeConcreteLibfunc,
 ) -> Result<CompiledInvocation, InvocationError> {
-    // Check that the size of the inner type is nonzero and the argument is a simple deref
-    // expression.
-    //
-    // This guarantees that values are written to the memory address pointed by the `Nullable<>`
-    // instance in the case it is not `null`.
-    // It follows that this address cannot be zero, since the Cairo-AIR guarantees that all
-    // memory accesses have address >= 1.
-    //
-    // Therefore, we can be sure that the address is nonzero if and only if the instance is not
-    // `null`.
-    assert!(
-        builder.program_info.type_sizes[&libfunc.ty] > 0,
-        "Nullable<> cannot be used for types of size 0."
-    );
-
     builder.refs[0]
         .expression
         .try_unpack_single()?
