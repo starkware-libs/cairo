8a2846ea2f03795cc85a2d295152480532e1c81d
Yuval Goldberg <yuval@starkware.co>
2023-03-19 16:27:00 +0200

    Added automatic addition of withdraw_gas calls in cycles

--- crates/cairo-lang-lowering/src/graph_algorithms/feedback_set.rs
+++ crates/cairo-lang-lowering/src/graph_algorithms/feedback_set.rs
@@ -1,7 +1,10 @@
 use std::collections::HashSet;
 
 use cairo_lang_diagnostics::Maybe;
+use cairo_lang_filesystem::flag::Flag;
+use cairo_lang_filesystem::ids::FlagId;
 use cairo_lang_semantic::ConcreteFunctionWithBodyId;
+use cairo_lang_utils::extract_matches;
 use cairo_lang_utils::graph_algos::feedback_set::calc_feedback_set;
 
 use super::concrete_function_node::ConcreteFunctionWithBodyNode;
@@ -14,6 +17,19 @@ pub fn function_with_body_feedback_set(
 ) -> Maybe<HashSet<ConcreteFunctionWithBodyId>> {
     let r = db.concrete_function_with_body_scc_representative(function);
     db.priv_function_with_body_feedback_set_of_representative(r)
+}
+
+/// Query implementation of [crate::db::LoweringGroup::needs_withdraw_gas].
+pub fn needs_withdraw_gas(
+    db: &dyn LoweringGroup,
+    function: ConcreteFunctionWithBodyId,
+) -> Maybe<bool> {
+    Ok(if let Some(flag) = db.get_flag(FlagId::new(db.upcast(), "add_withdraw_gas")) {
+        extract_matches!(*flag, Flag::AddWithdrawGas)
+            && db.function_with_body_feedback_set(function)?.contains(&function)
+    } else {
+        false
+    })
 }
 
 /// Query implementation of
