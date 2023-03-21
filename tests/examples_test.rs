97b38e9e24877828c0c4a799ad4f954c2fa159ea
Ori Ziv <oriz@starkware.co>
2023-03-21 15:39:57 +0200

    Added cost-fixing for starknet entry points.
    commit-id:c02e8eed

--- tests/examples_test.rs
+++ tests/examples_test.rs
@@ -183,7 +183,7 @@ fn run_function(
 ) -> RunResultValue {
     let runner = SierraCasmRunner::new(
         checked_compile_to_sierra(name, example_dir_data, auto_add_withdraw_gas),
-        available_gas.is_some(),
+        if available_gas.is_some() { Some(Default::default()) } else { None },
     )
     .expect("Failed setting up runner.");
     let result = runner
