7c71373a7b07216d3b5b443f54b1882c9cf9d48d
Ori Ziv <oriz@starkware.co>
2023-03-26 13:50:55 +0300

    Remove --path arg from runner CLIs.

--- crates/cairo-lang-runner/src/cli.rs
+++ crates/cairo-lang-runner/src/cli.rs
@@ -18,7 +18,6 @@ use clap::Parser;
 #[clap(version, verbatim_doc_comment)]
 struct Args {
     /// The file to compile and run.
-    #[arg(short, long)]
     path: String,
     /// In cases where gas is available, the amount of provided gas.
     #[arg(long)]
--- scripts/starknet_test.sh
+++ scripts/starknet_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cargo +nightly-2022-11-03 run --bin cairo-test -- \
-    --path crates/cairo-lang-starknet/cairo_level_tests/ --starknet
+    crates/cairo-lang-starknet/cairo_level_tests/ --starknet
