917087a00b19855c39959f46ea59ff5e1d45a592
Shahar Papini <spapini@starkware.co>
2023-03-27 14:45:56 +0300

    Cleaner correcter SCC phases

--- crates/cairo-lang-semantic/src/items/function_with_body.rs
+++ crates/cairo-lang-semantic/src/items/function_with_body.rs
@@ -185,10 +185,11 @@ pub fn function_with_body_direct_function_with_body_callees(
     Ok(db
         .function_with_body_direct_callees(function_id)?
         .into_iter()
-        .map(|function_id| function_id.try_get_function_with_body_id(db))
+        .map(|function_id| function_id.body(db))
         .collect::<Maybe<Vec<Option<_>>>>()?
         .into_iter()
         .flatten()
+        .map(|x| x.function_with_body_id(db))
         .collect())
 }
 
--- crates/cairo-lang-semantic/src/items/functions.rs
+++ crates/cairo-lang-semantic/src/items/functions.rs
@@ -185,20 +185,10 @@ impl FunctionId {
         try_extract_matches!(self.get_concrete(db).generic_function, GenericFunctionId::Extern)
     }
 
-    /// Returns the FunctionWithBodyId if this is a function with body, otherwise returns None.
-    pub fn try_get_function_with_body_id(
-        &self,
-        db: &dyn SemanticGroup,
-    ) -> Maybe<Option<FunctionWithBodyId>> {
-        Ok(match self.get_concrete(db).generic_function {
-            GenericFunctionId::Free(free_function_id) => {
-                Some(FunctionWithBodyId::Free(free_function_id))
-            }
-            GenericFunctionId::Impl(impl_generic_function_id) => {
-                impl_generic_function_id.impl_function(db)?.map(FunctionWithBodyId::Impl)
-            }
-            GenericFunctionId::Extern(_) => None,
-        })
+    /// Returns the [ConcreteFunctionWithBodyId] if this is a function with body, otherwise returns
+    /// None.
+    pub fn body(&self, db: &dyn SemanticGroup) -> Maybe<Option<ConcreteFunctionWithBodyId>> {
+        self.get_concrete(db).body(db)
     }
 
     pub fn name(&self, db: &dyn SemanticGroup) -> SmolStr {
@@ -369,7 +359,7 @@ pub struct ConcreteFunction {
     pub generic_args: Vec<semantic::GenericArgumentId>,
 }
 impl ConcreteFunction {
-    pub fn get_body(&self, db: &dyn SemanticGroup) -> Maybe<Option<ConcreteFunctionWithBodyId>> {
+    pub fn body(&self, db: &dyn SemanticGroup) -> Maybe<Option<ConcreteFunctionWithBodyId>> {
         let Some(generic_function) = GenericFunctionWithBodyId::from_generic(
             db,
             self.generic_function,
