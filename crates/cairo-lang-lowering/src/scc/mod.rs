168ff878a62316f93a0f3a4518479bd113d65583
Shahar Papini <spapini@starkware.co>
2023-03-27 14:45:56 +0300

    Cleaner correcter SCC phases

--- crates/cairo-lang-lowering/src/scc/mod.rs
+++ crates/cairo-lang-lowering/src/scc/mod.rs
@@ -1,32 +1,8 @@
-use std::cmp::Ordering;
-
-use cairo_lang_defs::ids::{FunctionWithBodyId, UnstableSalsaId};
+use cairo_lang_defs::ids::FunctionWithBodyId;
 use cairo_lang_utils::graph_algos::graph_node::GraphNode;
 use cairo_lang_utils::graph_algos::strongly_connected_components::compute_scc;
 
-use crate::db::{GenericSCCRepresentative, LoweringGroup};
-
-/// Query implementation of [crate::db::LoweringGroup::function_scc_representative].
-pub fn function_scc_representative(
-    db: &dyn LoweringGroup,
-    function: FunctionWithBodyId,
-) -> GenericSCCRepresentative {
-    GenericSCCRepresentative(
-        db.function_with_body_scc(function)
-            .into_iter()
-            .min_by(|x, y| match (x, y) {
-                (FunctionWithBodyId::Free(x), FunctionWithBodyId::Free(y)) => {
-                    x.get_internal_id().cmp(y.get_internal_id())
-                }
-                (FunctionWithBodyId::Impl(x), FunctionWithBodyId::Impl(y)) => {
-                    x.get_internal_id().cmp(y.get_internal_id())
-                }
-                (FunctionWithBodyId::Free(_), FunctionWithBodyId::Impl(_)) => Ordering::Less,
-                (FunctionWithBodyId::Impl(_), FunctionWithBodyId::Free(_)) => Ordering::Greater,
-            })
-            .unwrap_or(function),
-    )
-}
+use crate::db::LoweringGroup;
 
 /// Query implementation of [crate::db::LoweringGroup::function_with_body_scc].
 pub fn function_with_body_scc(
