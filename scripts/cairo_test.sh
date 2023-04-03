aebb35bf35435e960e7da2df113bce504e80da20
maciektr <maciej.tratnowiecki@swmansion.com>
2023-04-03 07:50:08 +0200

    Use `stable` rust version for CI builds (#2526)

--- scripts/cairo_test.sh
+++ scripts/cairo_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cargo +nightly-2022-11-03 run --bin cairo-test -- corelib/ && \
-cargo +nightly-2022-11-03 run --bin cairo-test -- tests/bug_samples/ --starknet
+cargo run --bin cairo-test -- corelib/ && \
+cargo run --bin cairo-test -- tests/bug_samples/ --starknet
