5fdf26c6d8726314ce0a70a26f4198d0713b2f04
Marek Kaput <marek.kaput@swmansion.com>
2023-04-05 16:08:46 +0200

    Support KV cfg items in `#[cfg(...)]` attribute
    commit-id:2e5e055c

--- crates/cairo-lang-plugins/src/plugins/config.rs
+++ crates/cairo-lang-plugins/src/plugins/config.rs
@@ -1,12 +1,13 @@
 use std::sync::Arc;
 
-use cairo_lang_defs::plugin::{MacroPlugin, PluginResult};
+use cairo_lang_defs::plugin::{MacroPlugin, PluginDiagnostic, PluginResult};
 use cairo_lang_filesystem::cfg::{Cfg, CfgSet};
+use cairo_lang_semantic::items::attribute::{ast_attribute_to_semantic, Attribute, AttributeArg};
 use cairo_lang_semantic::plugin::{AsDynMacroPlugin, SemanticPlugin};
-use cairo_lang_syntax::node::ast::ArgListParenthesized;
 use cairo_lang_syntax::node::db::SyntaxGroup;
 use cairo_lang_syntax::node::helpers::QueryAttrs;
-use cairo_lang_syntax::node::{ast, TypedSyntaxNode};
+use cairo_lang_syntax::node::{ast, Terminal, TypedSyntaxNode};
+use unescaper::unescape;
 
 /// Plugin that enables ignoring modules not involved in the current config.
 /// Mostly useful for marking test modules to prevent usage of their functionality out of tests,
@@ -17,21 +18,17 @@ pub struct ConfigPlugin;
 
 impl MacroPlugin for ConfigPlugin {
     fn generate_code(&self, db: &dyn SyntaxGroup, item_ast: ast::Item) -> PluginResult {
+        let mut diagnostics = Vec::new();
         let cfg_set = db.cfg_set();
         for attr in item_ast.query_attr(db, "cfg") {
-            if let ast::OptionArgListParenthesized::ArgListParenthesized(args) = attr.arguments(db)
-            {
-                let pattern = parse_predicate(db, args);
+            let attr = ast_attribute_to_semantic(db, attr);
+            if let Some(pattern) = parse_predicate(db, attr, &mut diagnostics) {
                 if !cfg_set.is_superset(&pattern) {
-                    return PluginResult {
-                        code: None,
-                        diagnostics: vec![],
-                        remove_original_item: true,
-                    };
+                    return PluginResult { code: None, diagnostics, remove_original_item: true };
                 }
             }
         }
-        PluginResult::default()
+        PluginResult { diagnostics, ..Default::default() }
     }
 }
 impl AsDynMacroPlugin for ConfigPlugin {
@@ -45,7 +42,50 @@ impl AsDynMacroPlugin for ConfigPlugin {
 impl SemanticPlugin for ConfigPlugin {}
 
 /// Parse `#[cfg(...)]` attribute arguments as a predicate matching [`Cfg`] items.
-fn parse_predicate(db: &dyn SyntaxGroup, args: ArgListParenthesized) -> CfgSet {
-    // TODO(mkaput): Support more complex expressions.
-    CfgSet::from_iter([Cfg::tag(args.args(db).as_syntax_node().get_text(db).trim())])
+fn parse_predicate(
+    db: &dyn SyntaxGroup,
+    attr: Attribute,
+    diagnostics: &mut Vec<PluginDiagnostic>,
+) -> Option<CfgSet> {
+    let mut set = CfgSet::new();
+    for arg in attr.args {
+        match arg {
+            AttributeArg::Key(arg) => {
+                let key = arg.path.as_syntax_node().get_text_without_trivia(db);
+                set.insert(Cfg::tag(key));
+            }
+            AttributeArg::KeyValue(arg) => {
+                let key = arg.path.as_syntax_node().get_text_without_trivia(db);
+                let value = if let ast::Expr::ShortString(terminal) = arg.value {
+                    let text = terminal.text(db);
+                    let (literal, _) = text[1..]
+                        .rsplit_once('\'')
+                        .expect("Code should be parse validated at this moment.");
+                    let Some(unescaped_literal) = unescape(literal).ok() else {
+                        diagnostics.push(PluginDiagnostic {
+                            stable_ptr: arg.value_stable_ptr.untyped(),
+                            message: "Improperly escaped string.".into(),
+                        });
+                        return None;
+                    };
+                    unescaped_literal
+                } else {
+                    diagnostics.push(PluginDiagnostic {
+                        stable_ptr: arg.value_stable_ptr.untyped(),
+                        message: "Expected short string.".into(),
+                    });
+                    return None;
+                };
+                set.insert(Cfg::kv(key, value));
+            }
+            _ => {
+                diagnostics.push(PluginDiagnostic {
+                    stable_ptr: arg.stable_ptr(),
+                    message: "This attribute accepts only key or key-value arguments.".into(),
+                });
+                return None;
+            }
+        }
+    }
+    Some(set)
 }
