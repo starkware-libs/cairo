4566ef0b8c0898be3d19505acab00471b44d0170
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2023-04-05 14:17:01 +0200

    Removed unwrap

--- crates/cairo-lang-protostar/src/cli.rs
+++ crates/cairo-lang-protostar/src/cli.rs
@@ -1,7 +1,7 @@
 //! Compiles and runs a Cairo program.
-use std::fs;
+use std::{fs};
 
-use anyhow::Context;
+use anyhow::{anyhow, Context};
 use cairo_lang_protostar::build_protostar_casm_from_sierra;
 use cairo_lang_protostar::test_collector::collect_tests;
 use clap::Parser;
@@ -19,15 +19,19 @@ struct Args {
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    let (sierra_code, collected) = collect_tests(&args.file, None, None, None)?;
+    let (sierra_code_opt, collected) = collect_tests(&args.file, None, None, None)?;
+
+    let sierra_code = sierra_code_opt.ok_or(
+        anyhow!("Expected sierra code")
+    )?;
 
     if let Some(out_path) = args.output_sierra {
-        fs::write(out_path, format!("{}", sierra_code.as_ref().unwrap()))
+        fs::write(out_path, format!("{}", sierra_code))
             .context("Failed to write output.")?;
     }
 
     if let Some(output_contents) =
-        build_protostar_casm_from_sierra(&collected, sierra_code.unwrap(), args.output_casm)?
+        build_protostar_casm_from_sierra(&collected, sierra_code, args.output_casm)?
     {
         println!("{}", output_contents);
     }
