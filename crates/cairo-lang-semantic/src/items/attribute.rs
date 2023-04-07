6cea854102bb74339d31ff8d0a4ba5d9802c26da
Marek Kaput <marek.kaput@swmansion.com>
2023-04-07 10:34:48 +0200

    Use semantic model for processing attribute arguments in derive and panicable plugins
    commit-id:eb14cfa3

--- crates/cairo-lang-semantic/src/items/attribute.rs
+++ crates/cairo-lang-semantic/src/items/attribute.rs
@@ -1,14 +1,15 @@
+use std::fmt;
+
 use cairo_lang_debug::DebugWithDb;
 use cairo_lang_defs::ids::{LanguageElementId, ModuleId};
 use cairo_lang_diagnostics::Maybe;
-use cairo_lang_syntax::node::ast::OptionAttributeArgs;
 use cairo_lang_syntax::node::db::SyntaxGroup;
 use cairo_lang_syntax::node::{ast, Terminal, TypedSyntaxNode};
 use smol_str::SmolStr;
 
 use crate::db::SemanticGroup;
 
-/// Semantic representation of an attribute.
+/// Semantic representation of an [ast::Attribute].
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Attribute {
     pub stable_ptr: ast::AttributePtr,
@@ -18,12 +19,8 @@ pub struct Attribute {
     pub args_stable_ptr: ast::OptionAttributeArgsPtr,
 }
 
-impl DebugWithDb<dyn SemanticGroup> for Attribute {
-    fn fmt(
-        &self,
-        f: &mut std::fmt::Formatter<'_>,
-        db: &(dyn SemanticGroup + 'static),
-    ) -> std::fmt::Result {
+impl<'a> DebugWithDb<dyn SemanticGroup + 'a> for Attribute {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>, db: &(dyn SemanticGroup + 'a)) -> fmt::Result {
         write!(f, r#"Attribute {{ id: "{}""#, self.id)?;
         if !self.args.is_empty() {
             write!(f, ", args: [")?;
@@ -36,7 +33,7 @@ impl DebugWithDb<dyn SemanticGroup> for Attribute {
     }
 }
 
-/// Returns the semantic attributes for the given AST attribute list.
+/// Returns the semantic attributes for the given [ast::AttributeList].
 pub fn ast_attributes_to_semantic(
     syntax_db: &dyn SyntaxGroup,
     attributes: ast::AttributeList,
@@ -45,27 +42,33 @@ pub fn ast_attributes_to_semantic(
     attributes
         .elements(syntax_db)
         .into_iter()
-        .map(|attribute| {
-            let attr_id = attribute.attr(syntax_db);
-            let attr_args = attribute.args(syntax_db);
-
-            Attribute {
-                stable_ptr: attribute.stable_ptr(),
-                id: attr_id.text(syntax_db),
-                id_stable_ptr: attr_id.stable_ptr(),
-                args: match attr_args {
-                    OptionAttributeArgs::AttributeArgs(ref attribute_args) => {
-                        attribute_args.arg_list(syntax_db).elements(syntax_db)
-                    }
-                    OptionAttributeArgs::Empty(_) => vec![],
-                },
-                args_stable_ptr: attr_args.stable_ptr(),
-            }
-        })
+        .map(|a| ast_attribute_to_semantic(syntax_db, a))
         .collect()
 }
 
-/// Query implementation of [crate::db::SemanticGroup::module_attributes].
+/// Returns the semantic attribute for the given [ast::Attribute].
+pub fn ast_attribute_to_semantic(
+    syntax_db: &dyn SyntaxGroup,
+    attribute: ast::Attribute,
+) -> Attribute {
+    let attr_id = attribute.attr(syntax_db);
+    let attr_args = attribute.args(syntax_db);
+
+    Attribute {
+        stable_ptr: attribute.stable_ptr(),
+        id: attr_id.text(syntax_db),
+        id_stable_ptr: attr_id.stable_ptr(),
+        args: match attr_args {
+            ast::OptionAttributeArgs::AttributeArgs(ref attribute_args) => {
+                attribute_args.arg_list(syntax_db).elements(syntax_db)
+            }
+            ast::OptionAttributeArgs::Empty(_) => vec![],
+        },
+        args_stable_ptr: attr_args.stable_ptr(),
+    }
+}
+
+/// Query implementation of [SemanticGroup::module_attributes].
 pub fn module_attributes(db: &dyn SemanticGroup, module_id: ModuleId) -> Maybe<Vec<Attribute>> {
     Ok(match module_id {
         ModuleId::CrateRoot(_) => vec![],
