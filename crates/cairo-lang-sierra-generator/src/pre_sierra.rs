4f0db16d0ce49290a953ae84969b7e4fd518b8ed
ilyalesokhin-starkware <ilya@starkware.co>
2023-04-09 09:25:06 +0300

    Move disable_ap_tracking to the block generator. (#2717)

--- crates/cairo-lang-sierra-generator/src/pre_sierra.rs
+++ crates/cairo-lang-sierra-generator/src/pre_sierra.rs
@@ -28,9 +28,6 @@ impl std::fmt::Display for LabelId {
 pub struct Function {
     /// The source function which was compiled.
     pub id: sierra::ids::FunctionId,
-    /// Number of statements in the body that are the function prolog, including the label and the
-    /// local variables definition.
-    pub prolog_size: usize,
     /// The body of the function.
     pub body: Vec<Statement>,
     /// A label pointing to the first instruction of the function.
