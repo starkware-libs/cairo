ab648b6414c54629a0240a66b65d3b669d1c50c5
maciektr <maciej.tratnowiecki@swmansion.com>
2023-04-11 14:36:30 +0200

    Prefer Scarb-bundled Cairo-LS

--- crates/cairo-lang-language-server/src/bin/language_server.rs
+++ crates/cairo-lang-language-server/src/bin/language_server.rs
@@ -1,34 +1,6 @@
-use cairo_lang_compiler::db::RootDatabase;
-use cairo_lang_filesystem::cfg::{Cfg, CfgSet};
-use cairo_lang_language_server::{Backend, State};
-use cairo_lang_starknet::db::StarknetRootDatabaseBuilderEx;
-use cairo_lang_utils::logging::init_logging;
-use tower_lsp::{LspService, Server};
+use cairo_lang_language_server::serve_language_service;
 
 #[tokio::main]
 async fn main() {
-    init_logging(log::LevelFilter::Warn);
-
-    #[cfg(feature = "runtime-agnostic")]
-    use tokio_util::compat::{TokioAsyncReadCompatExt, TokioAsyncWriteCompatExt};
-
-    let (stdin, stdout) = (tokio::io::stdin(), tokio::io::stdout());
-    #[cfg(feature = "runtime-agnostic")]
-    let (stdin, stdout) = (stdin.compat(), stdout.compat_write());
-
-    let db = RootDatabase::builder()
-        .with_cfg(CfgSet::from_iter([Cfg::name("test")]))
-        .detect_corelib()
-        .with_starknet()
-        .build()
-        .expect("Failed to initialize Cairo compiler database.");
-
-    let (service, socket) = LspService::build(|client| Backend {
-        client,
-        db_mutex: db.into(),
-        state_mutex: State::default().into(),
-    })
-    .custom_method("vfs/provide", Backend::vfs_provide)
-    .finish();
-    Server::new(stdin, stdout, socket).serve(service).await;
+    serve_language_service().await;
 }
