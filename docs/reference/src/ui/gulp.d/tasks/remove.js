8a86166b8f219c7fea116bc0e5ec89f2a0f8e87f
JameStark <113911244+JameStark@users.noreply.github.com>
2023-04-12 11:24:07 +0100

    Split documentation into appropriate components and remove Antora bui… (#2763)

--- docs/reference/src/ui/gulp.d/tasks/remove.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const fs = require('fs-extra')
-const { Transform } = require('stream')
-const map = (transform) => new Transform({ objectMode: true, transform })
-const vfs = require('vinyl-fs')
-
-module.exports = (files) => () =>
-  vfs.src(files, { allowEmpty: true }).pipe(map((file, enc, next) => fs.remove(file.path, next)))
--- docs/reference/src/ui/src/helpers/increment.js
+++
@@ -1,3 +0,0 @@
-'use strict'
-
-module.exports = (value) => (value || 0) + 1
