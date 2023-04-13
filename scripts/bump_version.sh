81c4eb942dace6849a8067dc43263da99579bef8
Shahar Papini <43779613+spapinistarkware@users.noreply.github.com>
2023-04-13 15:52:28 +0300

    Bump to 1.0.0-alpha.7 (#2800)

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CURRENT_VERSION='1.0.0-alpha.6'
+CURRENT_VERSION='1.0.0-alpha.7'
 NEW_VERSION="$@"
 sed -i "s/$CURRENT_VERSION/$NEW_VERSION/g" $(find . -type f -iname "*.toml") ./scripts/bump_version.sh
