7f5a8c4c652d5be322a110aa53a1a0488e00adb1
Shahar Papini <spapini@starkware.co>
2023-04-20 12:33:50 +0300

    Lookup methods in crate

---
+++ crates/cairo-lang-semantic/src/lsp_helpers.rs
@@ -0,0 +1,55 @@
+use cairo_lang_defs::ids::{ModuleId, TraitFunctionId};
+use cairo_lang_filesystem::ids::CrateId;
+
+use crate::db::SemanticGroup;
+use crate::types::TypeHead;
+
+/// A filter for types.
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub enum TypeFilter {
+    /// No filter is applied.
+    NoFilter,
+    /// Only methods with the given type head are returned.
+    TypeHead(TypeHead),
+}
+
+/// Query implementation of [crate::db::SemanticGroup::methods_in_module].
+pub fn methods_in_module(
+    db: &dyn SemanticGroup,
+    module_id: ModuleId,
+    type_filter: TypeFilter,
+) -> Vec<TraitFunctionId> {
+    let mut result = Vec::new();
+    for trait_id in db.module_traits_ids(module_id).unwrap_or_default() {
+        for (_, trait_function) in db.trait_functions(trait_id).unwrap_or_default() {
+            let Ok(signature) = db.trait_function_signature(trait_function) else { continue; };
+            let Some(first_param) = signature.params.first() else { continue; };
+            if first_param.name != "self" {
+                continue;
+            }
+            if let TypeFilter::TypeHead(type_head) = &type_filter {
+                if let Some(head) = first_param.ty.head(db) {
+                    if &head != type_head {
+                        continue;
+                    }
+                }
+            }
+
+            result.push(trait_function)
+        }
+    }
+    result
+}
+
+/// Query implementation of [crate::db::SemanticGroup::methods_in_crate].
+pub fn methods_in_crate(
+    db: &dyn SemanticGroup,
+    crate_id: CrateId,
+    type_filter: TypeFilter,
+) -> Vec<TraitFunctionId> {
+    let mut result = Vec::new();
+    for module_id in db.crate_modules(crate_id).iter() {
+        result.extend(methods_in_module(db, *module_id, type_filter.clone()))
+    }
+    result
+}
