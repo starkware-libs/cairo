4ab18faa92c011de96f4b532da9788000fa1f810
Gil Ben-Shachar <111221618+gilbens-starkware@users.noreply.github.com>
2023-04-30 18:36:55 +0300

    Change sierra version to 3 felts. (#2984)

--- crates/cairo-lang-starknet/src/sierra_version.rs
+++ crates/cairo-lang-starknet/src/sierra_version.rs
@@ -1,10 +1,12 @@
 #[derive(Debug, PartialEq, Eq)]
 pub struct VersionId {
-    pub version: String,
+    pub major: usize,
+    pub minor: usize,
+    pub patch: usize,
 }
 
 impl VersionId {
     pub fn current_version_id() -> Self {
-        Self { version: "0.1.8".into() }
+        Self { major: 1, minor: 0, patch: 0 }
     }
 }
