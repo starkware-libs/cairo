cdd31272999ce17e642352a3055462e9a2afc910
Ori Ziv <oriz@starkware.co>
2023-05-09 10:36:06 +0300

    Added u512_divmod_by_u256.
    commit-id:7cfac875

--- crates/cairo-lang-runner/src/casm_run/mod.rs
+++ crates/cairo-lang-runner/src/casm_run/mod.rs
@@ -1,6 +1,6 @@
 use std::any::Any;
 use std::collections::HashMap;
-use std::ops::Deref;
+use std::ops::{Deref, Shl};
 
 use ark_ff::fields::{Fp256, MontBackend, MontConfig};
 use ark_ff::{Field, PrimeField};
@@ -928,6 +928,42 @@ pub fn execute_core_hint(
                 Felt252::from(remainder.clone() % pow_2_128.clone())
             )?;
             insert_value_to_cellref!(vm, remainder_high, Felt252::from(remainder / pow_2_128))?;
+        }
+        CoreHint::Uint512DivModByUint256 {
+            dividend0,
+            dividend1,
+            dividend2,
+            dividend3,
+            divisor0,
+            divisor1,
+            quotient0,
+            quotient1,
+            quotient2,
+            quotient3,
+            remainder0,
+            remainder1,
+        } => {
+            let pow_2_128 = BigUint::from(u128::MAX) + 1u32;
+            let dividend0 = get_val(vm, dividend0)?.to_biguint();
+            let dividend1 = get_val(vm, dividend1)?.to_biguint();
+            let dividend2 = get_val(vm, dividend2)?.to_biguint();
+            let dividend3 = get_val(vm, dividend3)?.to_biguint();
+            let divisor0 = get_val(vm, divisor0)?.to_biguint();
+            let divisor1 = get_val(vm, divisor1)?.to_biguint();
+            let dividend: BigUint =
+                dividend0 + dividend1.shl(128) + dividend2.shl(256) + dividend3.shl(384);
+            let divisor = divisor0 + divisor1.shl(128);
+            let (quotient, remainder) = dividend.div_rem(&divisor);
+            let (quotient, limb0) = quotient.div_rem(&pow_2_128);
+            insert_value_to_cellref!(vm, quotient0, Felt252::from(limb0))?;
+            let (quotient, limb1) = quotient.div_rem(&pow_2_128);
+            insert_value_to_cellref!(vm, quotient1, Felt252::from(limb1))?;
+            let (limb3, limb2) = quotient.div_rem(&pow_2_128);
+            insert_value_to_cellref!(vm, quotient2, Felt252::from(limb2))?;
+            insert_value_to_cellref!(vm, quotient3, Felt252::from(limb3))?;
+            let (limb1, limb0) = remainder.div_rem(&pow_2_128);
+            insert_value_to_cellref!(vm, remainder0, Felt252::from(limb0))?;
+            insert_value_to_cellref!(vm, remainder1, Felt252::from(limb1))?;
         }
         CoreHint::SquareRoot { value, dst } => {
             let val = get_val(vm, value)?.to_biguint();
--- crates/cairo-lang-sierra/src/extensions/modules/int/mod.rs
+++ crates/cairo-lang-sierra/src/extensions/modules/int/mod.rs
@@ -1,6 +1,7 @@
 pub mod unsigned;
 pub mod unsigned128;
 pub mod unsigned256;
+pub mod unsigned512;
 
 /// Operators for integers.
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
