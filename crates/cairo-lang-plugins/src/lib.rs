27e250cd7b82f0ec8a01fb5bffd3324ac6860c9f
Lancelot de Ferrière <wraitii@gmail.com>
2023-05-11 17:39:54 +0200

    Arithmetics in constants

--- crates/cairo-lang-plugins/src/lib.rs
+++ crates/cairo-lang-plugins/src/lib.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 
 use cairo_lang_semantic::plugin::SemanticPlugin;
 
-use crate::plugins::{ConfigPlugin, DerivePlugin, GenerateTraitPlugin, PanicablePlugin};
+use crate::plugins::{ArithmeticPlugin, ConfigPlugin, DerivePlugin, GenerateTraitPlugin, PanicablePlugin};
 
 pub mod plugins;
 
@@ -13,6 +13,7 @@ mod test;
 /// Gets the list of default plugins to load into the Cairo compiler.
 pub fn get_default_plugins() -> Vec<Arc<dyn SemanticPlugin>> {
     vec![
+        Arc::new(ArithmeticPlugin::default()),
         Arc::new(DerivePlugin::default()),
         Arc::new(GenerateTraitPlugin::default()),
         Arc::new(PanicablePlugin::default()),
---
+++ tests/bug_samples/issue3130.cairo
@@ -0,0 +1,7 @@
+const a: felt252 = (4 + 2 * 3) * 256;
+const b: felt252 = 0xff & (24 + 5 * 2);
+const c: felt252 = -0xff & (24 + 5 * 2);
+const d: felt252 = 0xff | (24 + 5 * 2);
+
+#[test]
+fn main() {}
