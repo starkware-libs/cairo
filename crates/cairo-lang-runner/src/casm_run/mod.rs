17e2e8f529458dbc54b140f7bb88ae7084ffca08
Shahar Papini <spapini@starkware.co>
2023-05-11 10:47:27 +0300

    Externalize core base hint execution

--- crates/cairo-lang-runner/src/casm_run/mod.rs
+++ crates/cairo-lang-runner/src/casm_run/mod.rs
@@ -771,7 +771,7 @@ impl HintProcessor for CairoHintProcessor<'_> {
     }
 }
 
-fn execute_core_hint_base(
+pub fn execute_core_hint_base(
     vm: &mut VirtualMachine,
     exec_scopes: &mut ExecutionScopes,
     core_hint_base: &cairo_lang_casm::hints::CoreHintBase,
@@ -786,7 +786,7 @@ fn execute_core_hint_base(
     }
 }
 
-fn execute_deprecated_hint(
+pub fn execute_deprecated_hint(
     vm: &mut VirtualMachine,
     exec_scopes: &mut ExecutionScopes,
     deprecated_hint: &cairo_lang_casm::hints::DeprecatedHint,
