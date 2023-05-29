a42c290302e5aa263722603d962f529b09aba368
Maciek Kamiński <aundumla@gmail.com>
2023-05-29 14:37:12 +0200

    Optimize derive(StorageAccess)

--- crates/cairo-lang-starknet/src/plugin/storage_access.rs
+++ crates/cairo-lang-starknet/src/plugin/storage_access.rs
@@ -13,37 +13,93 @@ use indoc::formatdoc;
 /// Derive the `StorageAccess` trait for structs annotated with `derive(starknet::StorageAccess)`.
 pub fn handle_struct(db: &dyn SyntaxGroup, struct_ast: ast::ItemStruct) -> PluginResult {
     let mut reads_values = Vec::new();
+    let mut reads_values_at_offset = Vec::new();
     let mut reads_fields = Vec::new();
     let mut writes = Vec::new();
+    let mut writes_at_offset = Vec::new();
     let mut sizes = Vec::new();
 
     for (i, field) in struct_ast.members(db).elements(db).iter().enumerate() {
         let field_name = field.name(db).as_syntax_node().get_text_without_trivia(db);
         let field_type = field.type_clause(db).ty(db).as_syntax_node().get_text_without_trivia(db);
 
-        reads_values.push(format!(
-            "let {field_name} = \
-             starknet::StorageAccess::<{field_type}>::read_at_offset_internal(address_domain, \
-             base, current_offset)?;"
-        ));
-        if i < struct_ast.members(db).elements(db).len() - 1 {
+        if i == 0 {
             reads_values.push(format!(
-                "current_offset += \
-                 starknet::StorageAccess::<{field_type}>::size_internal({field_name});"
+                "let {field_name} = starknet::StorageAccess::<{field_type}>::read(address_domain, \
+                 base)?;"
             ));
+            reads_values_at_offset.push(format!(
+                "let {field_name} = \
+                 starknet::StorageAccess::<{field_type}>::read_at_offset_internal(address_domain, \
+                 base, offset)?;"
+            ));
+        } else {
+            let subsequent_read = format!(
+                "let {field_name} = \
+                 starknet::StorageAccess::<{field_type}>::read_at_offset_internal(address_domain, \
+                 base, current_offset)?;"
+            );
+            reads_values.push(subsequent_read.clone());
+            reads_values_at_offset.push(subsequent_read);
+        }
+        if i < struct_ast.members(db).elements(db).len() - 1 {
+            if i == 0 {
+                reads_values.push(format!(
+                    "let mut current_offset = \
+                     starknet::StorageAccess::<{field_type}>::size_internal({field_name});"
+                ));
+                reads_values_at_offset.push(format!(
+                    "let mut current_offset = offset + \
+                     starknet::StorageAccess::<{field_type}>::size_internal({field_name});"
+                ));
+            } else {
+                let subsequent_read = format!(
+                    "current_offset += \
+                     starknet::StorageAccess::<{field_type}>::size_internal({field_name});"
+                );
+                reads_values.push(subsequent_read.clone());
+                reads_values_at_offset.push(subsequent_read);
+            }
         }
 
         reads_fields.push(format!("{field_name},"));
 
-        writes.push(format!(
-            "starknet::StorageAccess::<{field_type}>::write_at_offset_internal(address_domain, \
-             base, current_offset, value.{field_name})?;"
-        ));
-        if i < struct_ast.members(db).elements(db).len() - 1 {
+        if i == 0 {
             writes.push(format!(
-                "current_offset += \
-                 starknet::StorageAccess::<{field_type}>::size_internal(value.{field_name});"
+                "starknet::StorageAccess::<{field_type}>::write(address_domain, base, \
+                 value.{field_name})?;"
             ));
+            writes_at_offset.push(format!(
+                "starknet::StorageAccess::<{field_type}>::write_at_offset_internal(address_domain, \
+                 base, offset, value.{field_name})?;"
+            ));
+        } else {
+            let subsequent_write = format!(
+                "starknet::StorageAccess::<{field_type}>::write_at_offset_internal(address_domain, \
+                 base, current_offset, value.{field_name})?;"
+            );
+            writes.push(subsequent_write.clone());
+            writes_at_offset.push(subsequent_write);
+        }
+
+        if i < struct_ast.members(db).elements(db).len() - 1 {
+            if i == 0 {
+                writes.push(format!(
+                    "let mut current_offset = \
+                     starknet::StorageAccess::<{field_type}>::size_internal(value.{field_name});"
+                ));
+                writes_at_offset.push(format!(
+                    "let mut current_offset = offset + \
+                     starknet::StorageAccess::<{field_type}>::size_internal(value.{field_name});"
+                ));
+            } else {
+                let subsequent_write = format!(
+                    "current_offset += \
+                     starknet::StorageAccess::<{field_type}>::size_internal(value.{field_name});"
+                );
+                writes.push(subsequent_write.clone());
+                writes_at_offset.push(subsequent_write);
+            }
         }
         sizes.push(format!(
             "starknet::StorageAccess::<{field_type}>::size_internal(value.{field_name})"
@@ -55,16 +111,6 @@ pub fn handle_struct(db: &dyn SyntaxGroup, struct_ast: ast::ItemStruct) -> Plugi
         impl StorageAccess{struct_name} of starknet::StorageAccess::<{struct_name}> {{
             fn read(address_domain: u32, base: starknet::StorageBaseAddress) -> \
          starknet::SyscallResult<{struct_name}> {{
-                StorageAccess{struct_name}::read_at_offset_internal(address_domain, base, 0_u8)
-            }}
-            fn write(address_domain: u32, base: starknet::StorageBaseAddress, value: \
-         {struct_name}) -> starknet::SyscallResult<()> {{
-                StorageAccess{struct_name}::write_at_offset_internal(address_domain, base, 0_u8, \
-         value)
-            }}
-            fn read_at_offset_internal(address_domain: u32, base: StorageBaseAddress, offset: u8) \
-         -> starknet::SyscallResult<{struct_name}> {{
-                let mut current_offset = offset;
                 {reads_values}
                 starknet::SyscallResult::Ok(
                     {struct_name} {{
@@ -72,11 +118,24 @@ pub fn handle_struct(db: &dyn SyntaxGroup, struct_ast: ast::ItemStruct) -> Plugi
                     }}
                 )
             }}
-            #[inline(always)]
-            fn write_at_offset_internal(address_domain: u32, base: StorageBaseAddress, offset: u8, \
-         value: {struct_name}) -> starknet::SyscallResult<()> {{
-                let mut current_offset = offset;
+            fn write(address_domain: u32, base: starknet::StorageBaseAddress, value: \
+         {struct_name}) -> starknet::SyscallResult<()> {{
                 {writes}
+                starknet::SyscallResult::Ok(())
+            }}
+            fn read_at_offset_internal(address_domain: u32, base: starknet::StorageBaseAddress, \
+         offset: u8) -> starknet::SyscallResult<{struct_name}> {{
+                {reads_values_at_offset}
+                starknet::SyscallResult::Ok(
+                    {struct_name} {{
+                        {reads_fields}
+                    }}
+                )
+            }}
+            #[inline(always)]
+            fn write_at_offset_internal(address_domain: u32, base: starknet::StorageBaseAddress, \
+         offset: u8, value: {struct_name}) -> starknet::SyscallResult<()> {{
+                {writes_at_offset}
                 starknet::SyscallResult::Ok(())
             }}
             #[inline(always)]
@@ -85,9 +144,11 @@ pub fn handle_struct(db: &dyn SyntaxGroup, struct_ast: ast::ItemStruct) -> Plugi
             }}
         }}",
         struct_name = struct_ast.name(db).as_syntax_node().get_text_without_trivia(db),
+        reads_values_at_offset = reads_values_at_offset.join("\n        "),
         reads_values = reads_values.join("\n        "),
         reads_fields = reads_fields.join("\n                "),
         writes = writes.join("\n        "),
+        writes_at_offset = writes_at_offset.join("\n        "),
         sizes = sizes.join(" +\n        ")
     );
 
