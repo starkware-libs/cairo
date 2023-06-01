6c9364be4af8b4df2c2d12d1c08e99a2a1551772
Ori Ziv <oriz@starkware.co>
2023-06-01 10:22:59 +0300

    Fixed sierra check to use all pr commits.
    commit-id:1f5509e1

--- scripts/sierra_update_check.sh
+++ scripts/sierra_update_check.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Assuming all updates are provided as inputs - finding if they are in any of the relevant crates.
-echo $@ | grep -e 'crate/cairo-lang-sierra/' -e 'crate/cairo-lang-sierra-gas/' -e 'crate/cairo-lang-sierra-ap-change/' -e 'crate/cairo-lang-sierra-to-casm/' > /dev/null
+MERGE_BASE=$(git merge-base origin/main HEAD)
+git diff --name-only $MERGE_BASE | grep -e 'crate/cairo-lang-sierra/' -e 'crate/cairo-lang-sierra-gas/' -e 'crate/cairo-lang-sierra-ap-change/' -e 'crate/cairo-lang-sierra-to-casm/' > /dev/null
 if [ $? -eq 0 ]; then
     # If so, check if the commit message contains an explaination tag.
-    git log -1 --pretty=%B | grep SIERRA_UPDATED_REASON_TAG= > /dev/null
+    git log $MERGE_BASE..HEAD --pretty=format:"%b" | grep SIERRA_UPDATED_REASON_TAG= > /dev/null
 else
     # If not, we don't care.
     exit 0
