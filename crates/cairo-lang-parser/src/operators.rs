98be95969225935bd32d17d87b82e0978597a995
Ilya Lesokhin <ilya@starkware.co>
2023-06-04 15:41:28 +0300

    Fix precedence.

--- crates/cairo-lang-parser/src/operators.rs
+++ crates/cairo-lang-parser/src/operators.rs
@@ -19,15 +19,15 @@ pub fn get_post_operator_precedence(kind: SyntaxKind) -> Option<usize> {
         SyntaxKind::TerminalAt | SyntaxKind::TerminalNot => Some(2),
         SyntaxKind::TerminalMul | SyntaxKind::TerminalDiv | SyntaxKind::TerminalMod => Some(3),
         SyntaxKind::TerminalPlus | SyntaxKind::TerminalMinus => Some(4),
+        SyntaxKind::TerminalAnd => Some(5),
+        SyntaxKind::TerminalOr => Some(6),
+        SyntaxKind::TerminalXor => Some(7),
         SyntaxKind::TerminalEqEq
         | SyntaxKind::TerminalNeq
         | SyntaxKind::TerminalLT
         | SyntaxKind::TerminalGT
         | SyntaxKind::TerminalLE
-        | SyntaxKind::TerminalGE => Some(5),
-        SyntaxKind::TerminalAnd => Some(6),
-        SyntaxKind::TerminalOr => Some(7),
-        SyntaxKind::TerminalXor => Some(8),
+        | SyntaxKind::TerminalGE => Some(8),
         SyntaxKind::TerminalEq
         | SyntaxKind::TerminalPlusEq
         | SyntaxKind::TerminalMinusEq
