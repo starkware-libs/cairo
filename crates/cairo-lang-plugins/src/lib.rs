6a9174e8b6147c725c4e89970e0eb44e60fea281
Lancelot de Ferrière <wraitii@gmail.com>
2023-06-07 17:27:32 +0200

    Rename, fix, improve

--- crates/cairo-lang-plugins/src/lib.rs
+++ crates/cairo-lang-plugins/src/lib.rs
@@ -3,7 +3,9 @@ use std::sync::Arc;
 
 use cairo_lang_semantic::plugin::SemanticPlugin;
 
-use crate::plugins::{ArithmeticPlugin, ConfigPlugin, DerivePlugin, GenerateTraitPlugin, PanicablePlugin};
+use crate::plugins::{
+    ConfigPlugin, ConstevalIntMacroPlugin, DerivePlugin, GenerateTraitPlugin, PanicablePlugin,
+};
 
 pub mod plugins;
 
@@ -13,7 +15,7 @@ mod test;
 /// Gets the list of default plugins to load into the Cairo compiler.
 pub fn get_default_plugins() -> Vec<Arc<dyn SemanticPlugin>> {
     vec![
-        Arc::new(ArithmeticPlugin::default()),
+        Arc::new(ConstevalIntMacroPlugin::default()),
         Arc::new(DerivePlugin::default()),
         Arc::new(GenerateTraitPlugin::default()),
         Arc::new(PanicablePlugin::default()),
