169a4889d2e976b9bbb062ea3f15bf565a1d765a
Ori Ziv <oriz@starkware.co>
2023-06-15 00:23:09 +0300

    Moved non-unsigned-only functions to the int mod files.
    commit-id:b1e3dbe0

--- crates/cairo-lang-sierra-to-casm/src/invocations/int/mod.rs
+++ crates/cairo-lang-sierra-to-casm/src/invocations/int/mod.rs
@@ -1,4 +1,20 @@
+use cairo_lang_casm::cell_expression::CellExpression;
+use cairo_lang_sierra::extensions::int::{IntConstConcreteLibfunc, IntTraits};
+
+use super::{CompiledInvocation, CompiledInvocationBuilder, InvocationError};
+use crate::references::ReferenceExpression;
+
 pub mod unsigned;
 pub mod unsigned128;
 pub mod unsigned256;
 pub mod unsigned512;
+
+/// Builds invocations for uint const values.
+fn build_const<TIntTraits: IntTraits>(
+    libfunc: &IntConstConcreteLibfunc<TIntTraits>,
+    builder: CompiledInvocationBuilder<'_>,
+) -> Result<CompiledInvocation, InvocationError> {
+    Ok(builder.build_only_reference_changes(
+        [ReferenceExpression::from_cell(CellExpression::Immediate(libfunc.c.into()))].into_iter(),
+    ))
+}
