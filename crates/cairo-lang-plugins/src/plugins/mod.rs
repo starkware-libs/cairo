46ee498b5661a553c752b90994a5726713c3460d
Gil Ben-Shachar <gil@starkware.co>
2023-06-25 15:11:36 +0300

    Add inline macro plugin and use it for consteval_int.

--- crates/cairo-lang-plugins/src/plugins/mod.rs
+++ crates/cairo-lang-plugins/src/plugins/mod.rs
@@ -1,11 +1,12 @@
 pub use config::*;
-pub use consteval_int::*;
 pub use derive::*;
 pub use generate_trait::*;
+pub use inline_macro_plugin::*;
 pub use panicable::*;
 
 mod config;
-mod consteval_int;
 mod derive;
 mod generate_trait;
+mod inline_macro_plugin;
+mod inline_macros;
 mod panicable;
