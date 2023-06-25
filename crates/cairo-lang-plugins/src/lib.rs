8e4d068dd3cc185cceca595d2d67598153f5184e
Gil Ben-Shachar <gil@starkware.co>
2023-06-25 15:11:36 +0300

    Add inline macro plugin and use it for consteval_int.

--- crates/cairo-lang-plugins/src/lib.rs
+++ crates/cairo-lang-plugins/src/lib.rs
@@ -4,7 +4,7 @@ use std::sync::Arc;
 use cairo_lang_semantic::plugin::SemanticPlugin;
 
 use crate::plugins::{
-    ConfigPlugin, ConstevalIntMacroPlugin, DerivePlugin, GenerateTraitPlugin, PanicablePlugin,
+    ConfigPlugin, DerivePlugin, GenerateTraitPlugin, InlineMacroPlugin, PanicablePlugin,
 };
 
 pub mod plugins;
@@ -15,10 +15,10 @@ mod test;
 /// Gets the list of default plugins to load into the Cairo compiler.
 pub fn get_default_plugins() -> Vec<Arc<dyn SemanticPlugin>> {
     vec![
-        Arc::new(ConstevalIntMacroPlugin::default()),
         Arc::new(DerivePlugin::default()),
         Arc::new(GenerateTraitPlugin::default()),
         Arc::new(PanicablePlugin::default()),
         Arc::new(ConfigPlugin::default()),
+        Arc::new(InlineMacroPlugin::default()),
     ]
 }
