004ef87e1e5162644ece8a12c2efbe08efe9e9ef
Ori Ziv <oriz@starkware.co>
2023-06-26 09:52:49 +0300

    Handling basic audit comments.
    commit-id:79ce8c59

--- crates/cairo-lang-sierra-to-casm/src/invocations/int/unsigned128.rs
+++ crates/cairo-lang-sierra-to-casm/src/invocations/int/unsigned128.rs
@@ -3,7 +3,7 @@ use cairo_lang_casm::casm_build_extend;
 use cairo_lang_sierra::extensions::int::unsigned128::Uint128Concrete;
 use cairo_lang_sierra::extensions::int::IntOperator;
 use num_bigint::BigInt;
-use num_traits::One;
+use num_traits::{Num, One};
 
 use crate::invocations::{
     add_input_variables, get_non_fallthrough_statement_id, misc, CompiledInvocation,
@@ -466,10 +466,11 @@ pub fn build_u128_byte_reverse(
     // two 64-bit words.
 
     // Right align the value.
-    let shift = 1_u128 << (8 + 16 + 32 + 64);
     casm_build_extend! {casm_builder,
-        const shift_imm = shift;
-        tempvar result = temp / shift_imm;
+        // The inverse of `2**(8 + 16 + 32 + 64)` in the field.
+        const shift_inverse =
+            -BigInt::from_str_radix("800000000000011000000000000000000", 16).unwrap();
+        tempvar result = temp * shift_inverse;
     }
 
     Ok(builder.build_from_casm_builder(
