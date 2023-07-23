af8efcded326ee8cac217cb1453a009deeaed3ec
Ori Ziv <oriz@starkware.co>
2023-07-23 14:12:40 +0300

    Dropping inner items in config plugin.

--- crates/cairo-lang-plugins/src/lib.rs
+++ crates/cairo-lang-plugins/src/lib.rs
@@ -15,10 +15,12 @@ mod test;
 /// Gets the list of default plugins to load into the Cairo compiler.
 pub fn get_default_plugins() -> Vec<Arc<dyn SemanticPlugin>> {
     vec![
+        // Config plugin should be first, as it removes items from the AST, and other plugins may
+        // add items prior to the removal of the original.
+        Arc::new(ConfigPlugin::default()),
         Arc::new(DerivePlugin::default()),
         Arc::new(GenerateTraitPlugin::default()),
         Arc::new(PanicablePlugin::default()),
-        Arc::new(ConfigPlugin::default()),
         Arc::new(InlineMacroPlugin),
     ]
 }
--- crates/cairo-lang-plugins/src/plugins/config.rs
+++ crates/cairo-lang-plugins/src/plugins/config.rs
@@ -1,14 +1,19 @@
+use std::ops::Deref;
 use std::sync::Arc;
 
-use cairo_lang_defs::plugin::{MacroPlugin, PluginDiagnostic, PluginResult};
+use cairo_lang_defs::plugin::{
+    DynGeneratedFileAuxData, MacroPlugin, PluginDiagnostic, PluginGeneratedFile, PluginResult,
+};
 use cairo_lang_filesystem::cfg::{Cfg, CfgSet};
-use cairo_lang_semantic::plugin::{AsDynMacroPlugin, SemanticPlugin};
+use cairo_lang_semantic::plugin::{AsDynMacroPlugin, SemanticPlugin, TrivialPluginAuxData};
 use cairo_lang_syntax::attribute::structured::{
     Attribute, AttributeArg, AttributeArgVariant, AttributeStructurize,
 };
 use cairo_lang_syntax::node::db::SyntaxGroup;
+use cairo_lang_syntax::node::element_list::ElementList;
 use cairo_lang_syntax::node::helpers::QueryAttrs;
-use cairo_lang_syntax::node::{ast, Terminal};
+use cairo_lang_syntax::node::kind::SyntaxKind;
+use cairo_lang_syntax::node::{ast, SyntaxNode, Terminal, TypedSyntaxNode};
 
 /// Plugin that enables ignoring modules not involved in the current config.
 /// Mostly useful for marking test modules to prevent usage of their functionality out of tests,
@@ -19,17 +24,33 @@ pub struct ConfigPlugin;
 
 impl MacroPlugin for ConfigPlugin {
     fn generate_code(&self, db: &dyn SyntaxGroup, item_ast: ast::Item) -> PluginResult {
-        let mut diagnostics = Vec::new();
-        let cfg_set = db.cfg_set();
-        for attr in item_ast.query_attr(db, "cfg") {
-            let attr = attr.structurize(db);
-            if let Some(pattern) = parse_predicate(db, attr, &mut diagnostics) {
-                if !cfg_set.is_superset(&pattern) {
-                    return PluginResult { code: None, diagnostics, remove_original_item: true };
-                }
-            }
+        let mut data = ConfigMacroHelper {
+            cfg_set: db.cfg_set(),
+            result_code: String::new(),
+            code_changed: false,
+            diagnostics: Vec::new(),
+        };
+        if data.should_drop(db, &item_ast) {
+            return PluginResult {
+                code: None,
+                diagnostics: data.diagnostics,
+                remove_original_item: true,
+            };
         }
-        PluginResult { diagnostics, ..Default::default() }
+        data.traverse(db, item_ast.as_syntax_node());
+        if data.code_changed {
+            PluginResult {
+                code: Some(PluginGeneratedFile {
+                    name: "config".into(),
+                    content: data.result_code.clone(),
+                    aux_data: DynGeneratedFileAuxData(Arc::new(TrivialPluginAuxData {})),
+                }),
+                diagnostics: data.diagnostics,
+                remove_original_item: true,
+            }
+        } else {
+            PluginResult { code: None, diagnostics: data.diagnostics, remove_original_item: false }
+        }
     }
 }
 impl AsDynMacroPlugin for ConfigPlugin {
@@ -41,6 +62,67 @@ impl AsDynMacroPlugin for ConfigPlugin {
     }
 }
 impl SemanticPlugin for ConfigPlugin {}
+
+/// Helper for the [`ConfigPlugin`] that accumulates the code of the item, while dropping items not
+/// matching the configuration.
+struct ConfigMacroHelper {
+    cfg_set: Arc<CfgSet>,
+    result_code: String,
+    code_changed: bool,
+    diagnostics: Vec<PluginDiagnostic>,
+}
+impl ConfigMacroHelper {
+    /// Traverse the syntax tree, accumulates the code without the dropped parts.
+    fn traverse(&mut self, db: &dyn SyntaxGroup, syntax_node: SyntaxNode) {
+        if let Some(text) = syntax_node.text(db) {
+            self.result_code.push_str(&text);
+        }
+        match syntax_node.kind(db) {
+            SyntaxKind::ItemList => {
+                self.expand_list_node(db, ast::ItemList::from_syntax_node(db, syntax_node))
+            }
+            SyntaxKind::TraitItemList => {
+                self.expand_list_node(db, ast::TraitItemList::from_syntax_node(db, syntax_node))
+            }
+            SyntaxKind::ImplItemList => {
+                self.expand_list_node(db, ast::ImplItemList::from_syntax_node(db, syntax_node))
+            }
+            _ => {
+                for child in syntax_node.children(db) {
+                    self.traverse(db, child);
+                }
+            }
+        }
+    }
+
+    /// Expands the node of an item list, while dropping elements that needs to be dropped.
+    fn expand_list_node<
+        Item: QueryAttrs + TypedSyntaxNode,
+        List: Deref<Target = ElementList<Item, 1>>,
+    >(
+        &mut self,
+        db: &dyn SyntaxGroup,
+        list: List,
+    ) {
+        for item in list.elements(db) {
+            if self.should_drop(db, &item) {
+                self.code_changed = true;
+            } else {
+                self.traverse(db, item.as_syntax_node())
+            }
+        }
+    }
+
+    /// Check if the given item should be dropped from the AST.
+    fn should_drop<Item: QueryAttrs>(&mut self, db: &dyn SyntaxGroup, item: &Item) -> bool {
+        item.query_attr(db, "cfg").into_iter().any(|attr| {
+            matches!(
+                parse_predicate(db, attr.structurize(db), &mut self.diagnostics),
+                Some(pattern) if !self.cfg_set.is_superset(&pattern)
+            )
+        })
+    }
+}
 
 /// Parse `#[cfg(...)]` attribute arguments as a predicate matching [`Cfg`] items.
 fn parse_predicate(
