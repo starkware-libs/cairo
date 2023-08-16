89ac1866dfc2bbeaf9ff62709a198ad35bda6fd0
Ori Ziv <oriz@starkware.co>
2023-08-16 16:57:47 +0300

    Improved array! macro diagnostics.
    commit-id:944d2078

--- crates/cairo-lang-semantic/src/inline_macros/array.rs
+++ crates/cairo-lang-semantic/src/inline_macros/array.rs
@@ -1,3 +1,4 @@
+use cairo_lang_defs::patcher::PatchBuilder;
 use cairo_lang_defs::plugin::{InlineMacroExprPlugin, InlinePluginResult, PluginGeneratedFile};
 use cairo_lang_syntax::node::db::SyntaxGroup;
 use cairo_lang_syntax::node::{ast, TypedSyntaxNode};
@@ -15,25 +16,28 @@ impl InlineMacroExprPlugin for ArrayMacro {
         let ast::WrappedExprList::BracketedExprList(args) = syntax.arguments(db) else {
             return unsupported_bracket_diagnostic(db, syntax);
         };
+        let mut builder = PatchBuilder::new(db);
         let args = args.expressions(db).elements(db);
-        let mut code = "{
-            let mut __array_builder_macro_result__ = ArrayTrait::new();"
-            .to_string();
+        builder.add_str(
+            "{
+            let mut __array_builder_macro_result__ = ArrayTrait::new();",
+        );
         for arg in args {
-            code.push_str(&format!(
-                "\n            array::ArrayTrait::append(ref __array_builder_macro_result__, {});",
-                arg.as_syntax_node().get_text(db)
-            ));
+            builder.add_str(
+                "\n            array::ArrayTrait::append(ref __array_builder_macro_result__, ",
+            );
+            builder.add_node(arg.as_syntax_node());
+            builder.add_str(");");
         }
-        code.push_str(
+        builder.add_str(
             "\n            __array_builder_macro_result__
         }",
         );
         InlinePluginResult {
             code: Some(PluginGeneratedFile {
                 name: "array_inline_macro".into(),
-                content: code,
-                diagnostics_mappings: vec![],
+                content: builder.code,
+                diagnostics_mappings: builder.diagnostics_mappings,
                 aux_data: None,
             }),
             diagnostics: vec![],
