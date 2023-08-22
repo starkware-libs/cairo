073e8d55f2dda8323c2f686d90ba9402c65e3fba
Ori Ziv <oriz@starkware.co>
2023-08-22 09:54:07 +0300

    Made Destruct implementation do a single deconstruct.
    Now would work for empty structs correctly as well.
    
    commit-id:4bd24d3b

--- crates/cairo-lang-plugins/src/plugins/derive/destruct.rs
+++ crates/cairo-lang-plugins/src/plugins/derive/destruct.rs
@@ -13,29 +13,34 @@ pub fn handle_destruct(
     result: &mut DeriveResult,
 ) {
     let full_typename = info.full_typename();
+    let ty = &info.name;
     let header = info.format_impl_header("Destruct", &["Destruct"]);
     let body = indent_by(
         8,
         match &info.specific_info {
             TypeVariantInfo::Enum(variants) => {
                 formatdoc! {"
-                match self {{
-                    {}
-                }}",
+                    match self {{
+                        {}
+                    }}",
                     variants.iter().map(|variant| {
-                    format!(
-                        "{ty}::{variant}(x) => traits::Destruct::destruct(x),",
-                        ty=info.name,
-                        variant=variant.name,
-                    )
-                }).join("\n    ")}
+                        format!(
+                            "{ty}::{}(x) => traits::Destruct::destruct(x),",
+                            variant.name,
+                        )
+                    }).join("\n    ")
+                }
             }
-            TypeVariantInfo::Struct(members) => members
-                .iter()
-                .map(|member| {
-                    format!("traits::Destruct::destruct(self.{member});", member = member.name,)
-                })
-                .join("\n"),
+            TypeVariantInfo::Struct(members) => {
+                format!(
+                    "let {ty} {{ {} }} = self;{}",
+                    members.iter().map(|member| &member.name).join(", "),
+                    members
+                        .iter()
+                        .map(|member| format!("\ntraits::Destruct::destruct({});", member.name))
+                        .join(""),
+                )
+            }
             TypeVariantInfo::Extern => {
                 result.diagnostics.push(unsupported_for_extern_diagnostic(stable_ptr));
                 return;
