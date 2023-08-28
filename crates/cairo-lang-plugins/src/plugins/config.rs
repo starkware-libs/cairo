2eb8e02e20f0115f2a9e702e657c364d8ce348cb
Ori Ziv <oriz@starkware.co>
2023-08-28 14:36:18 +0300

    Made diagnostics be properly mapped on cfg rewrites.
    commit-id:4d02e15a

--- crates/cairo-lang-plugins/src/plugins/config.rs
+++ crates/cairo-lang-plugins/src/plugins/config.rs
@@ -1,16 +1,15 @@
-use std::ops::Deref;
-use std::sync::Arc;
+use std::vec;
 
+use cairo_lang_defs::patcher::PatchBuilder;
 use cairo_lang_defs::plugin::{MacroPlugin, PluginDiagnostic, PluginGeneratedFile, PluginResult};
 use cairo_lang_filesystem::cfg::{Cfg, CfgSet};
 use cairo_lang_syntax::attribute::structured::{
     Attribute, AttributeArg, AttributeArgVariant, AttributeStructurize,
 };
 use cairo_lang_syntax::node::db::SyntaxGroup;
-use cairo_lang_syntax::node::element_list::ElementList;
 use cairo_lang_syntax::node::helpers::QueryAttrs;
-use cairo_lang_syntax::node::kind::SyntaxKind;
-use cairo_lang_syntax::node::{ast, SyntaxNode, Terminal, TypedSyntaxNode};
+use cairo_lang_syntax::node::{ast, Terminal, TypedSyntaxNode};
+use cairo_lang_utils::try_extract_matches;
 
 /// Plugin that enables ignoring modules not involved in the current config.
 /// Mostly useful for marking test modules to prevent usage of their functionality out of tests,
@@ -21,119 +20,110 @@ pub struct ConfigPlugin;
 
 impl MacroPlugin for ConfigPlugin {
     fn generate_code(&self, db: &dyn SyntaxGroup, item_ast: ast::Item) -> PluginResult {
-        let mut data = ConfigMacroHelper {
-            cfg_set: db.cfg_set(),
-            result_code: String::new(),
-            diagnostics: Vec::new(),
-        };
-        if data.should_drop(db, &item_ast) {
-            PluginResult { code: None, diagnostics: data.diagnostics, remove_original_item: true }
-        } else if !data.rewrite_required(db, &item_ast) {
-            PluginResult { code: None, diagnostics: data.diagnostics, remove_original_item: false }
-        } else {
-            data.traverse(db, item_ast.as_syntax_node());
+        let cfg_set = db.cfg_set();
+        let mut diagnostics = vec![];
+        if should_drop(db, &cfg_set, &item_ast, &mut diagnostics) {
+            PluginResult { code: None, diagnostics, remove_original_item: true }
+        } else if let Some(builder) =
+            handle_undropped_item(db, &cfg_set, item_ast, &mut diagnostics)
+        {
             PluginResult {
                 code: Some(PluginGeneratedFile {
                     name: "config".into(),
-                    content: data.result_code.clone(),
-                    diagnostics_mappings: vec![],
+                    content: builder.code,
+                    diagnostics_mappings: builder.diagnostics_mappings,
                     aux_data: None,
                 }),
-                diagnostics: data.diagnostics,
+                diagnostics,
                 remove_original_item: true,
             }
+        } else {
+            PluginResult { code: None, diagnostics, remove_original_item: false }
         }
     }
 }
 
-/// Helper for the [`ConfigPlugin`] that accumulates the code of the item, while dropping items not
-/// matching the configuration.
-struct ConfigMacroHelper {
-    cfg_set: Arc<CfgSet>,
-    result_code: String,
-    diagnostics: Vec<PluginDiagnostic>,
+/// Handle an item that is not dropped from the AST directly.
+/// In case it includes a dropped elements and needs to be rewritten it returns the appropriate
+/// PatchBuilder. Otherwise returns `None`, and it won't be rewritten or dropped.
+fn handle_undropped_item<'a>(
+    db: &'a dyn SyntaxGroup,
+    cfg_set: &CfgSet,
+    item_ast: ast::Item,
+    diagnostics: &mut Vec<PluginDiagnostic>,
+) -> Option<PatchBuilder<'a>> {
+    match item_ast {
+        ast::Item::Trait(trait_item) => {
+            let body = try_extract_matches!(trait_item.body(db), ast::MaybeTraitBody::Some)?;
+            let items =
+                get_kept_items_nodes(db, cfg_set, body.items(db).elements(db), diagnostics)?;
+            let mut builder = PatchBuilder::new(db);
+            builder.add_node(trait_item.attributes(db).as_syntax_node());
+            builder.add_node(trait_item.trait_kw(db).as_syntax_node());
+            builder.add_node(trait_item.name(db).as_syntax_node());
+            builder.add_node(trait_item.generic_params(db).as_syntax_node());
+            builder.add_node(body.lbrace(db).as_syntax_node());
+            for item in items {
+                builder.add_node(item);
+            }
+            builder.add_node(body.rbrace(db).as_syntax_node());
+            Some(builder)
+        }
+        ast::Item::Impl(impl_item) => {
+            let body = try_extract_matches!(impl_item.body(db), ast::MaybeImplBody::Some)?;
+            let items =
+                get_kept_items_nodes(db, cfg_set, body.items(db).elements(db), diagnostics)?;
+            let mut builder = PatchBuilder::new(db);
+            builder.add_node(impl_item.attributes(db).as_syntax_node());
+            builder.add_node(impl_item.impl_kw(db).as_syntax_node());
+            builder.add_node(impl_item.name(db).as_syntax_node());
+            builder.add_node(impl_item.generic_params(db).as_syntax_node());
+            builder.add_node(impl_item.of_kw(db).as_syntax_node());
+            builder.add_node(impl_item.trait_path(db).as_syntax_node());
+            builder.add_node(body.lbrace(db).as_syntax_node());
+            for item in items {
+                builder.add_node(item);
+            }
+            builder.add_node(body.rbrace(db).as_syntax_node());
+            Some(builder)
+        }
+        _ => None,
+    }
 }
-impl ConfigMacroHelper {
-    /// Traverse the syntax tree, accumulates the code without the dropped parts.
-    fn traverse(&mut self, db: &dyn SyntaxGroup, syntax_node: SyntaxNode) {
-        if let Some(text) = syntax_node.text(db) {
-            self.result_code.push_str(&text);
-        }
-        match syntax_node.kind(db) {
-            SyntaxKind::ItemList => {
-                self.expand_list_node(db, ast::ItemList::from_syntax_node(db, syntax_node))
-            }
-            SyntaxKind::TraitItemList => {
-                self.expand_list_node(db, ast::TraitItemList::from_syntax_node(db, syntax_node))
-            }
-            SyntaxKind::ImplItemList => {
-                self.expand_list_node(db, ast::ImplItemList::from_syntax_node(db, syntax_node))
-            }
-            _ => {
-                for child in syntax_node.children(db) {
-                    self.traverse(db, child);
-                }
-            }
-        }
-    }
 
-    /// Expands the node of an item list, while dropping elements that needs to be dropped.
-    fn expand_list_node<
-        Item: QueryAttrs + TypedSyntaxNode,
-        List: Deref<Target = ElementList<Item, 1>>,
-    >(
-        &mut self,
-        db: &dyn SyntaxGroup,
-        list: List,
-    ) {
-        for item in list.elements(db) {
-            if !self.should_drop(db, &item) {
-                self.traverse(db, item.as_syntax_node())
-            }
+/// Get the list of items that should be kept in the AST.
+/// Returns `None` if all items should be kept.
+fn get_kept_items_nodes<Item: QueryAttrs + TypedSyntaxNode>(
+    db: &dyn SyntaxGroup,
+    cfg_set: &CfgSet,
+    all_items: Vec<Item>,
+    diagnostics: &mut Vec<PluginDiagnostic>,
+) -> Option<Vec<cairo_lang_syntax::node::SyntaxNode>> {
+    let mut any_dropped = false;
+    let mut kept_items_nodes = vec![];
+    for item in all_items {
+        if should_drop(db, cfg_set, &item, diagnostics) {
+            any_dropped = true;
+        } else {
+            kept_items_nodes.push(item.as_syntax_node());
         }
     }
+    if any_dropped { Some(kept_items_nodes) } else { None }
+}
 
-    /// Checks if any items within the trait should be dropped.
-    /// This is a more shallow lookup than full traverse.
-    fn rewrite_required_for_trait(&mut self, db: &dyn SyntaxGroup, item: &ast::ItemTrait) -> bool {
-        match item.body(db) {
-            ast::MaybeTraitBody::Some(body) => {
-                body.items(db).elements(db).into_iter().any(|item| self.should_drop(db, &item))
-            }
-            ast::MaybeTraitBody::None(_) => false,
-        }
-    }
-
-    /// Checks if any items within the impl should be dropped.
-    /// This is a more shallow lookup than full traverse.
-    fn rewrite_required_for_impl(&mut self, db: &dyn SyntaxGroup, item: &ast::ItemImpl) -> bool {
-        match item.body(db) {
-            ast::MaybeImplBody::Some(body) => {
-                body.items(db).elements(db).into_iter().any(|item| self.should_drop(db, &item))
-            }
-            ast::MaybeImplBody::None(_) => false,
-        }
-    }
-
-    /// Checks if any items within this item should be dropped.
-    /// This is a more shallow lookup than full traverse.
-    fn rewrite_required(&mut self, db: &dyn SyntaxGroup, item: &ast::Item) -> bool {
-        match item {
-            ast::Item::Trait(trait_item) => self.rewrite_required_for_trait(db, trait_item),
-            ast::Item::Impl(impl_item) => self.rewrite_required_for_impl(db, impl_item),
-            _ => false,
-        }
-    }
-
-    /// Check if the given item should be dropped from the AST.
-    fn should_drop<Item: QueryAttrs>(&mut self, db: &dyn SyntaxGroup, item: &Item) -> bool {
-        item.query_attr(db, "cfg").into_iter().any(|attr| {
-            matches!(
-                parse_predicate(db, attr.structurize(db), &mut self.diagnostics),
-                Some(pattern) if !self.cfg_set.is_superset(&pattern)
-            )
-        })
-    }
+/// Check if the given item should be dropped from the AST.
+fn should_drop<Item: QueryAttrs>(
+    db: &dyn SyntaxGroup,
+    cfg_set: &CfgSet,
+    item: &Item,
+    diagnostics: &mut Vec<PluginDiagnostic>,
+) -> bool {
+    item.query_attr(db, "cfg").into_iter().any(|attr| {
+        matches!(
+            parse_predicate(db, attr.structurize(db), diagnostics),
+            Some(pattern) if !cfg_set.is_superset(&pattern)
+        )
+    })
 }
 
 /// Parse `#[cfg(...)]` attribute arguments as a predicate matching [`Cfg`] items.
