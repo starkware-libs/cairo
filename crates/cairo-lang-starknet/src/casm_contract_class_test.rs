e3a4005029ddab791da954849921bd0574f03fe5
MrDenkoV <stanislaw.denkowski@swmansion.com>
2023-08-31 15:26:02 +0200

    Changed extension convention

--- crates/cairo-lang-starknet/src/casm_contract_class_test.rs
+++ crates/cairo-lang-starknet/src/casm_contract_class_test.rs
@@ -8,7 +8,8 @@ use crate::casm_contract_class::{BigUintAsHex, CasmContractClass, StarknetSierra
 use crate::contract_class::ContractClass;
 use crate::test_utils::{get_example_file_path, get_test_contract};
 
-/// Tests that the casm compiled from <test_case>.cairo is the same as in <test_case>.casm.json.
+/// Tests that the casm compiled from <test_case>.cairo is the same as in
+/// <test_case>.compiled_contract_class.json.
 #[test_case("account")]
 #[test_case("test_contract")]
 #[test_case("new_syntax_test_contract")]
@@ -23,7 +24,9 @@ fn test_casm_contract_from_contract_class(example_file_name: &str) {
         CasmContractClass::from_contract_class(contract_class, add_pythonic_hints).unwrap();
 
     compare_contents_or_fix_with_path(
-        &get_example_file_path(format!("{example_file_name}.casm.json").as_str()),
+        &get_example_file_path(
+            format!("{example_file_name}.compiled_contract_class.json").as_str(),
+        ),
         serde_json::to_string_pretty(&casm_contract).unwrap() + "\n",
     );
 }
@@ -31,8 +34,10 @@ fn test_casm_contract_from_contract_class(example_file_name: &str) {
 #[test_case("test_contract")]
 #[test_case("new_syntax_test_contract")]
 fn test_casm_contract_from_contract_class_failure(example_file_name: &str) {
-    let f = std::fs::File::open(get_example_file_path(&format!("{example_file_name}.sierra.json")))
-        .unwrap();
+    let f = std::fs::File::open(get_example_file_path(&format!(
+        "{example_file_name}.contract_class.json"
+    )))
+    .unwrap();
     let mut contract_class: ContractClass = serde_json::from_reader(BufReader::new(f)).unwrap();
     contract_class.sierra_program[17] = BigUintAsHex { value: Felt252::prime() };
 
