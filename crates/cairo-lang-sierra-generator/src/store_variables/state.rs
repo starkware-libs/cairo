ad686f4e468fafa56400a081fa5ba9e1c3ba4f18
Ilya Lesokhin <ilya@starkware.co>
2023-09-12 11:27:09 +0300

    Fix var name.

--- crates/cairo-lang-sierra-generator/src/store_variables/state.rs
+++ crates/cairo-lang-sierra-generator/src/store_variables/state.rs
@@ -151,8 +151,8 @@ impl State {
     /// Updates [Self::known_stack] and [Self::temporary_variables] if necessary.
     pub fn rename_var(&mut self, src: &sierra::ids::VarId, dst: &sierra::ids::VarId) {
         self.known_stack.clone_if_on_stack(src, dst);
-        if let Some(uninitialized_local_var_id) = self.temporary_variables.get(src) {
-            self.temporary_variables.insert(dst.clone(), uninitialized_local_var_id.clone());
+        if let Some(ty) = self.temporary_variables.get(src) {
+            self.temporary_variables.insert(dst.clone(), ty.clone());
         }
     }
 }
