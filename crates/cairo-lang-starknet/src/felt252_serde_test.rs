45534044c30b277ec1a623cc9637121a86562183
Yuval Goldberg <yuval@starkware.co>
2023-09-19 12:55:34 +0300

    Added ABI test with components

--- crates/cairo-lang-starknet/src/felt252_serde_test.rs
+++ crates/cairo-lang-starknet/src/felt252_serde_test.rs
@@ -15,6 +15,7 @@ use crate::test_utils::get_example_file_path;
 #[test_case("test_contract")]
 #[test_case("new_syntax_test_contract")]
 #[test_case("hello_starknet")]
+#[test_case("upgradable_counter")]
 fn test_felt252_serde(example_file_name: &str) {
     let sierra = ProgramParser::new()
         .parse(
