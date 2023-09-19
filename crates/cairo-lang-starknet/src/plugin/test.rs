b065d0124ab13589991c149ab98d801b480b2c49
Gil Ben-Shachar <gil@starkware.co>
2023-09-19 15:35:28 +0300

    Add an ERC20 component example.

--- crates/cairo-lang-starknet/src/plugin/test.rs
+++ crates/cairo-lang-starknet/src/plugin/test.rs
@@ -78,6 +78,7 @@ cairo_lang_test_utils::test_file_test_with_runner!(
         with_component: "with_component",
         with_component_diagnostics: "with_component_diagnostics",
         with_ownable: "with_ownable",
+        with_erc20: "with_erc20",
         upgradable_counter: "upgradable_counter",
     },
     ExpandContractTestRunner
