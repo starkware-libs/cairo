303037952022c42b02c4e47676c924b62f6edc3b
Yuval Goldberg <yuval@starkware.co>
2023-09-19 12:55:34 +0300

    Added ABI test with components

--- crates/cairo-lang-starknet/src/contract_class_test.rs
+++ crates/cairo-lang-starknet/src/contract_class_test.rs
@@ -59,6 +59,7 @@ fn test_serialization() {
 #[test_case("new_syntax_test_contract")]
 #[test_case("hello_starknet")]
 #[test_case("erc20")]
+#[test_case("upgradable_counter")]
 fn test_full_contract_deserialization(example_file_name: &str) {
     let contract = get_test_contract(format!("{example_file_name}.cairo").as_str());
     let serialized = serde_json::to_string_pretty(&contract).unwrap();
@@ -74,6 +75,7 @@ fn test_full_contract_deserialization(example_file_name: &str) {
 #[test_case("hello_starknet")]
 #[test_case("erc20")]
 #[test_case("token_bridge")]
+#[test_case("upgradable_counter")]
 fn test_compile_path(example_file_name: &str) {
     let contract = get_test_contract(format!("{example_file_name}.cairo").as_str());
 
