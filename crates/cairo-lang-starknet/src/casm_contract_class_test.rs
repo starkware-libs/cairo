223c0344b2fc671d401905f6d6f8e7a1e826725e
Gil Ben-Shachar <gil@starkware.co>
2023-10-05 11:49:12 +0300

    Add contract casm test from the contracts crate.

--- crates/cairo-lang-starknet/src/casm_contract_class_test.rs
+++ crates/cairo-lang-starknet/src/casm_contract_class_test.rs
@@ -6,7 +6,10 @@ use test_case::test_case;
 
 use crate::casm_contract_class::{BigUintAsHex, CasmContractClass, StarknetSierraCompilationError};
 use crate::contract_class::ContractClass;
-use crate::test_utils::{get_example_file_path, get_test_contract};
+use crate::test_utils::{
+    get_contract_file_name_from_path, get_example_file_path, get_test_contract,
+    get_test_contract_from_contracts_crate,
+};
 
 /// Tests that the casm compiled from <test_case>.cairo is the same as in
 /// <test_case>.compiled_contract_class.json.
@@ -50,5 +53,26 @@ fn test_casm_contract_from_contract_class_failure(example_file_name: &str) {
     assert_eq!(
         CasmContractClass::from_contract_class(contract_class, add_pythonic_hints),
         Err(StarknetSierraCompilationError::ValueOutOfRange)
+    );
+}
+
+/// Tests that the casm compiled from a contract in the contract_crate is the same as in
+/// <test_case>.compiled_contract_class.json.
+/// TODO(Gil): Merge with `test_casm_contract_from_contract_class`.
+#[test_case("multi_component::contract_with_4_components")]
+fn test_casm_contract_from_contract_class_from_contracts_crate(example_contract_path: &str) {
+    let contract_class = get_test_contract_from_contracts_crate(
+        format!("cairo_level_tests::contracts::{example_contract_path}").as_str(),
+    );
+    let example_file_name = get_contract_file_name_from_path(example_contract_path);
+    let add_pythonic_hints = true;
+    let casm_contract =
+        CasmContractClass::from_contract_class(contract_class, add_pythonic_hints).unwrap();
+
+    compare_contents_or_fix_with_path(
+        &get_example_file_path(
+            format!("{example_file_name}.compiled_contract_class.json").as_str(),
+        ),
+        serde_json::to_string_pretty(&casm_contract).unwrap() + "\n",
     );
 }
