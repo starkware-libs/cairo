23b92b4818b6e6e0f4a5417992a7c93a74f8ceca
yuvalsw <105596564+yuvalsw@users.noreply.github.com>
2023-11-01 10:49:57 +0200

    Added macro `extract_macro_unnamed_args` (#4330)

--- crates/cairo-lang-semantic/src/inline_macros/mod.rs
+++ crates/cairo-lang-semantic/src/inline_macros/mod.rs
@@ -8,6 +8,7 @@ use std::sync::Arc;
 use cairo_lang_defs::plugin::{InlineMacroExprPlugin, InlinePluginResult, PluginDiagnostic};
 use cairo_lang_syntax::node::ast::{self};
 use cairo_lang_syntax::node::db::SyntaxGroup;
+use cairo_lang_syntax::node::helpers::WrappedArgListHelper;
 use cairo_lang_syntax::node::TypedSyntaxNode;
 use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
 
@@ -34,7 +35,7 @@ pub fn unsupported_bracket_diagnostic(
     InlinePluginResult {
         code: None,
         diagnostics: vec![PluginDiagnostic {
-            stable_ptr: macro_ast.stable_ptr().untyped(),
+            stable_ptr: macro_ast.arguments(db).left_bracket_stable_ptr(db),
             message: format!(
                 "Macro `{}` does not support this bracket type.",
                 macro_ast.path(db).as_syntax_node().get_text_without_trivia(db)
@@ -55,6 +56,19 @@ pub fn extract_single_unnamed_arg(
     }
 }
 
+/// Extracts `n` unnamed arguments.
+pub fn extract_unnamed_args(
+    db: &dyn SyntaxGroup,
+    macro_arguments: &ast::ArgList,
+    n: usize,
+) -> Option<Vec<ast::Expr>> {
+    let elements = macro_arguments.elements(db);
+    if elements.len() != n {
+        return None;
+    }
+    elements.iter().map(|x| try_extract_unnamed_arg(db, x)).collect()
+}
+
 /// Gets the syntax of an argument, and extracts the value if it is unnamed.
 pub fn try_extract_unnamed_arg(db: &dyn SyntaxGroup, arg_ast: &ast::Arg) -> Option<ast::Expr> {
     if let ast::ArgClause::Unnamed(arg_clause) = arg_ast.arg_clause(db) {
@@ -62,4 +76,63 @@ pub fn try_extract_unnamed_arg(db: &dyn SyntaxGroup, arg_ast: &ast::Arg) -> Opti
     } else {
         None
     }
+}
+
+/// Macro to extract unnamed arguments of an inline macro.
+/// Gets the expected number of unnamed arguments, and the pattern for the allowed bracket types,
+/// and returns a fixed size array with the argument expressions.
+///
+/// Example usage (2 arguments, allowing `()` or `{}` brackets):
+/// let [arg1, arg2] = extract_macro_unnamed_args!(
+///     db,
+///     syntax,
+///     2,
+///     ast::WrappedArgList::ParenthesizedArgList(_) | ast::WrappedArgList::BracedArgList(_)
+/// );
+#[macro_export]
+macro_rules! extract_macro_unnamed_args {
+    ($db:expr, $syntax:expr, $n:expr, $pattern:pat) => {{
+        if !matches!($syntax.arguments($db), $pattern) {
+            return $crate::inline_macros::unsupported_bracket_diagnostic($db, $syntax);
+        }
+        // `unwrap` is ok because the above `matches` condition ensures it's not None (unless
+        // the pattern contains the `Missing` variant).
+        let macro_arg_list = cairo_lang_syntax::node::helpers::WrappedArgListHelper::arg_list(
+            &$syntax.arguments($db),
+            $db,
+        )
+        .unwrap();
+
+        let args = $crate::inline_macros::extract_unnamed_args($db, &macro_arg_list, $n);
+        let Some(args) = args else {
+            let diagnostics = vec![PluginDiagnostic {
+                stable_ptr: $syntax.stable_ptr().untyped(),
+                message: format!(
+                    "Macro `{}` must have exactly {} unnamed arguments.",
+                    $syntax.path($db).as_syntax_node().get_text_without_trivia($db),
+                    $n
+                ),
+            }];
+            return InlinePluginResult { code: None, diagnostics };
+        };
+        let args: [ast::Expr; $n] = args.try_into().unwrap();
+        args
+    }};
+}
+
+/// Macro to extract a single unnamed argument of an inline macro.
+/// Gets the pattern for the allowed bracket types, and returns the argument expression.
+///
+/// Example usage (allowing `()` or `{}` brackets):
+/// let arg = extract_macro_single_unnamed_arg!(
+///     db,
+///     syntax,
+///     ast::WrappedArgList::ParenthesizedArgList(_) | ast::WrappedArgList::BracedArgList(_)
+/// );
+#[macro_export]
+macro_rules! extract_macro_single_unnamed_arg {
+    ($db:expr, $syntax:expr, $pattern:pat) => {{
+        let [x] = $crate::extract_macro_unnamed_args!($db, $syntax, 1, $pattern);
+        x
+    }};
 }
