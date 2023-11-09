cf6314667afc20cac3f7e612064d8a7735654e97
Ori Ziv <oriz@starkware.co>
2023-11-09 16:19:29 +0200

    Added binary validating sierra class hashes.
    Added ability to calculate compiled class hash for support.

--- crates/cairo-lang-starknet/src/casm_contract_class.rs
+++ crates/cairo-lang-starknet/src/casm_contract_class.rs
@@ -34,6 +34,7 @@ use num_integer::Integer;
 use num_traits::Signed;
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
+use starknet_crypto::{poseidon_hash_many, FieldElement};
 use thiserror::Error;
 
 use crate::allowed_libfuncs::AllowedLibfuncsError;
@@ -98,6 +99,54 @@ pub struct CasmContractClass {
     #[serde(skip_serializing_if = "skip_if_none")]
     pub pythonic_hints: Option<Vec<(usize, Vec<String>)>>,
     pub entry_points_by_type: CasmContractEntryPoints,
+}
+impl CasmContractClass {
+    /// Returns the hash value for the compiled contract class.
+    pub fn compiled_class_hash(&self) -> Felt252 {
+        // Compute hashes on each component separately.
+        let external_funcs_hash = self.entry_points_hash(&self.entry_points_by_type.external);
+        let l1_handlers_hash = self.entry_points_hash(&self.entry_points_by_type.l1_handler);
+        let constructors_hash = self.entry_points_hash(&self.entry_points_by_type.constructor);
+        let bytecode_hash = poseidon_hash_many(
+            &self
+                .bytecode
+                .iter()
+                .map(|big_uint| {
+                    FieldElement::from_byte_slice_be(&big_uint.value.to_bytes_be()).unwrap()
+                })
+                .collect_vec(),
+        );
+
+        // Compute total hash by hashing each component on top of the previous one.
+        Felt252::from_bytes_be(
+            &poseidon_hash_many(&[
+                FieldElement::from_byte_slice_be(b"COMPILED_CLASS_V1").unwrap(),
+                external_funcs_hash,
+                l1_handlers_hash,
+                constructors_hash,
+                bytecode_hash,
+            ])
+            .to_bytes_be(),
+        )
+    }
+    /// Returns the hash for a set of entry points.
+    fn entry_points_hash(&self, entry_points: &[CasmContractEntryPoint]) -> FieldElement {
+        let mut entry_point_hash_elements = vec![];
+        for entry_point in entry_points {
+            entry_point_hash_elements.push(
+                FieldElement::from_byte_slice_be(&entry_point.selector.to_bytes_be()).unwrap(),
+            );
+            entry_point_hash_elements.push(FieldElement::from(entry_point.offset));
+            entry_point_hash_elements.push(poseidon_hash_many(
+                &entry_point
+                    .builtins
+                    .iter()
+                    .map(|builtin| FieldElement::from_byte_slice_be(builtin.as_bytes()).unwrap())
+                    .collect_vec(),
+            ));
+        }
+        poseidon_hash_many(&entry_point_hash_elements)
+    }
 }
 
 /// Context for resolving types.
