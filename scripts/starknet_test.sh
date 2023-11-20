cfae7d00d2a5fdef816d6dde7e0c411071581eca
clint <clint@zklink.org>
2023-11-20 14:04:38 +0800

    Added assert_ne! test macro

--- scripts/starknet_test.sh
+++ scripts/starknet_test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cargo run --bin cairo-test -- \
-    crates/cairo-lang-starknet/cairo_level_tests/ --starknet
+    crates/cairo-lang-starknet/cairo_level_tests/ --starknet && \
+cargo run --bin cairo-test -- \
+    crates/cairo-lang-test-runner/test_data/ --starknet
