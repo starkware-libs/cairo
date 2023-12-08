287be954dec67d38ac1f03e22218938676bafced
Lior Goldberg <lior@starkware.co>
2023-12-08 22:22:46 +0200

    Small refactor in function_call.rs.
    commit-id:18ad7b32

--- crates/cairo-lang-sierra/src/extensions/modules/function_call.rs
+++ crates/cairo-lang-sierra/src/extensions/modules/function_call.rs
@@ -3,7 +3,35 @@ use crate::extensions::lib_func::{
     SpecializationContext,
 };
 use crate::extensions::{NamedLibfunc, OutputVarReferenceInfo, SpecializationError};
-use crate::program::{Function, GenericArg};
+use crate::program::{Function, FunctionSignature, GenericArg};
+
+/// Returns the [OutputVarInfo] instances for the return types of the given function signature.
+fn get_output_var_infos(
+    context: &dyn SignatureSpecializationContext,
+    signature: FunctionSignature,
+) -> Result<Vec<OutputVarInfo>, SpecializationError> {
+    let mut curr_stack_idx = 0;
+    let mut get_stack_idx = || {
+        let idx = curr_stack_idx;
+        curr_stack_idx += 1;
+        idx
+    };
+
+    signature
+        .ret_types
+        .iter()
+        .map(|ty| {
+            Ok(OutputVarInfo {
+                ty: ty.clone(),
+                ref_info: if context.get_type_info(ty.clone())?.zero_sized {
+                    OutputVarReferenceInfo::ZeroSized
+                } else {
+                    OutputVarReferenceInfo::NewTempVar { idx: get_stack_idx() }
+                },
+            })
+        })
+        .collect::<Result<Vec<_>, _>>()
+}
 
 /// Libfunc used to call user functions.
 #[derive(Default)]
@@ -17,38 +45,17 @@ impl NamedLibfunc for FunctionCallLibfunc {
         context: &dyn SignatureSpecializationContext,
         args: &[GenericArg],
     ) -> Result<LibfuncSignature, SpecializationError> {
-        let mut curr_stack_idx = 0;
-        let mut get_stack_idx = || {
-            let idx = curr_stack_idx;
-            curr_stack_idx += 1;
-            idx
+        let [GenericArg::UserFunc(function_id)] = args else {
+            return Err(SpecializationError::UnsupportedGenericArg);
         };
 
-        match args {
-            [GenericArg::UserFunc(function_id)] => {
-                let signature = context.get_function_signature(function_id)?;
-                let ap_change = context.get_function_ap_change(function_id)?;
-                Ok(LibfuncSignature::new_non_branch(
-                    signature.param_types.clone(),
-                    signature
-                        .ret_types
-                        .iter()
-                        .map(|ty| {
-                            Ok(OutputVarInfo {
-                                ty: ty.clone(),
-                                ref_info: if context.get_type_info(ty.clone())?.zero_sized {
-                                    OutputVarReferenceInfo::ZeroSized
-                                } else {
-                                    OutputVarReferenceInfo::NewTempVar { idx: get_stack_idx() }
-                                },
-                            })
-                        })
-                        .collect::<Result<Vec<_>, _>>()?,
-                    ap_change,
-                ))
-            }
-            _ => Err(SpecializationError::UnsupportedGenericArg),
-        }
+        let signature = context.get_function_signature(function_id)?;
+        let ap_change = context.get_function_ap_change(function_id)?;
+        Ok(LibfuncSignature::new_non_branch(
+            signature.param_types.clone(),
+            get_output_var_infos(context, signature)?,
+            ap_change,
+        ))
     }
 
     fn specialize(
@@ -56,13 +63,14 @@ impl NamedLibfunc for FunctionCallLibfunc {
         context: &dyn SpecializationContext,
         args: &[GenericArg],
     ) -> Result<Self::Concrete, SpecializationError> {
-        match args {
-            [GenericArg::UserFunc(function_id)] => Ok(Self::Concrete {
-                function: context.get_function(function_id)?,
-                signature: self.specialize_signature(context.upcast(), args)?,
-            }),
-            _ => Err(SpecializationError::UnsupportedGenericArg),
-        }
+        let [GenericArg::UserFunc(function_id)] = args else {
+            return Err(SpecializationError::UnsupportedGenericArg);
+        };
+
+        Ok(Self::Concrete {
+            function: context.get_function(function_id)?,
+            signature: self.specialize_signature(context.upcast(), args)?,
+        })
     }
 }
 
