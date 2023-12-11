27d50e159587d33cfbe8027dd4e63832ff5b1635
Ilya Lesokhin <ilya@starkware.co>
2023-12-11 14:02:32 +0200

    Add const folding optimization.

---
+++ crates/cairo-lang-lowering/src/optimizations/const_folding.rs
@@ -0,0 +1,108 @@
+#[cfg(test)]
+#[path = "const_folding_test.rs"]
+mod test;
+
+use std::collections::HashMap;
+
+use cairo_lang_semantic::corelib;
+use num_bigint::BigInt;
+use num_traits::Zero;
+
+use crate::db::LoweringGroup;
+use crate::ids::FunctionLongId;
+use crate::{
+    FlatBlockEnd, FlatLowered, MatchEnumInfo, MatchExternInfo, MatchInfo, Statement, StatementCall,
+    StatementDesnap, StatementLiteral, StatementSnapshot, VarUsage,
+};
+
+/// We keep track of two var states:
+/// - Literal: the variable is a literal value
+/// - Var: the variable can be replaced by another variable
+/// We don't keep track of snap/desnap as we assume that they are cancelled out before usage.
+enum VarState {
+    Literal(BigInt),
+    Var(VarUsage),
+}
+
+/// Performs constant folding on the lowered program.
+/// The optimization works better when the blocks are topologically sorted.
+pub fn const_folding(db: &dyn LoweringGroup, lowered: &mut FlatLowered) {
+    if lowered.blocks.is_empty() {
+        return;
+    }
+
+    // Note that we can keep the var_states across blocks because the lowering
+    // is in static single assignment form.
+    let mut var_states = HashMap::new();
+
+    let semantic_db = db.upcast();
+    let felt_sub = db.intern_lowering_function(FunctionLongId::Semantic(
+        corelib::get_core_function_id(semantic_db, "felt252_sub".into(), vec![]),
+    ));
+
+    for block in lowered.blocks.iter_mut() {
+        for stmt in block.statements.iter_mut() {
+            match stmt {
+                Statement::Literal(StatementLiteral { value, output }) => {
+                    var_states.insert(*output, VarState::Literal(value.clone()));
+                }
+                Statement::Snapshot(StatementSnapshot {
+                    input,
+                    output_original,
+                    output_snapshot,
+                }) => {
+                    if let Some(VarState::Literal(val)) = var_states.get(&input.var_id) {
+                        let val = val.clone();
+                        var_states.insert(*output_original, VarState::Literal(val.clone()));
+                        var_states.insert(*output_snapshot, VarState::Literal(val));
+                    }
+                }
+                Statement::Desnap(StatementDesnap { input, output }) => {
+                    if let Some(VarState::Literal(val)) = var_states.get(&input.var_id) {
+                        let val = val.clone();
+                        var_states.insert(*output, VarState::Literal(val));
+                    }
+                }
+                Statement::Call(StatementCall { function, ref mut inputs, outputs, .. }) => {
+                    for input in &mut inputs.iter_mut() {
+                        match var_states.get(&input.var_id) {
+                            Some(VarState::Var(new_var)) => {
+                                *input = *new_var;
+                            }
+                            Some(VarState::Literal(_)) | None => {}
+                        }
+                    }
+
+                    // (a - 0) can be replaced by a.
+                    if function == &felt_sub {
+                        if let Some(VarState::Literal(val)) = var_states.get(&inputs[1].var_id) {
+                            if val.is_zero() {
+                                var_states.insert(outputs[0], VarState::Var(inputs[0]));
+                            }
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        match &mut block.end {
+            FlatBlockEnd::Return(ref mut inputs)
+            | FlatBlockEnd::Match {
+                info: MatchInfo::Extern(MatchExternInfo { ref mut inputs, .. }),
+            } => {
+                for input in inputs.iter_mut() {
+                    if let Some(VarState::Var(new_var)) = var_states.get(&input.var_id) {
+                        *input = *new_var;
+                    }
+                }
+            }
+            FlatBlockEnd::Match { info: MatchInfo::Enum(MatchEnumInfo { ref mut input, .. }) } => {
+                if let Some(VarState::Var(new_var)) = var_states.get(&input.var_id) {
+                    *input = *new_var;
+                }
+            }
+            _ => {}
+        };
+    }
+}
