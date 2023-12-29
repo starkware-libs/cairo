ea89ba238e616e1d1cb961ce75a575033051e87e
Lior <lior@starkware.co>
2023-12-29 13:44:09 +0200

    Add coupons flag to ExperimentalFeaturesConfig.
    commit-id:2e2da7fe

--- crates/cairo-lang-project/src/test.rs
+++ crates/cairo-lang-project/src/test.rs
@@ -31,7 +31,10 @@ fn test_serde() {
                     "crate3".into(),
                     CrateSettings {
                         edition: Default::default(),
-                        experimental_features: ExperimentalFeaturesConfig { negative_impls: true },
+                        experimental_features: ExperimentalFeaturesConfig {
+                            negative_impls: true,
+                            coupons: false,
+                        },
                     },
                 ),
             ]
@@ -47,25 +50,56 @@ fn test_serde() {
             crate1 = "dir1"
             crate2 = "dir2"
             crate3 = "dir3"
-            
+
             [config.global]
             edition = "2023_01"
 
             [config.global.experimental_features]
             negative_impls = false
+            coupons = false
 
             [config.override.crate1]
             edition = "2023_10"
 
             [config.override.crate1.experimental_features]
             negative_impls = false
+            coupons = false
 
             [config.override.crate3]
             edition = "2023_01"
 
             [config.override.crate3.experimental_features]
             negative_impls = true
+            coupons = false
         "# }
     );
     assert_eq!(config, toml::from_str(&serialized).unwrap());
+}
+
+#[test]
+fn test_serde_defaults() {
+    let config_str = indoc! { r#"
+        [crate_roots]
+
+        [config.global]
+        edition = "2023_01"
+
+        [config.global.experimental_features]
+        negative_impls = false
+    "# };
+    let result = indoc! { r#"
+        [crate_roots]
+
+        [config.global]
+        edition = "2023_01"
+
+        [config.global.experimental_features]
+        negative_impls = false
+        coupons = false
+
+        [config.override]
+    "# };
+
+    let config: ProjectConfigContent = toml::from_str(config_str).unwrap();
+    assert_eq!(result, toml::to_string(&config).unwrap());
 }
