dbb537dbea4a6dfd9c20b84a6c8d1d8c01600da6
Ori Ziv <oriz@starkware.co>
2024-01-01 14:06:56 +0200

    Added redeposit as an extra store_temp piggy-back.
    commit-id:6174ca5e

--- crates/cairo-lang-filesystem/src/flag.rs
+++ crates/cairo-lang-filesystem/src/flag.rs
@@ -4,4 +4,7 @@ pub enum Flag {
     /// Whether automatically add `withdraw_gas` calls in code cycles.
     /// Default is true - automatically add.
     AddWithdrawGas(bool),
+    /// Whether automatically add `redeposit_gas` calls on stores of the gas builtin.
+    /// Default is false - automatically add.
+    AddRedepositGas(bool),
 }
