be4c921fc7c49ea452f0bcda55a76cfd560b2068
Artur Michalek <artur.michalek@swmansion.com>
2024-01-03 13:29:31 +0100

    Review suggestion

--- crates/cairo-lang-runner/src/lib.rs
+++ crates/cairo-lang-runner/src/lib.rs
@@ -447,13 +447,13 @@ impl SierraCasmRunner {
                 ap += 1;
             }
             for (i, v) in values.iter().enumerate() {
-                let arr_at = i16::try_from(i + 1).unwrap();
+                let arr_at: i16 = (i + 1).into_or_panic();
                 casm_extend! {ctx,
                     [ap + 0] = (v.to_bigint());
-                    [ap + 0] = [[ap - arr_at] + i16::try_from(i).unwrap()], ap++;
+                    [ap + 0] = [[ap - arr_at] + i.into_or_panic()], ap++;
                 }
             }
-            ap_offset += i16::try_from(1 + values.len()).unwrap();
+            ap_offset += (1 + values.len()).into_or_panic();
         }
         let mut array_args_data_iter = array_args_data.iter();
         let after_arrays_data_offset = ap_offset;
