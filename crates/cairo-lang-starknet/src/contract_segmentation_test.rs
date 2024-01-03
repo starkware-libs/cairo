9515071a325c3fb39d7f0f7ed452162f62065e44
Gil Ben-Shachar <gil@starkware.co>
2024-01-03 13:50:30 +0200

    Handle consts segment in contract segmenetation.

--- crates/cairo-lang-starknet/src/contract_segmentation_test.rs
+++ crates/cairo-lang-starknet/src/contract_segmentation_test.rs
@@ -2,12 +2,12 @@ use cairo_lang_sierra::ProgramParser;
 use cairo_lang_test_utils::parse_test_file::{TestFileRunner, TestRunnerResult};
 use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
 
-use super::{find_segments, get_segment_lengths};
+use super::{compute_bytecode_segment_lengths, find_functions_segments, get_segment_lengths};
 
-/// Tests `find_segments` returns the correct result.
+/// Tests `bytecode_segment_length` and 'find_functions_segments' returns the correct result.
 #[derive(Default)]
-struct FindSegmentTest;
-impl TestFileRunner for FindSegmentTest {
+struct BytecodeSegmentLengthTest;
+impl TestFileRunner for BytecodeSegmentLengthTest {
     fn run(
         &mut self,
         inputs: &OrderedHashMap<String, String>,
@@ -17,16 +17,48 @@ impl TestFileRunner for FindSegmentTest {
             .parse(inputs.get("sierra").expect("Missing argument 'sierra' in test scenario."))
             .unwrap();
 
-        let result_str = format!("{:?}", find_segments(&sierra_program));
-        TestRunnerResult::success([("segments".to_string(), result_str)].into_iter().collect())
+        let function_segments_str = format!("{:?}", find_functions_segments(&sierra_program));
+
+        let cairo_program =
+            cairo_lang_sierra_to_casm::metadata::calc_metadata(&sierra_program, Default::default())
+                .ok()
+                .and_then(|metadata| {
+                    let gas_usage_check = false;
+                    cairo_lang_sierra_to_casm::compiler::compile(
+                        &sierra_program,
+                        &metadata,
+                        gas_usage_check,
+                    )
+                    .ok()
+                });
+        let (cairo_program_str, bytecode_lengths_str) = match cairo_program {
+            Some(cairo_program) => {
+                let bytecode_len = cairo_program.assemble().bytecode.len();
+                let bytecode_lengths =
+                    compute_bytecode_segment_lengths(&sierra_program, &cairo_program, bytecode_len);
+                (format!("{}", cairo_program), format!("{:?}", bytecode_lengths))
+            }
+            None => {
+                ("Casm compilation failed.".to_string(), "Casm compilation failed.".to_string())
+            }
+        };
+        TestRunnerResult::success(
+            [
+                ("cairo_program".to_string(), cairo_program_str),
+                ("function_segments".to_string(), function_segments_str),
+                ("bytecode_lengths".to_string(), bytecode_lengths_str),
+            ]
+            .into_iter()
+            .collect(),
+        )
     }
 }
 
 cairo_lang_test_utils::test_file_test_with_runner!(
-    test_find_segment,
+    test_bytecode_segment_lengths,
     "src/contract_segmentation_test_data",
-    { find_segment: "find_segment" },
-    FindSegmentTest
+    { find_functions_segments: "bytecode_segment_lengths" },
+    BytecodeSegmentLengthTest
 );
 
 #[test]
