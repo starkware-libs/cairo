5a5f27d34baa9b76a00504fbedfb4ce99646c9bb
Yuval Goldberg <yuval@starkware.co>
2024-01-04 17:48:51 +0200

    fixup: changed print_zeros to a numeric minimum min_weight

--- crates/cairo-lang-runner/src/profiling.rs
+++ crates/cairo-lang-runner/src/profiling.rs
@@ -11,38 +11,41 @@ pub struct ProfilingInfo {
     pub concrete_sierra_statements_weights: HashMap<StatementIdx, usize>,
 }
 
-/// Flags controlling how the profiling info is printed by the `ProfilingInfoPrinter`.
-pub struct ProfilingInfoPrinterFlags {
-    print_zeros: bool,
+/// Parameters controlling how the profiling info is printed by the `ProfilingInfoPrinter`.
+pub struct ProfilingInfoPrinterParams {
+    /// The minimal weight to print. Used for all printed stats. That is - the sum of the weights
+    /// per statement may smaller than the sum of the weights per concrete libfunc, that may be
+    /// smaller than the sum of the weights ber generic libfunc.
+    min_weight: usize,
     print_by_concrete_libfunc: bool,
     print_by_generic_libfunc: bool,
 }
 /// A printer for profiling info.
 pub struct ProfilingInfoPrinter {
     sierra_program: Program,
-    flags: ProfilingInfoPrinterFlags,
+    params: ProfilingInfoPrinterParams,
 }
 impl ProfilingInfoPrinter {
     pub fn new(sierra_program: Program) -> Self {
         Self {
             sierra_program,
-            flags: ProfilingInfoPrinterFlags {
-                print_zeros: false,
+            params: ProfilingInfoPrinterParams {
+                min_weight: 1,
                 print_by_concrete_libfunc: true,
                 print_by_generic_libfunc: true,
             },
         }
     }
 
-    /// Prints the profiling info according to the flags set in the printer.
+    /// Prints the profiling info according to the params set in the printer.
     pub fn print(&self, profiling_info: &ProfilingInfo) {
-        self.print_ex(profiling_info, &self.flags);
+        self.print_ex(profiling_info, &self.params);
     }
 
-    /// Prints the profiling info according to the given flags (can be used to override the printer
-    /// flags).
-    pub fn print_ex(&self, profiling_info: &ProfilingInfo, flags: &ProfilingInfoPrinterFlags) {
-        let mut concrete_libfuncs = if flags.print_by_concrete_libfunc {
+    /// Prints the profiling info according to the given params (can be used to override the printer
+    /// params).
+    pub fn print_ex(&self, profiling_info: &ProfilingInfo, params: &ProfilingInfoPrinterParams) {
+        let mut concrete_libfuncs = if params.print_by_concrete_libfunc {
             self.sierra_program
                 .libfunc_declarations
                 .iter()
@@ -52,7 +55,7 @@ impl ProfilingInfoPrinter {
             HashMap::new()
         };
 
-        let mut generic_libfuncs = if flags.print_by_generic_libfunc {
+        let mut generic_libfuncs = if params.print_by_generic_libfunc {
             self.sierra_program
                 .libfunc_declarations
                 .iter()
@@ -75,41 +78,41 @@ impl ProfilingInfoPrinter {
                 return;
             };
             if let Some(concrete_name) = gen_statement.concrete_name() {
-                if flags.print_by_concrete_libfunc {
+                if params.print_by_concrete_libfunc {
                     let concrete_name: SmolStr = concrete_name.into();
                     *(concrete_libfuncs.get_mut(&concrete_name).unwrap()) += weight;
                 }
 
-                if flags.print_by_generic_libfunc {
+                if params.print_by_generic_libfunc {
                     let generic_name: SmolStr = gen_statement.generic_name().unwrap().into();
                     *(generic_libfuncs.get_mut(&generic_name).unwrap()) += weight;
                 }
             } else {
                 return_weight += weight;
             }
-            if *weight > 0 || flags.print_zeros {
+            if *weight > params.min_weight {
                 println!("  statement {}: {} ({})", *statement_idx, *weight, gen_statement);
             }
         }
 
-        if flags.print_by_concrete_libfunc {
+        if params.print_by_concrete_libfunc {
             println!("Weight by concrete libfunc:");
             for (concrete_name, weight) in
                 concrete_libfuncs.iter().sorted_by(|x, y| Ord::cmp(&x.1, &y.1))
             {
-                if *weight > 0 || flags.print_zeros {
+                if *weight > params.min_weight {
                     println!("  libfunc {}: {}", concrete_name, *weight);
                 }
             }
             println!("  return: {return_weight}");
         }
 
-        if flags.print_by_generic_libfunc {
+        if params.print_by_generic_libfunc {
             println!("Weight by generic libfunc:");
             for (generic_name, weight) in
                 generic_libfuncs.iter().sorted_by(|x, y| Ord::cmp(&x.1, &y.1))
             {
-                if *weight > 0 || flags.print_zeros {
+                if *weight > params.min_weight {
                     println!("  libfunc {}: {}", generic_name, *weight);
                 }
             }
