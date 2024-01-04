c3d897f16e413e073f523cca0c2ca66def856892
Artur Michalek <artur.michalek@swmansion.com>
2024-01-04 10:45:47 +0100

    Use `AssembledCairoProgram`

--- crates/cairo-lang-sierra-to-casm/src/compiler.rs
+++ crates/cairo-lang-sierra-to-casm/src/compiler.rs
@@ -59,7 +59,7 @@ pub enum CompilationError {
 }
 
 /// The casm program representation.
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 pub struct CairoProgram {
     pub instructions: Vec<Instruction>,
     pub debug_info: CairoProgramDebugInfo,
@@ -106,7 +106,7 @@ impl CairoProgram {
 }
 
 /// The debug information of a compilation from Sierra to casm.
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub struct SierraStatementDebugInfo {
     /// The offset of the sierra statement within the bytecode.
     pub code_offset: usize,
@@ -115,7 +115,7 @@ pub struct SierraStatementDebugInfo {
 }
 
 /// The debug information of a compilation from Sierra to casm.
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 pub struct CairoProgramDebugInfo {
     /// The debug information per Sierra statement.
     pub sierra_statement_info: Vec<SierraStatementDebugInfo>,
@@ -152,7 +152,7 @@ impl ConstSegmentInfoBuilder {
 }
 
 /// The data of a single const in the const segment.
-#[derive(Debug, Eq, PartialEq, Default)]
+#[derive(Debug, Eq, PartialEq, Default, Clone)]
 pub struct ConstAllocation {
     /// The offset of the const within the constants segment.
     pub offset: CodeOffset,
@@ -161,7 +161,7 @@ pub struct ConstAllocation {
 }
 
 /// The information about the constants used in the program.
-#[derive(Debug, Eq, PartialEq, Default)]
+#[derive(Debug, Eq, PartialEq, Default, Clone)]
 pub struct ConstSegmentInfo {
     /// A map between the const type and the data of the const.
     pub const_allocations: OrderedHashMap<ConcreteTypeId, ConstAllocation>,
