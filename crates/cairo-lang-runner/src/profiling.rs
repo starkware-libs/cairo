7d66d3a229f94b9255e92985ef4a50113c2802e6
Yuval Goldberg <yuval@starkware.co>
2024-01-14 20:16:05 +0200

    fixup: UnorderedHashMap

--- crates/cairo-lang-runner/src/profiling.rs
+++ crates/cairo-lang-runner/src/profiling.rs
@@ -1,10 +1,8 @@
-use std::collections::HashMap;
 use std::fmt::Display;
 
 use cairo_lang_sierra::program::{GenStatement, Program, StatementIdx};
 use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
 use cairo_lang_utils::unordered_hash_map::UnorderedHashMap;
-use itertools::Itertools;
 use smol_str::SmolStr;
 
 #[cfg(test)]
@@ -101,9 +99,9 @@ impl ProfilingInfoProcessor {
                 .libfunc_declarations
                 .iter()
                 .map(|concrete_libfunc| (concrete_libfunc.long_id.to_string().into(), 0))
-                .collect::<HashMap<SmolStr, usize>>()
+                .collect::<UnorderedHashMap<SmolStr, usize>>()
         } else {
-            HashMap::new()
+            UnorderedHashMap::new()
         };
 
         let mut generic_libfuncs = if params.process_by_generic_libfunc {
@@ -111,18 +109,15 @@ impl ProfilingInfoProcessor {
                 .libfunc_declarations
                 .iter()
                 .map(|concrete_libfunc| (concrete_libfunc.long_id.generic_id.0.clone(), 0))
-                .collect::<HashMap<SmolStr, usize>>()
+                .collect::<UnorderedHashMap<SmolStr, usize>>()
         } else {
-            HashMap::new()
+            UnorderedHashMap::new()
         };
 
         let mut return_weight = 0;
         let mut statements_weights = OrderedHashMap::default();
-        for (statement_idx, weight) in raw_profiling_info
-            .sierra_statements_weights
-            .iter()
-            .sorted_by(|x, y| Ord::cmp(&(y.1, x.0.0), &(x.1, y.0.0)))
-        {
+
+        for (statement_idx, weight) in raw_profiling_info.sierra_statements_weights.iter_sorted() {
             let Some(gen_statement) = self.sierra_program.statements.get(statement_idx.0) else {
                 panic!("Failed fetching statement index {}", statement_idx.0);
             };
@@ -134,9 +129,9 @@ impl ProfilingInfoProcessor {
                     }
 
                     if params.process_by_generic_libfunc {
-                        // TODO(yg): This depends on the format of a concrete libfunc name. Fix to
-                        // be more resilient by getting the long ID from the short ID and from there
-                        // get the generic name. This can be done either using a DB, or with a
+                        // TODO(yuval): This depends on the format of a concrete libfunc name. Fix
+                        // to be more resilient by getting the long ID from the short ID and from
+                        // there get the generic name. This can be done either using a DB, or with a
                         // sierra program registry, if it's extended to have this mapping.
                         let generic_name: SmolStr = concrete_name.split('<').next().unwrap().into();
                         *(generic_libfuncs.get_mut(&generic_name).unwrap()) += weight;
@@ -153,9 +148,7 @@ impl ProfilingInfoProcessor {
 
         let mut concrete_libfuncs_weights = OrderedHashMap::default();
         if params.process_by_concrete_libfunc {
-            for (concrete_name, weight) in
-                concrete_libfuncs.iter().sorted_by(|x, y| Ord::cmp(&(y.1, x.0), &(x.1, y.0)))
-            {
+            for (concrete_name, weight) in concrete_libfuncs.iter_sorted() {
                 if *weight >= params.min_weight {
                     concrete_libfuncs_weights.insert(concrete_name.clone(), *weight);
                 }
@@ -164,9 +157,7 @@ impl ProfilingInfoProcessor {
 
         let mut generic_libfuncs_weights = OrderedHashMap::default();
         if params.process_by_generic_libfunc {
-            for (generic_name, weight) in
-                generic_libfuncs.iter().sorted_by(|x, y| Ord::cmp(&(y.1, x.0), &(x.1, y.0)))
-            {
+            for (generic_name, weight) in generic_libfuncs.iter_sorted() {
                 if *weight >= params.min_weight {
                     generic_libfuncs_weights.insert(generic_name.clone(), *weight);
                 }
