712011501d3b01ccab7e3c22adb85776ee27f321
Ori Ziv <oriz@starkware.co>
2024-01-15 22:41:49 +0200

    Merged `constrain_range` into `downcast`.
    commit-id:c9fbe1f4

--- crates/cairo-lang-sierra/src/extensions/modules/utils.rs
+++ crates/cairo-lang-sierra/src/extensions/modules/utils.rs
@@ -90,6 +90,10 @@ impl Range {
     pub fn is_small_range(&self) -> bool {
         self.size() <= BigInt::one().shl(128)
     }
+    /// Returns true if this range can contain all possible values of a CASM cell.
+    pub fn is_full_felt252_range(&self) -> bool {
+        self.size() >= Felt252::prime().into()
+    }
     /// Returns the size of the range.
     pub fn size(&self) -> BigInt {
         &self.upper - &self.lower
