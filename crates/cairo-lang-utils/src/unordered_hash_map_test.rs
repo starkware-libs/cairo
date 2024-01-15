55d402a7ceb2e941bff6df905947b073663994ee
Yuval Goldberg <yuval@starkware.co>
2024-01-15 12:28:28 +0200

    Added UnorderedHashMap::filter

--- crates/cairo-lang-utils/src/unordered_hash_map_test.rs
+++ crates/cairo-lang-utils/src/unordered_hash_map_test.rs
@@ -1,9 +1,4 @@
-#[cfg(not(feature = "std"))]
-use core::hash::Hasher;
-
 use super::UnorderedHashMap;
-#[cfg(feature = "std")]
-use crate::ordered_hash_map::OrderedHashMap;
 
 #[cfg(not(feature = "std"))]
 #[derive(Default)]
@@ -17,98 +12,76 @@ impl Hasher for MyHasher {
     }
 }
 
-#[cfg(not(feature = "std"))]
-#[test]
-fn test_no_std_aggregate_by() {
-    let hasher = hashbrown::hash_map::DefaultHashBuilder::default();
-    let mut source = UnorderedHashMap::with_hasher(hasher.clone());
-    source.insert(20, 1);
-    source.insert(8, 3);
-    source.insert(9, 5);
-    source.insert(11, 5);
-    let target = source.aggregate_by(|x| *x < 10, |x, y| x + y, &0);
-    let mut expected_target = UnorderedHashMap::with_hasher(hasher);
-    expected_target.insert(true, 8);
-    expected_target.insert(false, 6);
-
-    if target != expected_target {
-        panic!("target != expected_target");
-    }
-}
-
-#[cfg(feature = "std")]
-#[test]
-fn test_aggregate_by() {
-    let mut source = UnorderedHashMap::new();
-    source.insert(20, 1);
-    source.insert(8, 3);
-    source.insert(9, 5);
-    source.insert(11, 5);
-    let target = source.aggregate_by(|x| *x < 10, |x, y| x + y, &0);
-    let mut expected_target = UnorderedHashMap::new();
-    expected_target.insert(true, 8);
-    expected_target.insert(false, 6);
-
-    assert_eq!(target, expected_target);
-}
-
-#[cfg(feature = "std")]
 #[derive(PartialOrd, Ord, PartialEq, Eq, Hash, Debug, Clone)]
 struct XY {
     x: u32,
     y: u32,
 }
 
+/// A utility function for this test module to create an empty hashmap based on the configuration
+/// (std/no-std)
+#[cfg(not(feature = "std"))]
+fn new_hashmap<K, V>() -> UnorderedHashMap<K, V, hashbrown::hash_map::DefaultHashBuilder> {
+    let hasher = hashbrown::hash_map::DefaultHashBuilder::default();
+    UnorderedHashMap::with_hasher(hasher)
+}
 #[cfg(feature = "std")]
+fn new_hashmap<K, V>() -> UnorderedHashMap<K, V> {
+    UnorderedHashMap::new()
+}
+
+#[test]
+fn test_aggregate_by() {
+    let mut source = new_hashmap();
+    source.insert(20, 1);
+    source.insert(8, 3);
+    source.insert(9, 5);
+    source.insert(11, 5);
+    let target = source.aggregate_by(|x| *x < 10, |x, y| x + y, &0);
+    let mut expected_target = new_hashmap();
+    expected_target.insert(true, 8);
+    expected_target.insert(false, 6);
+
+    assert_eq!(target, expected_target);
+}
+
 #[test]
 fn test_iter_sorted() {
-    let mut unordered1 = UnorderedHashMap::new();
-    unordered1.insert(XY { x: 11, y: 0 }, 0);
-    unordered1.insert(XY { x: 11, y: 5 }, 0);
-    unordered1.insert(XY { x: 12, y: 2 }, 0);
-    unordered1.insert(XY { x: 10, y: 9 }, 0);
-    unordered1.insert(XY { x: 10, y: 8 }, 0);
+    let mut unordered = new_hashmap();
+    unordered.insert(XY { x: 11, y: 0 }, 0);
+    unordered.insert(XY { x: 11, y: 5 }, 0);
+    unordered.insert(XY { x: 12, y: 2 }, 0);
+    unordered.insert(XY { x: 10, y: 9 }, 0);
+    unordered.insert(XY { x: 10, y: 8 }, 0);
 
-    let mut unordered2 = UnorderedHashMap::new();
-    unordered2.insert(XY { x: 12, y: 2 }, 0);
-    unordered2.insert(XY { x: 10, y: 9 }, 0);
-    unordered2.insert(XY { x: 11, y: 5 }, 0);
-    unordered2.insert(XY { x: 10, y: 8 }, 0);
-    unordered2.insert(XY { x: 11, y: 0 }, 0);
-
-    let ordered1: OrderedHashMap<_, _> = unordered1.iter_sorted().collect();
-    let ordered2: OrderedHashMap<_, _> = unordered2.iter_sorted().collect();
-
-    assert_eq!(ordered1, ordered2);
+    let mut last = 0;
+    for (key, val) in unordered.iter_sorted_by_key(|a| a.0.x + a.0.y + a.1) {
+        let cur = key.x + key.y + val;
+        assert!(cur.cmp(&last) == core::cmp::Ordering::Greater);
+        last = cur;
+    }
 }
 
-#[cfg(feature = "std")]
 #[test]
 fn test_into_iter_sorted() {
-    let mut unordered1 = UnorderedHashMap::new();
-    unordered1.insert(XY { x: 11, y: 0 }, 0);
-    unordered1.insert(XY { x: 11, y: 5 }, 0);
-    unordered1.insert(XY { x: 12, y: 2 }, 0);
-    unordered1.insert(XY { x: 10, y: 9 }, 0);
-    unordered1.insert(XY { x: 10, y: 8 }, 0);
+    let mut unordered = new_hashmap();
+    unordered.insert(XY { x: 11, y: 0 }, 0);
+    unordered.insert(XY { x: 11, y: 5 }, 0);
+    unordered.insert(XY { x: 12, y: 2 }, 0);
+    unordered.insert(XY { x: 10, y: 9 }, 0);
+    unordered.insert(XY { x: 10, y: 8 }, 0);
 
-    let mut unordered2 = UnorderedHashMap::new();
-    unordered2.insert(XY { x: 12, y: 2 }, 0);
-    unordered2.insert(XY { x: 10, y: 9 }, 0);
-    unordered2.insert(XY { x: 11, y: 5 }, 0);
-    unordered2.insert(XY { x: 10, y: 8 }, 0);
-    unordered2.insert(XY { x: 11, y: 0 }, 0);
-
-    let ordered1: OrderedHashMap<_, _> = unordered1.into_iter_sorted().collect();
-    let ordered2: OrderedHashMap<_, _> = unordered2.into_iter_sorted().collect();
-
-    assert_eq!(ordered1, ordered2);
+    let mut last = 0;
+    for (key, val) in unordered.into_iter_sorted_by_key(|a| a.0.x + a.0.y + a.1) {
+        let cur = key.x + key.y + val;
+        assert!(cur.cmp(&last) == core::cmp::Ordering::Greater);
+        last = cur;
+    }
 }
 
-#[cfg(feature = "std")]
 #[test]
 fn test_iter_sorted_by_key() {
-    let mut unordered = UnorderedHashMap::new();
+    let mut unordered = new_hashmap();
     unordered.insert(XY { x: 11, y: 0 }, 4); // sum = 15
     unordered.insert(XY { x: 11, y: 5 }, 7); // sum = 23
     unordered.insert(XY { x: 12, y: 2 }, 5); // sum = 19
@@ -123,10 +96,9 @@ fn test_iter_sorted_by_key() {
     }
 }
 
-#[cfg(feature = "std")]
 #[test]
 fn test_into_iter_sorted_by_key() {
-    let mut unordered = UnorderedHashMap::new();
+    let mut unordered = new_hashmap();
     unordered.insert(XY { x: 11, y: 0 }, 4); // sum = 15
     unordered.insert(XY { x: 11, y: 5 }, 7); // sum = 23
     unordered.insert(XY { x: 12, y: 2 }, 5); // sum = 19
@@ -139,4 +111,22 @@ fn test_into_iter_sorted_by_key() {
         assert!(cur_sum >= last_sum);
         last_sum = cur_sum;
     }
+}
+
+#[test]
+fn test_filter() {
+    let mut unordered = new_hashmap();
+    unordered.insert(XY { x: 11, y: 0 }, 4);
+    unordered.insert(XY { x: 11, y: 5 }, 7);
+    unordered.insert(XY { x: 12, y: 2 }, 5);
+    unordered.insert(XY { x: 10, y: 9 }, 9);
+    unordered.insert(XY { x: 10, y: 8 }, 3);
+
+    let filtered = unordered.filter(|k, v| k.x != 11 && *v != 9);
+
+    let mut expected = new_hashmap();
+    expected.insert(XY { x: 12, y: 2 }, 5);
+    expected.insert(XY { x: 10, y: 8 }, 3);
+
+    assert_eq!(filtered, expected);
 }
