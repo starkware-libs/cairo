09e8b08cd74492307691e3b884564a366d3e2c2e
Ori Ziv <oriz@starkware.co>
2024-01-15 15:33:24 +0200

    Make cycle lookup dependency type based.

--- crates/cairo-lang-lowering/src/scc/mod.rs
+++ crates/cairo-lang-lowering/src/scc/mod.rs
@@ -3,19 +3,26 @@ use cairo_lang_utils::graph_algos::strongly_connected_components::compute_scc;
 
 use crate::db::LoweringGroup;
 use crate::ids::FunctionWithBodyId;
+use crate::DependencyType;
 
 /// Query implementation of [crate::db::LoweringGroup::function_with_body_scc].
 pub fn function_with_body_scc(
     db: &dyn LoweringGroup,
     function_id: FunctionWithBodyId,
+    dependency_type: DependencyType,
 ) -> Vec<FunctionWithBodyId> {
-    compute_scc(&FunctionWithBodyNode { function_with_body_id: function_id, db: db.upcast() })
+    compute_scc(&FunctionWithBodyNode {
+        function_with_body_id: function_id,
+        dependency_type,
+        db: db.upcast(),
+    })
 }
 
 /// A node to use in the SCC computation.
 #[derive(Clone)]
 struct FunctionWithBodyNode<'a> {
     function_with_body_id: FunctionWithBodyId,
+    dependency_type: DependencyType,
     db: &'a dyn LoweringGroup,
 }
 impl<'a> GraphNode for FunctionWithBodyNode<'a> {
@@ -23,11 +30,15 @@ impl<'a> GraphNode for FunctionWithBodyNode<'a> {
 
     fn get_neighbors(&self) -> Vec<Self> {
         self.db
-            .function_with_body_direct_function_with_body_callees(self.function_with_body_id)
+            .function_with_body_direct_function_with_body_callees(
+                self.function_with_body_id,
+                self.dependency_type,
+            )
             .unwrap_or_default()
             .into_iter()
             .map(|function_with_body_id| FunctionWithBodyNode {
                 function_with_body_id,
+                dependency_type: self.dependency_type,
                 db: self.db,
             })
             .collect()
