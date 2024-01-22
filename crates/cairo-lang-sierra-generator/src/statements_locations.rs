ba762eb53917f03f4798332f8a6ea0673e832c18
Gil Ben-Shachar <gil@starkware.co>
2024-01-22 19:22:18 +0200

    Add cairo functions basic profiling and change profiler tests accordingly.
    commit-id:f560cd85

--- crates/cairo-lang-sierra-generator/src/statements_locations.rs
+++ crates/cairo-lang-sierra-generator/src/statements_locations.rs
@@ -89,4 +89,17 @@ impl StatementsLocations {
         }
         Self { locations }
     }
+    /// A map between the statement index and a string representation of the high level function
+    /// that contains it. (See [containing_function_identifier]). It is used for places with db
+    /// access such as the profiler.
+    // TODO(Gil): Add a db access to the profiler and remove this function.
+    pub fn into_statements_functions_map(
+        &self,
+        db: &dyn SierraGenGroup,
+    ) -> HashMap<StatementIdx, String> {
+        self.locations
+            .iter_sorted()
+            .map(|(idx, location)| (*idx, containing_function_identifier(db, Some(*location))))
+            .collect()
+    }
 }
