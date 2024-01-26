9942f7708ab1f61e8af878740b4358f341aded79
Marek Kaput <marek.kaput@swmansion.com>
2024-01-26 16:48:17 +0100

    VSCode: Create `Context` class
    commit-id:78e7cc46

---
+++ vscode-cairo/src/context.ts
@@ -0,0 +1,28 @@
+import * as vscode from "vscode";
+
+export class Context {
+  public static create(extensionContext: vscode.ExtensionContext): Context {
+    const log = vscode.window.createOutputChannel("Cairo Extension");
+    extensionContext.subscriptions.push(log);
+
+    return new Context(extensionContext, log);
+  }
+
+  private constructor(
+    public readonly extension: vscode.ExtensionContext,
+    public readonly log: vscode.OutputChannel,
+  ) {}
+
+  /**
+   * Get Cairo extension-specific configuration.
+   *
+   * @remarks
+   * This is equivalent to calling:
+   * ```
+   * vscode.workspace.getConfiguration("cairo1")
+   * ```
+   */
+  public get config(): vscode.WorkspaceConfiguration {
+    return vscode.workspace.getConfiguration("cairo1");
+  }
+}
