5a0f80874129bfbb5ec68e370eddcc474215282d
Marek Kaput <marek.kaput@swmansion.com>
2024-02-12 08:03:23 +0100

    VSCode: Create `Context` class (#4934)

--- vscode-cairo/src/extension.ts
+++ vscode-cairo/src/extension.ts
@@ -1,18 +1,21 @@
 import * as vscode from "vscode";
 import * as lc from "vscode-languageclient/node";
 import { setupLanguageServer } from "./cairols";
+import { Context } from "./context";
 
 let client: lc.LanguageClient | undefined;
 
-export async function activate(context: vscode.ExtensionContext) {
-  const config = vscode.workspace.getConfiguration();
-  const outputChannel = vscode.window.createOutputChannel("Cairo extension");
-  context.subscriptions.push(outputChannel);
+export async function activate(extensionContext: vscode.ExtensionContext) {
+  const ctx = Context.create(extensionContext);
 
-  if (config.get<boolean>("cairo1.enableLanguageServer")) {
-    client = await setupLanguageServer(config, context, outputChannel);
+  if (ctx.config.get<boolean>("enableLanguageServer")) {
+    client = await setupLanguageServer(
+      vscode.workspace.getConfiguration(),
+      ctx.extension,
+      ctx.log,
+    );
   } else {
-    outputChannel.appendLine(
+    ctx.log.appendLine(
       "Language server is not enabled. Use the cairo1.enableLanguageServer config",
     );
   }
