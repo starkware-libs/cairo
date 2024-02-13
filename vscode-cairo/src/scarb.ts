a015ebe48163f8cb25a704755df5d0b6485528b9
Marek Kaput <marek.kaput@swmansion.com>
2024-02-13 12:06:21 +0100

    VSCode: Move Scarb searching logic to the `Scarb` class (#5012)

--- vscode-cairo/src/scarb.ts
+++ vscode-cairo/src/scarb.ts
@@ -2,6 +2,7 @@ import { spawn } from "child_process";
 import * as vscode from "vscode";
 import * as lc from "vscode-languageclient/node";
 import type { Context } from "./context";
+import { checkTool, findToolInAsdf, findToolInPath } from "./toolchain";
 
 let globalExecId = 0;
 
@@ -17,6 +18,55 @@ export class Scarb {
      */
     public readonly workspaceFolder?: vscode.WorkspaceFolder | undefined,
   ) {}
+
+  /**
+   * Tries to find Scarb on the host in context of the given workspace folder.
+   */
+  public static async find(
+    workspaceFolder: vscode.WorkspaceFolder | undefined,
+    ctx: Context,
+  ): Promise<Scarb> {
+    const path =
+      (await fromConfig()) || (await fromPath()) || (await fromAsdf());
+    if (!path) {
+      throw new Error("could not find Scarb executable on this machine");
+    }
+    return new Scarb(path, workspaceFolder);
+
+    async function fromConfig(): Promise<string | undefined> {
+      // TODO(mkaput): Config should probably be scoped to workspace folder.
+      let configPath = ctx.config.get("scarbPath");
+      if (configPath) {
+        configPath = await checkTool(configPath);
+        if (configPath) {
+          ctx.log.debug(`using Scarb from config: ${configPath}`);
+          return configPath;
+        }
+        throw new Error(`configured Scarb path does not exist: ${configPath}`);
+      }
+      return undefined;
+    }
+
+    async function fromPath(): Promise<string | undefined> {
+      const envPath = await findToolInPath("scarb");
+      if (envPath) {
+        ctx.log.debug(`using Scarb from PATH: ${envPath}`);
+        return envPath;
+      }
+      return undefined;
+    }
+
+    async function fromAsdf(): Promise<string | undefined> {
+      // Usually asdf scarb shim is already in PATH, but some users happened
+      // not to have it there.
+      const asdfPath = await findToolInAsdf("scarb");
+      if (asdfPath) {
+        ctx.log.debug(`using Scarb from asdf: ${asdfPath}`);
+        return asdfPath;
+      }
+      return undefined;
+    }
+  }
 
   public languageServerExecutable(): lc.Executable {
     const exec: lc.Executable = {
