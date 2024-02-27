2d9d050e407f3650ebbcba07bc0548396c52fac8
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2024-02-27 18:23:20 +0100

    chore: fmt

--- crates/cairo-lang-runner/src/casm_run/mod.rs
+++ crates/cairo-lang-runner/src/casm_run/mod.rs
@@ -2239,11 +2239,7 @@ impl FormattedItem {
     }
     /// Wraps the formatted item with quote, if it's a string. Otherwise returns it as is.
     pub fn quote_if_string(self) -> String {
-        if self.is_string {
-            format!("\"{}\"", self.item)
-        } else {
-            self.item
-        }
+        if self.is_string { format!("\"{}\"", self.item) } else { self.item }
     }
 }
 
