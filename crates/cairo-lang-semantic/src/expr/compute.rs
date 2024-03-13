aa48c2af7282af2aa3ed4e3388fb93f827608f36
yuvalsw <105596564+yuvalsw@users.noreply.github.com>
2024-03-13 19:13:59 +0200

    Reduce types in match/if/loop (#5256)

--- crates/cairo-lang-semantic/src/expr/compute.rs
+++ crates/cairo-lang-semantic/src/expr/compute.rs
@@ -1059,8 +1059,9 @@ fn compute_expr_match_semantic(
     // Unify arm types.
     let mut helper = FlowMergeTypeHelper::new(ctx.db);
     for (_, expr) in patterns_and_exprs.iter() {
+        let expr_ty = ctx.reduce_ty(expr.ty());
         if let Err((match_ty, arm_ty)) =
-            helper.try_merge_types(&mut ctx.resolver.inference(), ctx.db, expr.ty())
+            helper.try_merge_types(&mut ctx.resolver.inference(), ctx.db, expr_ty)
         {
             ctx.diagnostics.report_by_ptr(
                 expr.stable_ptr().untyped(),
@@ -1130,8 +1131,10 @@ fn compute_expr_if_semantic(ctx: &mut ComputationContext<'_>, syntax: &ast::Expr
     };
 
     let mut helper = FlowMergeTypeHelper::new(ctx.db);
+    let if_block_ty = ctx.reduce_ty(if_block.ty());
+    let else_block_ty = ctx.reduce_ty(else_block_ty);
     helper
-        .try_merge_types(&mut ctx.resolver.inference(), ctx.db, if_block.ty())
+        .try_merge_types(&mut ctx.resolver.inference(), ctx.db, if_block_ty)
         .and(helper.try_merge_types(&mut ctx.resolver.inference(), ctx.db, else_block_ty))
         .unwrap_or_else(|(block_if_ty, block_else_ty)| {
             ctx.diagnostics.report(syntax, IncompatibleIfBlockTypes { block_if_ty, block_else_ty });
@@ -2725,6 +2728,7 @@ pub fn compute_statement_semantic(
                     (Some(expr.id), expr.ty(), expr.stable_ptr().untyped())
                 }
             };
+            let ty = ctx.reduce_ty(ty);
             match &mut ctx.loop_ctx {
                 None => {
                     return Err(ctx.diagnostics.report(break_syntax, BreakOnlyAllowedInsideALoop));
