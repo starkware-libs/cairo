dd1e26196a5ee9210b9860a76cd621a97eb057c0
Ilya Lesokhin <ilya@starkware.co>
2024-03-14 10:30:56 +0200

    Share VarRenamer code.

---
+++ crates/cairo-lang-lowering/src/optimizations/var_renamer.rs
@@ -0,0 +1,28 @@
+use cairo_lang_utils::unordered_hash_map::UnorderedHashMap;
+
+use crate::utils::Rebuilder;
+use crate::{BlockId, VariableId};
+
+#[derive(Default)]
+pub struct VarRenamer {
+    pub renamed_vars: UnorderedHashMap<VariableId, VariableId>,
+}
+
+impl Rebuilder for VarRenamer {
+    fn map_var_id(&mut self, var: VariableId) -> VariableId {
+        let Some(mut new_var_id) = self.renamed_vars.get(&var).cloned() else {
+            return var;
+        };
+        while let Some(new_id) = self.renamed_vars.get(&new_var_id) {
+            assert_ne!(new_var_id, *new_id);
+            new_var_id = *new_id;
+        }
+
+        self.renamed_vars.insert(var, new_var_id);
+        new_var_id
+    }
+
+    fn map_block_id(&mut self, block: BlockId) -> BlockId {
+        block
+    }
+}
