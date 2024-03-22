db4a660a36e42b4fad06ebf11dabbd4606a6b638
Piotr Magiera <piotrmagiera150@gmail.com>
2024-03-22 13:46:58 +0100

    refactor

--- crates/cairo-lang-sierra-generator/src/statements_locations.rs
+++ crates/cairo-lang-sierra-generator/src/statements_locations.rs
@@ -1,95 +1,123 @@
 use std::ops::Add;
 
+use cairo_lang_defs::db::DefsGroup;
 use cairo_lang_defs::diagnostic_utils::StableLocation;
 use cairo_lang_diagnostics::ToOption;
-use cairo_lang_filesystem::ids::{FileLongId, VirtualFile};
+use cairo_lang_filesystem::ids::{FileId, FileLongId, VirtualFile};
 use cairo_lang_sierra::program::StatementIdx;
 use cairo_lang_syntax::node::{Terminal, TypedSyntaxNode};
 use cairo_lang_utils::unordered_hash_map::UnorderedHashMap;
 use itertools::Itertools;
 
-use crate::db::SierraGenGroup;
-
 #[cfg(test)]
 #[path = "statements_locations_test.rs"]
 mod test;
 
-/// Returns an identifier of the function that contains the given [StableLocation]. It is composed
-/// of the file name, and the path (modules and impls) to the function in the file.
+/// Returns an identifier of the function that contains the given [StableLocation].
+/// It is a fully qualified path to the function which contains:
+/// - fully qualified path to the file module,
+/// - relative path to the function in the file module.
 pub fn containing_function_identifier(
-    db: &dyn SierraGenGroup,
-    location: Option<StableLocation>,
+    db: &dyn DefsGroup,
+    location: StableLocation,
 ) -> Option<String> {
-    match location {
-        Some(location) => {
-            let mut file_id = location.file_id(db.upcast());
-            let file_name = file_id.file_name(db.upcast());
+    let file_id = location.file_id(db.upcast());
+    let absolute_semantic_path_to_file_module = file_module_absolute_identifier(db, file_id)?;
 
-            while let FileLongId::Virtual(VirtualFile { parent: Some(parent), .. }) =
-                db.lookup_intern_file(file_id)
-            {
-                file_id = parent;
+    let relative_semantic_path = function_identifier_relative_to_file_module(db, location);
+    Some(absolute_semantic_path_to_file_module.add("::").add(&relative_semantic_path))
+}
+
+/// Returns an identifier of the function that contains the given [StableLocation].
+/// It is a fully qualified path to the function which contains:
+/// - fully qualified path to the file module,
+/// - relative path to the function in the file module.
+///
+/// In case the fully qualified path to the file module cannot be found
+/// it is replaced in the fully qualified function path by the file name.
+pub fn containing_function_identifier_for_tests(
+    db: &dyn DefsGroup,
+    location: StableLocation,
+) -> String {
+    let file_id = location.file_id(db.upcast());
+    let absolute_semantic_path_to_file_module = file_module_absolute_identifier(db, file_id)
+        .unwrap_or_else(|| file_id.file_name(db.upcast()));
+
+    let relative_semantic_path = function_identifier_relative_to_file_module(db, location);
+    absolute_semantic_path_to_file_module.add("::").add(&relative_semantic_path)
+}
+
+/// Returns the path (modules and impls) to the function in the file.
+/// The path is relative to the file module.
+pub fn function_identifier_relative_to_file_module(
+    db: &dyn DefsGroup,
+    location: StableLocation,
+) -> String {
+    let syntax_db = db.upcast();
+    let mut relative_semantic_path_segments: Vec<String> = vec![];
+    let mut syntax_node = location.syntax_node(db);
+    loop {
+        // TODO(Gil): Extract this function into a trait of syntax kind to support future
+        // function containing items (specifically trait functions).
+        match syntax_node.kind(syntax_db) {
+            cairo_lang_syntax::node::kind::SyntaxKind::FunctionWithBody => {
+                let function_name =
+                    cairo_lang_syntax::node::ast::FunctionWithBody::from_syntax_node(
+                        syntax_db,
+                        syntax_node.clone(),
+                    )
+                    .declaration(syntax_db)
+                    .name(syntax_db)
+                    .text(syntax_db);
+                relative_semantic_path_segments.push(function_name.to_string());
             }
-
-            let maybe_file_modules = db.file_modules(file_id);
-            let absolute_semantic_path_to_file =
-                if let Some(file_modules) = maybe_file_modules.to_option() {
-                    file_modules.first().unwrap().full_path(db.upcast())
-                } else {
-                    file_name
-                };
-
-            let syntax_db = db.upcast();
-            let mut relative_semantic_path_segments: Vec<String> = vec![];
-            let mut syntax_node = location.syntax_node(db.upcast());
-            loop {
-                // TODO(Gil): Extract this function into a trait of syntax kind to support future
-                // function containing items (specifically trait functions).
-                match syntax_node.kind(syntax_db) {
-                    cairo_lang_syntax::node::kind::SyntaxKind::FunctionWithBody => {
-                        let function_name =
-                            cairo_lang_syntax::node::ast::FunctionWithBody::from_syntax_node(
-                                syntax_db,
-                                syntax_node.clone(),
-                            )
-                            .declaration(syntax_db)
-                            .name(syntax_db)
-                            .text(syntax_db);
-                        relative_semantic_path_segments.push(function_name.to_string());
-                    }
-                    cairo_lang_syntax::node::kind::SyntaxKind::ItemImpl => {
-                        let impl_name = cairo_lang_syntax::node::ast::ItemImpl::from_syntax_node(
-                            syntax_db,
-                            syntax_node.clone(),
-                        )
-                        .name(syntax_db)
-                        .text(syntax_db);
-                        relative_semantic_path_segments.push(impl_name.to_string());
-                    }
-                    cairo_lang_syntax::node::kind::SyntaxKind::ItemModule => {
-                        let module_name =
-                            cairo_lang_syntax::node::ast::ItemModule::from_syntax_node(
-                                syntax_db,
-                                syntax_node.clone(),
-                            )
-                            .name(syntax_db)
-                            .text(syntax_db);
-                        relative_semantic_path_segments.push(module_name.to_string());
-                    }
-                    _ => {}
-                }
-                if let Some(parent) = syntax_node.parent() {
-                    syntax_node = parent;
-                } else {
-                    break;
-                }
+            cairo_lang_syntax::node::kind::SyntaxKind::ItemImpl => {
+                let impl_name = cairo_lang_syntax::node::ast::ItemImpl::from_syntax_node(
+                    syntax_db,
+                    syntax_node.clone(),
+                )
+                .name(syntax_db)
+                .text(syntax_db);
+                relative_semantic_path_segments.push(impl_name.to_string());
             }
-
-            let relative_semantic_path = relative_semantic_path_segments.iter().rev().join("::");
-            Some(absolute_semantic_path_to_file.add("::").add(&relative_semantic_path))
+            cairo_lang_syntax::node::kind::SyntaxKind::ItemModule => {
+                let module_name = cairo_lang_syntax::node::ast::ItemModule::from_syntax_node(
+                    syntax_db,
+                    syntax_node.clone(),
+                )
+                .name(syntax_db)
+                .text(syntax_db);
+                relative_semantic_path_segments.push(module_name.to_string());
+            }
+            _ => {}
         }
-        None => None,
+        if let Some(parent) = syntax_node.parent() {
+            syntax_node = parent;
+        } else {
+            break;
+        }
     }
+
+    relative_semantic_path_segments.into_iter().rev().join("::")
+}
+
+/// This function returns a fully qualified path to the file module.
+/// `None` should be returned only for compiler tests where files of type `VirtualFile` may be non
+/// generated files.
+pub fn file_module_absolute_identifier(db: &dyn DefsGroup, mut file_id: FileId) -> Option<String> {
+    // `VirtualFile` is a generated file (e.g., by macros like `#[starknet::contract]`)
+    // that won't have a matching file module in the db. Instead, we find its non generated parent
+    // which is in the same module and have a matching file module in the db.
+    while let FileLongId::Virtual(VirtualFile { parent: Some(parent), .. }) =
+        db.lookup_intern_file(file_id)
+    {
+        file_id = parent;
+    }
+
+    let file_modules = db.file_modules(file_id).to_option()?;
+    let full_path = file_modules.first().unwrap().full_path(db.upcast());
+
+    Some(full_path)
 }
 
 /// The location of the Cairo source code which caused a statement to be generated.
@@ -116,10 +144,8 @@ impl StatementsLocations {
     // TODO(Gil): Add a db access to the profiler and remove this function.
     pub fn get_statements_functions_map(
         &self,
-        db: &dyn SierraGenGroup,
+        db: &dyn DefsGroup,
     ) -> UnorderedHashMap<StatementIdx, String> {
-        self.locations.map(|s| {
-            containing_function_identifier(db, Some(*s)).unwrap_or_else(|| "unknown".to_string())
-        })
+        self.locations.map(|s| containing_function_identifier_for_tests(db, *s))
     }
 }
