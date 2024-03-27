ceea99ea05137fd544aef1126b9c9c15821e1ec5
Piotr Magiera <piotrmagiera150@gmail.com>
2024-03-27 11:07:11 +0100

    fix docs

--- crates/cairo-lang-sierra-generator/src/statements_functions.rs
+++ crates/cairo-lang-sierra-generator/src/statements_functions.rs
@@ -6,8 +6,8 @@ use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
 use serde::{Deserialize, Serialize};
 
 /// The mapping from sierra statement index to fully qualified Cairo path of the Cairo function
-/// (if obtainable) which caused the statement to be generated.
-///  Should be created using [`StatementsLocation::extract_statements_functions`].
+/// (if obtainable) which caused the statement to be generated. Should be created using
+/// [`crate::statements_locations::StatementsLocations::extract_statements_functions`].
 #[derive(Clone, Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
 pub struct StatementsFunctions {
     pub statements_to_functions_map: HashMap<StatementIdx, Option<String>>,
