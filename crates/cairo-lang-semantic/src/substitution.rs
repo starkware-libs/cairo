8f0fbfb9dfc082c4b8e97f5a759951e0b482d993
Ilya Lesokhin <ilya@starkware.co>
2024-04-01 11:29:50 +0300

    Add early stop for fully concrete types.

--- crates/cairo-lang-semantic/src/substitution.rs
+++ crates/cairo-lang-semantic/src/substitution.rs
@@ -378,9 +378,17 @@ add_basic_rewrites!(
     <'a>,
     SubstitutionRewriter<'a>,
     DiagnosticAdded,
-    @exclude TypeLongId ImplId ConstValue
+    @exclude TypeId TypeLongId ImplId ConstValue
 );
 
+impl<'a> SemanticRewriter<TypeId, DiagnosticAdded> for SubstitutionRewriter<'a> {
+    fn internal_rewrite(&mut self, value: &mut TypeId) -> Maybe<RewriteResult> {
+        if value.is_fully_concrete(self.db) {
+            return Ok(RewriteResult::NoChange);
+        }
+        value.default_rewrite(self)
+    }
+}
 impl<'a> SemanticRewriter<TypeLongId, DiagnosticAdded> for SubstitutionRewriter<'a> {
     fn internal_rewrite(&mut self, value: &mut TypeLongId) -> Maybe<RewriteResult> {
         if let TypeLongId::GenericParameter(generic_param) = value {
@@ -416,6 +424,8 @@ impl<'a> SemanticRewriter<ImplId, DiagnosticAdded> for SubstitutionRewriter<'a> 
                 // Substitution is guaranteed to not contain its own variables.
                 return Ok(RewriteResult::Modified);
             }
+        } else if value.is_fully_concrete(self.db) {
+            return Ok(RewriteResult::NoChange);
         }
         value.default_rewrite(self)
     }
