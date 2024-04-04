90373cd5f8a7449708415e7c014af39a36613ed5
Ori Ziv <oriz@starkware.co>
2024-04-04 12:26:29 +0300

    Added `compiler_error!` inline macro.
    commit-id:e7657484

---
+++ crates/cairo-lang-plugins/src/plugins/compile_error.rs
@@ -0,0 +1,46 @@
+use cairo_lang_defs::extract_macro_single_unnamed_arg;
+use cairo_lang_defs::plugin::{MacroPlugin, MacroPluginMetadata, PluginDiagnostic, PluginResult};
+use cairo_lang_defs::plugin_utils::PluginResultTrait;
+use cairo_lang_syntax::node::db::SyntaxGroup;
+use cairo_lang_syntax::node::{ast, Terminal, TypedStablePtr, TypedSyntaxNode};
+
+/// Plugin that allows writing item level `compile_error!` causing a diagnostic.
+/// Useful for testing that `cfg` attributes are valid.
+#[derive(Debug, Default)]
+#[non_exhaustive]
+pub struct CompileErrorPlugin;
+
+impl MacroPlugin for CompileErrorPlugin {
+    fn generate_code(
+        &self,
+        db: &dyn SyntaxGroup,
+        item_ast: ast::ModuleItem,
+        _metadata: &MacroPluginMetadata<'_>,
+    ) -> PluginResult {
+        if let ast::ModuleItem::InlineMacro(inline_macro_ast) = item_ast {
+            if inline_macro_ast.name(db).text(db) == "compile_error" {
+                let compilation_error_arg = extract_macro_single_unnamed_arg!(
+                    db,
+                    &inline_macro_ast,
+                    ast::WrappedArgList::ParenthesizedArgList(_)
+                );
+                let ast::Expr::String(err_message) = compilation_error_arg else {
+                    return PluginResult::diagnostic_only(PluginDiagnostic::error(
+                        compilation_error_arg.stable_ptr().untyped(),
+                        "`compiler_error!` argument must be an unnamed string argument."
+                            .to_string(),
+                    ));
+                };
+                return PluginResult::diagnostic_only(PluginDiagnostic::error(
+                    inline_macro_ast.stable_ptr().untyped(),
+                    err_message.text(db).to_string(),
+                ));
+            }
+        }
+        PluginResult { code: None, diagnostics: vec![], remove_original_item: false }
+    }
+
+    fn declared_attributes(&self) -> Vec<String> {
+        vec![]
+    }
+}
