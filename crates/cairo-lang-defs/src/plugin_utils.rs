dd066aca0ecee820ebab32599029415fda9bcb24
orizi <104711814+orizi@users.noreply.github.com>
2024-04-04 15:18:35 +0300

    Added `compiler_error!` inline macro. (#5357)

--- crates/cairo-lang-defs/src/plugin_utils.rs
+++ crates/cairo-lang-defs/src/plugin_utils.rs
@@ -3,23 +3,71 @@ use cairo_lang_syntax::node::helpers::WrappedArgListHelper;
 use cairo_lang_syntax::node::{ast, SyntaxNode, TypedSyntaxNode};
 use itertools::Itertools;
 
-use crate::plugin::{InlinePluginResult, PluginDiagnostic};
+use crate::plugin::{InlinePluginResult, PluginDiagnostic, PluginResult};
+
+/// Trait providing a consistent interface for inline macro calls.
+pub trait InlineMacroCall {
+    type PathNode: TypedSyntaxNode;
+    type Result: PluginResultTrait;
+    fn arguments(&self, db: &dyn SyntaxGroup) -> ast::WrappedArgList;
+    fn path(&self, db: &dyn SyntaxGroup) -> Self::PathNode;
+}
+
+impl InlineMacroCall for ast::ExprInlineMacro {
+    type PathNode = ast::ExprPath;
+    type Result = InlinePluginResult;
+
+    fn arguments(&self, db: &dyn SyntaxGroup) -> ast::WrappedArgList {
+        self.arguments(db)
+    }
+
+    fn path(&self, db: &dyn SyntaxGroup) -> ast::ExprPath {
+        self.path(db)
+    }
+}
+
+impl InlineMacroCall for ast::ItemInlineMacro {
+    type PathNode = ast::TerminalIdentifier;
+    type Result = PluginResult;
+
+    fn arguments(&self, db: &dyn SyntaxGroup) -> ast::WrappedArgList {
+        self.arguments(db)
+    }
+
+    fn path(&self, db: &dyn SyntaxGroup) -> ast::TerminalIdentifier {
+        self.name(db)
+    }
+}
+
+/// Trait providing a consistent interface for the result of a macro plugins.
+pub trait PluginResultTrait {
+    fn diagnostic_only(diagnostic: PluginDiagnostic) -> Self;
+}
+
+impl PluginResultTrait for InlinePluginResult {
+    fn diagnostic_only(diagnostic: PluginDiagnostic) -> Self {
+        InlinePluginResult { code: None, diagnostics: vec![diagnostic] }
+    }
+}
+
+impl PluginResultTrait for PluginResult {
+    fn diagnostic_only(diagnostic: PluginDiagnostic) -> Self {
+        PluginResult { code: None, diagnostics: vec![diagnostic], remove_original_item: true }
+    }
+}
 
 /// Returns diagnostics for an unsupported bracket type.
-pub fn unsupported_bracket_diagnostic(
+pub fn unsupported_bracket_diagnostic<CallAst: InlineMacroCall>(
     db: &dyn SyntaxGroup,
-    macro_ast: &ast::ExprInlineMacro,
-) -> InlinePluginResult {
-    InlinePluginResult {
-        code: None,
-        diagnostics: vec![PluginDiagnostic::error(
-            macro_ast.arguments(db).left_bracket_stable_ptr(db),
-            format!(
-                "Macro `{}` does not support this bracket type.",
-                macro_ast.path(db).as_syntax_node().get_text_without_trivia(db)
-            ),
-        )],
-    }
+    macro_ast: &CallAst,
+) -> CallAst::Result {
+    CallAst::Result::diagnostic_only(PluginDiagnostic::error(
+        macro_ast.arguments(db).left_bracket_stable_ptr(db),
+        format!(
+            "Macro `{}` does not support this bracket type.",
+            macro_ast.path(db).as_syntax_node().get_text_without_trivia(db)
+        ),
+    ))
 }
 
 /// Extracts a single unnamed argument.
@@ -75,28 +123,30 @@ pub fn escape_node(db: &dyn SyntaxGroup, node: SyntaxNode) -> String {
 #[macro_export]
 macro_rules! extract_macro_unnamed_args {
     ($db:expr, $syntax:expr, $n:expr, $pattern:pat) => {{
-        if !matches!($syntax.arguments($db), $pattern) {
+        let arguments = $crate::plugin_utils::InlineMacroCall::arguments($syntax, $db);
+        if !matches!(arguments, $pattern) {
             return $crate::plugin_utils::unsupported_bracket_diagnostic($db, $syntax);
         }
         // `unwrap` is ok because the above `matches` condition ensures it's not None (unless
         // the pattern contains the `Missing` variant).
-        let macro_arg_list = cairo_lang_syntax::node::helpers::WrappedArgListHelper::arg_list(
-            &$syntax.arguments($db),
-            $db,
-        )
-        .unwrap();
+        let macro_arg_list =
+            cairo_lang_syntax::node::helpers::WrappedArgListHelper::arg_list(&arguments, $db)
+                .unwrap();
 
         let args = $crate::plugin_utils::extract_unnamed_args($db, &macro_arg_list, $n);
         let Some(args) = args else {
-            let diagnostics = vec![PluginDiagnostic::error(
-                $syntax.stable_ptr().untyped(),
-                format!(
-                    "Macro `{}` must have exactly {} unnamed arguments.",
-                    $syntax.path($db).as_syntax_node().get_text_without_trivia($db),
-                    $n
+            return $crate::plugin_utils::PluginResultTrait::diagnostic_only(
+                PluginDiagnostic::error(
+                    $syntax.stable_ptr().untyped(),
+                    format!(
+                        "Macro `{}` must have exactly {} unnamed arguments.",
+                        $crate::plugin_utils::InlineMacroCall::path($syntax, $db)
+                            .as_syntax_node()
+                            .get_text_without_trivia($db),
+                        $n
+                    ),
                 ),
-            )];
-            return InlinePluginResult { code: None, diagnostics };
+            );
         };
         let args: [ast::Expr; $n] = args.try_into().unwrap();
         args
