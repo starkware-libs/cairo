1a43db3e190056a0e416d895e1389dfd34379b6d
Ori Ziv <oriz@starkware.co>
2024-04-10 14:52:24 +0300

    Added `require` util.
    Used all around.
    
    commit-id:ec6469b5

--- crates/cairo-lang-semantic/src/literals.rs
+++ crates/cairo-lang-semantic/src/literals.rs
@@ -1,6 +1,6 @@
 use cairo_lang_defs::ids::NamedLanguageElementId;
 use cairo_lang_diagnostics::ToOption;
-use cairo_lang_utils::try_extract_matches;
+use cairo_lang_utils::{require, try_extract_matches};
 use id_arena::Arena;
 use num_bigint::BigInt;
 
@@ -19,17 +19,13 @@ pub fn try_extract_minus_literal(
     let [ExprFunctionCallArg::Value(expr_id)] = &expr.args[..] else {
         return None;
     };
-    if expr.coupon_arg.is_some() {
-        return None;
-    }
+    require(expr.coupon_arg.is_none())?;
     let literal = try_extract_matches!(&exprs[*expr_id], Expr::Literal)?;
     let imp = try_extract_matches!(
         expr.function.get_concrete(db).generic_function,
         GenericFunctionId::Impl
     )?;
     let trait_id = imp.impl_id.concrete_trait(db).to_option()?.trait_id(db);
-    if trait_id != get_core_trait(db, "Neg".into()) {
-        return None;
-    }
+    require(trait_id == get_core_trait(db, "Neg".into()))?;
     if imp.function.name(db.upcast()) != "neg" { None } else { Some(-literal.value.clone()) }
 }
