6766e12ddc8bd0e26720d620c51b44cfb6b27079
Gil Ben-Shachar <gil@starkware.co>
2024-04-15 14:56:30 +0300

    Add storage ptr for simple storage members.

--- crates/cairo-lang-starknet/src/plugin/storage.rs
+++ crates/cairo-lang-starknet/src/plugin/storage.rs
@@ -13,6 +13,7 @@ use super::consts::{
 use super::starknet_module::generation_data::StarknetModuleCommonGenerationData;
 use super::starknet_module::StarknetModuleKind;
 use super::utils::has_v0_attribute;
+use super::STORAGE_AS_POINTER_TRAIT;
 
 /// Generate getters and setters for the members of the storage struct.
 pub fn handle_storage_struct(
@@ -371,6 +372,17 @@ fn handle_simple_storage_member(address: &str, starknet_module_kind: StarknetMod
     pub mod $member_module_path$ {{$extra_uses$
         #[derive(Copy, Drop)]
         pub struct {member_state_name} {{}}
+
+        impl InternalPointerAccess{member_state_name}Impl of 
+            {STORAGE_AS_POINTER_TRAIT}<{member_state_name}, $type_path$> {{
+            fn as_ptr(self: @{member_state_name}) -> 
+                starknet::storage::StoragePointer<$type_path$> {{
+                starknet::storage::StoragePointer{{
+                    address: Internal{member_state_name}Impl::address(self)
+                }}
+            }}
+        }}
+
         pub trait Internal{member_state_name}Trait {{
             fn address(self: @{member_state_name}) -> starknet::storage_access::StorageBaseAddress;
             fn read(self: @{member_state_name}) -> $type_path$;
@@ -383,24 +395,13 @@ fn handle_simple_storage_member(address: &str, starknet_module_kind: StarknetMod
                 starknet::storage_access::storage_base_address_const::<{address}>()
             }}
             fn read(self: @{member_state_name}) -> $type_path$ {{
-                // Only address_domain 0 is currently supported.
-                let address_domain = 0_u32;
-                starknet::SyscallResultTrait::unwrap_syscall(
-                    {STORE_TRAIT}::<$type_path$>::read(
-                        address_domain,
-                        Internal{member_state_name}Impl::address(self),
-                    )
+                starknet::storage::StoragePointerAccess::read( 
+                    InternalPointerAccess{member_state_name}Impl::as_ptr(self)
                 )
             }}
             fn write(ref self: {member_state_name}, value: $type_path$) {{
-                // Only address_domain 0 is currently supported.
-                let address_domain = 0_u32;
-                starknet::SyscallResultTrait::unwrap_syscall(
-                    {STORE_TRAIT}::<$type_path$>::write(
-                        address_domain,
-                        Internal{member_state_name}Impl::address(@self),
-                        value,
-                    )
+                starknet::storage::StoragePointerAccess::write( 
+                    InternalPointerAccess{member_state_name}Impl::as_ptr(@self), value
                 )
             }}
         }}
