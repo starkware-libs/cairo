b7fdc98312331f50bce74279ed981b463cb81040
Marek Kaput <marek.kaput@swmansion.com>
2024-04-23 15:52:18 +0200

    LS: Expand `ProjectManifestPath` tests
    commit-id:ccfde818

--- crates/cairo-lang-language-server/src/project/project_manifest_path_test.rs
+++ crates/cairo-lang-language-server/src/project/project_manifest_path_test.rs
@@ -1,7 +1,26 @@
+use std::path::{Path, PathBuf};
+
 use assert_fs::prelude::*;
 use assert_fs::TempDir;
+use cairo_lang_project::PROJECT_FILE_NAME;
 
-use super::*;
+use super::ProjectManifestPath;
+use crate::toolchain::scarb::SCARB_TOML;
+
+fn check_some(
+    source: impl AsRef<Path>,
+    manifest: impl AsRef<Path>,
+    constructor: impl FnOnce(PathBuf) -> ProjectManifestPath,
+) {
+    let source = source.as_ref();
+    let manifest = manifest.as_ref().to_path_buf();
+    assert_eq!(ProjectManifestPath::discover(source), Some(constructor(manifest)));
+}
+
+fn check_none(source: impl AsRef<Path>) {
+    let source = source.as_ref();
+    assert_eq!(ProjectManifestPath::discover(source), None);
+}
 
 #[test]
 fn discover_cairo_project_toml() {
@@ -13,8 +32,12 @@ fn discover_cairo_project_toml() {
     let source_path = t.child("src/lib.cairo");
     source_path.touch().unwrap();
 
-    let actual = ProjectManifestPath::discover(source_path.path());
-    assert_eq!(actual, Some(ProjectManifestPath::CairoProject(manifest.to_path_buf())));
+    let sibling = t.child("foo.cairo");
+    sibling.touch().unwrap();
+
+    check_some(&source_path, &manifest, ProjectManifestPath::CairoProject);
+    check_some(&manifest, &manifest, ProjectManifestPath::CairoProject);
+    check_some(&sibling, &manifest, ProjectManifestPath::CairoProject);
 }
 
 #[test]
@@ -27,8 +50,12 @@ fn discover_scarb_toml() {
     let source_path = t.child("src/lib.cairo");
     source_path.touch().unwrap();
 
-    let actual = ProjectManifestPath::discover(source_path.path());
-    assert_eq!(actual, Some(ProjectManifestPath::Scarb(manifest.to_path_buf())));
+    let sibling = t.child("foo.cairo");
+    sibling.touch().unwrap();
+
+    check_some(&source_path, &manifest, ProjectManifestPath::Scarb);
+    check_some(&manifest, &manifest, ProjectManifestPath::Scarb);
+    check_some(&sibling, &manifest, ProjectManifestPath::Scarb);
 }
 
 #[test]
@@ -38,8 +65,7 @@ fn discover_no_manifest() {
     let source_path = t.child("src/lib.cairo");
     source_path.touch().unwrap();
 
-    let actual = ProjectManifestPath::discover(source_path.path());
-    assert_eq!(actual, None);
+    check_none(&source_path);
 }
 
 #[test]
@@ -49,12 +75,21 @@ fn discover_precedence() {
     let scarb_manifest = t.child(SCARB_TOML);
     scarb_manifest.touch().unwrap();
 
+    let scarb_lock = t.child("Scarb.lock");
+    scarb_lock.touch().unwrap();
+
     let cairo_manifest = t.child(PROJECT_FILE_NAME);
     cairo_manifest.touch().unwrap();
 
     let source_path = t.child("src/lib.cairo");
     source_path.touch().unwrap();
 
-    let actual = ProjectManifestPath::discover(source_path.path());
-    assert_eq!(actual, Some(ProjectManifestPath::CairoProject(cairo_manifest.to_path_buf())));
+    let sibling = t.child("foo.cairo");
+    sibling.touch().unwrap();
+
+    check_some(&source_path, &cairo_manifest, ProjectManifestPath::CairoProject);
+    check_some(&sibling, &cairo_manifest, ProjectManifestPath::CairoProject);
+    check_some(&cairo_manifest, &cairo_manifest, ProjectManifestPath::CairoProject);
+    check_some(&scarb_manifest, &cairo_manifest, ProjectManifestPath::CairoProject);
+    check_some(&scarb_lock, &cairo_manifest, ProjectManifestPath::CairoProject);
 }
