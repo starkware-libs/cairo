ef241fd5e134e11f24e1bbde06cfa7c310a30d08
Marek Kaput <marek.kaput@swmansion.com>
2024-04-24 18:57:45 +0200

    LS: Move project backend modules to a new form (#5473)

--- crates/cairo-lang-language-server/src/project/mod.rs
+++ crates/cairo-lang-language-server/src/project/mod.rs
@@ -1,5 +1,6 @@
 pub use self::project_manifest_path::*;
 
-// TODO(mkaput): This is `pub` temporarily.
-pub(crate) mod backends;
 mod project_manifest_path;
+// TODO(mkaput): These two are `pub` temporarily.
+pub(crate) mod scarb;
+pub(crate) mod unmanaged_core_crate;
