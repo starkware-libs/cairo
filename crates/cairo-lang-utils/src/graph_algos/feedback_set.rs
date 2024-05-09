04b7d0390acfabebfe170c3bfee67d92f4cd9282
Ori Ziv <oriz@starkware.co>
2024-05-09 21:32:47 +0300

    Made feedback set algorithm not ignore the nodes post cycle.

--- crates/cairo-lang-utils/src/graph_algos/feedback_set.rs
+++ crates/cairo-lang-utils/src/graph_algos/feedback_set.rs
@@ -7,6 +7,8 @@
 //! so here we implement some straight-forward algorithm that guarantees to cover all the cycles in
 //! the graph, but doesn't necessarily produce the minimum size of such a set.
 
+use std::collections::VecDeque;
+
 use super::graph_node::GraphNode;
 use super::scc_graph_node::SccGraphNode;
 use super::strongly_connected_components::ComputeScc;
@@ -19,45 +21,55 @@ mod feedback_set_test;
 
 /// Context for the feedback-set algorithm.
 struct FeedbackSetAlgoContext<Node: ComputeScc> {
+    /// Nodes that were discovered as reachable from the current run, but possibly were not yet
+    /// visited.
+    pending: VecDeque<SccGraphNode<Node>>,
     /// The accumulated feedback set so far in the process of the algorithm. In the end of the
     /// algorithm, this is also the result.
-    pub feedback_set: OrderedHashSet<Node::NodeId>,
+    feedback_set: OrderedHashSet<Node::NodeId>,
     /// Nodes that are currently during the recursion call on them. That is - if one of these is
     /// reached, it indicates it's in some cycle that was not "resolved" yet.
-    pub in_flight: UnorderedHashSet<Node::NodeId>,
-}
-impl<Node: ComputeScc> FeedbackSetAlgoContext<Node> {
-    fn new() -> Self {
-        FeedbackSetAlgoContext {
-            feedback_set: OrderedHashSet::default(),
-            in_flight: UnorderedHashSet::default(),
-        }
-    }
+    in_flight: UnorderedHashSet<Node::NodeId>,
+    /// Already visited nodes in the current run.
+    visited: UnorderedHashSet<Node::NodeId>,
 }
 
 /// Calculates the feedback set of an SCC.
 pub fn calc_feedback_set<Node: ComputeScc>(
-    node: &SccGraphNode<Node>,
+    node: SccGraphNode<Node>,
 ) -> OrderedHashSet<Node::NodeId> {
-    let mut ctx = FeedbackSetAlgoContext::<Node>::new();
-    calc_feedback_set_recursive(node, &mut ctx);
+    let mut ctx = FeedbackSetAlgoContext {
+        feedback_set: OrderedHashSet::default(),
+        in_flight: UnorderedHashSet::default(),
+        pending: VecDeque::new(),
+        visited: UnorderedHashSet::default(),
+    };
+    ctx.pending.push_back(node);
+    while let Some(node) = ctx.pending.pop_front() {
+        calc_feedback_set_recursive(node, &mut ctx);
+    }
     ctx.feedback_set
 }
 
 fn calc_feedback_set_recursive<Node: ComputeScc>(
-    node: &SccGraphNode<Node>,
+    node: SccGraphNode<Node>,
     ctx: &mut FeedbackSetAlgoContext<Node>,
 ) {
     let cur_node_id = node.get_id();
+    if ctx.visited.contains(&cur_node_id) {
+        return;
+    }
+    ctx.visited.insert(cur_node_id.clone());
     ctx.in_flight.insert(cur_node_id.clone());
-    for neighbor in node.get_neighbors() {
+    let mut neighbors = node.get_neighbors().into_iter();
+    for neighbor in neighbors.by_ref() {
         let neighbor_id = neighbor.get_id();
         if ctx.feedback_set.contains(&neighbor_id) {
             continue;
         } else if ctx.in_flight.contains(&neighbor_id) {
             ctx.feedback_set.insert(neighbor_id);
         } else {
-            calc_feedback_set_recursive(&neighbor, ctx);
+            calc_feedback_set_recursive(neighbor, ctx);
         }
 
         // `node` might have been added to the fset during this iteration of the loop. If so, no
@@ -66,5 +78,6 @@ fn calc_feedback_set_recursive<Node: ComputeScc>(
             break;
         }
     }
+    ctx.pending.extend(neighbors.filter(|n| !ctx.visited.contains(&n.get_id())));
     ctx.in_flight.remove(&cur_node_id);
 }
