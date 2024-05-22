27aacf6ec8f0812296ee63a60bcd7c66fd458bfc
Ilya Lesokhin <ilya@starkware.co>
2024-05-22 09:03:25 +0300

    Fix issues in libfuncs.

--- crates/cairo-lang-sierra-ap-change/src/core_libfunc_ap_change.rs
+++ crates/cairo-lang-sierra-ap-change/src/core_libfunc_ap_change.rs
@@ -390,7 +390,7 @@ pub fn core_libfunc_ap_change<InfoProvider: InvocationApChangeInfoProvider>(
             vec![ApChange::Known(2), ApChange::Known(2)]
         }
         Circuit(CircuitConcreteLibfunc::Eval(_)) => vec![ApChange::Known(1)],
-        Circuit(CircuitConcreteLibfunc::GetDescriptor(_)) => vec![ApChange::Known(3)],
+        Circuit(CircuitConcreteLibfunc::GetDescriptor(_)) => vec![ApChange::Known(6)],
         Circuit(CircuitConcreteLibfunc::InitCircuitData(_)) => vec![ApChange::Known(0)],
     }
 }
