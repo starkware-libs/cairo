d4ec20d9efd8d9e37fb79f545de8b1eff453d57e
TomerStarkware <tomer@starkware.co>
2024-05-29 13:56:22 +0300

    fixed resolver data when resolving genric consts

--- crates/cairo-lang-semantic/src/resolve/mod.rs
+++ crates/cairo-lang-semantic/src/resolve/mod.rs
@@ -1107,15 +1107,20 @@ impl<'db> Resolver<'db> {
                 // to inference finalization and use inference here. This will become more relevant
                 // when we support constant expressions, which need inference.
                 let environment = Environment::empty();
-                let mut resolver =
-                    Resolver::new(self.db, self.module_file_id, InferenceId::Canonical);
 
-                for param in self.generic_params.iter() {
-                    resolver.add_generic_param(*param);
-                }
+                // Using the resolver's data in the constant computation context, so impl vars are
+                // added to the correct inference.
+                let mut resolver_data =
+                    ResolverData::new(self.module_file_id, self.inference_data.inference_id);
+                std::mem::swap(&mut self.data, &mut resolver_data);
 
-                let mut ctx =
-                    ComputationContext::new(self.db, diagnostics, resolver, None, environment);
+                let mut ctx = ComputationContext::new(
+                    self.db,
+                    diagnostics,
+                    Resolver::with_data(self.db, resolver_data),
+                    None,
+                    environment,
+                );
                 let value = compute_expr_semantic(&mut ctx, generic_arg_syntax);
 
                 let (_, const_value) = resolve_const_expr_and_evaluate(
@@ -1125,6 +1130,9 @@ impl<'db> Resolver<'db> {
                     generic_arg_syntax.stable_ptr().untyped(),
                     const_param.ty,
                 );
+
+                // Update `self` data with const_eval_resolver's data.
+                std::mem::swap(&mut ctx.resolver.data, &mut self.data);
 
                 match const_value {
                     ConstValue::Int(value) => {
