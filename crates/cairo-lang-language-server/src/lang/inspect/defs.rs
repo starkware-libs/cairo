a2fc40797b4a105ded28277fc4cc9747b2a0831a
Marek Kaput <marek.kaput@swmansion.com>
2024-06-17 19:56:32 +0200

    LS: Implement hover definition for variables & params (#5806)

--- crates/cairo-lang-language-server/src/lang/inspect/defs.rs
+++ crates/cairo-lang-language-server/src/lang/inspect/defs.rs
@@ -1,10 +1,18 @@
 use cairo_lang_compiler::db::RootDatabase;
 use cairo_lang_defs::db::DefsGroup;
 use cairo_lang_defs::ids::LookupItemId;
+use cairo_lang_semantic::db::SemanticGroup;
+use cairo_lang_semantic::expr::pattern::QueryPatternVariablesFromDb;
+use cairo_lang_semantic::items::function_with_body::SemanticExprLookup;
+use cairo_lang_semantic::lookup_item::LookupItemEx;
 use cairo_lang_semantic::resolve::{ResolvedConcreteItem, ResolvedGenericItem};
-use cairo_lang_syntax::node::ast::TerminalIdentifier;
-use cairo_lang_syntax::node::TypedSyntaxNode;
+use cairo_lang_semantic::{Mutability, Variable};
+use cairo_lang_syntax::node::ast::{Param, PatternIdentifier, PatternPtr, TerminalIdentifier};
+use cairo_lang_syntax::node::kind::SyntaxKind;
+use cairo_lang_syntax::node::{SyntaxNode, Terminal, TypedSyntaxNode};
 use cairo_lang_utils::Upcast;
+use smol_str::SmolStr;
+use tracing::error;
 
 use crate::lang::db::LsSemanticGroup;
 use crate::markdown::Markdown;
@@ -16,10 +24,12 @@ use crate::{find_definition, ResolvedItem};
 /// Do not store it in any kind of state.
 pub enum SymbolDef {
     Item(ItemDef),
+    Variable(VariableDef),
 }
 
 impl SymbolDef {
     /// Finds definition of the symbol referred by the given identifier.
+    #[tracing::instrument(name = "SymbolDef::find", level = "trace", skip_all)]
     pub fn find(db: &RootDatabase, identifier: &TerminalIdentifier) -> Option<Self> {
         // Get the resolved item info and the syntax node of the definition.
         let (definition_item, definition_node) = {
@@ -48,25 +58,31 @@ impl SymbolDef {
             | ResolvedItem::Concrete(ResolvedConcreteItem::Variant(_))
             | ResolvedItem::Concrete(ResolvedConcreteItem::Trait(_))
             | ResolvedItem::Concrete(ResolvedConcreteItem::Impl(_)) => {
-                // Get the lookup item representing the defining item.
-                let lookup_item_id = db.find_lookup_item(&definition_node)?;
-
-                Some(Self::Item(ItemDef { lookup_item_id }))
+                ItemDef::new(db, &definition_node).map(Self::Item)
             }
 
-            // TODO(mkaput): Implement hover for variables.
-            ResolvedItem::Generic(ResolvedGenericItem::Variable(_)) => None,
+            ResolvedItem::Generic(ResolvedGenericItem::Variable(_)) => {
+                VariableDef::new(db, definition_node).map(Self::Variable)
+            }
         }
     }
 }
 
-/// Information about definition of an item (function, trait, impl, module, etc.).
+/// Information about the definition of an item (function, trait, impl, module, etc.).
 pub struct ItemDef {
     /// The [`LookupItemId`] associated with the item.
     lookup_item_id: LookupItemId,
 }
 
 impl ItemDef {
+    /// Constructs new [`ItemDef`] instance.
+    fn new(db: &RootDatabase, definition_node: &SyntaxNode) -> Option<Self> {
+        // Get the lookup item representing the defining item.
+        let lookup_item_id = db.find_lookup_item(definition_node)?;
+
+        Some(Self { lookup_item_id })
+    }
+
     /// Get item signature without its body.
     pub fn signature(&self, db: &RootDatabase) -> String {
         db.get_item_signature(self.lookup_item_id)
@@ -84,5 +100,120 @@ impl ItemDef {
                 md.ensure_trailing_newline();
                 md
             })
+    }
+}
+
+/// Information about the definition of a variable (local, function parameter).
+pub struct VariableDef {
+    name: SmolStr,
+    var: Variable,
+}
+
+impl VariableDef {
+    /// Constructs new [`VariableDef`] instance.
+    fn new(db: &RootDatabase, definition_node: SyntaxNode) -> Option<Self> {
+        match definition_node.kind(db.upcast()) {
+            SyntaxKind::TerminalIdentifier => {
+                let definition_node = definition_node.parent()?;
+                match definition_node.kind(db.upcast()) {
+                    SyntaxKind::PatternIdentifier => {
+                        let pattern_identifier =
+                            PatternIdentifier::from_syntax_node(db.upcast(), definition_node);
+                        Self::new_pattern_identifier(db, pattern_identifier)
+                    }
+                    kind => {
+                        error!(
+                            "variable definition node parent is not an pattern identifier: \
+                             {kind:?}"
+                        );
+                        None
+                    }
+                }
+            }
+
+            SyntaxKind::Param => {
+                let param = Param::from_syntax_node(db.upcast(), definition_node);
+                Self::new_param(db, param)
+            }
+
+            kind => {
+                error!("variable definition node is not an identifier nor param: {kind:?}");
+                None
+            }
+        }
+    }
+
+    /// Constructs new [`VariableDef`] instance for [`PatternIdentifier`].
+    fn new_pattern_identifier(
+        db: &RootDatabase,
+        pattern_identifier: PatternIdentifier,
+    ) -> Option<Self> {
+        let name = pattern_identifier.name(db.upcast()).text(db.upcast());
+
+        // Get the function which contains the variable/parameter.
+        let function_id =
+            db.find_lookup_item(&pattern_identifier.as_syntax_node())?.function_with_body()?;
+
+        // Get semantic model for the pattern.
+        let pattern = {
+            let pattern_ptr = PatternPtr::from(pattern_identifier.stable_ptr());
+            let id = db.lookup_pattern_by_ptr(function_id, pattern_ptr).ok()?;
+            db.pattern_semantic(function_id, id)
+        };
+
+        // Extract variable semantic from the found pattern.
+        let var = pattern
+            .variables(&QueryPatternVariablesFromDb(db.upcast(), function_id))
+            .into_iter()
+            .find(|pv| pv.name == name)?
+            .var
+            .into();
+
+        Some(Self { name, var })
+    }
+
+    /// Constructs new [`VariableDef`] instance for [`Param`].
+    fn new_param(db: &RootDatabase, param: Param) -> Option<Self> {
+        let name = param.name(db.upcast()).text(db.upcast());
+
+        // Get the function which contains the variable/parameter.
+        let function_id = db.find_lookup_item(&param.as_syntax_node())?.function_with_body()?;
+
+        // Get function signature.
+        let signature = db.function_with_body_signature(function_id).ok()?;
+
+        // Extract parameter semantic from the found signature.
+        let var = signature.params.into_iter().find(|p| p.name == name)?.into();
+
+        Some(Self { name, var })
+    }
+
+    /// Gets variable signature, which tries to resemble the way how it is defined in code.
+    pub fn signature(&self, db: &RootDatabase) -> String {
+        let Self { name, var } = self;
+
+        let prefix = match var {
+            Variable::Local(_) => "let ",
+            Variable::Param(_) => "",
+        };
+
+        let mutability = match var {
+            Variable::Local(local) => {
+                if local.is_mut {
+                    "mut "
+                } else {
+                    ""
+                }
+            }
+            Variable::Param(param) => match param.mutability {
+                Mutability::Immutable => "",
+                Mutability::Mutable => "mut ",
+                Mutability::Reference => "ref ",
+            },
+        };
+
+        let ty = var.ty().format(db.upcast());
+
+        format!("{prefix}{mutability}{name}: {ty}")
     }
 }
--- crates/cairo-lang-semantic/src/expr/pattern.rs
+++ crates/cairo-lang-semantic/src/expr/pattern.rs
@@ -1,4 +1,5 @@
 use cairo_lang_debug::DebugWithDb;
+use cairo_lang_defs::ids::FunctionWithBodyId;
 use cairo_lang_diagnostics::DiagnosticAdded;
 use cairo_lang_proc_macros::{DebugWithDb, SemanticObject};
 use cairo_lang_syntax::node::ast;
@@ -7,6 +8,7 @@ use id_arena::Arena;
 use smol_str::SmolStr;
 
 use super::fmt::ExprFormatter;
+use crate::db::SemanticGroup;
 use crate::{semantic, ConcreteStructId, ExprLiteral, ExprStringLiteral, LocalVariable, PatternId};
 
 /// Semantic representation of a Pattern.
@@ -44,27 +46,27 @@ impl Pattern {
         }
     }
 
-    pub fn variables(&self, arena: &Arena<Pattern>) -> Vec<PatternVariable> {
+    pub fn variables(&self, queryable: &dyn PatternVariablesQueryable) -> Vec<PatternVariable> {
         match self {
             Pattern::Variable(variable) => vec![variable.clone()],
             Pattern::Struct(pattern_struct) => pattern_struct
                 .field_patterns
                 .iter()
-                .flat_map(|(_member, pattern)| arena[*pattern].variables(arena))
+                .flat_map(|(_member, pattern)| queryable.query(*pattern))
                 .collect(),
             Pattern::Tuple(pattern_tuple) => pattern_tuple
                 .field_patterns
                 .iter()
-                .flat_map(|pattern| arena[*pattern].variables(arena))
+                .flat_map(|pattern| queryable.query(*pattern))
                 .collect(),
             Pattern::FixedSizeArray(pattern_fixed_size_array) => pattern_fixed_size_array
                 .elements_patterns
                 .iter()
-                .flat_map(|pattern| arena[*pattern].variables(arena))
+                .flat_map(|pattern| queryable.query(*pattern))
                 .collect(),
             Pattern::EnumVariant(pattern_enum_variant) => {
                 match &pattern_enum_variant.inner_pattern {
-                    Some(inner_pattern) => arena[*inner_pattern].variables(arena),
+                    Some(pattern) => queryable.query(*pattern),
                     None => vec![],
                 }
             }
@@ -93,6 +95,33 @@ impl Pattern {
 impl From<&Pattern> for SyntaxStablePtrId {
     fn from(pattern: &Pattern) -> Self {
         pattern.stable_ptr().into()
+    }
+}
+
+/// Polymorphic container of [`Pattern`] objects used for querying pattern variables.
+pub trait PatternVariablesQueryable {
+    /// Lookup the pattern in this container and then get [`Pattern::variables`] from it.
+    fn query(&self, id: PatternId) -> Vec<PatternVariable>;
+}
+
+impl PatternVariablesQueryable for Arena<Pattern> {
+    fn query(&self, id: PatternId) -> Vec<PatternVariable> {
+        self[id].variables(self)
+    }
+}
+
+/// Query a function for variables of patters defined within it.
+///
+/// This is a wrapper over [`SemanticGroup`] that takes [`FunctionWithBodyId`]
+/// and relays queries to [`SemanticGroup::pattern_semantic`].
+pub struct QueryPatternVariablesFromDb<'a>(
+    pub &'a (dyn SemanticGroup + 'static),
+    pub FunctionWithBodyId,
+);
+
+impl PatternVariablesQueryable for QueryPatternVariablesFromDb<'_> {
+    fn query(&self, id: PatternId) -> Vec<PatternVariable> {
+        self.0.pattern_semantic(self.1, id).variables(self)
     }
 }
 
