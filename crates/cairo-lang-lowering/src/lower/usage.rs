8e1f5d279eafcfda6d1f08be93412dc2ac654876
TomerStarkware <tomer@starkware.co>
2024-07-02 14:35:23 +0300

    fixed loop capture of snapshoted variables

--- crates/cairo-lang-lowering/src/lower/usage.rs
+++ crates/cairo-lang-lowering/src/lower/usage.rs
@@ -56,6 +56,8 @@ pub struct Usage {
     pub usage: OrderedHashMap<MemberPath, ExprVarMemberPath>,
     /// Member paths that are assigned to.
     pub changes: OrderedHashMap<MemberPath, ExprVarMemberPath>,
+    /// Member paths that are read as snapshots.
+    pub snap_usage: OrderedHashMap<MemberPath, ExprVarMemberPath>,
     /// Variables that are defined.
     pub introductions: OrderedHashSet<VarId>,
 }
@@ -68,6 +70,9 @@ impl Usage {
         }
         for (path, expr) in usage.changes.iter() {
             self.changes.insert(path.clone(), expr.clone());
+        }
+        for (path, expr) in usage.snap_usage.iter() {
+            self.snap_usage.insert(path.clone(), expr.clone());
         }
     }
 
@@ -107,6 +112,30 @@ impl Usage {
                 }
             }
         }
+        for (member_path, _) in self.snap_usage.clone() {
+            // Prune usages from snap_usage.
+            if self.usage.contains_key(&member_path) {
+                self.snap_usage.swap_remove(&member_path);
+                continue;
+            }
+
+            // Prune introductions from snap_usage.
+            if self.introductions.contains(&member_path.base_var()) {
+                self.snap_usage.swap_remove(&member_path);
+            }
+
+            // Prune snap_usage that are members of other snap_usage or usages.
+            let mut current_path = member_path.clone();
+            while let MemberPath::Member { parent, .. } = current_path {
+                current_path = *parent.clone();
+                if self.snap_usage.contains_key(&current_path)
+                    | self.usage.contains_key(&current_path)
+                {
+                    self.snap_usage.swap_remove(&member_path);
+                    break;
+                }
+            }
+        }
     }
 }
 
@@ -142,7 +171,26 @@ impl BlockUsages {
                     self.handle_expr(function_body, *value, current);
                 }
             },
-            Expr::Snapshot(expr) => self.handle_expr(function_body, expr.inner, current),
+            Expr::Snapshot(expr) => {
+                let expr_id = expr.inner;
+
+                match &function_body.exprs[expr_id] {
+                    Expr::Var(expr_var) => {
+                        current.snap_usage.insert(
+                            MemberPath::Var(expr_var.var),
+                            ExprVarMemberPath::Var(expr_var.clone()),
+                        );
+                    }
+                    Expr::MemberAccess(expr) => {
+                        if let Some(member_path) = &expr.member_path {
+                            current.snap_usage.insert(member_path.into(), member_path.clone());
+                        } else {
+                            self.handle_expr(function_body, expr.expr, current);
+                        }
+                    }
+                    _ => self.handle_expr(function_body, expr_id, current),
+                }
+            }
             Expr::Desnap(expr) => self.handle_expr(function_body, expr.inner, current),
             Expr::Assignment(expr) => {
                 self.handle_expr(function_body, expr.rhs, current);
