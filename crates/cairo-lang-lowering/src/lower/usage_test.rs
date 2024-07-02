9a580f741125324f2bbcdeb73a4922abe8dfcf46
TomerStarkware <tomer@starkware.co>
2024-07-02 14:35:23 +0300

    fixed loop capture of snapshoted variables

--- crates/cairo-lang-lowering/src/lower/usage_test.rs
+++ crates/cairo-lang-lowering/src/lower/usage_test.rs
@@ -58,20 +58,26 @@ fn test_function_usage(
             _ => unreachable!(),
         }
         writeln!(usages_str, " {}:{}:", position.line, position.col).unwrap();
-        write!(usages_str, "  Usage: ").unwrap();
+        write!(usages_str, "  Usage:").unwrap();
         for (_, expr) in usage.usage.iter() {
-            write!(usages_str, "{:?}, ", expr.debug(&expr_formatter)).unwrap();
+            write!(usages_str, " {:?},", expr.debug(&expr_formatter)).unwrap();
         }
         writeln!(usages_str).unwrap();
-        write!(usages_str, "  Changes: ").unwrap();
+        write!(usages_str, "  Changes:").unwrap();
         for (_, expr) in usage.changes.iter() {
-            write!(usages_str, "{:?}, ", expr.debug(&expr_formatter)).unwrap();
+            write!(usages_str, " {:?},", expr.debug(&expr_formatter)).unwrap();
         }
         writeln!(usages_str).unwrap();
-        write!(usages_str, "  Introductions: ").unwrap();
-        for var in &usage.introductions {
-            write!(usages_str, "{:?}, ", var.debug(&expr_formatter)).unwrap();
+        write!(usages_str, "  Snapshot_Usage:").unwrap();
+        for (_, expr) in usage.snap_usage.iter() {
+            write!(usages_str, " {:?},", expr.debug(&expr_formatter)).unwrap();
         }
+        writeln!(usages_str).unwrap();
+        write!(usages_str, "  Introductions:").unwrap();
+        for var in &usage.introductions {
+            write!(usages_str, " {:?},", var.debug(&expr_formatter)).unwrap();
+        }
+
         writeln!(usages_str).unwrap();
     }
 
