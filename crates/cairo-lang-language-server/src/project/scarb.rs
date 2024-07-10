5a5112b43d8bb125c39e9f84fe4f9790b5a267d6
Marek Kaput <marek.kaput@swmansion.com>
2024-07-10 16:51:30 +0200

    LS: New Project Model MVP
    commit-id:b2ab81f2

--- crates/cairo-lang-language-server/src/project/scarb.rs
+++ crates/cairo-lang-language-server/src/project/scarb.rs
@@ -1,29 +1,39 @@
+//! Query implementations for the Scarb-based projects.
+
 use std::fs;
 use std::path::Path;
+use std::sync::Arc;
 
 use anyhow::{bail, ensure, Context, Result};
 use cairo_lang_filesystem::db::{CrateSettings, Edition, ExperimentalFeaturesConfig};
+use cairo_lang_utils::LookupIntern;
 use scarb_metadata::{Metadata, PackageMetadata};
-use tracing::{debug, warn};
+use tracing::{debug, error, warn};
 
-use crate::lang::db::AnalysisDatabase;
-use crate::project::crate_data::Crate;
+use crate::project::main::{LsProjectsGroup, ProjectId};
+use crate::project::project_manifest_path::ProjectManifestPath;
+use crate::project::Crate;
 
-/// Updates crate roots in the database with the information from Scarb metadata.
+/// Gets the list of crates from a Scarb-based project.
 ///
-/// This function attempts to be graceful. Any erroneous cases will be reported as warnings in logs,
-/// and the database will be left intact for problematic crates.
+/// This function attempts to be graceful.
+/// Any erroneous cases will be reported as warnings in logs, and the database will be left intact
+/// for problematic crates.
 ///
-/// In all real-world scenarios, this function should always initialize the `core` crate.
+/// In all real-world scenarios, this function should always yield a `core` crate.
 /// Technically, it is possible for `scarb metadata` to omit `core` if working on a `no-core`
-/// package, but in reality enabling `no-core` makes sense only for the `core` package itself. To
-/// leave a trace of unreal cases, this function will log a warning if `core` is missing.
+/// package, but in reality enabling `no-core` makes sense only for the `core` package itself.
+/// To leave a trace of unreal cases, this function will log a warning if `core` is missing.
 // FIXME(mkaput): Currently this logic is feeding all compilation units of the single package at
 //  once. Often packages declare several targets (lib, starknet-contract, test), which currently
 //  causes overriding of the crate within single call of this function. This is an UX problem, for
 //  which we do not know the solution yet.
-pub fn update_crate_roots(metadata: &Metadata, db: &mut AnalysisDatabase) {
-    let mut crates = Vec::<Crate>::new();
+pub fn project_crates(db: &dyn LsProjectsGroup, project: ProjectId) -> Arc<[Arc<Crate>]> {
+    let Some(metadata) = db.scarb_metadata(project) else {
+        return [].into();
+    };
+
+    let mut crates = Vec::<Arc<Crate>>::new();
     for compilation_unit in &metadata.compilation_units {
         if compilation_unit.target.kind == "cairo-plugin" {
             debug!("skipping cairo plugin compilation unit: {}", compilation_unit.id);
@@ -60,111 +70,129 @@ pub fn update_crate_roots(metadata: &Metadata, db: &mut AnalysisDatabase) {
 
             let custom_main_file_stem = (file_stem != "lib").then_some(file_stem.into());
 
-            crates.push(Crate {
-                name: crate_name.into(),
-                root: root.into(),
-                custom_main_file_stem,
-                settings,
-            });
+            crates.push(
+                Crate {
+                    name: crate_name.into(),
+                    root: root.into(),
+                    custom_main_file_stem,
+                    settings,
+                }
+                .into(),
+            );
         }
     }
 
-    debug!("updating crate roots from scarb metadata: {crates:#?}");
-
-    if !crates.iter().any(Crate::is_core) {
-        warn!("core crate is missing in scarb metadata, did not initialize it");
+    if !crates.iter().any(|cr| cr.is_core()) {
+        warn!("the `core` crate is missing in scarb metadata, will try to use unmanaged `core`");
     }
 
-    for cr in crates {
-        cr.apply(db);
+    return crates.into();
+
+    /// Perform sanity checks on crate _source path_, and chop it into directory path and file stem.
+    fn validate_and_chop_source_path<'a>(
+        source_path: &'a Path,
+        crate_name: &str,
+    ) -> Result<(&'a Path, &'a str)> {
+        let metadata = fs::metadata(source_path)
+            .with_context(|| format!("io error when accessing source path of: {crate_name}"))?;
+
+        ensure!(
+            !metadata.is_dir(),
+            "source path of component `{crate_name}` must not be a directory: {source_path}",
+            source_path = source_path.display()
+        );
+
+        let Some(root) = source_path.parent() else {
+            bail!(
+                "unexpected fs root as a source path of component `{crate_name}`: {source_path}",
+                source_path = source_path.display()
+            );
+        };
+
+        ensure!(
+            root.is_absolute(),
+            "source path must be absolute: {source_path}",
+            source_path = source_path.display()
+        );
+
+        let Some(file_stem) = source_path.file_stem() else {
+            bail!(
+                "failed to get file stem for component `{crate_name}`: {source_path}",
+                source_path = source_path.display()
+            );
+        };
+
+        let Some(file_stem) = file_stem.to_str() else {
+            bail!("file stem is not utf-8: {source_path}", source_path = source_path.display());
+        };
+
+        Ok((root, file_stem))
+    }
+
+    /// Get the [`Edition`] from [`PackageMetadata`], or assume the default edition.
+    fn scarb_package_edition(package: &Option<&PackageMetadata>, crate_name: &str) -> Edition {
+        package
+            .and_then(|p| p.edition.clone())
+            .and_then(|e| {
+                serde_json::from_value(e.into())
+                    .with_context(|| format!("failed to parse edition of package: {crate_name}"))
+                    .inspect_err(|e| warn!("{e:?}"))
+                    .ok()
+            })
+            .unwrap_or_default()
+    }
+
+    /// Convert a slice of [`scarb_metadata::Cfg`]s to a [`cairo_lang_filesystem::cfg::CfgSet`].
+    ///
+    /// The conversion is done the same way as in Scarb (except no panicking):
+    /// <https://github.com/software-mansion/scarb/blob/9fe97c8eb8620a1e2103e7f5251c5a9189e75716/scarb/src/ops/metadata.rs#L295-L302>
+    fn scarb_cfg_set_to_cairo(
+        cfg_set: &[scarb_metadata::Cfg],
+        crate_name: &str,
+    ) -> Option<cairo_lang_filesystem::cfg::CfgSet> {
+        serde_json::to_value(cfg_set)
+            .and_then(serde_json::from_value)
+            .with_context(|| {
+                format!(
+                    "scarb metadata cfg did not convert identically to cairo one for crate: \
+                     {crate_name}"
+                )
+            })
+            .inspect_err(|e| warn!("{e:?}"))
+            .ok()
+    }
+
+    /// Get [`ExperimentalFeaturesConfig`] from [`PackageMetadata`] fields.
+    fn scarb_package_experimental_features(
+        package: &Option<&PackageMetadata>,
+    ) -> ExperimentalFeaturesConfig {
+        let contains = |feature: &str| -> bool {
+            let Some(package) = package else { return false };
+            package.experimental_features.iter().any(|f| f == feature)
+        };
+
+        ExperimentalFeaturesConfig {
+            negative_impls: contains("negative_impls"),
+            coupons: contains("coupons"),
+        }
     }
 }
 
-/// Perform sanity checks on crate _source path_, and chop it into directory path and file stem.
-fn validate_and_chop_source_path<'a>(
-    source_path: &'a Path,
-    crate_name: &str,
-) -> Result<(&'a Path, &'a str)> {
-    let metadata = fs::metadata(source_path)
-        .with_context(|| format!("io error when accessing source path of: {crate_name}"))?;
-
-    ensure!(
-        !metadata.is_dir(),
-        "source path of component `{crate_name}` must not be a directory: {source_path}",
-        source_path = source_path.display()
-    );
-
-    let Some(root) = source_path.parent() else {
-        bail!(
-            "unexpected fs root as a source path of component `{crate_name}`: {source_path}",
-            source_path = source_path.display()
-        );
+/// Gets `scarb metadata` for the given project.
+pub fn scarb_metadata(db: &dyn LsProjectsGroup, project: ProjectId) -> Option<Arc<Metadata>> {
+    let project_manifest_path = project.lookup_intern(db);
+    let ProjectManifestPath::Scarb(manifest_path) = project_manifest_path else {
+        warn!("attempted to get scarb metadata for non-scarb project: {project_manifest_path}");
+        return None;
     };
 
-    ensure!(
-        root.is_absolute(),
-        "source path must be absolute: {source_path}",
-        source_path = source_path.display()
-    );
-
-    let Some(file_stem) = source_path.file_stem() else {
-        bail!(
-            "failed to get file stem for component `{crate_name}`: {source_path}",
-            source_path = source_path.display()
-        );
-    };
-
-    let Some(file_stem) = file_stem.to_str() else {
-        bail!("file stem is not utf-8: {source_path}", source_path = source_path.display());
-    };
-
-    Ok((root, file_stem))
-}
-
-/// Get the [`Edition`] from [`PackageMetadata`], or assume the default edition.
-fn scarb_package_edition(package: &Option<&PackageMetadata>, crate_name: &str) -> Edition {
-    package
-        .and_then(|p| p.edition.clone())
-        .and_then(|e| {
-            serde_json::from_value(e.into())
-                .with_context(|| format!("failed to parse edition of package: {crate_name}"))
-                .inspect_err(|e| warn!("{e:?}"))
-                .ok()
+    db.scarb_toolchain()
+        .metadata(&manifest_path)
+        .with_context(|| format!("failed to reload scarb workspace: {}", manifest_path.display()))
+        .inspect_err(|e| {
+            // TODO(mkaput): Send a notification to the language client about the error.
+            error!("{e:?}");
         })
-        .unwrap_or_default()
-}
-
-/// Convert a slice of [`scarb_metadata::Cfg`]s to a [`cairo_lang_filesystem::cfg::CfgSet`].
-///
-/// The conversion is done the same way as in Scarb (except no panicking):
-/// <https://github.com/software-mansion/scarb/blob/9fe97c8eb8620a1e2103e7f5251c5a9189e75716/scarb/src/ops/metadata.rs#L295-L302>
-fn scarb_cfg_set_to_cairo(
-    cfg_set: &[scarb_metadata::Cfg],
-    crate_name: &str,
-) -> Option<cairo_lang_filesystem::cfg::CfgSet> {
-    serde_json::to_value(cfg_set)
-        .and_then(serde_json::from_value)
-        .with_context(|| {
-            format!(
-                "scarb metadata cfg did not convert identically to cairo one for crate: \
-                 {crate_name}"
-            )
-        })
-        .inspect_err(|e| warn!("{e:?}"))
         .ok()
-}
-
-/// Get [`ExperimentalFeaturesConfig`] from [`PackageMetadata`] fields.
-fn scarb_package_experimental_features(
-    package: &Option<&PackageMetadata>,
-) -> ExperimentalFeaturesConfig {
-    let contains = |feature: &str| -> bool {
-        let Some(package) = package else { return false };
-        package.experimental_features.iter().any(|f| f == feature)
-    };
-
-    ExperimentalFeaturesConfig {
-        negative_impls: contains("negative_impls"),
-        coupons: contains("coupons"),
-    }
+        .map(Arc::new)
 }
--- crates/cairo-lang-language-server/src/project/unmanaged_core_crate.rs
+++ crates/cairo-lang-language-server/src/project/unmanaged_core_crate.rs
@@ -1,40 +1,89 @@
 use std::path;
 use std::path::{Path, PathBuf};
+use std::sync::Arc;
 
 use anyhow::Context;
-use cairo_lang_filesystem::db::init_dev_corelib;
-use tracing::{error, warn};
+use cairo_lang_filesystem::db::CORELIB_CRATE_NAME;
+use cairo_lang_project::ProjectConfig;
+use salsa::Durability;
+use smol_str::SmolStr;
+use tracing::error;
 
 use crate::config::Config;
-use crate::lang::db::AnalysisDatabase;
+use crate::project::Crate;
 
-/// Try to find a Cairo `core` crate (see [`find_unmanaged_core`]) and initialize it in the
-/// provided database.
-pub fn try_to_init_unmanaged_core(config: &Config, db: &mut AnalysisDatabase) {
-    if let Some(path) = find_unmanaged_core(config) {
-        init_dev_corelib(db, path);
-    } else {
-        warn!("failed to find unmanaged core crate")
-    }
+/// A group of queries responsible for dealing with the unmanaged Cairo `core` crate.
+///
+/// An unmanaged `core` is one that is used in projects whose managers do not handle the `core`
+/// crate themselves.
+/// This is the case for `cairo_project.toml`-based projects,
+/// detached files and not-yet-fully loaded Scarb workspaces.
+#[salsa::query_group(LsUnmanagedCoreDatabase)]
+pub trait LsUnmanagedCoreGroup {
+    /// Path to unmanaged `core` from LS configuration.
+    #[salsa::input]
+    fn config_unmanaged_core_path(&self) -> Option<PathBuf>;
+
+    /// Tries to find a Cairo `core` crate in various well-known places, for use in project backends
+    /// that do not manage the `core` crate (i.e., anything non-Scarb).
+    fn unmanaged_core(&self) -> Option<Arc<Crate>>;
 }
 
-/// Try to find a Cairo `core` crate in various well-known places, for use in project backends that
-/// do not manage the `core` crate (i.e., anything non-Scarb).
-///
-/// The path is guaranteed to be absolute, so it can be safely used as a `FileId` in LS Salsa DB.
-pub fn find_unmanaged_core(config: &Config) -> Option<PathBuf> {
+pub fn set_unmanaged_core_inputs_from_config(db: &mut dyn LsUnmanagedCoreGroup, config: &Config) {
+    db.set_config_unmanaged_core_path_with_durability(
+        config.unmanaged_core_path.clone(),
+        Durability::HIGH,
+    );
+}
+
+fn unmanaged_core(db: &dyn LsUnmanagedCoreGroup) -> Option<Arc<Crate>> {
+    let crate_path = find_unmanaged_core(db)?;
+
+    macro_rules! load_error {
+        ($message:literal) => {
+            error!(
+                concat!($message, ", language server features may not work: {crate_path}"),
+                crate_path = crate_path.display()
+            );
+        };
+    }
+
+    let name = SmolStr::new_static(CORELIB_CRATE_NAME);
+
+    let Some(project_config) = try_load_core_project_config(&crate_path) else {
+        load_error!("failed to load unmanaged `core` crate's cairo_project.toml");
+        return None;
+    };
+
+    let Some(source_root) = project_config.content.crate_roots.get(&name) else {
+        load_error!(
+            "the found unmanaged `core` crate's cairo_project.toml does not actually define the \
+             `core` crate"
+        );
+        return None;
+    };
+
+    let root = project_config.absolute_crate_root(source_root);
+
+    let settings = project_config.content.crates_config.get(&name).clone();
+
+    Some(Arc::new(Crate { name, root, custom_main_file_stem: None, settings }))
+}
+
+/// Tries to find a Cairo `core` crate in various well-known places.
+fn find_unmanaged_core(db: &dyn LsUnmanagedCoreGroup) -> Option<PathBuf> {
     // TODO(mkaput): First, try to find Scarb-managed `core` package if we have Scarb toolchain.
     //   The most reliable way to do this is to create an empty Scarb package, and run
     //   `scarb metadata` on it. The `core` package will be a component of this empty package.
     //   For minimal packages, `scarb metadata` should be pretty fast.
-    find_core_at_config_path(config)
+    find_core_at_config_path(db)
         .or_else(cairo_lang_filesystem::detect::detect_corelib)
         .and_then(ensure_absolute)
 }
 
 /// Attempts to find the `core` crate source root at the path provided in the configuration.
-fn find_core_at_config_path(config: &Config) -> Option<PathBuf> {
-    find_core_at_path(config.unmanaged_core_path.as_ref()?.as_path())
+fn find_core_at_config_path(db: &dyn LsUnmanagedCoreGroup) -> Option<PathBuf> {
+    find_core_at_path(&db.config_unmanaged_core_path()?)
 }
 
 /// Attempts to find the `core` crate source root at a given path.
@@ -48,7 +97,7 @@ fn find_core_at_config_path(config: &Config) -> Option<PathBuf> {
 /// [starkware-libs/cairo]: https://github.com/starkware-libs/cairo
 fn find_core_at_path(root_path: &Path) -> Option<PathBuf> {
     let mut path = root_path.to_owned();
-    path.push("corelib");
+    path.push("../../../../../corelib");
     path.push("src");
     if path.exists() {
         return Some(path);
@@ -65,6 +114,25 @@ fn find_core_at_path(root_path: &Path) -> Option<PathBuf> {
     }
 
     None
+}
+
+/// Try to load `core`'s `cairo_project.toml` at the given path, or if this path points to `core`'s
+/// `src` directory, looks in parent directory.
+///
+/// This fallback case is needed because:
+/// 1. `detect_corelib` returns path to `src`, or
+/// 2. the user who does not understand such details may provide such path in config.
+fn try_load_core_project_config(crate_path: &Path) -> Option<ProjectConfig> {
+    let attempt =
+        |path| ProjectConfig::from_directory(path).inspect_err(|err| error!("{err:?}")).ok();
+
+    attempt(crate_path).or_else(|| {
+        if crate_path.file_name().and_then(|s| s.to_str()) == Some("src") {
+            attempt(crate_path.parent().unwrap())
+        } else {
+            None
+        }
+    })
 }
 
 /// Makes a path absolute, or logs an error.
