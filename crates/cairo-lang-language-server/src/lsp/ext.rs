9870ae8390044e8c6809d2f98277085a69a815c3
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2024-07-12 11:10:21 +0200

    LS: Expand Macro command server side
    commit-id:21633c82

--- crates/cairo-lang-language-server/src/lsp/ext.rs
+++ crates/cairo-lang-language-server/src/lsp/ext.rs
@@ -1,6 +1,7 @@
 //! CairoLS extensions to the Language Server Protocol.
 
 use tower_lsp::lsp_types::request::Request;
+use tower_lsp::lsp_types::TextDocumentPositionParams;
 
 // TODO(mkaput): Provide this as a command in VSCode.
 /// Collect information about all Cairo crates that are currently being analyzed.
@@ -10,4 +11,13 @@ impl Request for ViewAnalyzedCrates {
     type Params = ();
     type Result = String;
     const METHOD: &'static str = "cairo/viewAnalyzedCrates";
+}
+
+/// Provides string with code after macros expansion
+pub struct ExpandMacro;
+
+impl Request for ExpandMacro {
+    type Params = TextDocumentPositionParams;
+    type Result = Option<String>;
+    const METHOD: &'static str = "cairo/expandMacro";
 }
