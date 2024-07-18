5152a9eca2176cfbc05a573f6837800497e9c4c0
TomerStarkware <144585788+TomerStarkware@users.noreply.github.com>
2024-07-18 11:52:23 +0300

    added closure expression to semantic (#6015)

--- crates/cairo-lang-semantic/src/corelib.rs
+++ crates/cairo-lang-semantic/src/corelib.rs
@@ -403,7 +403,8 @@ pub fn unwrap_error_propagation_type(
         | TypeLongId::Coupon(_)
         | TypeLongId::ImplType(_)
         | TypeLongId::Missing(_)
-        | TypeLongId::FixedSizeArray { .. } => None,
+        | TypeLongId::FixedSizeArray { .. }
+        | TypeLongId::Closure(_) => None,
         // TODO(yuval): for trait function default implementation, this may need to change.
         TypeLongId::TraitType(_) => {
             panic!("Trait types should only appear in traits, where there are no function bodies.")
