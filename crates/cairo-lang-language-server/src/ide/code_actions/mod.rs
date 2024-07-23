44e8a32e54b34a85507ea09ec72c48c45fa82b5f
Gil Ben-Shachar <gil@starkware.co>
2024-07-23 18:40:56 +0300

    Add missing trait quick fix.
    commit-id:41236fd0

--- crates/cairo-lang-language-server/src/ide/code_actions/mod.rs
+++ crates/cairo-lang-language-server/src/ide/code_actions/mod.rs
@@ -1,13 +1,15 @@
+use anyhow::bail;
 use cairo_lang_syntax::node::SyntaxNode;
 use tower_lsp::lsp_types::{
-    CodeAction, CodeActionOrCommand, CodeActionParams, CodeActionResponse, Diagnostic,
-    NumberOrString,
+    CodeAction, CodeActionKind, CodeActionOrCommand, CodeActionParams, CodeActionResponse,
+    Diagnostic, NumberOrString,
 };
-use tracing::debug;
+use tracing::{debug, error};
 
 use crate::lang::db::{AnalysisDatabase, LsSyntaxGroup};
 use crate::lang::lsp::{LsProtoGroup, ToCairo};
 
+mod add_missing_trait;
 mod rename_unused_variable;
 
 /// Compute commands for a given text document and range. These commands are typically code fixes to
@@ -70,6 +72,14 @@ fn get_code_actions_for_diagnostic(
                 params.text_document.uri.clone(),
             )]
         }
+        // E0002 is CannotCallMethod, we want to add a "Quick Fix" to add a "use" for a candidate
+        // trait.
+        "E0002" => add_missing_trait::add_missing_trait(
+            db,
+            node,
+            diagnostic.clone(),
+            params.text_document.uri.clone(),
+        ),
         code => {
             debug!("no code actions for diagnostic code: {code}");
             vec![]
