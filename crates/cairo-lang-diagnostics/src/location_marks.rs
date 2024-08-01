7d67313a329715d5aebee66ce93f1e22454edb98
LucasLvy <lucaslevy10@gmail.com>
2024-08-01 12:48:01 +0200

    refacto(location_mark): make it more modular for color introduction

--- crates/cairo-lang-diagnostics/src/location_marks.rs
+++ crates/cairo-lang-diagnostics/src/location_marks.rs
@@ -1,5 +1,3 @@
-use std::fmt::Write;
-
 use cairo_lang_filesystem::span::{TextPosition, TextSpan, TextWidth};
 
 use crate::DiagnosticLocation;
@@ -8,16 +6,18 @@ use crate::DiagnosticLocation;
 #[path = "location_marks_test.rs"]
 mod test;
 
-pub fn get_location_marks(
+/// Gets the string from a diagnostic location. If the span is on a single line it will return the
+/// whole line if it's on multiple lines it return the exact span. It also returns the start column
+/// of the span and the marker length (if it's a multiline span both will be 0)
+fn extract_content(
     db: &dyn cairo_lang_filesystem::db::FilesGroup,
     location: &DiagnosticLocation,
-) -> String {
-    // TODO(ilya, 10/10/2023): Handle locations which spread over a few lines.
+) -> (String, usize, usize) {
     let content = db.file_content(location.file_id).expect("File missing from DB.");
     let summary = db.file_summary(location.file_id).expect("File missing from DB.");
 
     let mut span = location.span;
-    // Make sure that the span doesn't end after the end of the file.
+    // Ensure the span does not go beyond the end of the file.
     span.end = std::cmp::min(summary.last_offset, span.end);
 
     let TextPosition { line: first_line_idx, col: start_col } = span
@@ -36,58 +36,88 @@ pub fn get_location_marks(
         };
 
         let first_line_span = TextSpan { start: first_line_start, end: first_line_end };
-        let mut res = first_line_span.take(&content).to_string();
+        let line_content = first_line_span.take(&content).to_string();
+        let subspan_in_first_line =
+            TextSpan { start: span.start, end: std::cmp::min(first_line_end, span.end) };
+        let marker_length = subspan_in_first_line.n_chars(&content);
+        (line_content, start_col, marker_length)
+    } else {
+        let span_content = span.take(&content).to_string();
+        (span_content, 0, 0)
+    }
+}
+
+/// Generate the diagnostic marks on a [&str]
+///
+/// # Arguemnts
+///
+/// * `snippet` - The code to add marks to.
+/// * `start_col` - If the snippet is a 1 liner will start the mark at this index else will not use
+///   it.
+/// * `marker_length` - If the snippet is a 1 liner will use this as the marker length else will not
+///   use it.
+fn generate_marks(snippet: &str, start_col: usize, marker_length: usize) -> String {
+    let mut res = String::new();
+
+    // Single-line span handling
+    if !snippet.contains('\n') {
+        res.push_str(snippet);
         res.push('\n');
         for _ in 0..start_col {
             res.push(' ');
         }
         res.push('^');
-
-        let subspan_in_first_line =
-            TextSpan { start: span.start, end: std::cmp::min(first_line_end, span.end) };
-        let marker_length = subspan_in_first_line.n_chars(&content);
         if marker_length > 1 {
             for _ in 0..marker_length - 2 {
                 res.push('*');
             }
             res.push('^');
         }
-
-        res
-    } else {
-        let mut res = String::new();
-        // Number of indentations
-        let mut tab = 0;
-        let mut prefix = "\\   ";
-        let mut lines = span.take(&content).split('\n').peekable();
-        while let Some(line) = lines.next() {
-            let line = line.trim();
-            // Will check if we reached an `} else {` or an `} else {}` or `} else {};`
-            let is_else = line.starts_with('}')
-                && (line.ends_with('{') || line.ends_with("{}") || line.ends_with("{};"))
-                && line.contains("else");
-            if tab > 0
-                // If we close a scope that wasn't opened in the same line.
-                // Or if it's an else we need to deindent the line of the else.
-                && (((line.ends_with('}') || line.ends_with("};") || line.ends_with("},"))
-                    && !line.contains('{'))
-                    || is_else)
-            {
-                tab -= 1
-            }
-            res.write_str(&format!("{prefix}{}{}\n", "    ".repeat(tab), line)).unwrap();
-            // If we open a scope that is not closed on the same line.
-            // If it's an else we need to indent for the inside of the else
-            if line.contains('{') && !line.contains('}') || is_else {
-                tab += 1
-            }
-            prefix = "|   ";
-            // If it's the last line point to where the span ends.
-            if lines.peek().is_none() {
-                res.write_str(&format!("|___{}^", "_".repeat(std::cmp::max(line.len(), 1) - 1)))
-                    .unwrap();
-            }
-        }
-        res
+        return res;
     }
+
+    // Multi-line span handling
+    let mut lines = snippet.split('\n').peekable();
+    let mut tab = 0;
+    let mut prefix = "\\   ";
+
+    while let Some(line) = lines.next() {
+        let line = line.trim();
+        // Will check if we reached an `} else {` or an `} else {}` or `} else {};`
+        let is_else = line.starts_with('}')
+            && (line.ends_with('{') || line.ends_with("{}") || line.ends_with("{};"))
+            && line.contains("else");
+
+        if tab > 0
+            // If we close a scope that wasn't opened in the same line.
+            // Or if it's an else we need to deindent the line of the else.
+            && (((line.ends_with('}') || line.ends_with("};") || line.ends_with("},"))
+                && !line.contains('{'))
+                || is_else)
+        {
+            tab -= 1;
+        }
+
+        res.push_str(&format!("{prefix}{}{}\n", "    ".repeat(tab), line));
+        // If we open a scope that is not closed on the same line.
+        // If it's an else we need to indent for the inside of the else
+
+        if line.contains('{') && !line.contains('}') || is_else {
+            tab += 1;
+        }
+        // If it's the last line point to where the span ends.
+        prefix = "|   ";
+        if lines.peek().is_none() {
+            res.push_str(&format!("|___{}^", "_".repeat(std::cmp::max(line.len(), 1) - 1)));
+        }
+    }
+
+    res
+}
+pub fn get_location_marks(
+    db: &dyn cairo_lang_filesystem::db::FilesGroup,
+    location: &DiagnosticLocation,
+) -> String {
+    let (content, start_col, marker_length) = extract_content(db, location);
+    generate_marks(&content, start_col, marker_length)
 }
