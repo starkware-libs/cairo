a037015fd81c13e48fe64579b9aa8a372c19d264
Ori Ziv <oriz@starkware.co>
2024-08-06 18:20:35 +0300

    Smoothed diagnostics refresh to not happen for every keystroke.
    Now only starts after previous one was done.

--- crates/cairo-lang-language-server/src/lib.rs
+++ crates/cairo-lang-language-server/src/lib.rs
@@ -264,6 +264,7 @@ struct Backend {
     // TODO(spapini): Remove this once we support ParallelDatabase.
     // State mutex should only be taken after db mutex is taken, to avoid deadlocks.
     db_mutex: tokio::sync::Mutex<AnalysisDatabase>,
+    refresh_lock: tokio::sync::Mutex<()>,
     state_mutex: tokio::sync::Mutex<State>,
     config: tokio::sync::RwLock<Config>,
     scarb_toolchain: ScarbToolchain,
@@ -288,6 +289,7 @@ impl Backend {
             client_capabilities: Default::default(),
             tricks,
             db_mutex: db.into(),
+            refresh_lock: Default::default(),
             state_mutex: State::default().into(),
             config: Config::default().into(),
             scarb_toolchain,
@@ -329,6 +331,9 @@ impl Backend {
     /// Refresh diagnostics and send diffs to client.
     #[tracing::instrument(level = "debug", skip_all)]
     async fn refresh_diagnostics(&self) -> LSPResult<()> {
+        // Making sure only a single set refreshes diagnostics as the same time.
+        // This way, following small updates to the db would not cause refreshes per sub-change.
+        let _lock = self.refresh_lock.lock().await;
         let open_files = self.state_mut().await.open_files.clone();
 
         // First, refresh diagnostics for each open file.
