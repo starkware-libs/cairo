f715fb06b3c295e3a963ca466438def46263fbba
Mateusz Kowalski <wawel37@gmail.com>
2024-09-09 14:38:03 +0200

    Doc comments/support module level comments (#6317)

--- crates/cairo-lang-doc/src/documentable_item.rs
+++ crates/cairo-lang-doc/src/documentable_item.rs
@@ -1,22 +1,33 @@
 use cairo_lang_defs::db::DefsGroup;
 use cairo_lang_defs::diagnostic_utils::StableLocation;
 use cairo_lang_defs::ids::{LanguageElementId, LookupItemId, MemberId, VariantId};
+use cairo_lang_filesystem::ids::CrateId;
 
 /// Item which documentation can be fetched from source code.
 #[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
 pub enum DocumentableItemId {
+    Crate(CrateId),
     LookupItem(LookupItemId),
     Member(MemberId),
     Variant(VariantId),
 }
 
 impl DocumentableItemId {
-    pub fn stable_location(&self, db: &dyn DefsGroup) -> StableLocation {
+    pub fn stable_location(&self, db: &dyn DefsGroup) -> Option<StableLocation> {
         match self {
-            DocumentableItemId::LookupItem(lookup_item_id) => lookup_item_id.stable_location(db),
-            DocumentableItemId::Member(member_id) => member_id.stable_location(db),
-            DocumentableItemId::Variant(variant_id) => variant_id.stable_location(db),
+            DocumentableItemId::Crate(_) => None,
+            DocumentableItemId::LookupItem(lookup_item_id) => {
+                Some(lookup_item_id.stable_location(db))
+            }
+            DocumentableItemId::Member(member_id) => Some(member_id.stable_location(db)),
+            DocumentableItemId::Variant(variant_id) => Some(variant_id.stable_location(db)),
         }
+    }
+}
+
+impl From<CrateId> for DocumentableItemId {
+    fn from(value: CrateId) -> Self {
+        DocumentableItemId::Crate(value)
     }
 }
 
