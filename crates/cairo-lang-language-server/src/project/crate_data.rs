097549ce57531efc87bbdfd99de125b107364b79
Marek Kaput <marek.kaput@swmansion.com>
2024-10-03 18:07:38 +0200

    LS: Properly set crate dependencies and their settings
    fixes #6444
    
    commit-id:836bbb27

--- crates/cairo-lang-language-server/src/project/crate_data.rs
+++ crates/cairo-lang-language-server/src/project/crate_data.rs
@@ -8,7 +8,7 @@ use cairo_lang_filesystem::db::{
 };
 use cairo_lang_filesystem::ids::{CrateId, CrateLongId, Directory};
 use cairo_lang_utils::{Intern, LookupIntern};
-use smol_str::{SmolStr, ToSmolStr};
+use smol_str::SmolStr;
 
 use crate::lang::db::AnalysisDatabase;
 
@@ -17,6 +17,11 @@ use crate::lang::db::AnalysisDatabase;
 pub struct Crate {
     /// Crate name.
     pub name: SmolStr,
+
+    /// Globally unique crate ID used for differentiating between crates with the same name.
+    ///
+    /// `None` is reserved for the core crate.
+    pub discriminator: Option<SmolStr>,
 
     /// The root directory of the crate.
     ///
@@ -36,9 +41,14 @@ pub struct Crate {
 impl Crate {
     /// Applies this crate to the [`AnalysisDatabase`].
     pub fn apply(&self, db: &mut AnalysisDatabase) {
+        assert!(
+            (self.name == CORELIB_CRATE_NAME) ^ self.discriminator.is_some(),
+            "invariant violation: only the `core` crate should have no discriminator"
+        );
+
         let crate_id = CrateLongId::Real {
             name: self.name.clone(),
-            discriminator: self.settings.version.as_ref().map(ToSmolStr::to_smolstr),
+            discriminator: self.discriminator.clone(),
         }
         .intern(db);
 
@@ -57,7 +67,7 @@ impl Crate {
     ///
     /// Returns `None` if the crate is virtual or the crate configuration is missing.
     pub fn reconstruct(db: &AnalysisDatabase, crate_id: CrateId) -> Option<Self> {
-        let CrateLongId::Real { name, .. } = crate_id.lookup_intern(db) else {
+        let CrateLongId::Real { name, discriminator } = crate_id.lookup_intern(db) else {
             return None;
         };
 
@@ -69,7 +79,7 @@ impl Crate {
 
         let custom_main_file_stems = extract_custom_file_stems(db, crate_id);
 
-        Some(Self { name, root, custom_main_file_stems, settings })
+        Some(Self { name, discriminator, root, custom_main_file_stems, settings })
     }
 
     /// States whether this is the `core` crate.
