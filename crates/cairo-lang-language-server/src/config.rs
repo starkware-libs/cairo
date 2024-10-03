53defe041c66ac9619f6839029fa4cd060b650f2
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2024-10-03 16:53:28 +0200

    LS: Calculate diagnostics on background

--- crates/cairo-lang-language-server/src/config.rs
+++ crates/cairo-lang-language-server/src/config.rs
@@ -10,9 +10,8 @@ use tracing::{debug, error, warn};
 
 use crate::lsp::capabilities::client::ClientCapabilitiesExt;
 use crate::lsp::result::{LSPResult, LSPResultEx};
-use crate::server::client::{Notifier, Requester};
+use crate::server::client::Requester;
 use crate::server::schedule::Task;
-use crate::state::State;
 
 // TODO(mkaput): Write a macro that will auto-generate this struct and the `reload` logic.
 // TODO(mkaput): Write a test that checks that fields in this struct are sorted alphabetically.
@@ -48,7 +47,6 @@ impl Config {
         &mut self,
         requester: &mut Requester<'_>,
         client_capabilities: &ClientCapabilities,
-        on_reloaded: fn(&mut State, &Notifier),
     ) -> LSPResult<()> {
         if !client_capabilities.workspace_configuration_support() {
             warn!(
@@ -80,7 +78,7 @@ impl Config {
             // This conversion is O(1), and makes popping from front also O(1).
             let mut response = VecDeque::from(response);
 
-            Task::local(move |state, notifier, _, _| {
+            Task::local(move |state, _, _, _| {
                 state.config.unmanaged_core_path = response
                     .pop_front()
                     .as_ref()
@@ -91,8 +89,6 @@ impl Config {
                     response.pop_front().as_ref().and_then(Value::as_bool).unwrap_or_default();
 
                 debug!("reloaded configuration: {:#?}", state.config);
-
-                on_reloaded(state, &notifier);
             })
         };
 
