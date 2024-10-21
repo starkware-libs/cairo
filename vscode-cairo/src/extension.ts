da9674891dc4d22e609a862944292178dabef9e4
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-10-21 20:24:03 +0800

    chore: remove unnecessary ctx var check

--- vscode-cairo/src/extension.ts
+++ vscode-cairo/src/extension.ts
@@ -15,9 +15,7 @@ export async function activate(extensionContext: vscode.ExtensionContext) {
     ctx.log.warn("note: set `cairo1.enableLanguageServer` to `true` to enable it");
   }
 
-  if (ctx.config.get("showInStatusBar")) {
-    ctx.statusBar.setupStatusBar(client);
-  }
+  ctx.statusBar.setupStatusBar(client);
 }
 
 export function deactivate(): Thenable<void> | undefined {
