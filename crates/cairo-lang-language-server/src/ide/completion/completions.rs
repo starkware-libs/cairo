7a13d8e2e8f75108a71a8c4426a22afc1b7240c6
Jan Smółka <jp.smolka@gmail.com>
2024-10-25 13:04:12 +0200

    LS: Add completions of struct members
    commit-id:9fb1df2c

--- crates/cairo-lang-language-server/src/ide/completion/completions.rs
+++ crates/cairo-lang-language-server/src/ide/completion/completions.rs
@@ -12,6 +12,7 @@ use cairo_lang_semantic::diagnostic::{NotFoundItemType, SemanticDiagnostics};
 use cairo_lang_semantic::expr::inference::InferenceId;
 use cairo_lang_semantic::items::function_with_body::SemanticExprLookup;
 use cairo_lang_semantic::items::us::SemanticUseEx;
+use cairo_lang_semantic::items::visibility::peek_visible_in;
 use cairo_lang_semantic::lookup_item::{HasResolverData, LookupItemEx};
 use cairo_lang_semantic::resolve::{ResolvedConcreteItem, ResolvedGenericItem, Resolver};
 use cairo_lang_semantic::types::peel_snapshots;
@@ -317,4 +318,68 @@ fn module_has_trait(
         }
     }
     Some(false)
+}
+
+/// Discovers struct members missing in the constructor call and returns completions containing
+/// their names with type hints.
+pub fn struct_constructor_completions(
+    db: &AnalysisDatabase,
+    lookup_items: Vec<LookupItemId>,
+    constructor: ast::ExprStructCtorCall,
+) -> Option<Vec<CompletionItem>> {
+    let module_id = db.find_module_containing_node(&constructor.as_syntax_node())?;
+    let lookup_item_id = lookup_items.into_iter().next()?;
+    let function_id = lookup_item_id.function_with_body()?;
+
+    let already_present_members = constructor
+        .arguments(db)
+        .arguments(db)
+        .elements(db)
+        .into_iter()
+        .filter_map(|member| match member {
+            ast::StructArg::StructArgSingle(struct_arg_single) => {
+                Some(struct_arg_single.identifier(db).token(db).as_syntax_node().get_text(db))
+            }
+            // although tail covers all remaining unspecified members, we still want to show them in
+            // completion.
+            ast::StructArg::StructArgTail(_) => None,
+        })
+        .collect::<Vec<_>>();
+
+    let constructor_expr_id =
+        db.lookup_expr_by_ptr(function_id, constructor.stable_ptr().into()).ok()?;
+
+    let semantic_expr = db.expr_semantic(function_id, constructor_expr_id);
+
+    let cairo_lang_semantic::Expr::StructCtor(constructor_semantic_expr) = semantic_expr else {
+        return None;
+    };
+
+    let struct_parent_module_id =
+        constructor_semantic_expr.concrete_struct_id.struct_id(db).parent_module(db);
+
+    let struct_members =
+        db.concrete_struct_members(constructor_semantic_expr.concrete_struct_id).ok()?;
+
+    let completions = struct_members
+        .iter()
+        .filter_map(|(name, data)| {
+            let name = name.to_string();
+
+            let visible = peek_visible_in(db, data.visibility, struct_parent_module_id, module_id);
+
+            if !visible || already_present_members.contains(&name) {
+                None
+            } else {
+                Some(CompletionItem {
+                    label: name,
+                    detail: Some(data.ty.format(db)),
+                    kind: Some(CompletionItemKind::VALUE),
+                    ..Default::default()
+                })
+            }
+        })
+        .collect::<Vec<_>>();
+
+    if completions.is_empty() { None } else { Some(completions) }
 }
