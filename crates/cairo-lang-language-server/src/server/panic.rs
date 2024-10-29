4576675c41d1f5599da710d5f81add4c3eaee7c4
Marek Kaput <marek.kaput@swmansion.com>
2024-10-29 14:58:23 +0100

    LS: Fix `is_cancelled` types (#6530)

--- crates/cairo-lang-language-server/src/server/panic.rs
+++ crates/cairo-lang-language-server/src/server/panic.rs
@@ -3,7 +3,12 @@ use std::any::Any;
 use salsa::Cancelled;
 
 /// Checks if the panic was caused by Salsa cancellation.
-pub fn is_cancelled(err: &(dyn Any + Send)) -> bool {
+///
+/// This function takes an explicit reference to `Box<dyn Any + Send>` (instead of
+/// `&(dyn Any + Send)` for example), to facilitate using it while handling `catch_unwind` results.
+/// This is because downcasting works differently for `&(dyn Any + Send)`, and this function would
+/// not work in that use case.
+pub fn is_cancelled(err: &Box<dyn Any + Send>) -> bool {
     // Salsa is broken and sometimes when cancelled throws regular assert instead of `Cancelled`.
     err.is::<Cancelled>()
         || err.downcast_ref::<&str>().is_some_and(|msg| {
