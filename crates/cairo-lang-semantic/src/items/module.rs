a0d581a127cd865a3c635b01146b379ae37b399b
Tomer Cohen <tomer.c@starkware.co>
2024-10-31 13:39:35 +0200

    Handling use star in Semantics.

--- crates/cairo-lang-semantic/src/items/module.rs
+++ crates/cairo-lang-semantic/src/items/module.rs
@@ -2,7 +2,8 @@ use std::sync::Arc;
 
 use cairo_lang_defs::db::DefsGroup;
 use cairo_lang_defs::ids::{
-    LanguageElementId, LookupItemId, ModuleId, ModuleItemId, NamedLanguageElementId, TraitId,
+    GlobalUseId, LanguageElementId, LookupItemId, ModuleId, ModuleItemId, NamedLanguageElementId,
+    TraitId,
 };
 use cairo_lang_diagnostics::{Diagnostics, DiagnosticsBuilder, Maybe};
 use cairo_lang_syntax::attribute::structured::{Attribute, AttributeListStructurize};
@@ -32,6 +33,7 @@ pub struct ModuleItemInfo {
 pub struct ModuleSemanticData {
     /// The items in the module without duplicates.
     pub items: OrderedHashMap<SmolStr, ModuleItemInfo>,
+    pub global_uses: OrderedHashMap<GlobalUseId, Visibility>,
     pub diagnostics: Diagnostics<SemanticDiagnostic>,
 }
 
@@ -110,7 +112,21 @@ pub fn priv_module_semantic_data(
             );
         }
     }
-    Ok(Arc::new(ModuleSemanticData { items, diagnostics: diagnostics.build() }))
+
+    let global_uses = db
+        .module_global_uses(module_id)?
+        .iter()
+        .map(|(global_use_id, use_path_star)| {
+            let item = ast::UsePath::Star(use_path_star.clone()).get_item(syntax_db);
+            let visibility = item.visibility(syntax_db);
+            // TODO(Tomer-StarkWare): Add implementation for public imports.
+            if !matches!(visibility, ast::Visibility::Default(_)) {
+                diagnostics.report(use_path_star, SemanticDiagnosticKind::NonPrivateUseStar);
+            }
+            (*global_use_id, Visibility::Private)
+        })
+        .collect();
+    Ok(Arc::new(ModuleSemanticData { items, global_uses, diagnostics: diagnostics.build() }))
 }
 
 pub fn module_item_by_name(
--- crates/cairo-lang-semantic/src/items/us.rs
+++ crates/cairo-lang-semantic/src/items/us.rs
@@ -1,6 +1,8 @@
 use std::sync::Arc;
 
-use cairo_lang_defs::ids::{LanguageElementId, LookupItemId, ModuleItemId, UseId};
+use cairo_lang_defs::ids::{
+    GlobalUseId, LanguageElementId, LookupItemId, ModuleId, ModuleItemId, UseId,
+};
 use cairo_lang_diagnostics::{Diagnostics, Maybe, ToMaybe};
 use cairo_lang_proc_macros::DebugWithDb;
 use cairo_lang_syntax::node::db::SyntaxGroup;
@@ -12,7 +14,9 @@ use cairo_lang_utils::Upcast;
 use crate::SemanticDiagnostic;
 use crate::db::SemanticGroup;
 use crate::diagnostic::SemanticDiagnosticKind::*;
-use crate::diagnostic::{NotFoundItemType, SemanticDiagnostics, SemanticDiagnosticsBuilder};
+use crate::diagnostic::{
+    ElementKind, NotFoundItemType, SemanticDiagnostics, SemanticDiagnosticsBuilder,
+};
 use crate::expr::inference::InferenceId;
 use crate::resolve::{ResolvedGenericItem, Resolver, ResolverData};
 
@@ -49,6 +53,49 @@ pub fn priv_use_semantic_data(db: &dyn SemanticGroup, use_id: UseId) -> Maybe<Us
     Ok(UseData { diagnostics: diagnostics.build(), resolved_item, resolver_data })
 }
 
+/// Query implementation of [crate::db::SemanticGroup::priv_global_use_semantic_data].
+pub fn priv_global_use_semantic_data(
+    db: &dyn SemanticGroup,
+    global_use_id: GlobalUseId,
+) -> Maybe<UseGlobalData> {
+    let module_file_id = global_use_id.module_file_id(db.upcast());
+    let mut diagnostics = SemanticDiagnostics::default();
+    let inference_id = InferenceId::GlobalUseStar(global_use_id);
+    let star_ast = ast::UsePath::Star(db.module_global_use_by_id(global_use_id)?.to_maybe()?);
+    let mut resolver = Resolver::new(db, module_file_id, inference_id);
+
+    let item = star_ast.get_item(db.upcast());
+    let segments = get_use_path_segments(db.upcast(), star_ast.clone())?;
+    resolver.set_feature_config(&global_use_id, &item, &mut diagnostics);
+    let resolved_item = resolver.resolve_generic_path(
+        &mut diagnostics,
+        segments,
+        NotFoundItemType::Identifier,
+        None,
+    );
+    let resolver_data = Arc::new(resolver.data);
+    let imported_module = resolved_item.and_then(|item| {
+        if let ResolvedGenericItem::Module(module_id) = item {
+            Ok(module_id)
+        } else {
+            Err(diagnostics.report(&star_ast, UnexpectedElement {
+                expected: vec![ElementKind::Module],
+                actual: (&item).into(),
+            }))
+        }
+    });
+
+    Ok(UseGlobalData { diagnostics: diagnostics.build(), imported_module, resolver_data })
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, DebugWithDb)]
+#[debug_db(dyn SemanticGroup + 'static)]
+pub struct UseGlobalData {
+    diagnostics: Diagnostics<SemanticDiagnostic>,
+    imported_module: Maybe<ModuleId>,
+    resolver_data: Arc<ResolverData>,
+}
+
 /// Returns the segments that are the parts of the use path.
 ///
 /// The segments are returned in the order they appear in the use path.
@@ -61,13 +108,15 @@ pub fn get_use_path_segments(
     db: &dyn SyntaxGroup,
     use_path: ast::UsePath,
 ) -> Maybe<Vec<ast::PathSegment>> {
-    let mut rev_segments = vec![match &use_path {
-        ast::UsePath::Leaf(use_ast) => use_ast.ident(db),
-        ast::UsePath::Single(use_ast) => use_ast.ident(db),
-        ast::UsePath::Multi(_) | ast::UsePath::Star(_) => {
+    let mut rev_segments = vec![];
+    match &use_path {
+        ast::UsePath::Leaf(use_ast) => rev_segments.push(use_ast.ident(db)),
+        ast::UsePath::Single(use_ast) => rev_segments.push(use_ast.ident(db)),
+        ast::UsePath::Star(_) => {}
+        ast::UsePath::Multi(_) => {
             panic!("Only `UsePathLeaf` and `UsePathSingle` are supported.")
         }
-    }];
+    }
     let mut current_use_path = use_path;
     while let Some(parent_use_path) = get_parent_single_use_path(db, &current_use_path) {
         rev_segments.push(parent_use_path.ident(db));
