a0aa915a5989d5a4ef388237fbdc61a749126f60
Marek Kaput <marek.kaput@swmansion.com>
2024-11-08 17:37:46 +0100

    LS: Add batching files for diagnostics and plug it into refreshing logic
    This commit loses separation between open and other files in trace
    profiles, but with recent speedups these spans stopped being useful.
    
    commit-id:bd1161e7

---
+++ crates/cairo-lang-language-server/src/lang/diagnostics/file_batches.rs
@@ -0,0 +1,69 @@
+use std::collections::HashSet;
+use std::num::NonZero;
+
+use cairo_lang_defs::db::DefsGroup;
+use cairo_lang_filesystem::db::FilesGroup;
+use cairo_lang_filesystem::ids::FileId;
+use itertools::Itertools;
+use lsp_types::Url;
+use tracing::info_span;
+
+use crate::lang::db::AnalysisDatabase;
+use crate::lang::lsp::LsProtoGroup;
+
+#[cfg(test)]
+#[path = "file_batches_test.rs"]
+mod test;
+
+/// Finds all analyzable files in `db` and constructs `n` batches of them to be processed.
+///
+/// Files specified in `open_files` will be prioritised and distributed evenly among the batches.
+#[tracing::instrument(skip(db, open_files))]
+pub fn make_file_batches(
+    db: &AnalysisDatabase,
+    open_files: &HashSet<Url>,
+    n: NonZero<usize>,
+) -> Vec<Vec<FileId>> {
+    let open: Vec<FileId> = info_span!("find_open_files")
+        .in_scope(|| open_files.iter().filter_map(|uri| db.file_for_url(uri)).dedup().collect());
+
+    // NOTE: It would be tempting (and actually was done like so in the past) to immediately
+    //   schedule refreshing diagnostics for the open files here to deliver results faster.
+    //   However, at the time of writing this code, our profiling of the biggest Cairo codebases
+    //   found out that finding other files is a very negligible (few milliseconds) process.
+    //   Getting them here makes the rest of diagnostics code much simpler.
+
+    let rest: Vec<FileId> = info_span!("find_other_files").in_scope(|| {
+        let mut rest_of_files: HashSet<FileId> = HashSet::default();
+        for crate_id in db.crates() {
+            for module_id in db.crate_modules(crate_id).iter() {
+                // Schedule only module main files for refreshing.
+                // All other related files will be refreshed along with it in a single job.
+                if let Ok(file) = db.module_main_file(*module_id) {
+                    if !open.contains(&file) {
+                        rest_of_files.insert(file);
+                    }
+                }
+            }
+        }
+        Vec::from_iter(rest_of_files)
+    });
+
+    make_batches(open, rest, n)
+}
+
+/// Returns `n` optimally distributed batches of the `primary` and `secondary` vectors, merged.
+///
+/// All items from the `primary` vector will always be before any `secondary` item.
+/// The best care will be taken for all batches to have the same length.
+///
+/// ## Panics
+/// This function will panic if `n == 0`.
+fn make_batches<T: Copy>(primary: Vec<T>, secondary: Vec<T>, n: NonZero<usize>) -> Vec<Vec<T>> {
+    let n = n.get();
+    let chain = primary.into_iter().chain(secondary);
+    let batches =
+        (1..=n).map(|offset| chain.clone().skip(offset - 1).step_by(n).collect()).collect();
+    debug_assert_eq!(batches.len(), n);
+    batches
+}
--- crates/cairo-lang-language-server/src/lang/diagnostics/mod.rs
+++ crates/cairo-lang-language-server/src/lang/diagnostics/mod.rs
@@ -1,17 +1,20 @@
 use std::collections::HashMap;
+use std::num::NonZero;
 use std::panic::{AssertUnwindSafe, catch_unwind};
 
 use lsp_types::Url;
 use tracing::{error, trace};
 
 use self::file_diagnostics::FileDiagnostics;
+use self::refresh::refresh_diagnostics;
 use self::trigger::trigger;
-use crate::lang::diagnostics::refresh::refresh_diagnostics;
+use crate::lang::diagnostics::file_batches::make_file_batches;
 use crate::server::client::Notifier;
 use crate::server::panic::cancelled_anyhow;
 use crate::server::schedule::thread::{self, JoinHandle, ThreadPriority};
 use crate::state::StateSnapshot;
 
+mod file_batches;
 mod file_diagnostics;
 mod lsp;
 mod refresh;
@@ -59,13 +62,7 @@ impl DiagnosticsController {
 
         while let Some(WorkerArgs { state, notifier }) = receiver.wait() {
             if let Err(err) = catch_unwind(AssertUnwindSafe(|| {
-                refresh_diagnostics(
-                    &state.db,
-                    &state.open_files,
-                    state.config.trace_macro_diagnostics,
-                    &mut file_diagnostics,
-                    notifier,
-                );
+                Self::control_tick(state, &mut file_diagnostics, notifier);
             })) {
                 if let Ok(err) = cancelled_anyhow(err, "diagnostics refreshing has been cancelled")
                 {
@@ -74,6 +71,25 @@ impl DiagnosticsController {
                     error!("caught panic while refreshing diagnostics");
                 }
             }
+        }
+    }
+
+    /// Runs a single tick of the diagnostics controller's event loop.
+    #[tracing::instrument(skip_all)]
+    fn control_tick(
+        state: StateSnapshot,
+        file_diagnostics: &mut HashMap<Url, FileDiagnostics>,
+        notifier: Notifier,
+    ) {
+        // TODO(mkaput): Make multiple batches and run them in parallel.
+        for batch in make_file_batches(&state.db, &state.open_files, NonZero::new(1).unwrap()) {
+            refresh_diagnostics(
+                &state.db,
+                batch,
+                state.config.trace_macro_diagnostics,
+                file_diagnostics,
+                notifier.clone(),
+            );
         }
     }
 }
