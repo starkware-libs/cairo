d62ea5640004511bfa3017dccc76626584c9348c
Marek Kaput <marek.kaput@swmansion.com>
2024-11-14 07:37:03 +0100

    LS: Implement multithreaded diagnostics refreshing
    commit-id:2a95e833

--- crates/cairo-lang-language-server/src/lang/diagnostics/refresh.rs
+++ crates/cairo-lang-language-server/src/lang/diagnostics/refresh.rs
@@ -1,14 +1,15 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 use cairo_lang_defs::ids::ModuleId;
 use cairo_lang_filesystem::ids::FileId;
 use cairo_lang_utils::LookupIntern;
+use lsp_types::Url;
 use lsp_types::notification::PublishDiagnostics;
-use lsp_types::{PublishDiagnosticsParams, Url};
-use tracing::{info_span, trace};
+use tracing::trace;
 
 use crate::lang::db::AnalysisDatabase;
 use crate::lang::diagnostics::file_diagnostics::FileDiagnostics;
+use crate::lang::diagnostics::project_diagnostics::ProjectDiagnostics;
 use crate::lang::lsp::LsProtoGroup;
 use crate::server::client::Notifier;
 
@@ -18,10 +19,9 @@ pub fn refresh_diagnostics(
     db: &AnalysisDatabase,
     batch: Vec<FileId>,
     trace_macro_diagnostics: bool,
-    file_diagnostics: &mut HashMap<Url, FileDiagnostics>,
+    project_diagnostics: ProjectDiagnostics,
     notifier: Notifier,
 ) {
-    let mut files_with_set_diagnostics: HashSet<Url> = HashSet::default();
     let mut processed_modules: HashSet<ModuleId> = HashSet::default();
 
     for file in batch {
@@ -30,31 +30,10 @@ pub fn refresh_diagnostics(
             file,
             trace_macro_diagnostics,
             &mut processed_modules,
-            &mut files_with_set_diagnostics,
-            file_diagnostics,
+            &project_diagnostics,
             &notifier,
         );
     }
-
-    info_span!("clear_old_diagnostics").in_scope(|| {
-        let mut removed_files = Vec::new();
-
-        file_diagnostics.retain(|uri, _| {
-            let retain = files_with_set_diagnostics.contains(uri);
-            if !retain {
-                removed_files.push(uri.clone());
-            }
-            retain
-        });
-
-        for file in removed_files {
-            notifier.notify::<PublishDiagnostics>(PublishDiagnosticsParams {
-                uri: file,
-                diagnostics: vec![],
-                version: None,
-            });
-        }
-    });
 }
 
 /// Refresh diagnostics for a single file.
@@ -63,8 +42,7 @@ fn refresh_file_diagnostics(
     file: FileId,
     trace_macro_diagnostics: bool,
     processed_modules: &mut HashSet<ModuleId>,
-    files_with_set_diagnostics: &mut HashSet<Url>,
-    file_diagnostics: &mut HashMap<Url, FileDiagnostics>,
+    project_diagnostics: &ProjectDiagnostics,
     notifier: &Notifier,
 ) {
     let Some(file_uri) = db.url_for_file(file) else {
@@ -76,20 +54,33 @@ fn refresh_file_diagnostics(
         return;
     };
 
-    if !new_file_diagnostics.is_empty() {
-        files_with_set_diagnostics.insert(file_uri.clone());
-    }
-
-    // Since we are using Arcs, this comparison should be efficient.
-    if let Some(old_file_diagnostics) = file_diagnostics.get(&file_uri) {
-        if old_file_diagnostics == &new_file_diagnostics {
-            return;
+    if project_diagnostics.insert(&file_uri, new_file_diagnostics.clone()) {
+        if let Some(params) = new_file_diagnostics.to_lsp(db, trace_macro_diagnostics) {
+            notifier.notify::<PublishDiagnostics>(params);
         }
+    }
+}
 
-        file_diagnostics.insert(file_uri.clone(), new_file_diagnostics.clone());
-    };
+/// Wipes diagnostics for any files not present in the preserve set.
+#[tracing::instrument(skip_all)]
+pub fn clear_old_diagnostics(
+    db: &AnalysisDatabase,
+    files_to_preserve: HashSet<Url>,
+    project_diagnostics: ProjectDiagnostics,
+    notifier: Notifier,
+) {
+    let removed = project_diagnostics.clear_old(&files_to_preserve);
+    for mut file_diagnostics in removed {
+        // It might be that we are removing a file that actually had some diagnostics.
+        // For example, this might happen if a file with a syntax error is deleted.
+        // We are reusing just removed `FileDiagnostics` instead of constructing a fresh one
+        // to preserve any extra state it might contain.
+        file_diagnostics.clear();
 
-    if let Some(params) = new_file_diagnostics.to_lsp(db, trace_macro_diagnostics) {
-        notifier.notify::<PublishDiagnostics>(params);
+        // We can safely assume `trace_macro_diagnostics` = false here, as we are explicitly
+        // sending a "no diagnostics" message.
+        if let Some(params) = file_diagnostics.to_lsp(db, false) {
+            notifier.notify::<PublishDiagnostics>(params);
+        }
     }
 }
