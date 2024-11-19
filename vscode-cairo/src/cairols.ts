3573420f49b749e7d31fe2a122926a5fa6ec8c1a
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2024-11-19 18:01:29 +0100

    LS: Proc macro server failed notification
    commit-id:d35d8322

--- vscode-cairo/src/cairols.ts
+++ vscode-cairo/src/cairols.ts
@@ -148,6 +148,42 @@ export async function setupLanguageServer(ctx: Context): Promise<lc.LanguageClie
   });
 
   client.onNotification(
+    new lc.NotificationType<
+      { reason: "noMoreRetries"; retries: number; inMinutes: number } | { reason: "spawnFail" }
+    >("cairo/procMacroServerInitializationFailed"),
+    async (errorMessage) => {
+      const goToLogs = "Go to logs";
+
+      switch (errorMessage.reason) {
+        case "noMoreRetries": {
+          const { inMinutes, retries } = errorMessage;
+
+          const selectedValue = await vscode.window.showErrorMessage(
+            `Starting proc-macro-server failed ${retries} times in ${inMinutes} minutes, the proc-macro-server will not be restarted. Procedural macros will not be analyzed. See the output for more information`,
+            goToLogs,
+          );
+
+          if (selectedValue === goToLogs) {
+            client.outputChannel.show(true);
+          }
+          break;
+        }
+        case "spawnFail": {
+          const selectedValue = await vscode.window.showErrorMessage(
+            "Starting proc-macro-server failed, the proc-macro-server will not be restarted. Procedural macros will not be analyzed. See the output for more information",
+            goToLogs,
+          );
+
+          if (selectedValue === goToLogs) {
+            client.outputChannel.show(true);
+          }
+          break;
+        }
+      }
+    },
+  );
+
+  client.onNotification(
     new lc.NotificationType<string>("cairo/corelib-version-mismatch"),
     async (errorMessage) => {
       const restart = "Restart CairoLS";
