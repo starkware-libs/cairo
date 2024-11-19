f294bd8044c6b3efd3fd0a49cf952ca2b07dcfc1
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2024-11-19 18:41:22 +0100

    LS: Proc macro plugins
    commit-id:655a951f

--- crates/cairo-lang-language-server/src/lang/proc_macros/db.rs
+++ crates/cairo-lang-language-server/src/lang/proc_macros/db.rs
@@ -1,3 +1,9 @@
+use rustc_hash::FxHashMap;
+use scarb_proc_macro_server_types::methods::ProcMacroResult;
+use scarb_proc_macro_server_types::methods::expand::{
+    ExpandAttributeParams, ExpandDeriveParams, ExpandInlineMacroParams,
+};
+
 use super::client::ClientStatus;
 
 /// `salsa` interface for proc macros support in LS.
@@ -5,5 +11,19 @@ use super::client::ClientStatus;
 pub trait ProcMacroGroup {
     /// Allows access to client from compiler plugins `.generate_code()` methods.
     #[salsa::input]
+    fn attribute_macro_resolution(&self) -> FxHashMap<ExpandAttributeParams, ProcMacroResult>;
+    #[salsa::input]
+    fn derive_macro_resolution(&self) -> FxHashMap<ExpandDeriveParams, ProcMacroResult>;
+    #[salsa::input]
+    fn inline_macro_resolution(&self) -> FxHashMap<ExpandInlineMacroParams, ProcMacroResult>;
+
+    #[salsa::input]
     fn proc_macro_client_status(&self) -> ClientStatus;
+}
+
+pub fn init_proc_macro_group(db: &mut dyn ProcMacroGroup) {
+    db.set_attribute_macro_resolution(Default::default());
+    db.set_derive_macro_resolution(Default::default());
+    db.set_inline_macro_resolution(Default::default());
+    db.set_proc_macro_client_status(Default::default());
 }
