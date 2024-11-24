9de38e5c497899a78b93219d0dc866826b038a5e
orizi <104711814+orizi@users.noreply.github.com>
2024-11-24 14:19:45 +0200

    Updated nightly version. (#6728)

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export RUSTUP_TOOLCHAIN="${RUSTUP_TOOLCHAIN:-nightly-2024-11-17}"
+export RUSTUP_TOOLCHAIN="${RUSTUP_TOOLCHAIN:-nightly-2024-11-23}"
 
 cargo clippy "$@" --all-targets --all-features -- -D warnings -D future-incompatible \
     -D nonstandard-style -D rust-2018-idioms -D unused
