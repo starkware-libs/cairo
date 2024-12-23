4a44fcf77fba1e4dd863a40d5f45ea92c485de89
wizard <112275929+famouswizard@users.noreply.github.com>
2024-12-23 23:26:34 +0300

    fix: Fix incorrect usage of line breaks within string literals (#6914)

--- scripts/release_crates.sh
+++ scripts/release_crates.sh
@@ -54,8 +54,8 @@ CRATES_TO_PUBLISH=(
 # - 5 (the number of crates that are for internal use only).
 NUM_CRATES_IN_WORKSPACE=$(find crates/ -name Cargo.toml | wc -l)
 if [ "${#CRATES_TO_PUBLISH[@]}" -ne "$((NUM_CRATES_IN_WORKSPACE - 5))" ]; then
-    echo "The number of crates to publish is not equal to the number of crates in the workspace,
-    new crates were probably added, please update the list of crates to publish."
+    echo "The number of crates to publish is not equal to the number of crates in the workspace,"
+    echo "new crates were probably added, please update the list of crates to publish."
     exit 1
 fi
 
