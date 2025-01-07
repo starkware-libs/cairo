6d4dcc8f9ba47e51cf4a3d41ea14d7b85d9e5011
Dean Carmel <dean@starkware.co>
2025-01-07 16:52:32 +0200

    added closures to protected zone

--- crates/cairo-lang-formatter/src/node_properties.rs
+++ crates/cairo-lang-formatter/src/node_properties.rs
@@ -276,6 +276,14 @@ impl SyntaxNodeFormat for SyntaxNode {
                 | SyntaxKind::ExprUnary => Some(3),
                 _ => None,
             },
+
+            Some(SyntaxKind::ExprClosure) => match self.kind(db) {
+                SyntaxKind::ClosureParamWrapperNAry => Some(3),
+                SyntaxKind::ReturnTypeClause => Some(2),
+                SyntaxKind::ExprBlock => Some(1),
+                _ => None,
+            },
+
             Some(SyntaxKind::ExprIf) => match self.kind(db) {
                 SyntaxKind::ExprBlock => Some(1),
                 SyntaxKind::ConditionExpr | SyntaxKind::ConditionLet => Some(2),
@@ -527,12 +535,7 @@ impl SyntaxNodeFormat for SyntaxNode {
                         trailing: trailing_break_point,
                     }
                 }
-                SyntaxKind::ParamList
-                    if !matches!(
-                        parent_kind(db, self),
-                        Some(SyntaxKind::ClosureParamWrapperNAry)
-                    ) =>
-                {
+                SyntaxKind::ParamList => {
                     let leading_break_point = BreakLinePointProperties::new(
                         2,
                         BreakLinePointIndentation::IndentedWithTail,
--- crates/cairo-lang-formatter/test_data/cairo_files/linebreaking.cairo
+++ crates/cairo-lang-formatter/test_data/cairo_files/linebreaking.cairo
@@ -82,3 +82,8 @@ fn comment_remains_after_last_comma() {
 fn struct_line_breaking() {
     let MyStruct {long_long_long_long_key_a, long_long_long_long_key_b, long_long_long_long_key_c, long_long_long_long_key_d, long_long_long_long_key_e, } = my_val;
 }
+fn closure_line_breaking() {
+    let closure_with_a_very_very_very_very_very_very_very_very_large_name = |left: usize, right: usize| -> usize {
+            left + right
+        };
+}
