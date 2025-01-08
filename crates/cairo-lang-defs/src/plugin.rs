845948a2a8c4eaae122e2f330e2dd68bcb48990a
JohnBonny <158583902+JohnBonny@users.noreply.github.com>
2025-01-09 00:30:18 +0100

    Fixing Typographical Errors in Documentation (#7032)

--- crates/cairo-lang-defs/src/plugin.rs
+++ crates/cairo-lang-defs/src/plugin.rs
@@ -95,7 +95,7 @@ pub struct MacroPluginMetadata<'a> {
     pub edition: Edition,
 }
 
-// TOD(spapini): Move to another place.
+// TODO(spapini): Move to another place.
 /// A trait for a macro plugin: external plugin that generates additional code for items.
 pub trait MacroPlugin: std::fmt::Debug + Sync + Send {
     /// Generates code for an item. If no code should be generated returns None.
