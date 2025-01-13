447ead8ad06a8cc970c118816366aac3bc222931
cairoIover <193099744+cairoIover@users.noreply.github.com>
2025-01-13 11:34:46 +0100

    re-gen syntax, typos fix, format

--- crates/cairo-lang-syntax/src/node/ast.rs
+++ crates/cairo-lang-syntax/src/node/ast.rs
@@ -12357,6 +12357,7 @@ impl From<&OptionTerminalConst> for SyntaxStablePtrId {
 }
 impl OptionTerminalConst {
     /// Checks if a kind of a variant of [OptionTerminalConst].
+
     pub fn is_variant(kind: SyntaxKind) -> bool {
         matches!(kind, SyntaxKind::OptionTerminalConstEmpty | SyntaxKind::TerminalConst)
     }
