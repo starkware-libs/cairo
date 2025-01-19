d5009ac08913e7b7865ffcb11a5450e0b9c14600
Dean Carmel <dean@starkware.co>
2025-01-19 11:03:57 +0200

    fixed diagnostic path bug

--- crates/cairo-lang-semantic/src/lsp_helpers.rs
+++ crates/cairo-lang-semantic/src/lsp_helpers.rs
@@ -154,14 +154,12 @@ fn visible_traits_in_module_ex(
             _ => continue,
         }
     }
-    // Traverse the submodules of the current module.
     for submodule_id in db.module_submodules_ids(module_id).ok()?.iter().copied() {
         if !is_visible(submodule_id.name(db.upcast()))? {
             continue;
         }
         modules_to_visit.push(ModuleId::Submodule(submodule_id));
     }
-    // Add the traits of the current module.
     for trait_id in db.module_traits_ids(module_id).ok()?.iter().copied() {
         if !is_visible(trait_id.name(db.upcast()))? {
             continue;
@@ -170,14 +168,9 @@ fn visible_traits_in_module_ex(
     }
 
     for submodule in modules_to_visit {
-        for (trait_id, path) in visible_traits_in_module_ex(
-            db,
-            submodule,
-            user_module_file_id,
-            include_parent,
-            visited_modules,
-        )?
-        .iter()
+        for (trait_id, path) in
+            visible_traits_in_module_ex(db, submodule, user_module_file_id, false, visited_modules)?
+                .iter()
         {
             result.push((*trait_id, format!("{}::{}", submodule.name(db.upcast()), path)));
         }
@@ -210,8 +203,9 @@ pub fn visible_traits_in_crate(
     db: &dyn SemanticGroup,
     crate_id: CrateId,
     user_module_file_id: ModuleFileId,
+    is_current_crate: bool,
 ) -> Arc<[(TraitId, String)]> {
-    let crate_name = crate_id.name(db.upcast());
+    let crate_name = if is_current_crate { "crate" } else { &crate_id.name(db.upcast()) };
     let crate_as_module = ModuleId::CrateRoot(crate_id);
     db.visible_traits_in_module(crate_as_module, user_module_file_id, false)
         .iter()
@@ -258,11 +252,9 @@ pub fn visible_traits_from_module(
             .intern(db)
         })
     ) {
-        if crate_id == current_crate_id {
-            continue;
-        }
-        module_visible_traits
-            .extend_from_slice(&db.visible_traits_in_crate(crate_id, module_file_id)[..]);
+        module_visible_traits.extend_from_slice(
+            &db.visible_traits_in_crate(crate_id, module_file_id, crate_id == current_crate_id)[..],
+        );
     }
     let mut result: OrderedHashMap<TraitId, String> = OrderedHashMap::default();
     for (trait_id, path) in module_visible_traits {
