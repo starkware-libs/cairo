b90e1e6151ef12a16efe973abcb7d867a89bd475
TomerStarkware <144585788+TomerStarkware@users.noreply.github.com>
2025-01-30 14:20:00 +0200

    changed loop generated funcion key from exprid to stable_ptr for enab… (#7183)

--- crates/bin/get-lowering/src/main.rs
+++ crates/bin/get-lowering/src/main.rs
@@ -7,7 +7,7 @@ use anyhow::Context;
 use cairo_lang_compiler::db::RootDatabase;
 use cairo_lang_compiler::project::{check_compiler_path, setup_project};
 use cairo_lang_debug::debug::DebugWithDb;
-use cairo_lang_defs::ids::TopLevelLanguageElementId;
+use cairo_lang_defs::ids::{NamedLanguageElementId, TopLevelLanguageElementId};
 use cairo_lang_filesystem::ids::CrateId;
 use cairo_lang_lowering::FlatLowered;
 use cairo_lang_lowering::add_withdraw_gas::add_withdraw_gas;
@@ -27,7 +27,7 @@ use cairo_lang_semantic::items::functions::{
 };
 use cairo_lang_starknet::starknet_plugin_suite;
 use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
-use cairo_lang_utils::{Intern, LookupIntern};
+use cairo_lang_utils::{Intern, LookupIntern, Upcast};
 use clap::Parser;
 use convert_case::Casing;
 use itertools::Itertools;
@@ -73,9 +73,9 @@ struct Args {
     #[arg(short, long)]
     all: bool,
 
-    /// The id the expr id of the generated function to output.
+    /// The index of the generated function to output.
     #[arg(long)]
-    expr_id: Option<usize>,
+    generated_function_index: Option<usize>,
 
     /// The output file name (default: stdout).
     output: Option<String>,
@@ -233,33 +233,35 @@ fn main() -> anyhow::Result<()> {
 
     let res = if let Some(function_path) = args.function_path {
         let mut function_id = get_func_id_by_name(db, &main_crate_ids, function_path)?;
-        if let Some(expr_id) = args.expr_id {
+        if let Some(generated_function_index) = args.generated_function_index {
             let multi = db
                 .priv_function_with_body_multi_lowering(
                     function_id.function_with_body_id(db).base_semantic_function(db),
                 )
                 .unwrap();
-            let key = *multi
+            let keys = multi
                 .generated_lowerings
                 .keys()
-                .find(|key| match key {
-                    GeneratedFunctionKey::Loop(id) => id.index() == expr_id,
-                    // TODO(ilya): Support other types of generated functions.
-                    _ => false,
+                .sorted_by_key(|key| match key {
+                    GeneratedFunctionKey::Loop(id) => {
+                        (id.0.lookup(db).span_without_trivia(db.upcast()), "".into())
+                    }
+                    GeneratedFunctionKey::TraitFunc(trat_function, id) => (
+                        id.syntax_node(db).span_without_trivia(db.upcast()),
+                        trat_function.name(db),
+                    ),
                 })
-                .with_context(|| {
-                    format!(
-                        "expr_id not found - available expr_ids: {:?}",
-                        multi
-                            .generated_lowerings
-                            .keys()
-                            .filter_map(|key| match key {
-                                GeneratedFunctionKey::Loop(id) => Some(id.index()),
-                                _ => None,
-                            })
-                            .collect_vec()
-                    )
-                })?;
+                .take(generated_function_index + 1)
+                .collect_vec();
+
+            let key = **keys.get(generated_function_index).with_context(|| {
+                format!(
+                    "Invalid generated function index. There are {} generated functions in the \
+                     function",
+                    keys.len()
+                )
+            })?;
+
             function_id = db.intern_lowering_concrete_function_with_body(
                 ConcreteFunctionWithBodyLongId::Generated(GeneratedFunction {
                     parent: function_id.base_semantic_function(db),
