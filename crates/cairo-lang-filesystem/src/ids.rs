150965a6c78532c65bf4452867f40b07759e753c
TomerStarkware <tomer@starkware.co>
2025-02-02 15:11:54 +0200

    added precompilation for lowering

--- crates/cairo-lang-filesystem/src/ids.rs
+++ crates/cairo-lang-filesystem/src/ids.rs
@@ -4,6 +4,7 @@ use std::sync::Arc;
 
 use cairo_lang_utils::{Intern, LookupIntern, define_short_id};
 use path_clean::PathClean;
+use serde::{Deserialize, Serialize};
 use smol_str::SmolStr;
 
 use crate::db::{CORELIB_CRATE_NAME, FilesGroup};
@@ -17,7 +18,7 @@ pub enum CrateLongId {
     /// A crate that appears in crate_roots(), and on the filesystem.
     Real { name: SmolStr, discriminator: Option<SmolStr> },
     /// A virtual crate, not a part of the crate_roots(). Used mainly for tests.
-    Virtual { name: SmolStr, file_id: FileId, settings: String },
+    Virtual { name: SmolStr, file_id: FileId, settings: String, cache_file: Option<BlobId> },
 }
 impl CrateLongId {
     pub fn name(&self) -> SmolStr {
@@ -78,14 +79,14 @@ pub enum FileLongId {
     External(salsa::InternId),
 }
 /// Whether the file holds syntax for a module or for an expression.
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+#[derive(Clone, Debug, Hash, PartialEq, Eq, Serialize, Deserialize)]
 pub enum FileKind {
     Module,
     Expr,
 }
 
 /// A mapping for a code rewrite.
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+#[derive(Clone, Debug, Hash, PartialEq, Eq, Serialize, Deserialize)]
 pub struct CodeMapping {
     pub span: TextSpan,
     pub origin: CodeOrigin,
@@ -108,7 +109,7 @@ impl CodeMapping {
 }
 
 /// The origin of a code mapping.
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+#[derive(Clone, Debug, Hash, PartialEq, Eq, Serialize, Deserialize)]
 pub enum CodeOrigin {
     /// The origin is a copied node staring at the given offset.
     Start(TextOffset),
@@ -212,5 +213,20 @@ impl Directory {
                 }
             }
         }
+    }
+}
+
+/// A FileId for data that is not necessarily a valid UTF-8 string.
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub enum BlobLongId {
+    OnDisk(PathBuf),
+    Virtual(Arc<[u8]>),
+}
+
+define_short_id!(BlobId, BlobLongId, FilesGroup, lookup_intern_blob, intern_blob);
+
+impl BlobId {
+    pub fn new(db: &dyn FilesGroup, path: PathBuf) -> BlobId {
+        BlobLongId::OnDisk(path.clean()).intern(db)
     }
 }
