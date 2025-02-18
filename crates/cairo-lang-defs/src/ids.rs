f7521f6d75f1b264153d6c617fa4b47365acb48b
Piotr Figiela <77412592+Draggu@users.noreply.github.com>
2025-02-18 16:38:16 +0100

    Make obtaining visible items more generic
    Previously it was working with traits only, now it works with other items as well.
    
    commit-id:ca8409a6

--- crates/cairo-lang-defs/src/ids.rs
+++ crates/cairo-lang-defs/src/ids.rs
@@ -457,6 +457,27 @@ define_language_element_id_as_enum! {
         ExternFunction(ExternFunctionId),
     }
 }
+
+define_language_element_id_as_enum! {
+    #[toplevel]
+    /// Id for an item that can be brought into scope with a `use` statement.
+    /// Basically [`ModuleItemId`] without [`UseId`] and with [`VariantId`].
+    pub enum ImportableId {
+        Constant(ConstantId),
+        Submodule(SubmoduleId),
+        FreeFunction(FreeFunctionId),
+        Struct(StructId),
+        Enum(EnumId),
+        Variant(VariantId),
+        TypeAlias(ModuleTypeAliasId),
+        ImplAlias(ImplAliasId),
+        Trait(TraitId),
+        Impl(ImplDefId),
+        ExternType(ExternTypeId),
+        ExternFunction(ExternFunctionId),
+    }
+}
+
 define_top_level_language_element_id!(
     SubmoduleId,
     SubmoduleLongId,
