18a86763db8395a0f540482b31e43482195a988e
Ori Ziv <oriz@starkware.co>
2025-02-23 20:31:08 +0200

    Made `Store` based on derive-info.
    commit-id:944b4973

--- crates/cairo-lang-starknet/src/plugin/derive/store.rs
+++ crates/cairo-lang-starknet/src/plugin/derive/store.rs
@@ -1,8 +1,10 @@
 use cairo_lang_defs::patcher::RewriteNode;
 use cairo_lang_defs::plugin::{MacroPluginMetadata, PluginDiagnostic};
+use cairo_lang_plugins::plugins::utils::{DeriveInfo, TypeVariant};
+use cairo_lang_syntax::node::ast;
 use cairo_lang_syntax::node::db::SyntaxGroup;
 use cairo_lang_syntax::node::helpers::QueryAttrs;
-use cairo_lang_syntax::node::{Terminal, TypedStablePtr, TypedSyntaxNode, ast};
+use cairo_lang_utils::extract_matches;
 use indent::indent_by;
 use indoc::formatdoc;
 
@@ -18,12 +20,14 @@ pub fn handle_store_derive(
     diagnostics: &mut Vec<PluginDiagnostic>,
     metadata: &MacroPluginMetadata<'_>,
 ) -> Option<RewriteNode> {
-    match item_ast {
-        ast::ModuleItem::Struct(struct_ast) => {
+    let info = DeriveInfo::new(db, item_ast)?;
+    match &info.type_variant {
+        TypeVariant::Struct => {
             // In case of a struct, we need to generate the `Store` trait implementation as well as
             // a sub-pointers implementation.
-            let store_trait_code = handle_struct_store(db, struct_ast)?;
-            let sub_pointers_code = if !struct_ast.members(db).elements(db).is_empty() {
+            let store_trait_code = handle_struct_store(&info)?;
+            let sub_pointers_code = if !info.members_info.is_empty() {
+                let struct_ast = extract_matches!(item_ast, ast::ModuleItem::Struct);
                 let configs = struct_members_storage_configs(db, struct_ast, diagnostics);
                 handle_storage_interface_struct(db, struct_ast, &configs, metadata)
                     .into_rewrite_node()
@@ -39,59 +43,60 @@ pub fn handle_store_derive(
                 .into(),
             ))
         }
-        ast::ModuleItem::Enum(enum_ast) => handle_enum(db, enum_ast, diagnostics),
-        _ => None,
+        TypeVariant::Enum => handle_enum(db, &info, diagnostics),
     }
 }
 
 /// Derive the `Store` trait for structs annotated with `derive(starknet::Store)`.
-fn handle_struct_store(db: &dyn SyntaxGroup, struct_ast: &ast::ItemStruct) -> Option<RewriteNode> {
-    let mut fields = Vec::new();
+fn handle_struct_store(info: &DeriveInfo) -> Option<RewriteNode> {
+    let full_name = &info.full_typename();
+    let mut fields_write_start = Vec::new();
+    let mut fields_read_end = Vec::new();
+    let mut fields_write_wrapping = Vec::new();
     let mut reads_values = Vec::new();
     let mut reads_values_at_offset = Vec::new();
     let mut writes = Vec::new();
     let mut writes_at_offset = Vec::new();
     let mut sizes = Vec::new();
-    for (i, field) in struct_ast.members(db).elements(db).iter().enumerate() {
-        let field_name = field.name(db).as_syntax_node().get_text_without_trivia(db);
-        let field_type = field.type_clause(db).ty(db).as_syntax_node().get_text_without_trivia(db);
+    for (i, field) in info.members_info.iter().enumerate() {
+        let field_name = &field.name;
+        let imp = field.impl_name(STORE_TRAIT);
+        let wrapper = field.destruct_with();
 
-        fields.push(format!("{field_name},"));
+        fields_write_start.push(format!("{field_name},"));
+        fields_read_end.push(format!("{field_name}: {field_name}.value,"));
+        fields_write_wrapping
+            .push(format!("let {field_name} = {wrapper} {{ value: {field_name} }};"));
 
         if i == 0 {
             reads_values.push(format!(
-                "let {field_name} = \
-                 {STORE_TRAIT}::<{field_type}>::read(__store_derive_address_domain__, \
-                 __store_derive_base__)?;"
+                "let {field_name} = {wrapper} {{ value: \
+                 {imp}::read(__store_derive_address_domain__, __store_derive_base__)? }};"
             ));
             reads_values_at_offset.push(format!(
-                "let {field_name} = \
-                 {STORE_TRAIT}::<{field_type}>::read_at_offset(__store_derive_address_domain__, \
-                 __store_derive_base__, __store_derive_offset__)?;"
+                "let {field_name} = {wrapper} {{ value: \
+                 {imp}::read_at_offset(__store_derive_address_domain__, __store_derive_base__, \
+                 __store_derive_offset__)? }};"
             ));
         } else {
             let subsequent_read = format!(
-                "let {field_name} = \
-                 {STORE_TRAIT}::<{field_type}>::read_at_offset(__store_derive_address_domain__, \
-                 __store_derive_base__, __store_derive_current_offset__)?;"
+                "let {field_name} = {wrapper} {{ value: \
+                 {imp}::read_at_offset(__store_derive_address_domain__, __store_derive_base__, \
+                 __store_derive_current_offset__)? }};"
             );
             reads_values.push(subsequent_read.clone());
             reads_values_at_offset.push(subsequent_read);
         }
-        if i < struct_ast.members(db).elements(db).len() - 1 {
+        if i < info.members_info.len() - 1 {
             if i == 0 {
-                reads_values.push(format!(
-                    "let mut __store_derive_current_offset__ = \
-                     {STORE_TRAIT}::<{field_type}>::size();"
-                ));
+                reads_values
+                    .push(format!("let mut __store_derive_current_offset__ = {imp}::size();"));
                 reads_values_at_offset.push(format!(
                     "let mut __store_derive_current_offset__ = __store_derive_offset__ + \
-                     {STORE_TRAIT}::<{field_type}>::size();"
+                     {imp}::size();"
                 ));
             } else {
-                let subsequent_read = format!(
-                    "__store_derive_current_offset__ += {STORE_TRAIT}::<{field_type}>::size();"
-                );
+                let subsequent_read = format!("__store_derive_current_offset__ += {imp}::size();");
                 reads_values.push(subsequent_read.clone());
                 reads_values_at_offset.push(subsequent_read);
             }
@@ -99,90 +104,87 @@ fn handle_struct_store(db: &dyn SyntaxGroup, struct_ast: &ast::ItemStruct) -> Op
 
         if i == 0 {
             writes.push(format!(
-                "{STORE_TRAIT}::<{field_type}>::write(__store_derive_address_domain__, \
-                 __store_derive_base__, {field_name})?;"
+                "{imp}::write(__store_derive_address_domain__, __store_derive_base__, \
+                 {field_name}.value)?;"
             ));
             writes_at_offset.push(format!(
-                "{STORE_TRAIT}::<{field_type}>::write_at_offset(__store_derive_address_domain__, \
-                 __store_derive_base__, __store_derive_offset__, {field_name})?;"
+                "{imp}::write_at_offset(__store_derive_address_domain__, __store_derive_base__, \
+                 __store_derive_offset__, {field_name}.value)?;"
             ));
         } else {
             let subsequent_write = format!(
-                "{STORE_TRAIT}::<{field_type}>::write_at_offset(__store_derive_address_domain__, \
-                 __store_derive_base__, __store_derive_current_offset__, {field_name})?;"
+                "{imp}::write_at_offset(__store_derive_address_domain__, __store_derive_base__, \
+                 __store_derive_current_offset__, {field_name}.value)?;"
             );
             writes.push(subsequent_write.clone());
             writes_at_offset.push(subsequent_write);
         }
 
-        if i < struct_ast.members(db).elements(db).len() - 1 {
+        if i < info.members_info.len() - 1 {
             if i == 0 {
-                writes.push(format!(
-                    "let mut __store_derive_current_offset__ = \
-                     {STORE_TRAIT}::<{field_type}>::size();"
-                ));
+                writes.push(format!("let mut __store_derive_current_offset__ = {imp}::size();"));
                 writes_at_offset.push(format!(
                     "let mut __store_derive_current_offset__ = __store_derive_offset__ + \
-                     {STORE_TRAIT}::<{field_type}>::size();"
+                     {imp}::size();"
                 ));
             } else {
-                let subsequent_write = format!(
-                    "__store_derive_current_offset__ += {STORE_TRAIT}::<{field_type}>::size();"
-                );
+                let subsequent_write = format!("__store_derive_current_offset__ += {imp}::size();");
                 writes.push(subsequent_write.clone());
                 writes_at_offset.push(subsequent_write);
             }
         }
-        sizes.push(format!("{STORE_TRAIT}::<{field_type}>::size()"));
+        sizes.push(format!("{imp}::size()"));
     }
 
     let store_impl = formatdoc!(
         "
-        impl Store{struct_name} of {STORE_TRAIT}::<{struct_name}> {{
+        {header} {{
             fn read(address_domain: u32, base: starknet::storage_access::StorageBaseAddress) -> \
-         starknet::SyscallResult<{struct_name}> {{
+         starknet::SyscallResult<{full_name}> {{
                 let __store_derive_address_domain__ = address_domain;
                 let __store_derive_base__ = base;
                 {reads_values}
                 starknet::SyscallResult::Ok(
-                    {struct_name} {{
-                        {fields}
+                    {full_name} {{
+                        {fields_read_end}
                     }}
                 )
             }}
             fn write(address_domain: u32, base: starknet::storage_access::StorageBaseAddress, \
-         value: {struct_name}) -> starknet::SyscallResult<()> {{
+         value: {full_name}) -> starknet::SyscallResult<()> {{
                 let __store_derive_address_domain__ = address_domain;
                 let __store_derive_base__ = base;
-                let {struct_name} {{
-                    {fields}
+                let {full_name} {{
+                    {fields_write_start}
                 }} = value;
+                {fields_write_wrapping}
                 {writes}
                 starknet::SyscallResult::Ok(())
             }}
             fn read_at_offset(address_domain: u32, base: \
          starknet::storage_access::StorageBaseAddress, offset: u8) -> \
-         starknet::SyscallResult<{struct_name}> {{
+         starknet::SyscallResult<{full_name}> {{
                 let __store_derive_address_domain__ = address_domain;
                 let __store_derive_base__ = base;
                 let __store_derive_offset__ = offset;
                 {reads_values_at_offset}
                 starknet::SyscallResult::Ok(
-                    {struct_name} {{
-                        {fields}
+                    {full_name} {{
+                        {fields_read_end}
                     }}
                 )
             }}
             #[inline(always)]
             fn write_at_offset(address_domain: u32, base: \
-         starknet::storage_access::StorageBaseAddress, offset: u8, value: {struct_name}) -> \
+         starknet::storage_access::StorageBaseAddress, offset: u8, value: {full_name}) -> \
          starknet::SyscallResult<()> {{
                 let __store_derive_address_domain__ = address_domain;
                 let __store_derive_base__ = base;
                 let __store_derive_offset__ = offset;
-                let {struct_name} {{
-                    {fields}
+                let {full_name} {{
+                    {fields_write_start}
                 }} = value;
+                {fields_write_wrapping}
                 {writes_at_offset}
                 starknet::SyscallResult::Ok(())
             }}
@@ -192,8 +194,10 @@ fn handle_struct_store(db: &dyn SyntaxGroup, struct_ast: &ast::ItemStruct) -> Op
             }}
         }}
         ",
-        struct_name = struct_ast.name(db).as_syntax_node().get_text_without_trivia(db),
-        fields = fields.join("\n                "),
+        header = info.impl_header(STORE_TRAIT, &[STORE_TRAIT, "core::traits::Destruct"]),
+        fields_write_start = fields_write_start.join("\n                "),
+        fields_write_wrapping = fields_write_wrapping.join("\n                "),
+        fields_read_end = fields_read_end.join("\n                "),
         reads_values_at_offset = reads_values_at_offset.join("\n        "),
         reads_values = reads_values.join("\n        "),
         writes = writes.join("\n        "),
@@ -207,10 +211,11 @@ fn handle_struct_store(db: &dyn SyntaxGroup, struct_ast: &ast::ItemStruct) -> Op
 /// Derive the `starknet::Store` trait for enums annotated with `derive(starknet::Store)`.
 fn handle_enum(
     db: &dyn SyntaxGroup,
-    enum_ast: &ast::ItemEnum,
+    info: &DeriveInfo,
     diagnostics: &mut Vec<PluginDiagnostic>,
 ) -> Option<RewriteNode> {
-    let enum_name = enum_ast.name(db).as_syntax_node().get_text_without_trivia(db);
+    let enum_name = &info.name;
+    let full_name = &info.full_typename();
     let mut match_idx = Vec::new();
     let mut match_idx_at_offset = Vec::new();
 
@@ -220,11 +225,11 @@ fn handle_enum(
     let mut match_size = "".to_string();
 
     let mut default_index = None;
-    for (i, variant) in enum_ast.variants(db).elements(db).iter().enumerate() {
-        let indicator = if variant.attributes(db).has_attr(db, "default") {
+    for (i, variant) in info.members_info.iter().enumerate() {
+        let indicator = if variant.attributes.has_attr(db, "default") {
             if default_index.is_some() {
                 diagnostics.push(PluginDiagnostic::error(
-                    variant.stable_ptr().untyped(),
+                    &variant.attributes,
                     "Multiple variants annotated with `#[default]`".to_string(),
                 ));
                 return None;
@@ -234,19 +239,14 @@ fn handle_enum(
         } else {
             i + usize::from(default_index.is_none())
         };
-        let variant_name = variant.name(db).text(db);
-        let variant_type = match variant.type_clause(db) {
-            ast::OptionTypeClause::Empty(_) => "()".to_string(),
-            ast::OptionTypeClause::TypeClause(tc) => {
-                tc.ty(db).as_syntax_node().get_text_without_trivia(db)
-            }
-        };
+        let variant_name = &variant.name;
+        let imp = variant.impl_name(STORE_TRAIT);
 
         match_idx.push(formatdoc!(
             "{indicator} => {{
                 starknet::SyscallResult::Ok(
                     {enum_name}::{variant_name}(
-                        {STORE_TRAIT}::read_at_offset(address_domain, base, 1_u8)?
+                        {imp}::read_at_offset(address_domain, base, 1_u8)?
                     )
                 )
             }}",
@@ -255,38 +255,37 @@ fn handle_enum(
             "{indicator} => {{
                 starknet::SyscallResult::Ok(
                     {enum_name}::{variant_name}(
-                        {STORE_TRAIT}::read_at_offset(address_domain, base, offset + 1_u8)?
+                        {imp}::read_at_offset(address_domain, base, offset + 1_u8)?
                     )
                 )
             }}",
         ));
         match_value.push(formatdoc!(
             "{enum_name}::{variant_name}(x) => {{
+                {imp}::write_at_offset(address_domain, base, 1_u8, x)?;
                 {STORE_TRAIT}::write(address_domain, base, {indicator})?;
-                {STORE_TRAIT}::write_at_offset(address_domain, base, 1_u8, x)?;
             }}"
         ));
         match_value_at_offset.push(formatdoc!(
             "{enum_name}::{variant_name}(x) => {{
+                {imp}::write_at_offset(address_domain, base, offset + 1_u8, x)?;
                 {STORE_TRAIT}::write_at_offset(address_domain, base, offset, {indicator})?;
-                {STORE_TRAIT}::write_at_offset(address_domain, base, offset + 1_u8, x)?;
             }}"
         ));
 
         if match_size.is_empty() {
-            match_size = format!("{STORE_TRAIT}::<{variant_type}>::size()");
+            match_size = format!("{imp}::size()");
         } else {
-            match_size =
-                format!("core::cmp::max({STORE_TRAIT}::<{variant_type}>::size(), {match_size})");
+            match_size = format!("core::cmp::max({imp}::size(), {match_size})");
         }
     }
 
     let zero_or_none = if default_index.is_some() { "" } else { "0 |" };
     let store_impl = formatdoc!(
         "
-        impl Store{enum_name} of {STORE_TRAIT}::<{enum_name}> {{
+        {header} {{
             fn read(address_domain: u32, base: starknet::storage_access::StorageBaseAddress) -> \
-         starknet::SyscallResult<{enum_name}> {{
+         starknet::SyscallResult<{full_name}> {{
                 let idx = {STORE_TRAIT}::<felt252>::read(address_domain, base)?;
                 match idx {{
                     {match_idx},
@@ -296,7 +295,7 @@ fn handle_enum(
                 }}
             }}
             fn write(address_domain: u32, base: starknet::storage_access::StorageBaseAddress, \
-         value: {enum_name}) -> starknet::SyscallResult<()> {{
+         value: {full_name}) -> starknet::SyscallResult<()> {{
                 match value {{
                     {match_value}
                 }};
@@ -304,7 +303,7 @@ fn handle_enum(
             }}
             fn read_at_offset(address_domain: u32, base: \
          starknet::storage_access::StorageBaseAddress, offset: u8) -> \
-         starknet::SyscallResult<{enum_name}> {{
+         starknet::SyscallResult<{full_name}> {{
                 let idx = {STORE_TRAIT}::<felt252>::read_at_offset(address_domain, base, offset)?;
                 match idx {{
                     {match_idx_at_offset},
@@ -315,7 +314,7 @@ fn handle_enum(
             }}
             #[inline(always)]
             fn write_at_offset(address_domain: u32, base: \
-         starknet::storage_access::StorageBaseAddress, offset: u8, value: {enum_name}) -> \
+         starknet::storage_access::StorageBaseAddress, offset: u8, value: {full_name}) -> \
          starknet::SyscallResult<()> {{
                 match value {{
                     {match_value_at_offset}
@@ -328,6 +327,7 @@ fn handle_enum(
             }}
         }}
         ",
+        header = info.impl_header(STORE_TRAIT, &[STORE_TRAIT]),
         match_idx = indent_by(12, match_idx.join(",\n")),
         match_idx_at_offset = indent_by(12, match_idx_at_offset.join(",\n")),
         match_value = indent_by(12, match_value.join(",\n")),
