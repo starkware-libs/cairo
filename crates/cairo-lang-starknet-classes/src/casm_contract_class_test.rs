9bde380329efa428db5edcbb831dae0640fe1671
Ilya Lesokhin <ilya@starkware.co>
2025-03-04 12:33:49 +0200

    Enable dedup_blocks.

--- crates/cairo-lang-starknet-classes/src/casm_contract_class_test.rs
+++ crates/cairo-lang-starknet-classes/src/casm_contract_class_test.rs
@@ -100,7 +100,7 @@ fn test_contract_libfuncs_coverage(name: &str) {
 
 /// Tests that compiled_class_hash() returns the correct hash, by comparing it to hard-coded
 /// constant that was computed by other implementations.
-#[test_case("account__account", "232bc404b83f288ef906e63777535a944d5e429354d9f16d835cca30823eb24")]
+#[test_case("account__account", "7e00a11fb9f225f0006a18398df9a42efdbe0d477c1e0e0f1f8065382f58446")]
 fn test_compiled_class_hash(name: &str, expected_hash: &str) {
     let compiled_json_path =
         get_example_file_path(format!("{name}.compiled_contract_class.json").as_str());
