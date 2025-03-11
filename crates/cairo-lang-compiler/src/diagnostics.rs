ff8489ebcb9fa05fe437e3c1e3486b867a818b76
Danil <37103154+Danyylka@users.noreply.github.com>
2025-03-11 09:01:14 +0100

    Update diagnostics.rs

--- crates/cairo-lang-compiler/src/diagnostics.rs
+++ crates/cairo-lang-compiler/src/diagnostics.rs
@@ -41,7 +41,7 @@ pub struct DiagnosticsReporter<'a> {
     // Ignore all warnings, the `ignore_warnings_crate_ids` field is irrelevant in this case.
     ignore_all_warnings: bool,
     /// Ignore warnings in specific crates. This should be a subset of `crate_ids`.
-    /// Adding ids that are not in `crate_ids` have no effect.
+    /// Adding ids that are not in `crate_ids` has no effect.
     ignore_warnings_crate_ids: Vec<CrateId>,
     /// Check diagnostics for these crates only.
     /// If empty, check all crates in the db.
