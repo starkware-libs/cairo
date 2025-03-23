a9c35ea07992160d912fce6ecee9031021b5a816
lfgtwo <lfgtwo@proton.me>
2025-03-23 11:33:42 +0800

    Remove All explicit into iteration

--- crates/cairo-lang-semantic/src/items/generics.rs
+++ crates/cairo-lang-semantic/src/items/generics.rs
@@ -341,7 +341,7 @@ pub fn priv_generic_param_data(
 
     let mut opt_generic_param_syntax = None;
     for param_syntax in
-        generic_params_syntax.generic_params(syntax_db).elements(syntax_db).into_iter()
+        generic_params_syntax.generic_params(syntax_db).elements(syntax_db)
     {
         let cur_generic_param_id =
             GenericParamLongId(module_file_id, param_syntax.stable_ptr()).intern(db);
