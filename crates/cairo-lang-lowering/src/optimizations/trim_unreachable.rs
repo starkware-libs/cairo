405055f966edaf059c5d9e54c7eafee22b938d88
Ilya Lesokhin <ilya@starkware.co>
2025-03-31 11:23:24 +0300

    Enable TrimUnreachable and add a testcase.

--- crates/cairo-lang-lowering/src/optimizations/trim_unreachable.rs
+++ crates/cairo-lang-lowering/src/optimizations/trim_unreachable.rs
@@ -5,7 +5,7 @@ mod test;
 use cairo_lang_semantic::{ConcreteTypeId, TypeLongId};
 
 use crate::db::LoweringGroup;
-use crate::{FlatBlockEnd, FlatLowered, MatchEnumInfo, MatchInfo, VarUsage};
+use crate::{BlockId, FlatBlockEnd, FlatLowered, MatchEnumInfo, MatchInfo, VarUsage, VariableId};
 
 /// Trims unreachable code.
 /// The unreachable code is detected by the introduction of an enum with 0 variants.
@@ -16,28 +16,40 @@ pub fn trim_unreachable(db: &dyn LoweringGroup, lowered: &mut FlatLowered) {
 
     let semantic_db = db.upcast();
     let mut fixes = vec![];
+
+    // Helper function to process a variable.
+    // Checks if the variable is an enum with zero variants.
+    // If it is, a fix is added to the list of fixes, and the function returns `true`,
+    // Otherwise, it returns `false.
+    let mut handle_var = |var_id: &VariableId, introduction_block| {
+        let variable = &lowered.variables[*var_id];
+        let TypeLongId::Concrete(ConcreteTypeId::Enum(concrete_emum_id)) =
+            db.lookup_intern_type(variable.ty)
+        else {
+            return false;
+        };
+
+        let Ok(variants) = db.enum_variants(concrete_emum_id.enum_id(semantic_db)) else {
+            return false;
+        };
+
+        if !variants.is_empty() {
+            return false;
+        }
+
+        fixes.push((introduction_block, *var_id, concrete_emum_id, variable.location));
+        true
+    };
+
+    lowered.parameters.iter().find(|param| handle_var(param, BlockId::root()));
+
     for block in lowered.blocks.iter_mut() {
         let FlatBlockEnd::Match { info } = &block.end else {
             continue;
         };
 
         for arm in info.arms() {
-            for output in &arm.var_ids {
-                let variable = &lowered.variables[*output];
-                let TypeLongId::Concrete(ConcreteTypeId::Enum(concrete_emum_id)) =
-                    db.lookup_intern_type(variable.ty)
-                else {
-                    continue;
-                };
-
-                let Ok(variants) = db.enum_variants(concrete_emum_id.enum_id(semantic_db)) else {
-                    continue;
-                };
-
-                if variants.is_empty() {
-                    fixes.push((arm.block_id, *output, concrete_emum_id, variable.location));
-                }
-            }
+            arm.var_ids.iter().find(|var_id| handle_var(var_id, arm.block_id));
         }
     }
 
