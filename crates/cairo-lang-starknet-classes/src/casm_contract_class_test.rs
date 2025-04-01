d7c1c14d15b2cbbf474a0ff30098369a276caa04
Ilya Lesokhin <ilya@starkware.co>
2025-04-01 11:51:19 +0300

    Apply more optimizations in `baseline` and trim the final strategy.

--- crates/cairo-lang-starknet-classes/src/casm_contract_class_test.rs
+++ crates/cairo-lang-starknet-classes/src/casm_contract_class_test.rs
@@ -100,7 +100,7 @@ fn test_contract_libfuncs_coverage(name: &str) {
 
 /// Tests that compiled_class_hash() returns the correct hash, by comparing it to hard-coded
 /// constant that was computed by other implementations.
-#[test_case("account__account", "90e75bbe39a0b07e7e90fa543880ee01bb5deaf88b9b169285409a8f825058")]
+#[test_case("account__account", "43c13f3363522adc83774e60b25e1525fb6280dc148a94ac5f3c5aa3ca74249")]
 fn test_compiled_class_hash(name: &str, expected_hash: &str) {
     let compiled_json_path =
         get_example_file_path(format!("{name}.compiled_contract_class.json").as_str());
