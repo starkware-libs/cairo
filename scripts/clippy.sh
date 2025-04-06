9ba63e947e76af3e52034af591f3fe3841cc2855
witty <131909329+0xwitty@users.noreply.github.com>
2025-04-06 13:53:29 +0300

    fix: argument order in cargo clippy command

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -2,5 +2,5 @@
 
 export RUSTUP_TOOLCHAIN="${RUSTUP_TOOLCHAIN:-nightly-2025-04-04}"
 
-cargo clippy "$@" --all-targets --all-features -- -D warnings -D future-incompatible \
+cargo clippy --all-targets --all-features "$@" -- -D warnings -D future-incompatible \
     -D nonstandard-style -D rust-2018-idioms -D unused
