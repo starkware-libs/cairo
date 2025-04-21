44c5fa1dde3f203bea88159fcd13d6c366c5aa3a
Ilya Lesokhin <ilya@starkware.co>
2025-04-21 10:59:16 +0300

    Add `redeposit_gas` only before convergence points.

--- crates/cairo-lang-lowering/src/optimizations/gas_redeposit.rs
+++ crates/cairo-lang-lowering/src/optimizations/gas_redeposit.rs
@@ -5,19 +5,22 @@ mod test;
 use cairo_lang_filesystem::flag::Flag;
 use cairo_lang_filesystem::ids::FlagId;
 use cairo_lang_semantic::corelib;
-use itertools::Itertools;
+use itertools::{Itertools, zip_eq};
 
+use crate::borrow_check::analysis::{Analyzer, BackAnalysis, StatementLocation};
 use crate::db::LoweringGroup;
-use crate::ids::{ConcreteFunctionWithBodyId, SemanticFunctionIdEx};
+use crate::ids::{ConcreteFunctionWithBodyId, LocationId, SemanticFunctionIdEx};
 use crate::implicits::FunctionImplicitsTrait;
-use crate::{BlockId, FlatBlockEnd, FlatLowered, Statement, StatementCall};
+use crate::{BlockId, FlatLowered, MatchInfo, Statement, StatementCall, VarRemapping, VarUsage};
 
 /// Adds redeposit gas actions.
 ///
 /// The algorithm is as follows:
 /// Check if the function will have the `GasBuiltin` implicit after the lower_implicits stage.
-/// If so, add a `redeposit_gas` call at the beginning of every branch in the code.
-/// Otherwise, do nothing.
+/// If so, after every block that ends with match, add a call to `redeposit_gas` in every arm
+/// that is followed by a convergence point or a return.
+/// Note that assuming `reorganize_blocks` stage is applied before this stage, every `goto`
+/// statement is a convergence point.
 ///
 /// Note that for implementation simplicity this stage must be applied before `LowerImplicits`
 /// stage.
@@ -46,6 +49,10 @@ pub fn gas_redeposit(
         "`GasRedeposit` stage must be called before `LowerImplicits` stage"
     );
 
+    let ctx = GasRedepositContext { fixes: vec![] };
+    let mut analysis = BackAnalysis::new(lowered, ctx);
+    analysis.get_root_info();
+
     let redeposit_gas = corelib::get_function_id(
         db,
         corelib::core_submodule(db, "gas"),
@@ -53,32 +60,11 @@ pub fn gas_redeposit(
         vec![],
     )
     .lowered(db);
-    let mut stack = vec![BlockId::root()];
-    let mut visited = vec![false; lowered.blocks.len()];
-    let mut redeposit_commands = vec![];
-    while let Some(block_id) = stack.pop() {
-        if visited[block_id.0] {
-            continue;
-        }
-        visited[block_id.0] = true;
-        let block = &lowered.blocks[block_id];
-        match &block.end {
-            FlatBlockEnd::Goto(block_id, _) => {
-                stack.push(*block_id);
-            }
-            FlatBlockEnd::Match { info } => {
-                let location = info.location().with_auto_generation_note(db, "withdraw_gas");
-                for arm in info.arms() {
-                    stack.push(arm.block_id);
-                    redeposit_commands.push((arm.block_id, location));
-                }
-            }
-            &FlatBlockEnd::Return(..) | FlatBlockEnd::Panic(_) => {}
-            FlatBlockEnd::NotSet => unreachable!("Block end not set"),
-        }
-    }
-    for (block_id, location) in redeposit_commands {
+    for (block_id, location) in analysis.analyzer.fixes {
         let block = &mut lowered.blocks[block_id];
+
+        // The `redeposit_gas` function is added at the beginning of the block as it result in
+        // smaller code when the GasBuiltin is revoked during the block.
         block.statements.insert(
             0,
             Statement::Call(StatementCall {
@@ -89,5 +75,56 @@ pub fn gas_redeposit(
                 location,
             }),
         );
+    }
+}
+
+pub struct GasRedepositContext {
+    /// The list of blocks where we need to insert redeposit_gas.
+    fixes: Vec<(BlockId, LocationId)>,
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub enum RedepositState {
+    /// Gas might be burned if we don't redeposit.
+    Required,
+    /// Rediposit was already added, no need to add another one.
+    Redeposited,
+}
+
+impl Analyzer<'_> for GasRedepositContext {
+    type Info = RedepositState;
+
+    fn visit_goto(
+        &mut self,
+        info: &mut Self::Info,
+        _statement_location: StatementLocation,
+        _target_block_id: BlockId,
+        _remapping: &VarRemapping,
+    ) {
+        // A goto is a convergence point, gas will get burned unless we redeposit it before the
+        // convergence.
+        *info = RedepositState::Required
+    }
+
+    fn merge_match(
+        &mut self,
+        _st: StatementLocation,
+        match_info: &MatchInfo,
+        infos: impl Iterator<Item = Self::Info>,
+    ) -> Self::Info {
+        for (info, arm) in zip_eq(infos, match_info.arms()) {
+            if info == RedepositState::Required {
+                self.fixes.push((arm.block_id, *match_info.location()));
+            }
+        }
+
+        // `redeposit_gas` was added, no need to add it until the next convergence point.
+        RedepositState::Redeposited
+    }
+
+    fn info_from_return(&mut self, _: StatementLocation, _vars: &[VarUsage]) -> Self::Info {
+        // If the function has multiple returns with different gas costs, gas will get burned unless
+        // we redeposit it.
+        RedepositState::Required
     }
 }
