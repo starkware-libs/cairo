de382a98be9a665da5efcb63fd65c5b6624ddea2
Ilya Lesokhin <ilya@starkware.co>
2025-04-23 13:41:47 +0300

    Add ExternalCodeSizeEstimator.

--- crates/cairo-lang-starknet-classes/src/casm_contract_class_test.rs
+++ crates/cairo-lang-starknet-classes/src/casm_contract_class_test.rs
@@ -100,7 +100,7 @@ fn test_contract_libfuncs_coverage(name: &str) {
 
 /// Tests that compiled_class_hash() returns the correct hash, by comparing it to hard-coded
 /// constant that was computed by other implementations.
-#[test_case("account__account", "53ffd8ca3756a6c293cd7a735646c39dd9fbf24c69205c2af861e37177e1b37")]
+#[test_case("account__account", "27ee73cbd07273bf8f7c07845c0e78dc4cb6ece78ba2ef32a541cb16127b9ba")]
 fn test_compiled_class_hash(name: &str, expected_hash: &str) {
     let compiled_json_path =
         get_example_file_path(format!("{name}.compiled_contract_class.json").as_str());
