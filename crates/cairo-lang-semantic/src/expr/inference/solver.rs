4e5867437fcc6f0818a685ad332a3f109c6b84b9
TomerStarkware <tomer@starkware.co>
2025-05-12 17:31:00 +0300

    added early check to eliminate candidates from solution set

--- crates/cairo-lang-semantic/src/expr/inference/solver.rs
+++ crates/cairo-lang-semantic/src/expr/inference/solver.rs
@@ -5,7 +5,7 @@ use cairo_lang_debug::DebugWithDb;
 use cairo_lang_defs::ids::LanguageElementId;
 use cairo_lang_proc_macros::SemanticObject;
 use cairo_lang_utils::LookupIntern;
-use itertools::Itertools;
+use itertools::{Itertools, zip_eq};
 
 use super::canonic::{CanonicalImpl, CanonicalMapping, CanonicalTrait, MapperError, ResultNoErrEx};
 use super::conform::InferenceConform;
@@ -15,9 +15,9 @@ use super::{
     InferenceVar, LocalImplVarId,
 };
 use crate::db::SemanticGroup;
-use crate::items::constant::ImplConstantId;
+use crate::items::constant::{ConstValueId, ImplConstantId};
 use crate::items::imp::{
-    ImplId, ImplImplId, ImplLookupContext, UninferredImpl, find_candidates_at_context,
+    ImplId, ImplImplId, ImplLongId, ImplLookupContext, UninferredImpl, find_candidates_at_context,
     find_closure_generated_candidate,
 };
 use crate::substitution::SemanticRewriter;
@@ -242,6 +242,35 @@ impl CandidateSolver {
         lookup_context: &ImplLookupContext,
         impl_type_bounds: Arc<BTreeMap<ImplTypeById, TypeId>>,
     ) -> InferenceResult<CandidateSolver> {
+        if !can_conform_generic_args(
+            &candidate.concrete_trait(db).unwrap().generic_args(db),
+            &canonical_trait.id.generic_args(db),
+            &impl_type_bounds,
+            db,
+        ) {
+            return Err(super::ErrorSet);
+        }
+        if let UninferredImpl::GenericParam(_) = candidate {
+            for (trait_args, candidate_args) in canonical_trait
+                .id
+                .generic_args(db)
+                .iter()
+                .zip_eq(candidate.concrete_trait(db).unwrap().generic_args(db))
+            {
+                if let (GenericArgumentId::Type(ty0), GenericArgumentId::Type(ty1)) =
+                    (trait_args, candidate_args)
+                {
+                    if !ty0.is_var_free(db) || !ty1.is_var_free(db) {
+                        continue;
+                    }
+
+                    if *ty0 != ty1 {
+                        return Err(super::ErrorSet);
+                    }
+                }
+            }
+        }
+
         let mut inference_data: InferenceData = InferenceData::new(InferenceId::Canonical);
         let mut inference = inference_data.inference(db);
         inference.data.impl_type_bounds = impl_type_bounds;
@@ -316,4 +345,186 @@ impl CandidateSolver {
             }
         })
     }
+}
+
+
+/// Checks if the generic arguments of the candidate could be conformed to the generic args of the trait.
+fn can_conform_generic_args(
+    gargs_candidate: &[GenericArgumentId],
+    gargs1: &[GenericArgumentId],
+    impl_type_bounds: &Arc<BTreeMap<ImplTypeById, TypeId>>,
+    db: &dyn SemanticGroup,
+) -> bool {
+    zip_eq(gargs_candidate, gargs1).all(|(garg_candidate, garg1)| {
+        can_conform_generic_arg(*garg_candidate, *garg1, impl_type_bounds, db)
+    })
+}
+
+/// Checks if a [GenericArgumentId] of the candidate could be conformed to a [GenericArgumentId] of the trait.
+fn can_conform_generic_arg(
+    garg_candidate: GenericArgumentId,
+    garg_canonical_trait: GenericArgumentId,
+    impl_type_bounds: &Arc<BTreeMap<ImplTypeById, TypeId>>,
+    db: &dyn SemanticGroup,
+) -> bool {
+    if garg_candidate == garg_canonical_trait {
+        return true;
+    }
+    match garg_candidate {
+        GenericArgumentId::Type(gty_candidate) => {
+            let GenericArgumentId::Type(gty_trait) = garg_canonical_trait else {
+                return false;
+            };
+            can_conform_ty(gty_candidate, gty_trait, impl_type_bounds, db)
+        }
+        GenericArgumentId::Constant(gc_candidate) => {
+            let GenericArgumentId::Constant(gc_trait) = garg_canonical_trait else {
+                return false;
+            };
+
+            can_conform_const(gc_candidate, gc_trait, impl_type_bounds, db)
+        }
+        GenericArgumentId::Impl(impl_candidate) => {
+            let GenericArgumentId::Impl(impl_trait) = garg_canonical_trait else {
+                return false;
+            };
+            can_conform_impl(impl_candidate, impl_trait, impl_type_bounds, db)
+        }
+        GenericArgumentId::NegImpl => {
+            matches!(garg_canonical_trait, GenericArgumentId::NegImpl)
+        }
+    }
+}
+
+/// Checks if a generic arg [ImplId] of the candidate could be conformed to a generic arg [ImplId] of the trait.
+fn can_conform_ty(
+    ty_candidate: TypeId,
+    ty_canonical_trait: TypeId,
+    impl_type_bounds: &Arc<BTreeMap<ImplTypeById, TypeId>>,
+    db: &dyn SemanticGroup,
+) -> bool {
+    if ty_candidate == ty_canonical_trait {
+        return true;
+    }
+    let long_ty_trait = ty_canonical_trait.lookup_intern(db);
+
+    if let TypeLongId::Var(_) = long_ty_trait {
+        return true;
+    }
+    let long_ty_candidate = ty_candidate.lookup_intern(db);
+
+    match long_ty_candidate {
+        TypeLongId::Concrete(concrete_candidate) => {
+            let TypeLongId::Concrete(concrete_canonical_trait) = long_ty_trait else {
+                return false;
+            };
+            if concrete_candidate.generic_type(db) != concrete_canonical_trait.generic_type(db) {
+                return false;
+            }
+            let gargs_candidate = concrete_candidate.generic_args(db);
+            let gargs_trait = concrete_canonical_trait.generic_args(db);
+            can_conform_generic_args(&gargs_candidate, &gargs_trait, impl_type_bounds, db)
+        }
+        TypeLongId::Tuple(tys_candidate) => {
+            let TypeLongId::Tuple(tys_trait) = long_ty_trait else {
+                return false;
+            };
+            if tys_candidate.len() != tys_trait.len() {
+                return false;
+            }
+            zip_eq(tys_candidate, tys_trait).all(|(subty_candidate, subty_trait)| {
+                can_conform_ty(subty_candidate, subty_trait, impl_type_bounds, db)
+            })
+        }
+        TypeLongId::Closure(closure_candidate) => {
+            let TypeLongId::Closure(closure_trait) = long_ty_trait else {
+                return false;
+            };
+            if closure_candidate.wrapper_location != closure_trait.wrapper_location {
+                return false;
+            }
+            if !zip_eq(closure_candidate.param_tys, closure_trait.param_tys).all(
+                |(subty_candidate, subty_trait)| {
+                    can_conform_ty(subty_candidate, subty_trait, impl_type_bounds, db)
+                },
+            ) {
+                return false;
+            }
+            if !zip_eq(closure_candidate.captured_types, closure_trait.captured_types).all(
+                |(subty_candidate, subty_trait)| {
+                    can_conform_ty(subty_candidate, subty_trait, impl_type_bounds, db)
+                },
+            ) {
+                return false;
+            }
+            can_conform_ty(closure_candidate.ret_ty, closure_trait.ret_ty, impl_type_bounds, db)
+        }
+        TypeLongId::FixedSizeArray { type_id, size } => {
+            let TypeLongId::FixedSizeArray { type_id: type_id1, size: size1 } = long_ty_trait
+            else {
+                return false;
+            };
+            if !can_conform_const(size, size1, impl_type_bounds, db) {
+                return false;
+            }
+            can_conform_ty(type_id, type_id1, impl_type_bounds, db)
+        }
+        TypeLongId::Snapshot(inner_ty_candidate) => {
+            let TypeLongId::Snapshot(inner_ty1) = long_ty_trait else {
+                return false;
+            };
+            can_conform_ty(inner_ty_candidate, inner_ty1, impl_type_bounds, db)
+        }
+        TypeLongId::GenericParameter(_)
+        | TypeLongId::Var(_)
+        | TypeLongId::ImplType(_)
+        | TypeLongId::Missing(_)
+        | TypeLongId::Coupon(_) => true,
+    }
+}
+
+/// Checks if a generic arg [ImplId] of the candidate could be conformed to a generic arg [ImplId] of the trait.
+fn can_conform_impl(
+    impl_candidate: ImplId,
+    impl_canonical_trait: ImplId,
+    impl_type_bounds: &Arc<BTreeMap<ImplTypeById, TypeId>>,
+    db: &dyn SemanticGroup,
+) -> bool {
+    let long_impl_trait = impl_canonical_trait.lookup_intern(db);
+    if impl_candidate == impl_canonical_trait {
+        return true;
+    }
+    if let ImplLongId::ImplVar(_) = long_impl_trait {
+        return true;
+    }
+    match impl_candidate.lookup_intern(db) {
+        ImplLongId::ImplVar(_) => true,
+        ImplLongId::Concrete(concrete_candidate) => {
+            let ImplLongId::Concrete(concrete_canonical_trait) = long_impl_trait else {
+                return false;
+            };
+            let concrete_candidate = concrete_candidate.lookup_intern(db);
+            let concrete_canonical_trait = concrete_canonical_trait.lookup_intern(db);
+            if concrete_candidate.impl_def_id != concrete_canonical_trait.impl_def_id {
+                return false;
+            }
+            let gargs_candidate = concrete_candidate.generic_args;
+            let gargs_trait = concrete_canonical_trait.generic_args;
+            can_conform_generic_args(&gargs_candidate, &gargs_trait, impl_type_bounds, db)
+        }
+        ImplLongId::GenericParameter(_)
+        | ImplLongId::ImplImpl(_)
+        | ImplLongId::SelfImpl(_)
+        | ImplLongId::GeneratedImpl(_) => true,
+    }
+}
+
+// TODO(Tomerstarkware) add consts to early candidate eliminiation
+fn can_conform_const(
+    _id_candidate: ConstValueId,
+    _id_canonical_trait: ConstValueId,
+    _impl_type_bounds: &Arc<BTreeMap<ImplTypeById, TypeId>>,
+    _db: &dyn SemanticGroup,
+) -> bool {
+    true
 }
