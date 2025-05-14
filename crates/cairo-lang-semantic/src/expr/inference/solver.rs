0027ad847412fdd1de99a53e5dd0f17bd9f66f8f
TomerStarkware <tomer@starkware.co>
2025-05-14 14:19:58 +0300

    added early check to eliminate candidates from solution set

--- crates/cairo-lang-semantic/src/expr/inference/solver.rs
+++ crates/cairo-lang-semantic/src/expr/inference/solver.rs
@@ -4,8 +4,8 @@ use std::sync::Arc;
 use cairo_lang_debug::DebugWithDb;
 use cairo_lang_defs::ids::LanguageElementId;
 use cairo_lang_proc_macros::SemanticObject;
-use cairo_lang_utils::LookupIntern;
-use itertools::Itertools;
+use cairo_lang_utils::{Intern, LookupIntern};
+use itertools::{Itertools, zip_eq};
 
 use super::canonic::{CanonicalImpl, CanonicalMapping, CanonicalTrait, MapperError, ResultNoErrEx};
 use super::conform::InferenceConform;
@@ -15,9 +15,9 @@ use super::{
     InferenceVar, LocalImplVarId,
 };
 use crate::db::SemanticGroup;
-use crate::items::constant::ImplConstantId;
+use crate::items::constant::{ConstValue, ConstValueId, ImplConstantId};
 use crate::items::imp::{
-    ImplId, ImplImplId, ImplLookupContext, UninferredImpl, find_candidates_at_context,
+    ImplId, ImplImplId, ImplLongId, ImplLookupContext, UninferredImpl, find_candidates_at_context,
     find_closure_generated_candidate,
 };
 use crate::substitution::SemanticRewriter;
@@ -242,6 +242,23 @@ impl CandidateSolver {
         lookup_context: &ImplLookupContext,
         impl_type_bounds: Arc<BTreeMap<ImplTypeById, TypeId>>,
     ) -> InferenceResult<CandidateSolver> {
+        let candidate_concrete_trait = candidate.concrete_trait(db).unwrap();
+        if candidate_concrete_trait.is_fully_concrete(db)
+            && canonical_trait.id.is_var_free(db)
+            && candidate_concrete_trait != canonical_trait.id
+        {
+            return Err(super::ErrorSet);
+        }
+        if !can_conform_generic_args(
+            db,
+            &candidate_concrete_trait.generic_args(db),
+            &canonical_trait.id.generic_args(db),
+            !matches!(candidate, UninferredImpl::GenericParam(_)),
+        ) {
+            return Err(super::ErrorSet);
+        }
+
+
         let mut inference_data: InferenceData = InferenceData::new(InferenceId::Canonical);
         let mut inference = inference_data.inference(db);
         inference.data.impl_type_bounds = impl_type_bounds;
@@ -315,5 +332,272 @@ impl CandidateSolver {
                 }
             }
         })
+    }
+}
+
+/// Checks if the generic arguments of the candidate could be conformed to the generic args of the
+/// trait.
+fn can_conform_generic_args(
+    db: &dyn SemanticGroup,
+    candidate_args: &[GenericArgumentId],
+    target_args: &[GenericArgumentId],
+    substitute_generic_params: bool,
+) -> bool {
+    zip_eq(candidate_args, target_args).all(|(candidate_arg, target_arg)| {
+        can_conform_generic_arg(db, *candidate_arg, *target_arg, substitute_generic_params)
+    })
+}
+
+/// Checks if a [GenericArgumentId] of the candidate could be conformed to a [GenericArgumentId] of
+/// the trait.
+fn can_conform_generic_arg(
+    db: &dyn SemanticGroup,
+    candidate_arg: GenericArgumentId,
+    target_arg: GenericArgumentId,
+    substitute_generic_params: bool,
+) -> bool {
+    if candidate_arg == target_arg {
+        return true;
+    }
+    if candidate_arg.is_fully_concrete(db) && target_arg.is_var_free(db) {
+        return false;
+    }
+    match (candidate_arg, target_arg) {
+        (GenericArgumentId::Type(candidate), GenericArgumentId::Type(target)) => {
+            can_conform_ty(db, candidate, target, substitute_generic_params)
+        }
+        (GenericArgumentId::Constant(candidate), GenericArgumentId::Constant(target)) => {
+            can_conform_const(db, candidate, target, substitute_generic_params)
+        }
+        (GenericArgumentId::Impl(candidate), GenericArgumentId::Impl(target)) => {
+            can_conform_impl(db, candidate, target, substitute_generic_params)
+        }
+        (GenericArgumentId::NegImpl, GenericArgumentId::NegImpl) => true,
+        _ => false,
+    }
+}
+
+/// Checks if a generic arg [TypeId] of the candidate could be conformed to a generic arg [TypeId]
+/// of the trait.
+fn can_conform_ty(
+    db: &dyn SemanticGroup,
+    candidate_ty: TypeId,
+    target_ty: TypeId,
+    substitute_generic_params: bool,
+) -> bool {
+    if candidate_ty == target_ty {
+        return true;
+    }
+    if candidate_ty.is_fully_concrete(db) && target_ty.is_var_free(db) {
+        return false;
+    }
+    if !substitute_generic_params && candidate_ty.is_var_free(db) && target_ty.is_var_free(db) {
+        return false;
+    }
+    let target_long_ty = target_ty.lookup_intern(db);
+
+    if let TypeLongId::Var(_) = target_long_ty {
+        return true;
+    }
+
+    let long_ty_candidate = candidate_ty.lookup_intern(db);
+
+    match (long_ty_candidate, target_long_ty) {
+        (TypeLongId::Concrete(candidate), TypeLongId::Concrete(target)) => {
+            candidate.generic_type(db) == target.generic_type(db)
+                && can_conform_generic_args(
+                    db,
+                    &candidate.generic_args(db),
+                    &target.generic_args(db),
+                    substitute_generic_params,
+                )
+        }
+        (TypeLongId::Concrete(_), _) => false,
+        (TypeLongId::Tuple(candidate_tys), TypeLongId::Tuple(target_tys)) => {
+            candidate_tys.len() == target_tys.len() && 
+            zip_eq(candidate_tys, target_tys).all(|(candidate_subty, target_subty)| {
+                can_conform_ty(db, candidate_subty, target_subty, substitute_generic_params)
+            })
+        }
+        (TypeLongId::Tuple(_), _) => false,
+        (TypeLongId::Closure(candidate_closure), TypeLongId::Closure(target_closure)) => {
+            if candidate_closure.wrapper_location != target_closure.wrapper_location {
+                return false;
+            }
+            if !zip_eq(candidate_closure.param_tys, target_closure.param_tys).all(
+                |(candidate_subty, target_subty)| {
+                    can_conform_ty(db, candidate_subty, target_subty, substitute_generic_params)
+                },
+            ) {
+                return false;
+            }
+            if !zip_eq(candidate_closure.captured_types, target_closure.captured_types).all(
+                |(candidate_subty, target_subty)| {
+                    can_conform_ty(db, candidate_subty, target_subty, substitute_generic_params)
+                },
+            ) {
+                return false;
+            }
+            can_conform_ty(
+                db,
+                candidate_closure.ret_ty,
+                target_closure.ret_ty,
+                substitute_generic_params,
+            )
+        }
+        (TypeLongId::Closure(_), _) => false,
+        (
+            TypeLongId::FixedSizeArray { type_id: candidate_type_id, size: candidate_size },
+            TypeLongId::FixedSizeArray { type_id: target_type_id, size: target_size },
+        ) => {
+            can_conform_const(db, candidate_size, target_size, substitute_generic_params) && 
+            can_conform_ty(db, candidate_type_id, target_type_id, substitute_generic_params)
+        }
+        (TypeLongId::FixedSizeArray { type_id: _, size: _ }, _) => false,
+        (TypeLongId::Snapshot(candidate_inner_ty), TypeLongId::Snapshot(target_inner_ty)) => {
+            can_conform_ty(db, candidate_inner_ty, target_inner_ty, substitute_generic_params)
+        }
+        (TypeLongId::Snapshot(_), _) => false,
+        (
+            TypeLongId::GenericParameter(_)
+            | TypeLongId::Var(_)
+            | TypeLongId::ImplType(_)
+            | TypeLongId::Missing(_)
+            | TypeLongId::Coupon(_),
+            _,
+        ) => true,
+    }
+}
+
+/// Checks if a generic arg [ImplId] of the candidate could be conformed to a generic arg [ImplId]
+/// of the trait.
+fn can_conform_impl(
+    db: &dyn SemanticGroup,
+    candidate_impl: ImplId,
+    target_impl: ImplId,
+    substitute_generic_params: bool,
+) -> bool {
+    let long_impl_trait = target_impl.lookup_intern(db);
+    if candidate_impl == target_impl {
+        return true;
+    }
+    if let ImplLongId::ImplVar(_) = long_impl_trait {
+        return true;
+    }
+    match (candidate_impl.lookup_intern(db), long_impl_trait) {
+        (ImplLongId::Concrete(candidate_concrete), ImplLongId::Concrete(target_concrete)) => {
+            let candidate_concrete = candidate_concrete.lookup_intern(db);
+            let target_concrete = target_concrete.lookup_intern(db);
+            if candidate_concrete.impl_def_id != target_concrete.impl_def_id {
+                return false;
+            }
+            let candidate_args = candidate_concrete.generic_args;
+            let target_args = target_concrete.generic_args;
+            can_conform_generic_args(db, &candidate_args, &target_args, substitute_generic_params)
+        }
+        (ImplLongId::Concrete(_), _) => false,
+        (
+            ImplLongId::GenericParameter(_)
+            | ImplLongId::ImplVar(_)
+            | ImplLongId::ImplImpl(_)
+            | ImplLongId::SelfImpl(_)
+            | ImplLongId::GeneratedImpl(_),
+            _,
+        ) => true,
+    }
+}
+
+/// Checks if a generic arg [ConstValueId] of the candidate could be conformed to a generic arg
+/// [ConstValueId] of the trait.
+fn can_conform_const(
+    db: &dyn SemanticGroup,
+    candidate_id: ConstValueId,
+    target_id: ConstValueId,
+    substitute_generic_params: bool,
+) -> bool {
+    if candidate_id == target_id {
+        return true;
+    }
+    if candidate_id.is_fully_concrete(db) && target_id.is_var_free(db) {
+        return false;
+    }
+    let target_long_const = target_id.lookup_intern(db);
+    if let ConstValue::Var(_, _) = target_long_const {
+        return true;
+    }
+    match (candidate_id.lookup_intern(db), target_long_const) {
+        (ConstValue::Int(big_int, type_id), ConstValue::Int(target_big_int, target_type_id)) => {
+            if big_int != target_big_int {
+                return false;
+            }
+            can_conform_ty(db, type_id, target_type_id, substitute_generic_params)
+        }
+        (ConstValue::Int(_, _), _) => false,
+        (
+            ConstValue::Struct(const_values, type_id),
+            ConstValue::Struct(target_const_values, target_type_id),
+        ) => {
+            if const_values.len() != target_const_values.len() {
+                return false;
+            }
+            if !can_conform_ty(db, type_id, target_type_id, substitute_generic_params) {
+                return false;
+            }
+            zip_eq(const_values, target_const_values).all(|(const_value, target_const_value)| {
+                can_conform_const(
+                    db,
+                    const_value.intern(db),
+                    target_const_value.intern(db),
+                    substitute_generic_params,
+                )
+            })
+        }
+        (ConstValue::Struct(_, _), _) => false,
+
+        (
+            ConstValue::Enum(concrete_variant, const_value),
+            ConstValue::Enum(target_concrete_variant, target_const_value),
+        ) => {
+            if !can_conform_ty(
+                db,
+                concrete_variant.ty,
+                target_concrete_variant.ty,
+                substitute_generic_params,
+            ) {
+                return false;
+            }
+            can_conform_const(
+                db,
+                const_value.intern(db),
+                target_const_value.intern(db),
+                substitute_generic_params,
+            )
+        }
+        (ConstValue::Enum(_, _), _) => false,
+        (ConstValue::NonZero(const_value), ConstValue::NonZero(target_const_value)) => {
+            can_conform_const(
+                db,
+                const_value.intern(db),
+                target_const_value.intern(db),
+                substitute_generic_params,
+            )
+        }
+        (ConstValue::NonZero(_), _) => false,
+        (ConstValue::Boxed(const_value), ConstValue::Boxed(target_const_value)) => {
+            can_conform_const(
+                db,
+                const_value.intern(db),
+                target_const_value.intern(db),
+                substitute_generic_params,
+            )
+        }
+        (ConstValue::Boxed(_), _) => false,
+        (
+            ConstValue::Generic(_)
+            | ConstValue::ImplConstant(_)
+            | ConstValue::Var(_, _)
+            | ConstValue::Missing(_),
+            _,
+        ) => true,
     }
 }
