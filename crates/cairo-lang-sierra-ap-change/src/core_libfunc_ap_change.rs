e5e57f6b24bec1399ab5087d37fdc21d4d326dff
liorgold2 <38202661+liorgold2@users.noreply.github.com>
2025-05-21 18:57:17 +0300

    Add gas_reserve_utilize libfunc. (#7757)

--- crates/cairo-lang-sierra-ap-change/src/core_libfunc_ap_change.rs
+++ crates/cairo-lang-sierra-ap-change/src/core_libfunc_ap_change.rs
@@ -222,6 +222,7 @@ pub fn core_libfunc_ap_change<InfoProvider: InvocationApChangeInfoProvider>(
             GasReserveConcreteLibfunc::Create(_) => {
                 vec![ApChange::Known(2), ApChange::Known(3)]
             }
+            GasReserveConcreteLibfunc::Utilize(_) => vec![ApChange::Known(0)],
         },
         Uint8(libfunc) => uint_ap_change(libfunc),
         Uint16(libfunc) => uint_ap_change(libfunc),
--- crates/cairo-lang-sierra-to-casm/src/invocations/gas_reserve.rs
+++ crates/cairo-lang-sierra-to-casm/src/invocations/gas_reserve.rs
@@ -1,9 +1,12 @@
-use cairo_lang_casm::builder::Var;
+use cairo_lang_casm::builder::{CasmBuilder, Var};
+use cairo_lang_casm::casm_build_extend;
 use cairo_lang_sierra::extensions::gas_reserve::GasReserveConcreteLibfunc;
 use num_bigint::BigInt;
 
 use crate::invocations::int::SmallDiffHelper;
-use crate::invocations::{CompiledInvocation, CompiledInvocationBuilder, InvocationError};
+use crate::invocations::{
+    CompiledInvocation, CompiledInvocationBuilder, InvocationError, add_input_variables,
+};
 
 /// Builds instructions for Sierra `GasReserve` operations.
 pub fn build(
@@ -12,6 +15,7 @@ pub fn build(
 ) -> Result<CompiledInvocation, InvocationError> {
     match libfunc {
         GasReserveConcreteLibfunc::Create(_) => build_gas_reserve_create(builder),
+        GasReserveConcreteLibfunc::Utilize(_) => build_gas_reserve_utilize(builder),
     }
 }
 
@@ -23,4 +27,26 @@ fn build_gas_reserve_create(
     let no_overflow_res: &[&[Var]] = &[&[data.range_check], &[data.a_minus_b], &[data.b]];
     let overflow_res: &[&[Var]] = &[&[data.range_check], &[data.a]];
     data.finalize(no_overflow_res, overflow_res)
+}
+
+/// Handles the gas_reserve_utilize invocation.
+fn build_gas_reserve_utilize(
+    builder: CompiledInvocationBuilder<'_>,
+) -> Result<CompiledInvocation, InvocationError> {
+    let [gas_counter, reserve] = builder.try_get_single_cells()?;
+    let mut casm_builder = CasmBuilder::default();
+    add_input_variables! {casm_builder,
+        deref gas_counter;
+        deref reserve;
+    };
+
+    casm_build_extend! {casm_builder,
+        let updated_gas = gas_counter + reserve;
+    };
+
+    Ok(builder.build_from_casm_builder(
+        casm_builder,
+        [("Fallthrough", &[&[updated_gas]], None)],
+        Default::default(),
+    ))
 }
