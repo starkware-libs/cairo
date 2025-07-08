d5052233f4b36cdc618f7a5633b5e95a8b3b582f
Ori Ziv <oriz@starkware.co>
2025-07-08 17:17:08 +0300

    Updated nightly version.
    commit-id:395604f3

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export RUSTUP_TOOLCHAIN="${RUSTUP_TOOLCHAIN:-nightly-2025-06-29}"
+export RUSTUP_TOOLCHAIN="${RUSTUP_TOOLCHAIN:-nightly-2025-07-08}"
 
 cargo clippy "$@" --all-targets --all-features -- -D warnings -D future-incompatible \
     -D nonstandard-style -D rust-2018-idioms -D unused
--- scripts/rust_fmt.sh
+++ scripts/rust_fmt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export RUSTUP_TOOLCHAIN="${RUSTUP_TOOLCHAIN:-nightly-2025-06-29}"
+export RUSTUP_TOOLCHAIN="${RUSTUP_TOOLCHAIN:-nightly-2025-07-08}"
 
 cargo fmt --all -- "$@"
