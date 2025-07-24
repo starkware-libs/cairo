896e852efb7ab0769e79b7ca045e0e912b86820f
Lior Goldberg <lior@starkware.co>
2025-07-24 21:23:02 +0300

    flow_control: Implement lower_graph for two node types.
    commit-id:3bb2b950

--- crates/cairo-lang-lowering/src/lower/flow_control/lower_graph.rs
+++ crates/cairo-lang-lowering/src/lower/flow_control/lower_graph.rs
@@ -1,18 +1,144 @@
 //! This module is responsible for the lowering of a flow control graph [FlowControlGraph].
 
-use super::graph::FlowControlGraph;
+use cairo_lang_utils::unordered_hash_map::UnorderedHashMap;
+
+use super::graph::{FlowControlGraph, NodeId};
 use crate::ids::LocationId;
-use crate::lower::block_builder::BlockBuilder;
-use crate::lower::context::{LoweredExpr, LoweringContext, LoweringResult};
+use crate::lower::block_builder::{
+    BlockBuilder, SealedBlockBuilder, SealedGotoCallsite, merge_block_builders,
+    merge_sealed_block_builders,
+};
+use crate::lower::context::{LoweredExpr, LoweringContext, LoweringFlowError, LoweringResult};
+use crate::{BlockEnd, BlockId, MatchInfo};
+
+mod lower_node;
 
 /// Lowers a flow control graph.
 #[allow(dead_code)]
 pub fn lower_graph(
-    _ctx: &mut LoweringContext<'_, '_>,
-    _builder: &mut BlockBuilder,
-    _graph: &FlowControlGraph,
+    ctx: &mut LoweringContext<'_, '_>,
+    builder: &mut BlockBuilder,
+    graph: &FlowControlGraph,
     location: LocationId,
 ) -> LoweringResult<LoweredExpr> {
-    // TODO(lior): replace the following dummy code with a real implementation.
-    Ok(LoweredExpr::Tuple { exprs: vec![], location })
+    // Extract the builder from the mutable reference and replace it with a dummy builder.
+    let dummy_block_builder = BlockBuilder::root(BlockId(0));
+    let builder_ = std::mem::replace(builder, dummy_block_builder);
+
+    let mut context = LowerGraphContext::new(ctx, builder_, graph, location);
+    // Go over the nodes in reverse order to make sure parent nodes are handled before their
+    // children.
+    for id in (0..graph.nodes.len()).rev() {
+        lower_node::lower_node(&mut context, NodeId(id)).map_err(LoweringFlowError::Failed)?;
+    }
+
+    let (res, new_builder) = context.finalize();
+    // Place the new builder in the mutable reference.
+    *builder = new_builder;
+    res
+}
+
+struct LowerGraphContext<'a, 'b, 'db> {
+    /// The lowering context.
+    ctx: &'a mut LoweringContext<'b, 'db>,
+    /// The flow control graph to lower.
+    graph: &'a FlowControlGraph,
+    /// The [BlockBuilder] for the result of the lowering, and the [MatchInfo] for its
+    /// finalization.
+    result: Option<(BlockBuilder, MatchInfo)>,
+    /// A map from a node id to the [BlockBuilder]s created by its parents.
+    /// When a node is visited, it creates a [BlockBuilder] for each of its child nodes
+    /// (see [Self::assign_child_block_id]).
+    /// Later, when the child node is visited, all the [BlockBuilder]s are merged into a
+    /// single [BlockBuilder] (see [Self::start_builder]), on which the node is lowered.
+    block_builders: UnorderedHashMap<NodeId, Vec<BlockBuilder>>,
+    /// A list of sealed blocks for the arms (excluding the arms that do not continue to the
+    /// callsite).
+    sealed_blocks: Vec<SealedGotoCallsite>,
+    /// The location of the expression being lowered.
+    /// This is used for the location of the variables created by block merges during the lowering.
+    location: LocationId,
+}
+
+impl<'a, 'b, 'db> LowerGraphContext<'a, 'b, 'db> {
+    /// Constructs a new [LowerGraphContext].
+    fn new(
+        ctx: &'a mut LoweringContext<'b, 'db>,
+        root_builder: BlockBuilder,
+        graph: &'a FlowControlGraph,
+        location: LocationId,
+    ) -> Self {
+        Self {
+            ctx,
+            graph,
+            result: None,
+            block_builders: [(graph.root(), vec![root_builder])].into_iter().collect(),
+            sealed_blocks: vec![],
+            location,
+        }
+    }
+
+    /// Assigns a new [BlockBuilder] and [BlockId] to the given child node.
+    /// Registers the [BlockBuilder] in the [Self::block_builders] map, under the child node's id.
+    fn assign_child_block_id(
+        &mut self,
+        child_id: NodeId,
+        parent_block_builder: &BlockBuilder,
+    ) -> BlockId {
+        let block_id = self.ctx.blocks.alloc_empty();
+        let child_builder = parent_block_builder.child_block_builder(block_id);
+        self.block_builders.entry(child_id).or_default().push(child_builder);
+        block_id
+    }
+
+    /// Creates a [BlockBuilder] for the given node, based on the parent nodes.
+    fn start_builder(&mut self, id: NodeId) -> BlockBuilder {
+        // Extract the builders of the parent nodes (the nodes leading to the current node).
+        // TODO(lior): Replace unwrap with handling a non-reachable node.
+        let parent_builders = self.block_builders.remove(&id).unwrap();
+        merge_block_builders(self.ctx, parent_builders, self.location)
+    }
+
+    // Finalization functions.
+
+    /// Finalizes a block with a match.
+    ///
+    /// The root block is not finalized immediately, since the API of lowering functions
+    /// requires that the block will not be finalized if it does not continue to the
+    /// callsite (for example, if all the arms panic or return from the function).
+    fn finalize_with_match(&mut self, id: NodeId, builder: BlockBuilder, info: MatchInfo) {
+        if id == self.graph.root() {
+            self.result = Some((builder, info));
+        } else {
+            builder.finalize(self.ctx, BlockEnd::Match { info });
+        }
+    }
+
+    /// Adds a sealed block to the context.
+    ///
+    /// The block is an arm's block that was already sealed and should be merged with the other
+    /// arms.
+    fn add_sealed_block(&mut self, sealed_block: SealedBlockBuilder) {
+        match sealed_block {
+            SealedBlockBuilder::GotoCallsite(sealed_block) => {
+                self.sealed_blocks.push(sealed_block);
+            }
+            SealedBlockBuilder::Ends(_) => {
+                // If the block ends with `SealedBlockBuilder::Ends`, ignore it as it doesn't
+                // return to the callsite and therefore should not be merged.
+            }
+        }
+    }
+
+    fn finalize(mut self) -> (LoweringResult<LoweredExpr>, BlockBuilder) {
+        let (builder, match_info) = self.result.take().unwrap();
+        if let Some((new_builder, lowered_expr)) =
+            merge_sealed_block_builders(self.ctx, self.sealed_blocks, self.location)
+        {
+            builder.finalize(self.ctx, BlockEnd::Match { info: match_info });
+            (Ok(lowered_expr), new_builder)
+        } else {
+            (Err(LoweringFlowError::Match(match_info)), builder)
+        }
+    }
 }
---
+++ crates/cairo-lang-lowering/src/lower/flow_control/lower_graph/lower_node.rs
@@ -0,0 +1,76 @@
+//! Functions for lowering nodes of a [super::FlowControlGraph].
+
+use cairo_lang_diagnostics::Maybe;
+use cairo_lang_semantic::{MatchArmSelector, corelib};
+
+use super::LowerGraphContext;
+use crate::lower::context::VarRequest;
+use crate::lower::flow_control::graph::{ArmExpr, BooleanIf, FlowControlNode, NodeId};
+use crate::lower::{lower_expr_to_var_usage, lower_tail_expr};
+use crate::{MatchArm, MatchEnumInfo, MatchInfo};
+
+/// Lowers the node with the given [NodeId].
+pub fn lower_node(ctx: &mut LowerGraphContext<'_, '_, '_>, id: NodeId) -> Maybe<()> {
+    match &ctx.graph.nodes[id.0] {
+        FlowControlNode::BooleanIf(node) => lower_boolean_if(ctx, id, node),
+        FlowControlNode::ArmExpr(node) => lower_arm_expr(ctx, id, node),
+    }
+}
+
+/// Lowers a [BooleanIf] node.
+fn lower_boolean_if(
+    ctx: &mut LowerGraphContext<'_, '_, '_>,
+    id: NodeId,
+    node: &BooleanIf,
+) -> Maybe<()> {
+    let db = ctx.ctx.db;
+
+    let mut builder = ctx.start_builder(id);
+
+    let lowered_condition = lower_expr_to_var_usage(ctx.ctx, &mut builder, node.condition);
+    // TODO(lior): Replace unwrap with a proper error handling.
+    let condition_var = lowered_condition.unwrap();
+
+    // Allocate/retrieve block ids for the two branches.
+    let true_branch_block_id = ctx.assign_child_block_id(node.true_branch, &builder);
+    let false_branch_block_id = ctx.assign_child_block_id(node.false_branch, &builder);
+
+    // Create dummy variables for the unit type inside the bool's variants.
+    let unit_ty = corelib::unit_ty(db);
+    let true_branch_var_id = ctx.ctx.new_var(VarRequest { ty: unit_ty, location: ctx.location });
+    let false_branch_var_id = ctx.ctx.new_var(VarRequest { ty: unit_ty, location: ctx.location });
+
+    // Finalize the block.
+    let match_info = MatchInfo::Enum(MatchEnumInfo {
+        concrete_enum_id: corelib::core_bool_enum(db),
+        input: condition_var,
+        arms: vec![
+            MatchArm {
+                arm_selector: MatchArmSelector::VariantId(corelib::false_variant(db)),
+                block_id: false_branch_block_id,
+                var_ids: vec![false_branch_var_id],
+            },
+            MatchArm {
+                arm_selector: MatchArmSelector::VariantId(corelib::true_variant(db)),
+                block_id: true_branch_block_id,
+                var_ids: vec![true_branch_var_id],
+            },
+        ],
+        location: ctx.location,
+    });
+
+    ctx.finalize_with_match(id, builder, match_info);
+    Ok(())
+}
+
+/// Lowers an [ArmExpr] node.
+fn lower_arm_expr(
+    ctx: &mut LowerGraphContext<'_, '_, '_>,
+    id: NodeId,
+    node: &ArmExpr,
+) -> Maybe<()> {
+    let builder = ctx.start_builder(id);
+    let sealed_block = lower_tail_expr(ctx.ctx, builder, node.expr)?;
+    ctx.add_sealed_block(sealed_block);
+    Ok(())
+}
