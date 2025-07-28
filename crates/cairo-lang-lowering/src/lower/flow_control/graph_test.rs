751d3d5052dfb6c70614231a36ce3a360fd9a842
liorgold2 <38202661+liorgold2@users.noreply.github.com>
2025-07-28 14:04:39 +0300

    flow_control: Add a skeleton and test for lower_graph. (#8035)

--- crates/cairo-lang-lowering/src/lower/flow_control/graph_test.rs
+++ crates/cairo-lang-lowering/src/lower/flow_control/graph_test.rs
@@ -1,14 +1,26 @@
 use cairo_lang_debug::DebugWithDb;
-use cairo_lang_semantic as semantic;
 use cairo_lang_semantic::db::SemanticGroup;
 use cairo_lang_semantic::expr::fmt::ExprFormatter;
 use cairo_lang_semantic::test_utils::setup_test_function;
+use cairo_lang_semantic::{self as semantic, ExprVarMemberPath};
+use cairo_lang_syntax::node::TypedStablePtr;
 use cairo_lang_test_utils::parse_test_file::TestRunnerResult;
 use cairo_lang_test_utils::verify_diagnostics_expectation;
+use cairo_lang_utils::extract_matches;
 use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
+use itertools::Itertools;
 
 use super::create_graph::create_graph_expr_if;
-use crate::test_utils::LoweringDatabaseForTesting;
+use super::graph::FlowControlGraph;
+use super::lower_graph::lower_graph;
+use crate::Lowered;
+use crate::lower::block_builder::BlockBuilder;
+use crate::lower::context::{LoweringContext, VarRequest};
+use crate::lower::test_utils::{create_encapsulating_ctx, create_lowering_context};
+use crate::lower::{
+    alloc_empty_block, lowered_expr_to_block_scope_end, wrap_sealed_block_as_function,
+};
+use crate::test_utils::{LoweringDatabaseForTesting, formatted_lowered};
 
 cairo_lang_test_utils::test_file_test!(
     create_graph,
@@ -51,11 +63,73 @@ fn test_create_graph(
 
     let error = verify_diagnostics_expectation(args, &semantic_diagnostics);
 
+    let mut encapsulating_ctx =
+        create_encapsulating_ctx(db, test_function.function_id, &test_function.signature);
+
+    let ctx = create_lowering_context(
+        db,
+        test_function.function_id,
+        &test_function.signature,
+        &mut encapsulating_ctx,
+    );
+
+    // Lower the graph.
+    let lowered = lower_graph_as_function(ctx, expr_id, &graph);
+    let lowered_str = formatted_lowered(db, Some(&lowered));
+
     TestRunnerResult {
         outputs: OrderedHashMap::from([
             ("graph".into(), format!("{graph:?}")),
             ("semantic_diagnostics".into(), semantic_diagnostics),
+            ("lowered".into(), lowered_str),
         ]),
         error,
+    }
+}
+
+/// Calls [lower_graph] on the expression as if it was a function's body. Returns the [Lowered]
+/// object.
+fn lower_graph_as_function(
+    mut ctx: LoweringContext<'_, '_>,
+    expr_id: semantic::ExprId,
+    graph: &FlowControlGraph,
+) -> Lowered {
+    let expr = &ctx.function_body.arenas.exprs[expr_id];
+    let location = ctx.get_location(expr.stable_ptr().untyped());
+
+    // Create a new block builder.
+    let mut builder = BlockBuilder::root(alloc_empty_block(&mut ctx));
+
+    // Add the function parameters to the block's semantics.
+    let parameters = ctx
+        .signature
+        .params
+        .clone()
+        .into_iter()
+        .map(|param| {
+            let location = ctx.get_location(param.stable_ptr().untyped());
+            let var = ctx.new_var(VarRequest { ty: param.ty(), location });
+            let param_var = extract_matches!(param, ExprVarMemberPath::Var);
+            builder.put_semantic(param_var.var, var);
+            var
+        })
+        .collect_vec();
+
+    // Lower the graph into the builder.
+    let block_expr = lower_graph(&mut ctx, &mut builder, graph, location);
+
+    let block_sealed = lowered_expr_to_block_scope_end(&mut ctx, builder, block_expr).unwrap();
+
+    let expr = ctx.function_body.arenas.exprs[expr_id].clone();
+
+    wrap_sealed_block_as_function(&mut ctx, block_sealed, expr.stable_ptr().untyped()).unwrap();
+
+    let blocks = ctx.blocks.build().expect("Root block must exist.");
+    Lowered {
+        diagnostics: ctx.diagnostics.build(),
+        variables: ctx.variables.variables,
+        blocks,
+        signature: ctx.signature.clone(),
+        parameters,
     }
 }
