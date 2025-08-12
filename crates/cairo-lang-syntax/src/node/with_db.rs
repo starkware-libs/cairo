ba1c4367f3b1da2baeb7825a34b0a70338b0c9f3
Maciej Trątnowiecki <maciej.tratnowiecki@swmansion.com>
2025-08-12 10:50:40 +0200

    Optimize building primitive token stream iterator

--- crates/cairo-lang-syntax/src/node/with_db.rs
+++ crates/cairo-lang-syntax/src/node/with_db.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use cairo_lang_filesystem::span::TextWidth;
 use cairo_lang_primitive_token::{PrimitiveSpan, PrimitiveToken, ToPrimitiveTokenStream};
 
@@ -20,23 +22,19 @@ impl<'a, Db: SyntaxGroup> ToPrimitiveTokenStream for SyntaxNodeWithDb<'a, Db> {
 
     fn to_primitive_token_stream(&self) -> Self::Iter {
         // The lifetime of the iterator should extend 'a because it derives from both node and db
-        SyntaxNodeWithDbIterator::new(
-            Box::new(
-                self.node.tokens(self.db).flat_map(|node| token_from_syntax_node(node, self.db)),
-            ),
-            self.db,
-        )
+        SyntaxNodeWithDbIterator::new(self.db, self.node)
     }
 }
 
 pub struct SyntaxNodeWithDbIterator<'a, Db: SyntaxGroup> {
-    inner: Box<dyn Iterator<Item = PrimitiveToken> + 'a>,
-    _db: &'a Db,
+    iter_stack: Vec<&'a SyntaxNode<'a>>,
+    buffer: VecDeque<PrimitiveToken>,
+    db: &'a Db,
 }
 
 impl<'a, Db: SyntaxGroup> SyntaxNodeWithDbIterator<'a, Db> {
-    pub fn new(inner: Box<dyn Iterator<Item = PrimitiveToken> + 'a>, db: &'a Db) -> Self {
-        Self { inner, _db: db }
+    pub fn new(db: &'a Db, node: &'a SyntaxNode<'a>) -> Self {
+        Self { db, buffer: Default::default(), iter_stack: vec![node] }
     }
 }
 
@@ -44,7 +42,29 @@ impl<Db: SyntaxGroup> Iterator for SyntaxNodeWithDbIterator<'_, Db> {
     type Item = PrimitiveToken;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.inner.next()
+        while self.buffer.is_empty() {
+            let node = self.iter_stack.pop()?;
+            for_each_terminals_ex(node, self.db, &mut self.iter_stack, &mut |node| {
+                token_from_syntax_node(node, self.db, &mut self.buffer);
+            })
+        }
+        self.buffer.pop_front()
+    }
+}
+
+fn for_each_terminals_ex<'db>(
+    node: &SyntaxNode<'db>,
+    db: &'db dyn SyntaxGroup,
+    iter_stack: &mut Vec<&'db SyntaxNode<'db>>,
+    callback: &mut impl FnMut(&SyntaxNode<'db>),
+) {
+    if node.green_node(db).kind.is_terminal() {
+        callback(node);
+        return;
+    }
+
+    for child in node.get_children(db).iter().rev() {
+        iter_stack.push(child);
     }
 }
 
@@ -56,15 +76,18 @@ impl<Db: SyntaxGroup> Iterator for SyntaxNodeWithDbIterator<'_, Db> {
 /// We split the content of the `SyntaxNode` into three parts: the prefix trivia, the main content
 /// of the node, and the suffix trivia. Each part is represented as a separate `PrimitiveToken`
 /// with its corresponding span.
-fn token_from_syntax_node(node: SyntaxNode<'_>, db: &dyn SyntaxGroup) -> Vec<PrimitiveToken> {
+fn token_from_syntax_node(
+    node: &SyntaxNode<'_>,
+    db: &dyn SyntaxGroup,
+    result: &mut VecDeque<PrimitiveToken>,
+) {
     let span_without_trivia = node.span_without_trivia(db);
     let span_with_trivia = node.span(db);
     let text = node.get_text(db);
-    let mut result = Vec::new();
     let prefix_len = span_without_trivia.start - span_with_trivia.start;
     let (prefix, rest) = text.split_at(prefix_len.as_u32() as usize);
     if prefix_len > TextWidth::ZERO {
-        result.push(PrimitiveToken {
+        result.push_back(PrimitiveToken {
             content: prefix.to_string(),
             span: Some(PrimitiveSpan {
                 start: span_with_trivia.start.as_u32() as usize,
@@ -75,7 +98,7 @@ fn token_from_syntax_node(node: SyntaxNode<'_>, db: &dyn SyntaxGroup) -> Vec<Pri
     let suffix_len = span_with_trivia.end - span_without_trivia.end;
     let (content, suffix) = rest.split_at(rest.len() - suffix_len.as_u32() as usize);
     if !content.is_empty() {
-        result.push(PrimitiveToken {
+        result.push_back(PrimitiveToken {
             content: content.to_string(),
             span: Some(PrimitiveSpan {
                 start: span_without_trivia.start.as_u32() as usize,
@@ -84,7 +107,7 @@ fn token_from_syntax_node(node: SyntaxNode<'_>, db: &dyn SyntaxGroup) -> Vec<Pri
         });
     }
     if suffix_len > TextWidth::ZERO {
-        result.push(PrimitiveToken {
+        result.push_back(PrimitiveToken {
             content: suffix.to_string(),
             span: Some(PrimitiveSpan {
                 start: span_without_trivia.end.as_u32() as usize,
@@ -92,5 +115,4 @@ fn token_from_syntax_node(node: SyntaxNode<'_>, db: &dyn SyntaxGroup) -> Vec<Pri
             }),
         });
     }
-    result
 }
