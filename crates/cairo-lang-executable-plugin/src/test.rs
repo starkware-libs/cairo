1930611ccb6d293749c8bb8390a87bc05165c2f8
Arcticae <tomekgsd@gmail.com>
2025-08-18 08:47:09 +0200

    Extract `cairo_lang_executable_plugin` as a crate

--- crates/cairo-lang-executable-plugin/src/test.rs
+++ crates/cairo-lang-executable-plugin/src/test.rs
@@ -1,15 +1,14 @@
 use std::sync::{LazyLock, Mutex};
 
 use cairo_lang_compiler::db::RootDatabase;
-use cairo_lang_compiler::diagnostics::DiagnosticsReporter;
-use cairo_lang_executable_plugin::executable_plugin_suite;
 use cairo_lang_filesystem::cfg::{Cfg, CfgSet};
+use cairo_lang_plugins::test_utils::expand_module_text;
 use cairo_lang_semantic::test_utils::setup_test_module;
 use cairo_lang_test_utils::parse_test_file::{TestFileRunner, TestRunnerResult};
 use cairo_lang_test_utils::{get_direct_or_file_content, verify_diagnostics_expectation};
 use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
 
-use crate::compile;
+use crate::executable_plugin_suite;
 
 /// Salsa database configured to find the corelib, when reused by different tests should be able to
 /// use the cached queries that rely on the corelib's code, which vastly reduces the tests runtime.
@@ -26,9 +25,9 @@ pub static SHARED_DB: LazyLock<Mutex<RootDatabase>> = LazyLock::new(|| {
 });
 
 #[derive(Default)]
-struct CompileExecutableTestRunner {}
+struct ExpandExecutableTestRunner {}
 
-impl TestFileRunner for CompileExecutableTestRunner {
+impl TestFileRunner for ExpandExecutableTestRunner {
     fn run(
         &mut self,
         inputs: &OrderedHashMap<String, String>,
@@ -37,23 +36,11 @@ impl TestFileRunner for CompileExecutableTestRunner {
         let db = SHARED_DB.lock().unwrap().snapshot();
         let (_, cairo_code) = get_direct_or_file_content(&inputs["cairo_code"]);
         let (test_module, semantic_diagnostics) = setup_test_module(&db, &cairo_code).split();
-        let result = compile::compile_executable_in_prepared_db(
-            &db,
-            None,
-            vec![test_module.crate_id],
-            DiagnosticsReporter::stderr().with_crates(&[test_module
-                .crate_id
-                .long(&db)
-                .clone()
-                .into_crate_input(&db)]),
-            Default::default(),
-        )
-        .map(|compiled| compiled.compiled_function.to_string())
-        .unwrap_or_else(|e| e.to_string());
+        let result = expand_module_text(&db, test_module.module_id, &mut vec![]);
         let error = verify_diagnostics_expectation(args, &semantic_diagnostics);
         TestRunnerResult {
             outputs: OrderedHashMap::from([
-                ("result".into(), result),
+                ("generated_cairo_code".into(), result),
                 ("expected_diagnostics".into(), semantic_diagnostics),
             ]),
             error,
@@ -62,10 +49,11 @@ impl TestFileRunner for CompileExecutableTestRunner {
 }
 
 cairo_lang_test_utils::test_file_test_with_runner!(
-    compile_executable,
-    "src/compile_test_data",
+    expand_executable,
+    "src/plugin_test_data",
     {
-        basic: "basic",
+        diagnostics: "diagnostics",
+        expansion: "expansion",
     },
-    CompileExecutableTestRunner
+    ExpandExecutableTestRunner
 );
