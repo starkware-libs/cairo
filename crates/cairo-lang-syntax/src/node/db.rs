cc64f4882519b7a14e7563e956302577b8102746
Eytan Singher <eytan.s@starkware.co>
2025-08-18 09:35:02 +0300

    Removed interned from SyntaxGroup and SierraGenGroup

--- crates/cairo-lang-syntax/src/node/db.rs
+++ crates/cairo-lang-syntax/src/node/db.rs
@@ -1,21 +1,11 @@
 use cairo_lang_filesystem::db::FilesGroup;
 use cairo_lang_utils::Upcast;
 
-use super::green::GreenNode;
-use super::ids::{GreenId, SyntaxStablePtrId};
-use super::stable_ptr::SyntaxStablePtr;
-use super::{SyntaxNode, SyntaxNodeLongId};
+use super::SyntaxNode;
 
 // Salsa database interface.
 #[cairo_lang_proc_macros::query_group]
 pub trait SyntaxGroup: FilesGroup + for<'a> Upcast<'a, dyn FilesGroup> {
-    #[salsa::interned]
-    fn intern_green<'a>(&'a self, field: GreenNode<'a>) -> GreenId<'a>;
-    #[salsa::interned]
-    fn intern_stable_ptr<'a>(&'a self, field: SyntaxStablePtr<'a>) -> SyntaxStablePtrId<'a>;
-    #[salsa::interned]
-    fn intern_syntax_node<'a>(&'a self, field: SyntaxNodeLongId<'a>) -> SyntaxNode<'a>;
-
     /// Query for caching [SyntaxNode::get_children].
     #[salsa::transparent]
     fn get_children<'a>(&'a self, node: SyntaxNode<'a>) -> &'a [SyntaxNode<'a>];
