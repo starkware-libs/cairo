841fe74d0e7d571a5bc5ebfd429af97722acff6b
Gilad Chase <gilad@starkware.com>
2025-08-25 17:19:44 +0300

    bump nightly: should solve intermittent rustup ci connection issues
    We want this fix: https://github.com/rust-lang/rustup/pull/4417

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export RUSTUP_TOOLCHAIN="${RUSTUP_TOOLCHAIN:-nightly-2025-08-05}"
+export RUSTUP_TOOLCHAIN="${RUSTUP_TOOLCHAIN:-nightly-2025-08-24}"
 
 RUSTDOCFLAGS="-Dwarnings" cargo doc --document-private-items --no-deps --all-features
