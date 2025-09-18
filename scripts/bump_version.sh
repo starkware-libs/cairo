bd6d62696eb8bb4f13b06dadf906db72c76b3e33
Ori Ziv <oriz@starkware.co>
2025-09-18 14:56:15 +0300

    Version bump and cairo-vm bump for dev release.

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 SCARB_REPO="https://github.com/software-mansion/scarb"
 
-CURRENT_VERSION='2.12.3'
+CURRENT_VERSION='2.12.4-dev.0'
 NEW_VERSION="$@"
 
 # NOTE: These two functions were copied from asdf-scarb.
