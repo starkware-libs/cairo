fef7539c415bd47e90a27bfa6b3fa42a281d6f21
maciektr <maciej.tratnowiecki@swmansion.com>
2025-10-01 13:00:07 +0200

    Add debug info annotations to executable struct (#8490)

---
+++ crates/cairo-lang-executable/src/debug_info.rs
@@ -0,0 +1,35 @@
+use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
+use serde::{Deserialize, Serialize};
+
+/// Debug information for an Executable.
+#[derive(Clone, Debug, Eq, PartialEq, Default, Serialize, Deserialize)]
+pub struct DebugInfo {
+    /// Non-crucial information about the program, for use by external libraries and tools.
+    ///
+    /// See [`Annotations`] type documentation for more information about this field.
+    #[serde(default, skip_serializing_if = "Annotations::is_empty")]
+    pub annotations: Annotations,
+}
+
+/// Store for non-crucial information about the program, for use by external libraries and tools.
+///
+/// Keys represent tool namespaces, and values are tool-specific annotations themselves.
+/// Annotation values are JSON values, so they can be arbitrarily complex.
+///
+/// ## Namespaces
+///
+/// In order to avoid collisions between tools, namespaces should be URL-like, contain tool name.
+/// It is not required for namespace URLs to exist, but it is preferable nonetheless.
+///
+/// A single tool might want to use multiple namespaces, for example to group together annotations
+/// coming from different subcomponents of the tool. In such case, namespaces should use path-like
+/// notation (e.g. `example.com/sub-namespace`).
+///
+/// For future-proofing, it might be a good idea to version namespaces, e.g. `example.com/v1`.
+///
+/// ### Example well-formed namespaces
+///
+/// - `scarb.swmansion.com`
+/// - `scarb.swmansion.com/v1`
+/// - `scarb.swmansion.com/build-info/v1`
+pub type Annotations = OrderedHashMap<String, serde_json::Value>;
