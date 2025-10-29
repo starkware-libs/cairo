76a737e4053b4d9543013aaeb5777d8c822aaaff
Galoretka <galoretochka@gmail.com>
2025-10-29 19:06:14 +0200

    fmt

--- crates/cairo-lang-sierra/src/extensions/modules/felt252_dict.rs
+++ crates/cairo-lang-sierra/src/extensions/modules/felt252_dict.rs
@@ -26,8 +26,8 @@ use crate::program::{ConcreteTypeLongId, GenericArg};
 /// Type representing a dictionary from a felt252 to types of size one.
 ///
 /// This is currently only bounded for all numeric types, Nullable, and Enum types with 2 or less
-/// variants, as this are the types that has proper default as 0, and therefore can be properly used
-/// as a value in the dictionary.
+/// variants, as these are the types that has proper default as 0, and therefore can be properly
+/// used as a value in the dictionary.
 #[derive(Default)]
 pub struct Felt252DictTypeWrapped {}
 impl GenericTypeArgGenericType for Felt252DictTypeWrapped {
