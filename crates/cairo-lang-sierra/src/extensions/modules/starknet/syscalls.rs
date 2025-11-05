cbe10e8dba27f9939aa1574f0c1a456cc5aab6ca
maksneprik <155268440+deus-labs@users.noreply.github.com>
2025-11-05 23:50:40 +0100

    fix: correct grammar in KeccakLibfunc documentation comment (#8616)

--- crates/cairo-lang-sierra/src/extensions/modules/starknet/syscalls.rs
+++ crates/cairo-lang-sierra/src/extensions/modules/starknet/syscalls.rs
@@ -138,7 +138,7 @@ impl SyscallGenericLibfunc for ReplaceClassLibfunc {
 
 /// Libfunc for the keccak system call.
 /// The libfunc does not add any padding and the input needs to be a multiple of 1088 bits
-/// (== 17 u64 word).
+/// (== 17 u64 words).
 #[derive(Default)]
 pub struct KeccakLibfunc {}
 impl SyscallGenericLibfunc for KeccakLibfunc {
