a231d345d5a79455cf15de27b6d5cb2eb4b688cc
oxBoni <godzilabozo@gmail.com>
2025-11-19 22:43:42 +0100

    Update lib.rs

--- crates/cairo-lang-test-plugin/src/lib.rs
+++ crates/cairo-lang-test-plugin/src/lib.rs
@@ -134,7 +134,7 @@ pub fn compile_test_prepared_db<'db>(
     let all_tests = find_all_tests(db, test_crate_ids);
 
     let func_ids = chain!(
-        executable_functions.clone().into_keys(),
+        executable_functions.keys().cloned(),
         all_entry_points.iter().cloned(),
         // TODO(maciektr): Remove test entrypoints after migration to executable attr.
         all_tests.iter().flat_map(|(func_id, _cfg)| {
