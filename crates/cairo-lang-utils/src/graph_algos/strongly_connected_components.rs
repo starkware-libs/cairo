146c3f49ce7806bccc1f6e5f3595eefb9f05ee00
oxBoni <godzilabozo@gmail.com>
2025-11-25 22:13:24 +0100

    Update strongly_connected_components.rs

--- crates/cairo-lang-utils/src/graph_algos/strongly_connected_components.rs
+++ crates/cairo-lang-utils/src/graph_algos/strongly_connected_components.rs
@@ -97,8 +97,8 @@ fn compute_scc_recursive<Node: GraphNode>(ctx: &mut SccAlgoContext<Node>, curren
             }
         };
 
-        // Update current_node in ctx.known_nodes.
-        ctx.known_nodes.insert(current_node_id.clone(), current_wrapper_node.clone());
+    // Update current_node in ctx.known_nodes after processing all neighbors.
+    ctx.known_nodes.insert(current_node_id.clone(), current_wrapper_node.clone());
     }
 
     if current_wrapper_node.lowlink != current_wrapper_node.index {
