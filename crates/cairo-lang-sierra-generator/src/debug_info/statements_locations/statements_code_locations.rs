05a8361a2c126a4d88b5a852c6bbf7fbd13f8c13
piotmag769 <piotrmagiera150@gmail.com>
2025-12-02 14:35:24 +0100

    Extract serializable function debug info
    commit-id:0499d906

--- crates/cairo-lang-sierra-generator/src/debug_info/statements_locations/statements_code_locations.rs
+++ crates/cairo-lang-sierra-generator/src/debug_info/statements_locations/statements_code_locations.rs
@@ -1,34 +1,11 @@
 use std::collections::HashMap;
 
-use cairo_lang_defs::diagnostic_utils::StableLocation;
-use cairo_lang_filesystem::ids::FileLongId;
 use cairo_lang_sierra::debug_info::Annotations;
 use cairo_lang_sierra::program::StatementIdx;
 use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
-use salsa::Database;
 use serde::{Deserialize, Serialize};
 
-/// A full path to a Cairo source file.
-#[derive(Clone, Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
-pub struct SourceFileFullPath(pub String);
-
-/// A location in a Cairo source file.
-#[derive(Clone, Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
-pub struct SourceCodeLocation {
-    /// Line index, 0 based.
-    pub line: usize,
-    /// Character index inside the line, 0 based.
-    pub col: usize,
-}
-
-/// A location in a Cairo source file.
-#[derive(Clone, Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
-pub struct SourceCodeSpan {
-    /// Beginning of the text span in the Cairo source file.
-    pub start: SourceCodeLocation,
-    /// End of the text span in the Cairo source file, not included.
-    pub end: SourceCodeLocation,
-}
+use crate::debug_info::{SourceCodeSpan, SourceFileFullPath};
 
 /// The mapping between Sierra statement indexes and locations in Cairo code
 /// (if obtainable) which caused the statement to be generated. Contains the additional information
@@ -50,24 +27,4 @@ impl From<StatementsSourceCodeLocations> for Annotations {
             serde_json::Value::from_iter([("statements_code_locations", mapping)]),
         )])
     }
-}
-
-/// Returns a location in the user file corresponding to the given [StableLocation].
-/// It consists of a full path to the file, a text span in the file and a boolean indicating
-/// if the location is a part of a macro expansion.
-pub fn maybe_code_location<'db>(
-    db: &'db dyn Database,
-    location: StableLocation<'db>,
-) -> Option<(SourceFileFullPath, SourceCodeSpan, bool)> {
-    let is_macro =
-        matches!(location.file_id(db).long(db), FileLongId::Virtual(_) | FileLongId::External(_));
-    let location = location.span_in_file(db).user_location(db);
-    let file_full_path = location.file_id.full_path(db);
-    let position = location.span.position_in_file(db, location.file_id)?;
-    let source_location = SourceCodeSpan {
-        start: SourceCodeLocation { col: position.start.col, line: position.start.line },
-        end: SourceCodeLocation { col: position.end.col, line: position.end.line },
-    };
-
-    Some((SourceFileFullPath(file_full_path), source_location, is_macro))
 }
