ca6e8b3dfdc9a607574d7d1de8b2743c63a85c8f
VolodymyrBg <aqdrgg19@gmail.com>
2025-12-10 22:21:15 +0200

    Update main.rs

--- crates/bin/cairo-run/src/main.rs
+++ crates/bin/cairo-run/src/main.rs
@@ -76,7 +76,6 @@ fn main() -> anyhow::Result<()> {
         .get_sierra_program(main_crate_ids.clone())
         .to_option()
         .context("Compilation failed without any diagnostics.")?
-        .clone();
     let replacer = DebugReplacer { db };
     if args.available_gas.is_none() && sierra_program.requires_gas_counter() {
         anyhow::bail!("Program requires gas counter, please provide `--available-gas` argument.");
