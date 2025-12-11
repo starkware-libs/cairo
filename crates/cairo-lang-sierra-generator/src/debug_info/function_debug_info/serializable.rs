7c795b99841fe3deff40291b298172868aa86a41
piotmag769 <piotrmagiera150@gmail.com>
2025-12-11 17:08:50 +0100

    Fix serializing function debug info (this time for real)
    commit-id:19f51cc2

--- crates/cairo-lang-sierra-generator/src/debug_info/function_debug_info/serializable.rs
+++ crates/cairo-lang-sierra-generator/src/debug_info/function_debug_info/serializable.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
 
 use cairo_lang_sierra::debug_info::Annotations;
-use cairo_lang_sierra::ids::{FunctionId, VarId};
 use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
 use serde::{Deserialize, Serialize};
 
@@ -9,13 +8,12 @@ use crate::debug_info::{SourceCodeSpan, SourceFileFullPath};
 
 /// The serializable debug info of all sierra functions in the program.
 pub struct SerializableAllFunctionsDebugInfo(
-    pub(super) HashMap<FunctionId, SerializableFunctionDebugInfo>,
+    pub(super) HashMap<SierraFunctionId, SerializableFunctionDebugInfo>,
 );
 
 impl From<SerializableAllFunctionsDebugInfo> for Annotations {
     fn from(value: SerializableAllFunctionsDebugInfo) -> Self {
-        let vec: Vec<_> = value.0.into_iter().map(|(id, debug_info)| (id.id, debug_info)).collect();
-        let mapping = serde_json::to_value(vec).unwrap();
+        let mapping = serde_json::to_value(value.0).unwrap();
         OrderedHashMap::from([(
             "github.com/software-mansion-labs/cairo-debugger".to_string(),
             serde_json::Value::from_iter([("functions_info", mapping)]),
@@ -33,7 +31,17 @@ pub struct SerializableFunctionDebugInfo {
     /// Mapping from a sierra variable to a cairo variable (its name and definition span).
     /// The sierra variable value corresponds to the cairo variable value at some point during
     /// execution of the function code.
-    pub sierra_to_cairo_variable: HashMap<VarId, (CairoVariableName, SourceCodeSpan)>,
+    pub sierra_to_cairo_variable: HashMap<SierraVarId, (CairoVariableName, SourceCodeSpan)>,
 }
+
+/// An id of a sierra function - equivalent of `id` field of [`cairo_lang_sierra::ids::FunctionId`].
+/// Used to make serialization of a hashmap with id as a key possible.
+#[derive(Serialize, Deserialize, PartialEq, Eq, Hash)]
+pub struct SierraFunctionId(pub u64);
+
+/// An id of a sierra variable - equivalent of `id` field of [`cairo_lang_sierra::ids::VarId`].
+/// Used to make serialization of a hashmap with id as a key possible.
+#[derive(Serialize, Deserialize, PartialEq, Eq, Hash)]
+pub struct SierraVarId(pub u64);
 
 pub type CairoVariableName = String;
