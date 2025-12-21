4827150a815d2a65c6cdfe06240f41c077953a9a
orizi <104711814+orizi@users.noreply.github.com>
2025-12-21 10:22:11 +0100

    Updated nightly to working endpoint. (#9254)

--- scripts/starknet_sierra_validate.sh
+++ scripts/starknet_sierra_validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export FULLNODE_URL="${1:-https://papyrus-integration-mainnet.sw-dev.io/rpc/v0_7}"
+export FULLNODE_URL="${1:-https://starknet-mainnet.core.chainstack.com/57088c25afa0ff21277c6ee5f3b536bb/rpc/v0_7}"
 export N_BLOCKS="${2:-1000}"
 
-cargo run --bin starknet-sierra-upgrade-validate -- --fullnode-url $FULLNODE_URL --last-n-blocks $N_BLOCKS
+cargo run --bin starknet-sierra-upgrade-validate --profile=release -- --fullnode-url $FULLNODE_URL --last-n-blocks $N_BLOCKS
