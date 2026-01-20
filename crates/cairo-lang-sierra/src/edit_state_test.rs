5cd5ce65d151ca494af9d5ab6b705139e52795aa
orizi <104711814+orizi@users.noreply.github.com>
2026-01-20 14:43:24 +0100

    refactor(sierra-to-casm): Made edit_state based on `&mut self`. (#9495)

--- crates/cairo-lang-sierra/src/edit_state_test.rs
+++ crates/cairo-lang-sierra/src/edit_state_test.rs
@@ -1,33 +1,48 @@
 use cairo_lang_test_utils::test;
 use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
 
-use crate::edit_state::{EditStateError, put_results, take_args};
+use crate::edit_state::{EditState, EditStateError};
 use crate::ids::VarId;
-
-pub type State = OrderedHashMap<VarId, i64>;
 
 #[test]
 fn empty() {
-    assert_eq!(take_args(State::default(), vec![].into_iter()), Ok((State::default(), vec![])));
-    assert_eq!(put_results(State::default(), vec![].into_iter()), Ok(State::default()));
+    assert_eq!(take_args([], vec![]), Ok((State::default(), vec![])));
+    assert_eq!(put_results([], vec![]), Ok(State::default()));
 }
 
 #[test]
 fn basic_mapping() {
+    assert_eq!(take_args([("arg".into(), 0)], vec!["arg".into()]), Ok((State::default(), vec![0])));
+    assert_eq!(put_results([], vec![("res".into(), 1)]), Ok(State::from([("res".into(), 1)])));
     assert_eq!(
-        take_args(State::from([("arg".into(), 0)]), vec![&"arg".into()].into_iter(),),
-        Ok((State::default(), vec![0]))
-    );
-    assert_eq!(
-        put_results(State::default(), vec![(&"res".into(), 1)].into_iter(),),
-        Ok(State::from([("res".into(), 1)]))
-    );
-    assert_eq!(
-        take_args(State::default(), vec![&"arg".into()].into_iter(),),
+        take_args([], vec!["arg".into()]),
         Err(EditStateError::MissingReference("arg".into()))
     );
     assert_eq!(
-        put_results(State::from([("res".into(), 1)]), vec![(&"res".into(), 1)].into_iter(),),
+        put_results([("res".into(), 1)], vec![("res".into(), 1)]),
         Err(EditStateError::VariableOverride("res".into()))
     );
+}
+
+/// The type for the state in the testing.
+pub type State = OrderedHashMap<VarId, i64>;
+
+/// Test helper wrapper for `VarManagement::take_vars`.
+fn take_args(
+    state: impl Into<State>,
+    ids: Vec<VarId>,
+) -> Result<(State, Vec<i64>), EditStateError> {
+    let mut state: State = state.into();
+    let taken = state.take_vars(ids.iter())?;
+    Ok((state, taken))
+}
+
+/// Test helper wrapper for `VarManagement::put_vars`.
+fn put_results(
+    state: impl Into<State>,
+    results: Vec<(VarId, i64)>,
+) -> Result<State, EditStateError> {
+    let mut state: State = state.into();
+    state.put_vars(results.iter().map(|(k, v)| (k, *v)))?;
+    Ok(state)
 }
