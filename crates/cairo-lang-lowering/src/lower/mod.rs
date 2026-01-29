06c3c660b1b449c9a5d676202ada171660b9d2af
Ori Ziv <oriz@starkware.co>
2026-01-29 15:14:30 +0200

    feature(semantic): Allow immediate consumption of `ref self` of tempraries.
    Closes #9483

--- crates/cairo-lang-lowering/src/lower/mod.rs
+++ crates/cairo-lang-lowering/src/lower/mod.rs
@@ -22,7 +22,7 @@ use cairo_lang_syntax::node::TypedStablePtr;
 use cairo_lang_syntax::node::ids::SyntaxStablePtrId;
 use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
 use cairo_lang_utils::unordered_hash_map::{Entry, UnorderedHashMap};
-use cairo_lang_utils::{Intern, extract_matches, try_extract_matches};
+use cairo_lang_utils::{Intern, extract_matches};
 use context::handle_lowering_flow_error;
 use defs::ids::TopLevelLanguageElementId;
 use flow_control::create_graph::{
@@ -60,7 +60,7 @@ use crate::ids::{
     GeneratedFunction, GeneratedFunctionKey, LocationId, SemanticFunctionIdEx,
     parameter_as_member_path,
 };
-use crate::lower::context::{LoopContext, LoopEarlyReturnInfo, LoweringResult, VarRequest};
+use crate::lower::context::{LoopContext, LoopEarlyReturnInfo, LoweringResult, RefArg, VarRequest};
 use crate::lower::generators::StructDestructure;
 use crate::{
     BlockId, Lowered, MatchArm, MatchEnumInfo, MatchExternInfo, MatchInfo, VarUsage, VariableId,
@@ -1267,11 +1267,18 @@ fn lower_expr_function_call<'db>(
 
     // TODO(spapini): Use the correct stable pointer.
     let arg_inputs = lower_exprs_to_var_usages(ctx, &expr.args, builder)?;
-    let ref_args_iter = expr
+    let ref_args = expr
         .args
         .iter()
-        .filter_map(|arg| try_extract_matches!(arg, ExprFunctionCallArg::Reference));
-    let ref_tys = ref_args_iter.clone().map(|ref_arg| ref_arg.ty()).collect();
+        .filter_map(|arg| match arg {
+            ExprFunctionCallArg::Value(_) => None,
+            ExprFunctionCallArg::Reference(ref_arg) => Some(RefArg::Ref(ref_arg.clone())),
+            ExprFunctionCallArg::TempReference(expr_id) => {
+                Some(RefArg::Temp(ctx.function_body.arenas.exprs[*expr_id].ty()))
+            }
+        })
+        .collect_vec();
+    let extra_ret_tys = ref_args.iter().map(|ref_arg| ref_arg.ty()).collect();
 
     let coupon_input = if let Some(coupon_arg) = expr.coupon_arg {
         Some(lower_expr_to_var_usage(ctx, builder, coupon_arg)?)
@@ -1294,7 +1301,7 @@ fn lower_expr_function_call<'db>(
             function: expr.function,
             concrete_enum_id: *concrete_enum_id,
             inputs: arg_inputs,
-            member_paths: ref_args_iter.cloned().collect(),
+            ref_args,
             location,
         };
 
@@ -1313,15 +1320,17 @@ fn lower_expr_function_call<'db>(
             function: expr.function,
             inputs: arg_inputs,
             coupon_input,
-            extra_ret_tys: ref_tys,
+            extra_ret_tys,
             ret_ty: expr.ty,
         },
         location,
     )?;
 
-    // Rebind the ref variables.
-    for (ref_arg, output_var) in zip_eq(ref_args_iter, ref_outputs) {
-        builder.update_ref(ctx, ref_arg, output_var.var_id);
+    // Rebind the ref variables (skip TempReference which has None).
+    for (ref_arg, output_var) in zip_eq(ref_args, ref_outputs) {
+        if let RefArg::Ref(ref_arg) = ref_arg {
+            builder.update_ref(ctx, &ref_arg, output_var.var_id);
+        }
     }
 
     Ok(res)
@@ -1746,17 +1755,21 @@ fn lower_exprs_to_var_usages<'db>(
     // variable, while still allowing `arr.append(arr.len())`.
     let mut value_iter = args
         .iter()
-        .filter_map(|arg| try_extract_matches!(arg, ExprFunctionCallArg::Value))
-        .map(|arg_expr_id| lower_expr_to_var_usage(ctx, builder, *arg_expr_id))
+        .filter_map(|arg| match arg {
+            ExprFunctionCallArg::Value(expr_id) | ExprFunctionCallArg::TempReference(expr_id) => {
+                Some(lower_expr_to_var_usage(ctx, builder, *expr_id))
+            }
+            ExprFunctionCallArg::Reference(_) => None,
+        })
         .collect::<Result<Vec<_>, _>>()?
         .into_iter();
     Ok(args
         .iter()
         .map(|arg| match arg {
-            semantic::ExprFunctionCallArg::Reference(ref_arg) => {
-                builder.get_ref(ctx, ref_arg).unwrap()
+            semantic::ExprFunctionCallArg::Reference(arg) => builder.get_ref(ctx, arg).unwrap(),
+            ExprFunctionCallArg::Value(_) | ExprFunctionCallArg::TempReference(_) => {
+                value_iter.next().unwrap()
             }
-            semantic::ExprFunctionCallArg::Value(_) => value_iter.next().unwrap(),
         })
         .collect())
 }
@@ -2298,7 +2311,7 @@ fn match_extern_variant_arm_input_types<'db>(
     extern_enum: &LoweredExprExternEnum<'db>,
 ) -> Vec<semantic::TypeId<'db>> {
     let variant_input_tys = extern_facade_return_tys(ctx, ty);
-    let ref_tys = extern_enum.member_paths.iter().map(|ref_arg| ref_arg.ty());
+    let ref_tys = extern_enum.ref_args.iter().map(|ref_arg| ref_arg.ty());
     chain!(ref_tys, variant_input_tys.into_iter()).collect()
 }
 
@@ -2309,10 +2322,12 @@ fn match_extern_arm_ref_args_bind<'db>(
     extern_enum: &LoweredExprExternEnum<'db>,
     subscope: &mut BlockBuilder<'db>,
 ) {
-    let ref_outputs: Vec<_> = arm_inputs.drain(0..extern_enum.member_paths.len()).collect();
+    let ref_outputs: Vec<_> = arm_inputs.drain(0..extern_enum.ref_args.len()).collect();
     // Bind the ref parameters.
-    for (ref_arg, output_var) in zip_eq(&extern_enum.member_paths, ref_outputs) {
-        subscope.update_ref(ctx, ref_arg, output_var);
+    for (ref_arg, output_var) in zip_eq(&extern_enum.ref_args, ref_outputs) {
+        if let RefArg::Ref(ref_arg) = ref_arg {
+            subscope.update_ref(ctx, ref_arg, output_var);
+        }
     }
 }
 
