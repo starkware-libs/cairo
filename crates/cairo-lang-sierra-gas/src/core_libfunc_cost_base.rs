abcc067f2ef8e389c0344c05320b4c123940574a
Ori Ziv <oriz@starkware.co>
2026-02-03 14:27:01 +0200

    Updated blake according to audit.
    SIERRA_UPDATE_PATCH_CHANGE_TAG=Updated un-authorized libfunc.

--- crates/cairo-lang-sierra-gas/src/core_libfunc_cost_base.rs
+++ crates/cairo-lang-sierra-gas/src/core_libfunc_cost_base.rs
@@ -2,6 +2,7 @@ use std::iter;
 use std::ops::Shl;
 
 use cairo_lang_sierra::extensions::array::ArrayConcreteLibfunc;
+use cairo_lang_sierra::extensions::blake::BlakeConcreteLibfunc;
 use cairo_lang_sierra::extensions::boolean::BoolConcreteLibfunc;
 use cairo_lang_sierra::extensions::bounded_int::{
     BoundedIntConcreteLibfunc, BoundedIntDivRemAlgorithm,
@@ -646,8 +647,10 @@ pub fn core_libfunc_cost(
                 vec![ConstCost::steps(2).into(), ConstCost::steps(2).into()]
             }
         },
-        Blake(_) => vec![BranchCost::Regular {
-            const_cost: ConstCost::steps(1),
+        Blake(
+            BlakeConcreteLibfunc::Blake2sCompress(_) | BlakeConcreteLibfunc::Blake2sFinalize(_),
+        ) => vec![BranchCost::Regular {
+            const_cost: ConstCost::steps(0),
             pre_cost: PreCost::builtin(CostTokenType::Blake),
         }],
         Trace(_) => vec![ConstCost::steps(1).into()],
