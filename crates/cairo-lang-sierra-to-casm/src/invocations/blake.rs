ea9c6d8d33701a1b0500cf80078e7e714d2925df
Ori Ziv <oriz@starkware.co>
2026-02-03 14:27:01 +0200

    Updated blake according to audit.
    SIERRA_UPDATE_PATCH_CHANGE_TAG=Updated un-authorized libfunc.

--- crates/cairo-lang-sierra-to-casm/src/invocations/blake.rs
+++ crates/cairo-lang-sierra-to-casm/src/invocations/blake.rs
@@ -1,44 +1,41 @@
-use cairo_lang_casm::builder::CasmBuilder;
-use cairo_lang_casm::casm_build_extend;
+use cairo_lang_casm::cell_expression::CellExpression;
+use cairo_lang_casm::cell_ref;
+use cairo_lang_casm::hints::CoreHint;
+use cairo_lang_casm::instructions::{Blake2sCompressInstruction, Instruction, InstructionBody};
+use cairo_lang_casm::operand::ResOperand;
 use cairo_lang_sierra::extensions::blake::BlakeConcreteLibfunc;
 
 use super::{CompiledInvocation, CompiledInvocationBuilder, InvocationError};
-use crate::invocations::add_input_variables;
+use crate::references::ReferenceExpression;
 
 /// Builds instructions for Sierra bool operations.
 pub fn build(
     libfunc: &BlakeConcreteLibfunc,
     builder: CompiledInvocationBuilder<'_>,
 ) -> Result<CompiledInvocation, InvocationError> {
-    let finalize = match libfunc {
-        BlakeConcreteLibfunc::Blake2sCompress(_) => false,
-        BlakeConcreteLibfunc::Blake2sFinalize(_) => true,
-    };
-
-    build_compress(builder, finalize)
+    match libfunc {
+        BlakeConcreteLibfunc::Blake2sCompress(_) => build_compress(builder, false),
+        BlakeConcreteLibfunc::Blake2sFinalize(_) => build_compress(builder, true),
+    }
 }
 
-/// Handles instructions for boolean AND.
+/// Handles instructions for blake2s compression.
 fn build_compress(
     builder: CompiledInvocationBuilder<'_>,
     finalize: bool,
 ) -> Result<CompiledInvocation, InvocationError> {
-    let [state, byte_count, message] = builder.try_get_single_cells()?;
-    let mut casm_builder = CasmBuilder::with_capacity(1, 0);
-    add_input_variables! {casm_builder,
-        deref state;
-        deref byte_count;
-        deref message;
+    let [Some(state), Some(byte_count), Some(message)] =
+        builder.try_get_single_cells()?.map(CellExpression::to_deref)
+    else {
+        return Err(InvocationError::InvalidReferenceExpressionForArgument);
     };
-    casm_build_extend! {casm_builder,
-        tempvar output;
-        const state_size = 8;
-        hint AllocConstantSize { size: state_size } into { dst: output };
-    };
-    casm_builder.blake2s_compress(state, byte_count, message, finalize);
-    Ok(builder.build_from_casm_builder(
-        casm_builder,
-        [("Fallthrough", &[&[output]], None)],
-        Default::default(),
-    ))
+    let hints = vec![
+        CoreHint::AllocConstantSize { dst: cell_ref!([ap]), size: ResOperand::Immediate(8.into()) }
+            .into(),
+    ];
+    let compress = Blake2sCompressInstruction { state, byte_count, message, finalize };
+    let instruction =
+        Instruction { hints, body: InstructionBody::Blake2sCompress(compress), inc_ap: true };
+    let fixed_output = ReferenceExpression::from_cell(CellExpression::Deref(cell_ref!([ap - 1])));
+    Ok(builder.build(vec![instruction], vec![], [[fixed_output].into_iter()].into_iter()))
 }
