2a59950c23b8c0ba4d5fd27e66c06c10f0868a5c
github-action-benchmark <github@users.noreply.github.com>
2026-02-18 00:12:27 +0000

    add Cairo Compiler Benchmarks (cargo) benchmark result for c6c7cdc9e6513d7ecc7e4253df6b2304da330336

---
+++ dev/bench/data.js
@@ -0,0 +1,36 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1771373547654,
+  "repoUrl": "https://github.com/starkware-libs/cairo",
+  "entries": {
+    "Cairo Compiler Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "name": "eytan-starkware",
+            "username": "eytan-starkware",
+            "email": "eytan.s@starkware.co"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "c6c7cdc9e6513d7ecc7e4253df6b2304da330336",
+          "message": "(bug fix): Pass github token to benchmarking action (#9690)",
+          "timestamp": "2026-02-17T14:52:54Z",
+          "url": "https://github.com/starkware-libs/cairo/commit/c6c7cdc9e6513d7ecc7e4253df6b2304da330336"
+        },
+        "date": 1771373546577,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "compile/fib",
+            "value": 1537382227,
+            "range": "± 10511534",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
