b8f4559c806066dcf6faca1d05310e4c090b4739
orizi <104711814+orizi@users.noreply.github.com>
2026-04-27 10:28:37 +0300

    Bumped nightly version. (#9880)

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export RUSTUP_TOOLCHAIN="${RUSTUP_TOOLCHAIN:-nightly-2026-04-09}"
+export RUSTUP_TOOLCHAIN="${RUSTUP_TOOLCHAIN:-nightly-2026-04-26}"
 
 RUSTDOCFLAGS="-Dwarnings" cargo doc --document-private-items --no-deps --all-features
