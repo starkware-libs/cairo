d534bf9e732eb82c170ab5a57de60e409298590a
eytan-starkware <eytan.s@starkware.co>
2026-05-03 15:54:50 +0300

    (feat): merging analysis tests (#9872)

--- crates/cairo-lang-lowering/src/analysis/dominator_test.rs
+++ crates/cairo-lang-lowering/src/analysis/dominator_test.rs
@@ -1,63 +1,9 @@
-//! File-based tests for the dominator analysis.
+//! Unit tests for dominator queries.
 
-use cairo_lang_semantic::test_utils::setup_test_function;
-use cairo_lang_test_utils::parse_test_file::TestRunnerResult;
-use cairo_lang_utils::ordered_hash_map::OrderedHashMap;
 use cairo_lang_utils::ordered_hash_set::OrderedHashSet;
-use itertools::Itertools;
 
 use super::Dominators;
-use crate::db::LoweringGroup;
-use crate::ids::ConcreteFunctionWithBodyId;
-use crate::test_utils::{LoweringDatabaseForTesting, formatted_lowered};
-use crate::{BlockId, LoweringStage};
-
-cairo_lang_test_utils::test_file_test!(
-    dominator,
-    "src/analysis/test_data",
-    {
-        dominator: "dominator",
-    },
-    test_dominator_analysis
-);
-
-fn test_dominator_analysis(
-    inputs: &OrderedHashMap<String, String>,
-    _args: &OrderedHashMap<String, String>,
-) -> TestRunnerResult {
-    let db = &mut LoweringDatabaseForTesting::default();
-    let (test_function, semantic_diagnostics) = setup_test_function(db, inputs).split();
-
-    let function_id =
-        ConcreteFunctionWithBodyId::from_semantic(db, test_function.concrete_function_id);
-
-    let lowered = db.lowered_body(function_id, LoweringStage::PostBaseline);
-
-    let (lowering_str, dominators_str) = if let Ok(lowered) = lowered.cloned() {
-        let lowering_str = formatted_lowered(db, Some(&lowered));
-        let dominators = Dominators::analyze(&lowered);
-
-        let dominators_str = lowered
-            .blocks
-            .iter()
-            .map(|(block_id, _)| {
-                let doms = dominators.dominators_of(block_id);
-                let mut dom_strs = doms.iter().map(|d| d.0).sorted().map(|d| format!("blk{d}"));
-                format!("blk{}: {{{}}}", block_id.0, dom_strs.join(", "))
-            })
-            .join("\n");
-
-        (lowering_str, dominators_str)
-    } else {
-        ("Lowering failed.".to_string(), "".to_string())
-    };
-
-    TestRunnerResult::success(OrderedHashMap::from([
-        ("semantic_diagnostics".into(), semantic_diagnostics),
-        ("lowering".into(), lowering_str),
-        ("dominators".into(), dominators_str),
-    ]))
-}
+use crate::BlockId;
 
 #[test]
 fn test_dominates() {
