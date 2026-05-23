abd9ac1832a98830a0e63e9ec5434d41d2726468
Jan Smółka <jp.smolka@gmail.com>
2026-05-24 00:25:04 +0200

    Emit a multi-map of Sierra to Cairo variables in `FunctionDebugInfo`

--- crates/cairo-lang-starknet/src/compile.rs
+++ crates/cairo-lang-starknet/src/compile.rs
@@ -181,7 +181,14 @@ fn compile_contract_with_prepared_and_checked_db<'db>(
 
     if compiler_config.add_functions_debug_info {
         annotations.extend(Annotations::from(
-            debug_info.functions_info.extract_serializable_debug_info(db),
+            debug_info
+                .functions_info
+                .replace_function_ids(&replacer)
+                .extract_serializable_debug_info(
+                    db,
+                    &sierra_program,
+                    &debug_info.statements_locations,
+                ),
         ))
     }
 
