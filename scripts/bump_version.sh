64cafd0a7680c979a22b0f0fda2c77fab90ecd98
Ori Ziv <oriz@starkware.co>
2026-09-22 07:48:59 +0300

    Fixed conflicts.

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -4,13 +4,7 @@ set -euo pipefail
 
 SCARB_REPO="https://github.com/software-mansion/scarb"
 
-<<<<<<< HEAD
 CURRENT_VERSION='2.20.0'
-||||||| fab648011
-CURRENT_VERSION='2.19.4'
-=======
-CURRENT_VERSION='2.19.5'
->>>>>>> dev-v2.19.5
 NEW_VERSION="$@"
 
 # NOTE: These two functions were copied from asdf-scarb.
